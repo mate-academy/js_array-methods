4df1a1a1092be7a041b2b4800e668265fb7c4d95
Alexandr <alexandrmail32@gmail.com>
2019-04-11 21:58:33 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,22 @@
  *
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function() {
-    // write code here
+  [].__proto__.join2 = function(arg) {
+    return this.reduce((str, item) => {
+      if (arg === undefined) {
+        arg = ',';
+      }
+      if (arg === null) {
+        arg += '';
+      }
+      if (this.indexOf(item) === this.length - 1 || !arg) {
+        return str + item;
+      }
+      if (item === null || item === undefined) {
+        return str + arg;
+      }
+      return str + item + arg;
+    }, '');
   };
 }
 
