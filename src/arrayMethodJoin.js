f417c52e16575358d0ad16a1182e796f5be346f5
Ivan Deiko <ivandeiko1997@gmail.com>
2019-04-11 17:33:53 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,25 @@
  *
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function() {
-    // write code here
+  [].__proto__.join2 = function(elemForParsing) {
+    let array = this;
+    let elem = elemForParsing === undefined ? ',' : elemForParsing;
+    let string = '';
+    if (array.length === 0) {
+      return string;
+    }
+    for (let i = 0; i < array.length; i++) {
+      if (typeof array[i] === 'undefined' || array[i] === null) {
+        array[i] = '';
+        string += elem;
+      }
+      if (array[i] !== array[array.length - 1]) {
+        string += ('' + array[i]) + elem;
+      } else {
+        string += array[i];
+      }
+    }
+    return string;
   };
 }
 
