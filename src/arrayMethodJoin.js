9f9bf3df2e630c138dfac02e1c074a4d9a97b43b
Vitalii <ilovepinkpony1@gmail.com>
2019-04-11 23:48:37 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,27 @@
  *
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function() {
-    // write code here
+  [].__proto__.join2 = function(token = ',') {
+    let str = '';
+    let addToken = String(token);
+    if (token === null) {
+      addToken = String(null);
+    }
+    for (let i = 0; i < this.length; i++) {
+      if (
+        this[i] === null
+        || this[i] === undefined // eslint-disable-line
+      ) {
+        str += addToken;
+        continue;
+      }
+      if (i === this.length - 1) {
+        str += this[i];
+        return str;
+      }
+      str += this[i] + addToken;
+    }
+    return str;
   };
 }
 
