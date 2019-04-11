da498c306bb1d82dff9b07f0ab14deeb4876372c
Dima Siianytsia <ds081181@gmail.com>
2019-04-11 19:27:45 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,19 @@
  *
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function() {
-    // write code here
+  [].__proto__.join2 = function(arg = ',') {
+    let stringFromArray = '';
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        stringFromArray += '';
+      } else {
+        stringFromArray += '' + this[i];
+      }
+      if (i < (this.length - 1)) {
+        stringFromArray += '' + arg;
+      }
+    }
+    return stringFromArray;
   };
 }
 
