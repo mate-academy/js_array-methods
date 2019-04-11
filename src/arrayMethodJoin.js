fb42042a55fb55b3529fa80f72ade11a605e7a0c
anna k <tyukondra@gmail.com>
2019-04-11 20:29:01 +0300

    'solution'

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,25 @@
  *
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function() {
+  [].__proto__.join2 = function(separator) {
     // write code here
+    let resultingString = '';
+    if (separator === undefined) {
+      separator = ',';
+    };
+    if (this.length === 0) {
+      return resultingString;
+    };
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        resultingString += separator;
+      } else if (i !== (this.length - 1)) {
+        resultingString += String(this[i]) + separator;
+      } else {
+        resultingString += String(this[this.length - 1]);
+      };
+    };
+    return resultingString;
   };
 }
 
