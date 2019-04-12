3c5ddf638b03e02419724a9349e89ab32493e156
Vitalii <ilovepinkpony1@gmail.com>
2019-04-12 14:14:03 +0300

    solutin

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,23 +7,16 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(token = ',') {
     let str = '';
-    let addToken = String(token);
-    if (token === null) {
-      addToken = String(null);
-    }
+    const element = token === null ? String(null) : String(token);
     for (let i = 0; i < this.length; i++) {
       if (
         this[i] === null
         || this[i] === undefined // eslint-disable-line
       ) {
-        str += addToken;
-        continue;
-      }
-      if (i === this.length - 1) {
+        str += element;
+      } else if (i === this.length - 1) {
         str += this[i];
-        return str;
-      }
-      str += this[i] + addToken;
+      } else str += this[i] + element;
     }
     return str;
   };
