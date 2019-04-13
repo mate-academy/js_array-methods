f2a2538b2f1c2551215e2660fa40a76dd07060b1
Onisko <Onisko@MacBook.local>
2019-04-13 13:41:04 +0300

    sol

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,19 @@
  *
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function() {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let strInsteadArray = '';
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        strInsteadArray += '';
+      } else {
+        strInsteadArray += '' + this[i];
+      }
+      if (i < (this.length - 1)) {
+        strInsteadArray += '' + separator;
+      }
+    }
+    return strInsteadArray;
   };
 }
 
