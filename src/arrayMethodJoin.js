d4d798e9255e25a1586dc518b39485475198a5ce
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-04-14 13:35:35 +0300

    Yevhen Kryvdiuk

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,29 @@
  *
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function() {
-    // write code here
+  [].__proto__.join2 = function(str) {
+    let string = '';
+    if (str === undefined) {
+      str = ',';
+    }
+    if (str === null) {
+      str = 'null';
+    }
+    if (this.length === 0) {
+      return string;
+    } else {
+      this.forEach((currentValue, index, array) => {
+        const supplemented = currentValue === undefined || currentValue === null
+          ? ''
+          : currentValue;
+        if (index !== array.length - 1) {
+          string += supplemented + str;
+        } else {
+          string += supplemented;
+        }
+      });
+      return string;
+    }
   };
 }
 
