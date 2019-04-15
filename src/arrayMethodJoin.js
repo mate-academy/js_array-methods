886e8fe59203a256fbc70147ed7b2901e3cf48a6
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-04-15 14:44:56 +0300

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,30 +5,23 @@
  *
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(str) {
+  [].__proto__.join2 = function(str = ',') {
     let string = '';
     let separator = str;
-    if (separator === undefined) {
-      separator = ',';
-    }
     if (separator === null) {
       separator = 'null';
     }
-    if (this.length === 0) {
-      return string;
-    } else {
-      this.forEach((currentValue, index, array) => {
-        const supplemented = currentValue === undefined || currentValue === null
-          ? ''
-          : currentValue;
-        if (index !== array.length - 1) {
-          string += supplemented + separator;
-        } else {
-          string += supplemented;
-        }
-      });
-      return string;
-    }
+    this.forEach((currentValue, index, array) => {
+      const supplemented = currentValue === undefined || currentValue === null
+        ? ''
+        : currentValue;
+      if (index !== array.length - 1) {
+        string += supplemented + separator;
+      } else {
+        string += supplemented;
+      }
+    });
+    return string;
   };
 }
 
