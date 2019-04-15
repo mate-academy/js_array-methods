72ed298ce9f29e12360763895de3d83b373921b1
gestap <gestap@gmail.com>
2019-04-15 12:30:44 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,10 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    return String(this.length === 0 ? ''
+      : this.map(el => el === undefined || el === null ? '' : el)
+        .reduce((sum, curent) => sum + String(separator) + curent));
   };
 }
 
