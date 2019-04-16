daa08dc02563641a391173d3c976c216474b45f1
anna k <tyukondra@gmail.com>
2019-04-16 18:02:27 +0300

    corrected_solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -17,7 +17,7 @@ function applyCustomJoin() {
     for (let i = 0; i < this.length; i++) {
       if (this[i] === null || this[i] === undefined) {
         resultingString += separator;
-      } else if (i !== (this.length - 1)) {
+      } else if (i !== this.length - 1) {
         resultingString += String(this[i]) + separator;
       } else {
         resultingString += String(this[i]);
