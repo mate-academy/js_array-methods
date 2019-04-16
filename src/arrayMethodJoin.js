c77ae6b81f4fb9f9e3991af846bce9a6b1e02f80
ghj ghj <StasGostev>
2019-04-16 11:21:42 +0300

    'Without_comments'

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,18 +7,15 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(symbol) {
     let length = this.length;
-    // if recieved empty array , returns empty string
     let result = length
       ? `${this[0]}`
       : '';
 
-    // typeof argument is undefined returns join by ','
     let joiner = symbol !== undefined
       ? `${symbol}`
       : ',';
 
     for (let i = 1; i < length; i++) {
-      // i = 1 we dont need to touch first elem!
       if (typeof this[i] === 'undefined' || this[i] === null) {
         result += joiner;
       } else {
