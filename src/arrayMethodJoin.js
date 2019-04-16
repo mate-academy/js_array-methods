75f482e6ed1eb90f87d43cb25ed310d5441997d8
gestap <gestap@gmail.com>
2019-04-16 14:39:28 +0300

    modified solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,9 +5,12 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    return String(this.length === 0 ? ''
-      : this.map(el => el === undefined || el === null ? '' : el)
-        .reduce((sum, curent) => sum + String(separator) + curent));
+    return String(this.length === 0
+      ? ''
+      : this.map(el => el === undefined || el === null
+        ? ''
+        : el)
+        .reduce((result, curent) => result + String(separator) + curent));
   };
 }
 
