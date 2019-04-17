778dffc6bd00506378b932318686dd51d85ba6c0
gestap <gestap@gmail.com>
2019-04-17 17:15:07 +0300

    modified solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,12 +5,14 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
+    let separatorStirng = separator;
+    if (typeof separator !== 'string') {
+      separatorStirng = JSON.stringify(separator);
+    }
     return String(this.length === 0
-      ? ''
-      : this.map(el => el === undefined || el === null
-        ? ''
-        : el)
-        .reduce((result, curent) => result + String(separator) + curent));
+      ? '' : this.map(el => el === undefined || el === null
+        ? '' : el)
+        .reduce((result, curent) => result + separatorStirng + curent));
   };
 }
 
