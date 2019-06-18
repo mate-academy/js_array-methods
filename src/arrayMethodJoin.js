11dd42563b783fc51f18249d1ccfe91f718a2ed5
OleksandrHlybchenko <logista@meta.ua>
2019-06-19 02:07:50 +0300

    for review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,26 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let mutableSeparator = separator;
+
+    if (separator === undefined || separator === false) {
+      mutableSeparator = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        result += mutableSeparator;
+      } else if (i !== this.length - 1) {
+        result += `${this[i]}${mutableSeparator}`;
+      } else {
+        result += this[i];
+      }
+    }
+    return result;
   };
 }
 
 module.exports = applyCustomJoin;
+
+// result[result.length] = this[i];
