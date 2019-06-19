5e9447bd0ce80e0efbfec3a147cea70e9253af9b
AnnaKuvarina <anya.kuvarina@gmail.com>
2019-06-19 16:17:16 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,37 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    const inputArray = [...this];
+    let correctSeparator;
+    let resultStr = '';
+
+    if (typeof separator === 'string'
+      || typeof separator === 'number') {
+      correctSeparator = separator;
+    } else if (typeof separator === 'undefined') {
+      correctSeparator = ',';
+    } else if (typeof separator === 'object' && separator !== null) {
+      correctSeparator = '[object Object]';
+    } else if (separator == null) {
+      correctSeparator = 'null';
+    }
+
+    if (!this.length || this[0] === null || this[0] === undefined) {
+      resultStr = '';
+    } else {
+      resultStr += inputArray[0];
+    }
+
+    for (let i = 1; i < inputArray.length; i++) {
+      if (inputArray[i] === null
+        || inputArray[i] === undefined) {
+        resultStr += correctSeparator + '';
+      } else {
+        resultStr += correctSeparator + inputArray[i];
+      }
+    }
+
+    return resultStr;
   };
 }
 
