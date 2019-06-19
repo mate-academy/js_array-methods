07d51af9baad6d6372aecc2e569cf8b92f0da417
Dmitry Yevlakhov <tragenstolz@gmail.com>
2019-06-19 14:23:06 +0300

    join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,22 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let string = (this[0] === null) ? '' : '' + this[0];
+    const separ = (separator === undefined) ? ',' : separator;
+
+    if (!this.length) {
+      return '';
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        string += '' + separ;
+      } else {
+        string += `${separ}${this[i]}`;
+      }
+    }
+
+    return string;
   };
 }
 
