961756e80958c9e94f40148891a9773c96c2d684
tearfulmerlin <t380937709275@gmail.com>
2019-06-20 20:42:07 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,39 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    let separ = separator;
+
+    if (this.length === 0) {
+      return result;
+    }
+
+    // define separator
+    if (separator === undefined) {
+      separ = ',';
+    }
+
+    if (separator === null) {
+      separ = 'null';
+    }
+
+    // define first element
+    if (this[0] !== null && this[0] !== undefined) {
+      result += this[0];
+    }
+
+    // main magic
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        result += separ;
+        continue;
+      }
+
+      result += separ + this[i];
+    }
+
+    return result;
   };
 }
 
