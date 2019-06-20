4428d9244236ca80929d979e5113b82c14c3b5b3
scandal0us <d.l.lutsak@gmail.com>
2019-06-21 02:49:17 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,18 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let joined = '';
+    if (!separator) {
+      for (let i = 0; i < this.length; i++) {
+        joined += this[i] + ',';
+      }
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        joined += this[i] + separator;
+      }
+    }
+    const result = joined.substring(0, joined.length - 1);
+    return result;
   };
 }
 
