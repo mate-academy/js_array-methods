8db321b9b330167cf67c6ea061f3b85104d91de2
IvanVFedenko <ivan.v.fedenko@gmail.com>
2019-06-20 22:33:43 +0300

    Solution
    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,12 +1,25 @@
 'use strict';
-
 /**
  * Implement method join
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    const defaultSeparator = ',';
+    let symbol = separator;
+    if (symbol === undefined) {
+      symbol = defaultSeparator;
+    };
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        result += symbol;
+      } else if (i !== this.length - 1) {
+        result += `${this[i]}${symbol}`;
+      } else {
+        result += `${this[i]}`;
+      };
+    };
+    return result;
   };
-}
-
+};
 module.exports = applyCustomJoin;
