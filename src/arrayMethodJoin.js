a4b26eabbcafa7c31a2430b6b6047d6a0a46ebd4
bratyslav <beginner.altavista@gmail.com>
2019-06-20 11:50:20 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,29 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    if (this.length === 0) {
+      return '';
+    }
+
+    let sep = '';
+    if (separator === undefined) {
+      sep = ',';
+    } else {
+      sep = separator;
+    }
+
+    let resultString = '';
+    const lastIndex = this.length - 1;
+    for (let i = 0; i < lastIndex; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        resultString = resultString + sep;
+      } else {
+        resultString = resultString + this[i] + sep;
+      }
+    }
+
+    resultString += this[lastIndex];
+    return resultString;
   };
 }
 
