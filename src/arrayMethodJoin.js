014788ab0a13f4114693aa784b714a0811aca7e7
Dmitry Yevlakhov <tragenstolz@gmail.com>
2019-06-20 21:58:33 +0300

    no cheats

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,20 +4,17 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let string = (this[0] === null) ? '' : '' + this[0];
-    const currentSeparator = (separator === undefined) ? ',' : separator;
 
     if (!this.length) {
       return '';
     }
 
     for (let i = 1; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        string += '' + currentSeparator;
-      } else {
-        string += `${currentSeparator}${this[i]}`;
-      }
+      (this[i] === null || this[i] === undefined)
+        ? string += '' + separator
+        : string += '' + separator + this[i];
     }
 
     return string;
