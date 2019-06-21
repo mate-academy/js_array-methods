68cc76c3e897c2a3aa6a7cbfc336240f64a796f6
AndriiSotnikov <andrewsotnikov88@gmail.com>
2019-06-21 10:29:28 +0300

    Solution for custom join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let str = '';
+
+    // const elem of this) {
+    //   this[this.length] = elem;
+    // }
+    // return this.length;
+
+    this.forEach(function(i, index, array) {
+      if (typeof (i) === 'undefined' || i === null) {
+        str += '';
+        if (index !== (array.length - 1)) {
+          str += separator;
+        }
+      } else {
+        str += i;
+        if (index !== (array.length - 1)) {
+          str += separator;
+        }
+      }
+    });
+    return str;
   };
 }
 
