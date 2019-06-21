85283e887121621852596f3a39e8dbdf04279026
AndriiSotnikov <andrewsotnikov88@gmail.com>
2019-06-21 10:31:05 +0300

    Solution for custom join deleted unnecessary lines

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,14 +5,7 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    // write code here
     let str = '';
-
-    // const elem of this) {
-    //   this[this.length] = elem;
-    // }
-    // return this.length;
-
     this.forEach(function(i, index, array) {
       if (typeof (i) === 'undefined' || i === null) {
         str += '';
