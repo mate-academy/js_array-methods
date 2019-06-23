9b5cb34d7fd5afe890e1c930e29cd78ac3facc9e
Serhii <davydko.serhiy@gmail.com>
2019-06-24 01:59:20 +0300

    add function and if-else condition

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,24 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
+    function nullReplace(arr) {
+      if (arr === null || arr === undefined) {
+        return '';
+      } else {
+        return arr;
+      }
+    }
     let newSep = separator;
     if (separator === null || separator === undefined) {
       newSep = 'null';
     } else if (this.length < 2) {
       newSep = '';
     }
-    let result = (this[0] === null || this[0] === undefined) ? '' : this[0];
+    let result = nullReplace(this[0]);
     for (let i = 1; i < this.length; i++) {
-      result += newSep + ((this[i] === null || this[i] === undefined)
-        ? '' : this[i]);
+      result += newSep + nullReplace(this[i]);
     }
-    const strResult = String(result);
-    return strResult;
+    return String(result);
     // write code here
   };
 }
