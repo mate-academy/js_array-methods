7fb68b4d223f3cb4e06fcdb4105a0973d10cf330
AndriiSotnikov <andrewsotnikov88@gmail.com>
2019-06-24 09:49:55 +0300

    Solution for custom join improved solution simpified ternary operator

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,8 +8,8 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let str = '';
     for (const [index, elem] of this.entries()) {
-      (elem !== undefined && elem !== null) ? str += elem : str += '';
-      index !== (this.length - 1) ? str += separator : str += '';
+      str = (elem !== undefined && elem !== null) ? str + elem : str;
+      str = (index !== (this.length - 1)) ? str + separator : str;
     }
     return str;
   };
