9a6879f32529498ea8b89027b02839dbd681fc1b
Serhii <davydko.serhiy@gmail.com>
2019-06-24 11:56:38 +0300

    add spaces, rename variable

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,23 +5,28 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    function nullReplace(arr) {
-      if (arr === null || arr === undefined) {
+    function nullReplace(item) {
+      if (item === null || item === undefined) {
         return '';
       } else {
-        return arr;
+        return item;
       }
     }
+
     let newSep = separator;
+
     if (separator === null || separator === undefined) {
       newSep = 'null';
     } else if (this.length < 2) {
       newSep = '';
     }
+
     let result = nullReplace(this[0]);
+
     for (let i = 1; i < this.length; i++) {
       result += newSep + nullReplace(this[i]);
     }
+
     return String(result);
     // write code here
   };
