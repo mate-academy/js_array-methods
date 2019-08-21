8ac4e5e8f866c2acbcd4da1d384eaa1d5afd52b5
Khrystyna Styslo <khrystyna.styslo@gmail.com>
2019-08-21 11:06:07 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,23 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let joinStr = '';
+    let defaultSep = separator;
+    const cleanArr = this.map(elem =>
+      elem === undefined || elem === null ? '' : elem);
+
+    if (defaultSep === undefined) {
+      defaultSep = ',';
+    } else {
+      defaultSep += '';
+    }
+
+    for (let i = 0; i < cleanArr.length; i++) {
+      i === cleanArr.length - 1
+        ? (joinStr += cleanArr[i])
+        : (joinStr += cleanArr[i] + defaultSep);
+    }
+    return joinStr;
   };
 }
 
