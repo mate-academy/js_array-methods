23af0013715d0b0fe4e5eeb45ce99ad4b60927f1
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-08-25 23:35:04 +0300

    develop

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,23 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let newSeparator = separator;
+    let newString = '';
+    if (separator === undefined) {
+      newSeparator = ',';
+    }
+    if (separator === null) {
+      newSeparator = 'null';
+    }
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1) {
+        newString += this[i];
+        break;
+      }
+      this[i] === null || this[i] === undefined ? newString += ','
+        : newString += this[i] + newSeparator;
+    }
+    return newString;
   };
 }
 
