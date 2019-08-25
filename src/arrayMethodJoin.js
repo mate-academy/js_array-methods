ca580fa329e4fa77cf8eb4034956b0467ae0d6d9
Miroslava <777shadowpriest777@gmail.com>
2019-08-25 16:03:21 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,22 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    const sep = separator === undefined ? ',' : separator;
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (this.length === 1) {
+      return `${this[0]}`;
+    }
+
+    return this.reduce((a, b) => {
+      const prev = (a === null || a === undefined) ? '' : a;
+      const current = (b === null || b === undefined) ? '' : b;
+
+      return `${prev}${sep}${current}`;
+    });
   };
 }
 
