8e5b38d90252426cf152dbc54b93a61ccac00652
unknown <shklyarigor2015@gmail.com>
2019-08-25 21:19:54 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,22 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let returnString = '';
+    let defaultSeparator = '';
+
+    defaultSeparator = separator === undefined ? ',' : separator + '';
+
+    for (let index = 0; index < this.length; index++) {
+      if (this[index] === null || this[index] === undefined) {
+        returnString += defaultSeparator;
+      } else if (index !== this.length - 1) {
+        returnString += this[index] + defaultSeparator;
+      } else {
+        returnString += this[index];
+      }
+    }
+
+    return returnString;
   };
 }
 
