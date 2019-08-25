d0be590685264f6b2f795360dad2b3219a3d2e70
Taisiia <taisiia.pomazan@gmail.com>
2019-08-25 22:11:14 +0300

    sol

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    if (this.length < 1) {
+      return '';
+    } else if (this[0] !== null && this[0] !== undefined) {
+      result += this[0];
+    };
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        result += `${separator}` + '';
+        continue;
+      };
+      result += `${separator}` + this[i];
+    };
+
+    return result;
   };
 }
 
