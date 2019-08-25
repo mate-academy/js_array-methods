1d9076a6b432915ebde214bc23f80b6db0098fe1
Volodsher <volodsher@gmail.com>
2019-08-25 21:47:29 +0300

    long but working

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,34 +4,22 @@
  * Implement method join
  */
 
-// let x = [0];
-// function joining(separator) {
-//   let str = '';
-//   let middleString = '';
-//   if (separator !== undefined) {
-//     str += x[0];
-//     for (let a = 1; a < x.length -  1; a++) {
-//       middleString = middleString + separator + x[a];
-//     };
-//     str = str + middleString + separator + x[x.length - 1];
-//   } else {
-//     str += x[0];
-//     for (let i = 1; i < x.length - 1; i++) {
-//       middleString = middleString + ',' + x[i];
-//     };
-//     str = str + middleString + ',' + x[x.length - 1];
-//   }
-
-//   return str;
-// }
-// console.log(joining());
-// let u = [[0]];
-// console.log(u.join());
-
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+    }
     let str = '';
     let middleString = '';
+    if (this.length === 0) {
+      return str;
+    };
+    if (this.length === 1) {
+      str += this[0];
+      return str;
+    };
     if (separator !== undefined) {
       str += this[0];
       for (let a = 1; a < this.length - 1; a++) {
