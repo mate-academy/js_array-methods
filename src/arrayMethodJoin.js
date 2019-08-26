d86abe79ee29c94622cbcbe18a5e58cdac50e701
KseniiaMazan <ksusamazan@gmail.com>
2019-08-26 14:27:21 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,17 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let resultStr = ``;
+    for (let i = 0; i < this.length; i++) {
+      const intermidiateVar = this[i] == null || undefined
+        ? ''
+        : this[i];
+      resultStr = i !== 0
+        ? `${resultStr}${separator}${intermidiateVar}`
+        : `${intermidiateVar}`;
+    }
+    return resultStr;
   };
 }
 
