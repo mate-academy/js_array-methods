e070ed5228b2ffe9624baa077d605dc1fbe0ac9a
aleks-mos <mossssss@ukr.net>
2019-08-26 18:04:40 +0300

    create doc

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,18 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let stringJoin = '';
+    const joinSeparator = (separator === undefined) ? ',' : separator;
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        stringJoin += `${joinSeparator}`;
+      } else if (i === this.length - 1) {
+        stringJoin += this[i];
+      } else {
+        stringJoin += `${this[i]}${joinSeparator}`;
+      }
+    }
+    return stringJoin;
   };
 }
 
