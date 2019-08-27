3252f3c502f24c82cb05a3fe33d78344ded8804c
Deman250 <deman.250052@gmail.com>
2019-08-27 12:40:33 +0300

    sol1

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,23 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let resultStr = '';
+    let sep = separator;
+    if (typeof separator === 'undefined') {
+      sep = ',';
+    }
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || typeof this[i] === 'undefined') {
+        this[i] = '';
+      }
+      if (i !== this.length - 1) {
+        resultStr = resultStr + this[i] + sep;
+      } else {
+        resultStr = resultStr + this[i];
+      }
+    }
+
+    return resultStr;
   };
 }
 
