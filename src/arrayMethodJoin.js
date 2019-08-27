bc079005938bd19cf4c8f758dd2b9e1b9ca96631
Deman250 <deman.250052@gmail.com>
2019-08-27 19:01:17 +0300

    sol2

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,20 +6,22 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     let resultStr = '';
-    let sep = separator;
+    let strSeparator = separator;
+
     if (typeof separator === 'undefined') {
-      sep = ',';
-    }
+      strSeparator = ',';
+    };
+
     for (let i = 0; i < this.length; i++) {
       if (this[i] === null || typeof this[i] === 'undefined') {
         this[i] = '';
-      }
+      };
       if (i !== this.length - 1) {
-        resultStr = resultStr + this[i] + sep;
+        resultStr = resultStr + this[i] + strSeparator;
       } else {
         resultStr = resultStr + this[i];
-      }
-    }
+      };
+    };
 
     return resultStr;
   };
