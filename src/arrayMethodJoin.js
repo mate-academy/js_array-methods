efd10bed3632fcd719b1c05048827289f217807c
Miroslava <777shadowpriest777@gmail.com>
2019-08-27 21:22:07 +0300

    refactored

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,24 +3,22 @@
 /**
  * Implement method join
  */
+/* eslint-disable no-param-reassign */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    const sep = separator === undefined ? ',' : separator;
+    separator = separator === undefined ? ',' : separator;
 
-    if (this.length === 0) {
-      return '';
+    let result = ``;
+
+    for (let i = 0; i < this.length; i++) {
+      const currentValue = (this[i] === null || this[i] === undefined)
+        ? ''
+        : this[i];
+
+      result += `${i === 0 ? '' : separator}${currentValue}`;
     }
 
-    if (this.length === 1) {
-      return `${this[0]}`;
-    }
-
-    return this.reduce((a, b) => {
-      const prev = (a === null || a === undefined) ? '' : a;
-      const current = (b === null || b === undefined) ? '' : b;
-
-      return `${prev}${sep}${current}`;
-    });
+    return result;
   };
 }
 
