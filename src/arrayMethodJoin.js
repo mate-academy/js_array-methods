be6be1acfb9156315fd94c18d37c12b8a5e39e7c
ArtemMarkevych1 <aw.markevich@gmail.com>
2019-08-27 08:51:12 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,25 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let convertedString = '';
+    let delimiter = separator;
+
+    const filterDataForJoin = this.map((element) =>
+      (element === undefined || element === null) ? '' : element
+    );
+
+    if (separator === undefined) {
+      delimiter = ',';
+    } else if (separator === null) {
+      delimiter = 'null';
+    }
+
+    for (let i = 0; i < filterDataForJoin.length; i++) {
+      convertedString += (i === filterDataForJoin.length - 1)
+        ? filterDataForJoin[i] : filterDataForJoin[i] + delimiter;
+    }
+
+    return convertedString;
   };
 }
 
