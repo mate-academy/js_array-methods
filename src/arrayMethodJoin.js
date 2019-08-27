93c025bd2b2e40f052bff74247642da9c5298dfd
KseniiaMazan <ksusamazan@gmail.com>
2019-08-27 16:14:18 +0300

    fixed solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,14 +6,17 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let resultStr = ``;
+
     for (let i = 0; i < this.length; i++) {
       const intermidiateVar = this[i] == null || undefined
         ? ''
         : this[i];
+
       resultStr = i !== 0
         ? `${resultStr}${separator}${intermidiateVar}`
         : `${intermidiateVar}`;
     }
+
     return resultStr;
   };
 }
