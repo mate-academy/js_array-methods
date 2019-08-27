8b1205e25374b6fb1945f52d4e21a66c915c2ca7
Vladimir <koretskyi@gmail.com>
2019-08-27 19:36:51 +0300

    my sol

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,18 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedString = '';
+    this.forEach((el, i) => {
+        if (el === null || el === undefined) {
+          joinedString += separator;
+        } else if (i !== this.length - 1) {
+        joinedString += el + `${separator}`;
+        } else {
+        joinedString += el;
+      }
+    })
+  return joinedString;
   };
 }
 
