6dc12425c02c32e221b4eddb5e08e0f1ecfd61df
vladyslav223 <vladyslav223@gmail.com>
2019-08-28 15:20:11 +0300

    lines added

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,15 +6,19 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
+
     for (let i = 0; i < this.length; i++) {
       if (this[i] === null || this[i] === undefined) {
         this[i] = '';
       }
+
       result += this[i];
+
       if (i < this.length - 1) {
         result += separator;
       }
     }
+
     return result;
   };
 }
