5ae96ea3214d09eb256d2aa848ef08d818f4288d
Pukhnichenko <victorpuhnichenko@gmail.com>
2019-08-28 12:40:03 +0300

    'Fixed-v.2'

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,24 +7,28 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     let newSeparator = separator;
     let newString = '';
+
     if (separator === undefined) {
       newSeparator = ',';
-    }
+    };
+
     if (separator === null) {
       newSeparator = 'null';
-    }
+    };
+
     if (this.length === 0) {
       return '';
-    }
+    };
+
     for (let i = 0; i < this.length; i++) {
       if (i === this.length - 1) {
         newString += this[i];
         return newString;
-      }
+      };
       newString += this[i] === null || this[i] === undefined
         ? ','
         : this[i] + newSeparator;
-    }
+    };
   };
 }
 
