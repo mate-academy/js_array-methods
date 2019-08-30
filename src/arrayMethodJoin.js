9c84bad3ec720c0ca4eaf0893f29563d5f2d228d
Illia Bezkorovainyi <illia.bezkorovainyi@heathmont.net>
2019-08-30 12:43:47 +0300

    refactoring

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,15 +4,14 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    let result = '';
-    let defaultSeparator = separator + '';
-    if (separator === undefined) {
-      defaultSeparator = ',';
-    }
+  [].__proto__.join2 = function(separator = ',') {
     if (this.length === 0) {
-      return result;
+      return '';
     }
+
+    let result = '';
+    const defaultSeparator = separator + '';
+
     for (let i = 0; i < this.length - 1; i++) {
       if (this[i] === null || this[i] === undefined) {
         result += '' + defaultSeparator;
