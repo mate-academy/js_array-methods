675b40ddfcd1b815b12e17af0124400e0a5f5782
VadimK <djvadya16@gmail.com>
2019-10-02 20:24:54 +0300

    done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,24 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let string = '';
+    let sep = separator;
+    if (sep === '' || sep === null
+    || typeof sep === 'object') {
+      sep = String(sep);
+    } else {
+      sep = sep || ',';
+    };
+
+    this.forEach(item => {
+      if (item === undefined || item === null) {
+        string += sep;
+      } else {
+        string += item + sep;
+      }
+    });
+
+    return string.slice(0, string.length - sep.length);
   };
 }
 
