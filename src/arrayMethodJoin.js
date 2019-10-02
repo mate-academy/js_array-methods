083a0e43bf8b731c24bac199d6d76cef116eb921
Serhii <gespenst.serge@yandex.ru>
2019-10-02 15:47:45 +0300

    sol

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,27 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let sep = String(separator);
+
+    if ((!sep && sep !== '') || sep === 'undefined') {
+      sep = ',';
+    }
+
+    let finalStr = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this.length - i === 1) {
+        finalStr += (this[i]);
+      } else {
+        finalStr += (this[i] + sep);
+      }
+    }
+
+    if (sep === 'null') {
+      return finalStr;
+    }
+
+    return finalStr.replace(/null|undefined/g, '');
   };
 }
 
