7695fd4c8fda752a24ce8d3994c519fdcacca057
Chepkyi Mykola <chepkiyn@gmail.com>
2019-10-03 16:22:22 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,17 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let sep = separator;
+    if (sep === undefined) {
+      sep = ',';
+    }
+    if (this.length === 0) {
+      return '';
+    }
+    return this.map(item =>
+      (item === null || item === undefined) ? '' : item.toString())
+      .reduce((str, item) =>
+        str + sep + item);
   };
 }
 
