ba3c02ce169f6238dde408f3c8cea06f6cf09dc7
Sosibona <empty.tara@gmail.com>
2019-10-03 18:31:35 +0300

    join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,22 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let string = '';
+    let separatorCopy = separator;
+    for (let i = 0; i < this.length; i++) {
+      if ((this[i] === null) || (this[i] === undefined)) {
+        this[i] = '';
+      }
+      if (separatorCopy === undefined) {
+        separatorCopy = ',';
+      }
+      if (i === this.length - 1) {
+        string = string + this[i];
+      } else {
+        string = string + this[i] + separatorCopy;
+      }
+    }
+    return string;
   };
 }
 
