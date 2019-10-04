ccba1d53aa3e1342f35a06679fc2cc65216622af
lubremets <lubremets@gmail.com>
2019-10-04 17:52:44 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,21 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+    const arrayLength = this.length;
+    for (let i = 0; i < arrayLength; i++) {
+      if ((this[i] !== null) && (this[i] !== undefined)) {
+        str += this[i];
+      }
+      if ((i < arrayLength - 1)) {
+        if (separator !== undefined) {
+          str += separator;
+        } else {
+          str += ',';
+        }
+      }
+    }
+    return str;
   };
 }
 
