17a3537e83ca9f902701a14a2542436318d9cecf
VadimK <djvadya16@gmail.com>
2019-10-04 16:14:07 +0300

    short solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,25 +5,8 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    let string = '';
-    let sep = separator;
-    if (sep === '' || sep === null
-    || typeof sep === 'object') {
-      sep = String(sep);
-    } else {
-      sep = sep || ',';
-    };
-
-    this.forEach(item => {
-      if (item === undefined || item === null) {
-        string += sep;
-      } else {
-        string += item + sep;
-      }
-    });
-
-    return string.slice(0, string.length - sep.length);
+    return separator === undefined ? this.toString()
+      : this.toString().replace(/,/g, separator);
   };
 }
-
 module.exports = applyCustomJoin;
