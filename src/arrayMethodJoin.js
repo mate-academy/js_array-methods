8ed2d398376d5f0daf97dec21c2d0440b2a0ebf9
Shurkodr <sashaglazunov2@gmail.com>
2019-10-04 12:26:09 +0300

    passed local tests

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,28 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+    if (this.length === 0) {
+      return '';
+    }
+    let separatorFun = separator;
+    if (separatorFun === undefined) {
+      separatorFun = ',';
+    }
+    if (separatorFun === null) {
+      separatorFun = 'null';
+    }
+    for (let i = 0; i < this.length; i++) {
+      let item = this[i];
+      if (item === null || item === undefined) {
+        item = '';
+      }
+      str += item.toString();
+      if (i !== this.length - 1) {
+        str += separatorFun.toString();
+      }
+    }
+    return str;
   };
 }
 
