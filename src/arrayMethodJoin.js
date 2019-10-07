0e82285ebd02fdaba47a62ba1a680d60ea0da2a9
YuraBobeshko <gorobech55@gmail.com>
2019-10-07 15:19:48 +0300

    sol

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,27 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
-  };
+
+  [].__proto__.join2 = function(separator = ',') {
+
+    let result = '';
+    const separate = String(separator);
+
+    for (let i = 0; i < this.length; i++) {
+
+      if (this[i] === null || this[i] === undefined) {
+        result += separate;
+      } else {
+        result += this[i] + separate;
+      }
+    }
+
+    if (separate === null) {
+      return result;
+    } else {
+      return result.slice(0, result.length - separate.length);
+    }
+  }
 }
 
 module.exports = applyCustomJoin;
