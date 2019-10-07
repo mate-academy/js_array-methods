c00364ff1d9d5b3248407a5798b79c90c4a1bb6a
VitaliyStarodub <starodub.vital@gmail.com>
2019-10-07 22:01:00 +0300

    'solution_vitalii'

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,27 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    const checkedSeparator = separatorCheck(separator);
+    let string = '';
+    for (let i = 0; i < this.length; i++) {
+      const checkedElement = arrayElementCheck(this[i]);
+      if (i < this.length - 1) {
+        string += checkedElement + checkedSeparator;
+      } else {
+        string += checkedElement;
+      }
+    }
+    return string;
   };
+}
+
+function separatorCheck(separator) {
+  return separator == null ? 'null' : separator;
+}
+function arrayElementCheck(element) {
+  return element == null || element === undefined ? '' : element;
 }
 
 module.exports = applyCustomJoin;
