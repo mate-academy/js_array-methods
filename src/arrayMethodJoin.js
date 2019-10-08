4409a2cc556a50c602b774524376386db52c46ff
MMashivskij <mmashivskij@gmail.com>
2019-10-08 17:25:38 +0300

    Solutions

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,41 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    if (this.length === 0) {
+      return '';
+    };
+    let newString = '';
+    switch (separator) {
+      case undefined:
+        for (let i = 0; i < this.length - 1; i++) {
+          if (this[i] === null || this[i] === undefined) {
+            this[i] = '';
+          };
+          newString += (this[i] + ',');
+        };
+        break;
+
+      case null:
+        for (let i = 0; i < this.length - 1; i++) {
+          if (this[i] === null || this[i] === undefined) {
+            this[i] = '';
+          };
+          newString += (this[i] + 'null');
+        };
+        break;
+
+      default:
+        for (let i = 0; i < this.length - 1; i++) {
+          if (this[i] === null || this[i] === undefined) {
+            this[i] = '';
+          };
+          newString += (this[i] + separator);
+        };
+        break;
+    };
+
+    return newString + this[this.length - 1];
   };
-}
+};
 
 module.exports = applyCustomJoin;
