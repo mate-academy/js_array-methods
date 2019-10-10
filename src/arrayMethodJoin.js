eee0e3e4e95ddac9f729504c09ef543bd823dfe9
MMashivskij <mmashivskij@gmail.com>
2019-10-10 14:28:02 +0300

    Fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,38 +4,17 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     if (this.length === 0) {
       return '';
     };
     let newString = '';
-    switch (separator) {
-      case undefined:
-        for (let i = 0; i < this.length - 1; i++) {
-          if (this[i] === null || this[i] === undefined) {
-            this[i] = '';
-          };
-          newString += (this[i] + ',');
-        };
-        break;
-
-      case null:
-        for (let i = 0; i < this.length - 1; i++) {
-          if (this[i] === null || this[i] === undefined) {
-            this[i] = '';
-          };
-          newString += (this[i] + 'null');
-        };
-        break;
-
-      default:
-        for (let i = 0; i < this.length - 1; i++) {
-          if (this[i] === null || this[i] === undefined) {
-            this[i] = '';
-          };
-          newString += (this[i] + separator);
-        };
-        break;
+    const newSeparator = String(separator);
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+      newString += (this[i] + newSeparator);
     };
 
     return newString + this[this.length - 1];
