cb895665488e2e093110a08fe58d1e08af5368b8
OlehTereshchuk <olehtereshchuk@ukr.net>
2019-11-19 13:54:58 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,26 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let result = '';
+    let delimeter = separator;
+
+    if (delimeter === undefined) {
+      delimeter = ',';
+    } else if (!this.length) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        result += separator;
+      } else if (i === this.length - 1) {
+        result += this[i];
+      } else {
+        result += this[i].toString() + separator;
+      }
+    }
+
+    return result;
   };
 }
 
