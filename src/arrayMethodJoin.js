243a9b38a26d747eefab54e95c3010e6c73e3023
natya2203 <nastya_2203@ukr.net>
2019-11-19 23:41:45 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,35 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = [];
+
+    if (separator === undefined) {
+      // eslint-disable-next-line no-param-reassign
+      separator = ',';
+    }
+
+    if (separator === null) {
+      // eslint-disable-next-line no-param-reassign
+      separator = 'null';
+    }
+
+    // eslint-disable-next-line no-param-reassign
+    separator = separator.toString();
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        result += '' + separator;
+      } else {
+        result += this[i] + separator;
+      }
+    }
+
+    result += this[this.length - 1];
+    return result;
   };
 }
 
