de4966245f0fbfbab7fa96fa9c76b0e7e9a5f89b
Oles Marola <oles_pw@ukr.net>
2019-11-19 14:13:39 +0200

    join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,38 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let string = '';
+    for (let i = 0; i < this.length; i++) {
+      if (i !== this.length - 1) {
+        const symbol = isUndefined(this[i]) || isNull(this[i]) ? '' : this[i];
+        string += `${symbol}${separator}`;
+      } else {
+        string += this[i];
+      }
+    }
+    return string;
   };
+}
+
+/**
+ * This function checks if value is undefined.
+ *
+ * @param {any} value
+ * @returns {boolean}
+ */
+function isUndefined(value) {
+  return value === undefined;
+}
+
+/**
+ * This function checks if value is null.
+ *
+ * @param {any} value
+ * @returns {boolean}
+ */
+function isNull(value) {
+  return value === null;
 }
 
 module.exports = applyCustomJoin;
