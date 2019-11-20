d65134bfcb9915386e2d7219d659e68531018278
Stanislav Mochyr <delenjer@gmail.com>
2019-11-20 16:39:43 +0200

    Solutio

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,26 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let str = '';
+    let glue = '';
+
+    if (separator === undefined) {
+      glue = ',';
+    } else {
+      glue = separator;
+    }
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        str += '';
+      } else {
+        str += this[i];
+      }
+      if (i !== this.length - 1) {
+        str += glue;
+      }
+    }
+
+    return str;
   };
 }
 
