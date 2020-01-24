57a01e056fb15b15f47e86123180e04d7be3f756
Anastasiia Bardyuzha <stasya.meshcheryakova@gmail.com>
2020-01-24 18:16:55 +0200

    done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,37 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let joinSeparator = separator;
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (joinSeparator === undefined) {
+      joinSeparator = ',';
+    }
+
+    if (joinSeparator === null) {
+      joinSeparator = 'null';
+    }
+
+    if (this[0] === null) {
+      this[0] = '';
+    }
+
+    let string = String(this[0]);
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      const point = joinSeparator + this[i];
+
+      string += point;
+    }
+
+    return string;
   };
 }
 
