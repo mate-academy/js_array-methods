2b5f161c068dc9849b941b3ca098cbb060cfc8b1
Olexander <kravchenko.sania@gmail.com>
2020-01-24 18:08:45 +0200

    solution arrayCustomJoin

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,27 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let innerSeparator = separator;
+
+    if (this.length === 0) {
+      return result;
+    }
+
+    if (!innerSeparator && innerSeparator !== '' && innerSeparator !== null) {
+      innerSeparator = ',';
+    } else if (innerSeparator === null) {
+      innerSeparator = 'null';
+    }
+
+    for (let i = 0; i < this.length - 1; ++i) {
+      result += (this[i] === null || this[i] === undefined)
+        ? innerSeparator
+        : this[i] + innerSeparator;
+    }
+    result += this[this.length - 1];
+
+    return result;
   };
 }
 
