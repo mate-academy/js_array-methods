12388a84de23f3e605fd8311912353f24fb8e57d
Vitalii <vitaliybondarenko1982@gmail.com>
2020-01-24 18:01:41 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,27 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = ``;
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      if (separator === undefined) {
+        result += this[i] + ',';
+      } else if (separator === null) {
+        result += this[i] + 'null';
+      } else {
+        result += this[i] + separator;
+      }
+    }
+
+    return result + this[this.length - 1];
   };
 }
 
