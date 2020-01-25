fcf41a5151485b1df4bd593ded4d385e0df95d2a
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-01-25 20:01:29 +0200

    add solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,27 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    let customSeparator = separator;
+
+    if (separator === null) {
+      customSeparator = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this.length === 1 || customSeparator === '') {
+        result += this[i];
+      } else if (this[i] === null || this[i] === undefined) {
+        result += '' + customSeparator;
+      } else if (i !== this.length - 1) {
+        result += this[i] + customSeparator;
+      } else {
+        result += this[i];
+      }
+    }
+
+    return result;
   };
 }
 
