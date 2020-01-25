e2bcc15c07b797638051dc7685e29f80d39176fc
Viacheslav <slavic.lavrenyuk@gmail.com>
2020-01-25 17:43:11 +0200

    Method Join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,27 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let joinResult = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        this[i] = '';
+      }
+
+      if (separator === undefined) {
+        joinResult += this[i] + ',';
+      } else if (separator === null) {
+        joinResult += this[i] + 'null';
+      } else {
+        joinResult += this[i] + separator;
+      }
+    }
+
+    return joinResult + this[this.length - 1];
   };
 }
 
