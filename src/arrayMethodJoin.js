d7673f0f114c8a950630d6f76502be0fd66e73c6
Senya Volkov <formobileprima@gmail.com>
2020-01-26 23:08:42 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,28 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let separatorSign = separator;
+    const argumantsArr = this;
+
+    for (let i = 0; i < argumantsArr.length; i++) {
+      if (separatorSign === undefined) {
+        separatorSign = ',';
+      } else if (separatorSign === null) {
+        separatorSign = 'null';
+      }
+
+      if (argumantsArr[i] === undefined || argumantsArr[i] === null) {
+        result += ',';
+        continue;
+      } else if (i + 1 === argumantsArr.length) {
+        result += argumantsArr[i];
+        continue;
+      }
+      result += argumantsArr[i] + separatorSign;
+    }
+
+    return result;
   };
 }
 
