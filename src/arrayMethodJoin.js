6dde19d661bb62038d304b47be463cec596d9017
Actireon <eheieiei37@gmail.com>
2020-01-26 22:22:24 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,37 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let mySeparator = separator;
+
+    switch (mySeparator) {
+      case undefined: {
+        mySeparator = ',';
+        break;
+      }
+
+      case null: {
+        mySeparator = 'null';
+        break;
+      }
+    }
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (this.length === 1) {
+      return String(this[0]);
+    }
+
+    this.forEach((item, index) => {
+      if (item === null || item === undefined) {
+        this[index] = '';
+      }
+    });
+
+    return this.reduce((str1, str2) => {
+      return str1 + mySeparator + str2;
+    });
   };
 }
 
