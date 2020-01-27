bc29c9bf88dd8f0b0875119e8a647dfab70b3154
morfeii <morfeii88@gmail.com>
2020-01-27 14:43:19 +0200

    add variable 'separatorStr'

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,17 +6,18 @@
 
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    if (this.length === 0) {
+    if (!this.length) {
       return '';
     }
 
     let resultString = '';
+    const separatorStr = String(separator);
 
     for (let i = 0; i < this.length - 1; i += 1) {
       if (this[i] === null || this[i] === undefined) {
         resultString += separator;
       } else if (separator === null) {
-        resultString += this[i] + String(separator);
+        resultString += this[i] + separatorStr;
       } else {
         resultString += this[i] + separator;
       }
