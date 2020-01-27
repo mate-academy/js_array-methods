de2db4fdbbc3aab06b02f6715fda984c3006a4bb
oksanaDaniluk <oksana.daniluk@ukr.net>
2020-01-27 15:05:32 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,26 @@
 'use strict';
 
-/**
- * Implement method join
- */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let arrayJoin = '';
+
+    if (!this.length) {
+      return arrayJoin;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        arrayJoin += separator;
+      } else if (i === this.length - 1) {
+        arrayJoin += String(this[i]);
+
+        return arrayJoin;
+      } else {
+        arrayJoin += String(this[i]) + separator;
+      }
+    }
+
+    return arrayJoin;
   };
 }
 
