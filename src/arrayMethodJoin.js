8005d72f80adb5eecaa1c77350becb6ebea8af4f
SahaPylypyuk <sashapulupyk@gmail.com>
2020-01-27 17:13:53 +0200

    Task implemented

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,26 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let string = '';
+
+    if (!this.length) {
+      return string;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        string += separator;
+      } else if (i === this.length - 1) {
+        string += String(this[i]);
+
+        return string;
+      } else {
+        string += String(this[i]) + separator;
+      }
+    }
+
+    return string;
   };
 }
 
