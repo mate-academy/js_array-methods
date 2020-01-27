928143f53971de0fccc5bd1c6d34da7cff23511b
lutfie <serverieva.lutfie@gmail.com>
2020-01-27 17:02:09 +0200

    added method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let outStr = '';
+    const stringSeparator = String(separator);
+
+    if (!this.length) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        outStr += stringSeparator;
+      } else if (i !== this.length - 1) {
+        outStr += this[i] + stringSeparator;
+      } else {
+        outStr += this[i];
+      }
+    }
+
+    return outStr;
   };
 }
 
