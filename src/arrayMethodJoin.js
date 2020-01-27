f83ba8561bf1e6e4554b5bc0b5f1d7a3fcd851c7
Vova Pyvovar <ditinapivo@gmail.com>
2020-01-27 17:11:17 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    const strSeparator = String(separator);
+
+    if (!this.length) {
+      return result;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if ((this[i] === null || this[i] === undefined)) {
+        result += strSeparator;
+      } else if (i !== this.length - 1) {
+        result += this[i] + strSeparator;
+      } else {
+        result += this[i];
+      }
+    }
+
+    return result;
   };
 }
 
