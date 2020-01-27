46844924b88dff28e840ced03855dc5f09fc3815
Roma <romakoder@gmail.com>
2020-01-27 07:50:02 +0200

    add solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,28 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let divider = separator;
+
+    if (this.length === 0) {
+      return '';
+    } else if (divider === undefined) {
+      divider = ',';
+    } else if (divider === null) {
+      divider = 'null';
+    } else if (this[0] === null) {
+      this[0] = '';
+    }
+
+    let result = this[0].toString();
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+      result += divider + this[i];
+    }
+
+    return result;
   };
 }
 
