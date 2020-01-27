43aa7970d9ed89f30398bbdd3d6ccaa6d7baeda7
Viktoria <valencia88vika@gmail.com>
2020-01-27 10:01:35 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,29 @@
 'use strict';
 
-/**
- * Implement method join
- */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    const len = this.length - 1;
+
+    for (let i = 0; i < len; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      if (separator === null) {
+        result = result + this[i] + 'null';
+      } else if (separator === undefined) {
+        result = result + this[i] + ',';
+      } else {
+        result = result + this[i] + separator;
+      }
+    }
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    return result + this[len];
   };
 }
 
