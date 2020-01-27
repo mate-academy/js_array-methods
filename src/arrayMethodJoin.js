ba2a8cc96afdbdaf1be16137f989ba1007d11378
Stanislav Mochyr <delenjer@gmail.com>
2020-01-27 18:43:14 +0200

    change

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,20 +7,15 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let symbol = '';
 
-    if (separator === undefined) {
-      symbol = ',';
-    } else {
-      symbol = separator;
-    }
+    (separator === undefined) ? symbol = ',' : symbol = separator;
 
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        result += '';
-      } else {
-        result += this[i];
-      }
+      (this[i] === null
+        || this[i] === undefined)
+        ? result += ''
+        : result += this[i];
 
       if (i !== this.length - 1) {
         result += symbol;
