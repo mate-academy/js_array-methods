f8e0d5103ac3cc2a38ea5b0c43df209e269ca684
Al145 <47367383+Al145@users.noreply.github.com>
2020-01-28 21:59:58 +0200

    solut

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,21 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let join = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        join += '' + `${separator}`;
+      } else if (i !== this.length - 1) {
+        join += this[i] + `${separator}`;
+      } else {
+        join += this[i];
+      }
+    }
+
+    return join;
   };
 }
 
