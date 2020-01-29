391fb8c1eca6b45d57dce0188d5792a51cc2ebf8
Roma <romakoder@gmail.com>
2020-01-29 12:25:12 +0200

    fixed comments

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,13 +4,11 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let divider = separator;
 
     if (this.length === 0) {
       return '';
-    } else if (divider === undefined) {
-      divider = ',';
     } else if (divider === null) {
       divider = 'null';
     } else if (this[0] === null) {
@@ -20,10 +18,12 @@ function applyCustomJoin() {
     let result = this[0].toString();
 
     for (let i = 1; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        this[i] = '';
+      let item = this[i];
+
+      if (item === null || item === undefined) {
+        item = '';
       }
-      result += divider + this[i];
+      result += divider + item;
     }
 
     return result;
