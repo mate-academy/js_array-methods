8aa6d7ea1d7012e7f38532bf85c669cc025fa600
ded-git <sakarailko@gmail.com>
2020-01-29 02:31:28 +0200

    fix lint

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,37 @@
+/* eslint-disable no-param-reassign */
 'use strict';
 
 /**
  * Implement method join
  */
+
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+
+    if (separator === undefined) {
+      separator = ',';
+    }
+
+    for (const element of this) {
+      switch (element) {
+        case this[this.length - 1]:
+          str += element;
+          break;
+        case null:
+          str += '';
+          str += ',';
+          break;
+        case undefined:
+          str += ',';
+          break;
+        default:
+          str += element;
+          str += separator;
+      }
+    }
+
+    return str;
   };
 }
 
