3a287ff65ddeea7436464e308fe63c475a2f6d28
AlenaLoik <CrazyBeeLviv@gmail.com>
2020-03-27 21:19:24 +0200

    join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,32 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let res;
+    let sep = separator;
+
+    if (separator === undefined) {
+      sep = ',';
+    }
+
+    if (this[0] === undefined || this[0] == null) {
+      res = '';
+    } else {
+      res = String(this[0]);
+    }
+
+    if (this === undefined) {
+      res = '';
+    } else {
+      for (let i = 1; i < this.length; i++) {
+        if (String(this[i]) === undefined || this[i] == null) {
+          res += sep + '';
+        } else {
+          res += sep + String(this[i]);
+        }
+      }
+    }
+
+    return res;
   };
 }
 
