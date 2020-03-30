5e8bde1031fab210f050aec11b62a6d69df5cac2
AlenaLoik <CrazyBeeLviv@gmail.com>
2020-03-30 16:51:52 +0300

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,29 +4,19 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    let res;
-    let sep = separator;
-
-    if (separator === undefined) {
-      sep = ',';
+  [].__proto__.join2 = function(separator = ',') {
+    if (!this) {
+      return '';
     }
 
-    if (this[0] === undefined || this[0] == null) {
-      res = '';
-    } else {
-      res = String(this[0]);
-    }
+    let res = this[0] === undefined || this[0] == null
+      ? '' : String(this[0]);
 
-    if (this === undefined) {
-      res = '';
-    } else {
-      for (let i = 1; i < this.length; i++) {
-        if (String(this[i]) === undefined || this[i] == null) {
-          res += sep + '';
-        } else {
-          res += sep + String(this[i]);
-        }
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        res += separator + '';
+      } else {
+        res += separator + String(this[i]);
       }
     }
 
