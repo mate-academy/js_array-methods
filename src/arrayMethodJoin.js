57671c7628e2f9f47b3ba6bc5ceb3418e299f267
KostyaPenyavskiy <50549720+KostyaPenyavskiy@users.noreply.github.com>
2020-04-01 14:53:47 +0300

    Update arrayMethodJoin.js

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,29 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let str = '';
-    let localSeparator = separator;
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] === null || this[i] === undefined) {
         this[i] = '';
-      }
+      } 
 
       if (i === this.length - 1) {
         str += this[i];
         continue;
       }
 
-      if (separator === null) {
-        localSeparator = 'null';
-      }
-
-      if (separator === undefined) {
-        localSeparator = ',';
-      }
-
-      str += this[i] + localSeparator;
+      str += this[i] + `${separator}`;
     }
 
     return str;
