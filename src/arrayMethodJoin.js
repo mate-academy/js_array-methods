e020b458c0cb2cb6c1c15412f9b9578934942711
Vitalii Korol <vitaliikorol@github.com>
2020-04-01 15:22:29 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,28 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let mySeparator = separator;
+
+    for (let i = 0; i < this.length; i++) {
+      if ((this[i] === undefined) || (this[i] === null)) {
+        this[i] = '';
+      }
+
+      if (i === this.length - 1) {
+        result += this[i];
+
+        return result;
+      }
+
+      if (separator === undefined) {
+        mySeparator = ',';
+      }
+
+      result += '' + this[i] + mySeparator;
+    }
+
+    return result;
   };
 }
 
