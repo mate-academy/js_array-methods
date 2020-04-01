a5827604da03a0ea7ec616b96387820401d9e372
kate <kate@test.com>
2020-04-01 16:01:47 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,29 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    const arr = this;
+    let sep = separator;
+    let res = '';
+
+    if (sep === undefined) {
+      sep = ',';
+    } else if (sep === null) {
+      sep = 'null';
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      const item = arr[i];
+
+      if (item !== null && item !== undefined) {
+        res += item.toString();
+      }
+
+      if (i + 1 < arr.length) {
+        res += sep.toString();
+      }
+    }
+
+    return res;
   };
 }
 
