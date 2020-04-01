656c0213d7934bb914481a4b42d3f5d39a79605b
Kostya <oliinyk.kostya@gmail.com>
2020-04-02 00:07:06 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,22 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+    let sep = separator;
+
+    if (separator === undefined) {
+      sep = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        str += '' + sep;
+      } else {
+        str += this[i] + sep;
+      }
+    }
+
+    return str.substring(0, (str.length - sep.length));
   };
 }
 
