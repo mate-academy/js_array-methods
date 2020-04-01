60d7b6f86b2cfda3a205c68f5302c78bf5c15588
WincherZ <wincher2016@gmail.com>
2020-04-01 23:38:12 +0300

    some chages

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,30 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const arr = this;
+    let str = '';
+
+    for (let i = 0; i < arr.length; i++) {
+      let str1 = arr[i];
+      let separator1 = separator;
+
+      if (arr[i] === null || arr[i] === undefined) {
+        str1 = '';
+      }
+
+      if (separator === null) {
+        separator1 = 'null';
+      }
+
+      if (i === arr.length - 1) {
+        str += str1;
+      } else {
+        str += str1 + separator1;
+      }
+    };
+
+    return str;
   };
 }
 
