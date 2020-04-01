b8b5c33a2ffb5616d9e17db602f365376e4af992
Igor Kulish <igormagnetic@gmail.com>
2020-04-01 18:04:44 +0300

    sol

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let join = '';
+
+    for (const value of this) {
+      if (value === this[this.length - 1]) {
+        join += value;
+      } else if (value === null || value === undefined) {
+        join += separator;
+      } else {
+        join += '' + value + separator;
+      }
+    }
+
+    return join;
   };
 }
 
