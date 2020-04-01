0d0a5c20b12c5b486c8df6c03774c1d6a5e29644
Yevhenii <DrozhzhynY>
2020-04-01 15:21:48 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,27 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let copySeparator = separator;
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (separator === undefined) {
+        copySeparator = ',';
+      }
+
+      if (this[i] === undefined || this[i] === null) {
+        result += copySeparator;
+        continue;
+      }
+
+      if (i === this.length - 1) {
+        result += this[i];
+      } else {
+        result += `${this[i]}${copySeparator}`;
+      }
+    }
+
+    return result;
   };
 }
 
