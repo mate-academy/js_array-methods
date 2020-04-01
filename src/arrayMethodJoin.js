c478cb66de91e7aa406d7694d087f8710f504e8b
Andreas_Just <andreas.wq@gmail.com>
2020-04-01 19:02:53 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,20 +5,8 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let sepStr = separator;
+    const sepStr = String(separator);
     let str = '';
-
-    if (this.length === 0) {
-      return '';
-    }
-
-    if (this.length === 1) {
-      return this.toString();
-    }
-
-    if (separator === null) {
-      sepStr = 'null';
-    }
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] === null || this[i] === undefined) {
