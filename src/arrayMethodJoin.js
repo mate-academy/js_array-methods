d0e2a74339f837dc997b7bdd29310d7f88dfd031
Andrey Faychak <faychak333@gmail.com>
2020-04-01 16:16:00 +0300

    'solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,28 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let result = '';
+    let newSeparator = separator;
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      if (separator === null) {
+        newSeparator = 'null';
+      } else if (separator === undefined) {
+        newSeparator = ',';
+      }
+
+      if (i === this.length - 1) {
+        result += this[i];
+      } else {
+        result += this[i] + newSeparator;
+      }
+    }
+
+    return result;
   };
 }
 
