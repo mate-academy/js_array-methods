f0a4a802d4bc5520e204d1f1a752446a824ed29b
Illia Oliynik <illiaoliynik@gmail.com>
2020-04-02 19:53:34 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,31 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let joinedArr = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        joinedArr += ',';
+        continue;
+      }
+
+      if (Object.is(this[i]) || this[i] === '') {
+        continue;
+      }
+
+      if (separator === undefined) {
+        // eslint-disable-next-line no-param-reassign
+        separator = ',';
+      }
+
+      if (i === this.length - 1) {
+        joinedArr += `${this[i]}`;
+        break;
+      }
+      joinedArr += `${this[i]}${separator}`;
+    }
+
+    return joinedArr;
   };
 }
 
