07741661f330a7d0f1f2b4f70d804b5c5d9d966c
Andrii Badekha <badekha@gmail.com>
2020-04-02 22:31:55 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,29 @@
 /**
  * Implement method join
  */
+
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+    let strSeparator = 0;
+
+    if (separator === null) {
+      strSeparator = 'null';
+    } else if (separator === undefined) {
+      strSeparator = ',';
+    } else {
+      strSeparator = separator.toString();
+    }
+
+    for (let elem of this) {
+      if (elem === null || elem === undefined) {
+        elem = '';
+      }
+
+      str += elem + strSeparator;
+    }
+
+    return str.slice(0, (str.length - strSeparator.length));
   };
 }
 
