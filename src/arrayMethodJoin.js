a9a5135f9d57744dca365754263ae731fae315f4
Nina <pinkangelssugar@gmail.com>
2020-04-02 21:36:13 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,34 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let separ = separator;
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        this[i] = '';
+      }
+    }
+
+    let stringFromArr = '';
+
+    if (separ === undefined) {
+      separ = ',';
+    }
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === 0) {
+        // eslint-disable-next-line no-undef
+        stringFromArr = this[i].toString();
+      } else {
+        stringFromArr += separ + this[i].toString();
+      }
+    }
+
+    return stringFromArr;
   };
 }
 
