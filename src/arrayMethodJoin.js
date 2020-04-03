475ead52416f26662d51027ff344fca5cfec5608
BuildTools <unconfigured@null.spigotmc.org>
2020-04-03 17:22:45 +0300

    ok

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,85 +1,31 @@
 'use strict';
 
 /**
-  * Implement method join
-  *
-  */
+ * Implement method join
+*/
+
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    const items = this;
+  [].__proto__.join2 = function(separator = ',') {
+    let resulString = '';
 
-    // тут делаем подготовку перед перебором
-    let result = '';
-
-    for (let i = 0; i < items.length; i++) {
-      const item = items[i];
-
-      switch (separator) {
-        case '-':
-          result += item;
-
-          if (i < items.length - 1) {
-            result += '-';
-          }
-
-          break;
-        case '':
-          result += item;
-
-          break;
-        case ' ':
-          result += item;
-
-          if (i < items.length - 1) {
-            result += ' ';
-          }
-          break;
-
-        case ',':
-          if (item !== undefined && item !== null) {
-            result += item;
-          }
-
-          if (i < items.length - 1) {
-            result += ',';
-          }
-          break;
-        case null:
-          if (item !== undefined) {
-            result += item;
-          }
-
-          if (i < items.length - 1) {
-            result += 'null';
-          }
-          break;
-        case undefined:
-
-          if (item !== undefined) {
-            result += item;
-          }
-
-          if (i < items.length - 1) {
-            result += ',';
-          }
-          break;
-
-        default:
-          if (item !== undefined) {
-            result += item;
-          }
-
-          if (i < items.length - 1) {
-            result += '[object Object]';
-          }
-          break;
+    for (let i = 0; i < this.length; i++) {
+      if (i !== this.length - 1) {
+        resulString += isNullOrUndefined(this[i]) + String(separator);
+      } else {
+        resulString += isNullOrUndefined(this[i]);
       }
-      // тут делаем то что надо с текущим элементом
-    }
+    };
 
-    return result;
-    // тут возвращаем значение
+    return resulString;
   };
+}
+
+function isNullOrUndefined(value) {
+  if (value === undefined || value === null) {
+    return '';
+  }
+
+  return value;
 }
 
 module.exports = applyCustomJoin;
