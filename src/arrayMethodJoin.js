b820deea0c433ea247eb202a7b3f9573dae77fe6
aleksey-10 <alexcare1337@gmail.com>
2020-05-26 12:14:56 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,24 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    if (!this) {
+      return '';
+    }
+
+    let str = '';
+    const checkItem = item => (item === null || item === undefined) ? '' : item;
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === 0) {
+        str = `${checkItem(this[i])}`;
+        continue;
+      }
+
+      str += `${separator
+        !== undefined ? separator : ','}${checkItem(this[i])}`;
+    }
+
+    return str;
   };
 }
 
