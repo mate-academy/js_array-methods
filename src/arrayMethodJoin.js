a195c539a1fc64e6da4d61a7b43c50aae5a295ec
denkondratiev <denys.kondratiev.job@gmail.com>
2020-05-27 17:06:37 +0300

    first

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (Number.isNaN(this[i])) {
+        this[i] = 'NaN';
+      };
+
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      };
+
+      result += i !== this.length - 1
+        ? this[i] + `${separator}`
+        : this[i];
+    }
+
+    return result;
   };
 }
 
