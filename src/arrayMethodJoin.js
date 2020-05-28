b5841857d2b8a14a593c02eda3e052cdeb75d782
DTR <t.terry.rice@gmail.com>
2020-05-28 14:36:58 +0300

    0.8

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,12 +1,26 @@
 'use strict';
-
+/* eslint-disable */
 /**
  * Implement method join
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    if(separator === undefined) {separator = ','}
+    separator +='';
+  console.log("separator = ", separator)
+    let joined = '';
+    for(let i = 0; i < this.length; i++) {
+      if(this[i] === null || this[i] === undefined) {this[i] = '';}
+
+      if(i === this.length-1){joined += this[i];console.log("i = ", i)}
+      else {joined += this[i] +separator; };
+    }
+
+    return joined;
+    //end
   };
+
 }
 
 module.exports = applyCustomJoin;
+
