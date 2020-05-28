201af35314a9cf6a6ccfab2d0f6058b919419d6b
LimonovInsp <bis.limonov@gmail.com>
2020-05-28 14:35:25 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinResult = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (Number.isNaN(this[i])) {
+        this[i] = 'NaN';
+      } else if (this[i] === undefined || this[i] === null) {
+        this[i] = '';
+      }
+
+      if (i === this.length - 1) {
+        joinResult += this[i];
+      } else {
+        joinResult += this[i] + `${separator}`;
+      }
+    }
+
+    return joinResult;
   };
 }
 
