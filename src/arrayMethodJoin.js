b02c1bfdfec2f7e85ea5c5ee61caeded44b00743
Campari229 <KennyKoin@gmail.com>
2020-05-28 15:53:57 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,28 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let joinedString = '';
+
+    if (!this.length) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (!this[i] === this[i]) {
+        this[i] = 'NaN';
+      } else if (this[i] === undefined || this[i] === null) {
+        this[i] = '';
+      }
+
+      if (i === 0) {
+        joinedString += this[i];
+        continue;
+      }
+
+      joinedString += `${separator !== undefined ? separator : ','}${this[i]}`;
+    }
+
+    return joinedString;
   };
 }
 
