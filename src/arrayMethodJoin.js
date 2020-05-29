c0dc4808d55e0129b1b52cddfa31255adf3fd077
Stanislav <stasusss07@gmail.com>
2020-05-29 14:03:35 +0300

    final solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,18 +7,16 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     if (this.length === 0) {
       return '';
-    } else if (this.length === 1) {
-      return `${this[0]}`;
     }
 
-    let resultString = `${this[0]}`;
+    const getItem = (item) => item === null || item === undefined ? '' : item;
 
-    for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        this[i] = '';
-      }
+    let resultString = getItem(this[0]);
 
-      resultString += `${separator}${this[i]}`;
+    for (let i = 1; i < this.length; i++) {
+      const item = getItem(this[i]);
+
+      resultString += `${separator}${item}`;
     }
 
     return resultString;
