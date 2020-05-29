22bde5be754b48470daa7471493f0ab3e6cf785d
tyooma <flying.alien.godzilla@gmail.com>
2020-05-29 15:22:59 +0300

    Join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,10 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const regEx = /,/g;
+
+    return this.toString().replace(regEx, separator);
   };
 }
 
