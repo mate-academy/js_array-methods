286c9c55bbdb9866702ff1f9f0cb1922c1456b5d
YanG-6668 <goch.yannn@gmail.com>
2020-05-29 16:26:06 +0300

    join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length === 0) {
+      return '';
+    }
+
+    let newString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        newString += separator;
+      } else if (i !== this.length - 1) {
+        newString += `${this[i]}${separator}`;
+      } else {
+        newString += this[i];
+      }
+    }
+
+    return newString;
   };
 }
+
+// console.log(applyCustomJoin([0, 1, 2, 3]));
+// console.log(applyCustomJoin([]));
+// console.log(applyCustomJoin([]));
 
 module.exports = applyCustomJoin;
