9733d858e80344a560655102ec5cda3d42734c86
Yakiv <yakov.sivukha@gmail.com>
2020-05-29 15:09:19 +0300

    refactored separator

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,13 +4,8 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let output = '';
-    let validSeparator = separator;
-
-    if (separator === undefined) {
-      validSeparator = ',';
-    }
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
@@ -18,7 +13,7 @@ function applyCustomJoin() {
       }
 
       if (i < this.length - 1) {
-        output += validSeparator;
+        output += separator;
       }
     }
 
