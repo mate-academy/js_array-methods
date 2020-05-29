b2fee5189d1d614a7cadb55a968968b6d4eb4c4f
Slava Polishchuk <dizerllc@gmail.com>
2020-05-29 13:07:29 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let splittedThis = '';
+
+    if (this.length === 1) {
+      return `${this}`;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        this[i] = '';
+      }
+
+      (i === this.length - 1)
+        ? splittedThis += `${this[i]}`
+        : splittedThis += `${this[i]}${separator}`;
+    }
+
+    return splittedThis;
   };
 }
-
 module.exports = applyCustomJoin;
