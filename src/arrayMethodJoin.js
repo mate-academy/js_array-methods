04119c077899efa13db41d0ef9ebb92173b25c8c
Bohdan Polienko <mignatov756@gmail.com>
2020-06-11 12:44:33 +0300

    Fixes after code review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,38 +7,26 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     let newString = '';
 
-    for (let i = 0; i < this.length; i++) {
+    if (this.length === 0) {
+      return newString;
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
       if (this[i] === null || this[i] === undefined) {
         newString += separator;
-        continue;
       } else {
         if (separator === null) {
-          if (this.length - 2 >= i) {
-            newString += this[i] + 'null';
-          } else {
-            newString += this[i];
-          }
+          newString += this[i] + 'null';
         } else if (separator === '') {
-          if (this.length - 2 >= i) {
-            newString += this[i];
-          } else {
-            newString += this[i];
-          }
+          newString += this[i];
         } else if (separator === undefined) {
-          if (this.length - 2 >= i) {
-            newString += this[i] + ',';
-          } else {
-            newString += this[i];
-          }
+          newString += this[i] + ',';
         } else {
-          if (this.length - 2 >= i) {
-            newString += this[i] + separator;
-          } else {
-            newString += this[i];
-          }
+          newString += this[i] + separator;
         }
       }
     }
+    newString += this[this.length - 1];
 
     return newString;
   };
