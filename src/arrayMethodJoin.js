16a506532398a10e126c84e0099fd49b90958fab
Halynka Lozinska <galinkalozinska96@gmail.com>
2020-06-25 18:27:24 +0300

    Fixed Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,34 +8,23 @@ function applyCustomJoin() {
     let newPhrase = '';
     let newSeparator = separator;
 
-    switch (separator) {
-      case undefined :
-        newSeparator = ',';
-        break;
-      case null :
-        newSeparator = 'null';
-        break;
-      default:
-        break;
+    if (separator === undefined) {
+      newSeparator = ',';
     }
 
     for (let i = 0; i < this.length; i++) {
-      switch (this[i]) {
-        case undefined:
-        case null:
-          if (i !== (this.length - 1)) {
-            newPhrase += '' + newSeparator;
-          } else {
-            newPhrase += '';
-          }
-          break;
-        default :
-          if (i !== (this.length - 1)) {
-            newPhrase += this[i] + newSeparator;
-          } else {
-            newPhrase += this[i];
-          }
-          break;
+      if ((this[i] === undefined) || (this[i] === null)) {
+        if (i !== (this.length - 1)) {
+          newPhrase = newPhrase + '' + newSeparator;
+        } else {
+          newPhrase += '';
+        }
+      } else {
+        if (i !== (this.length - 1)) {
+          newPhrase = newPhrase + this[i] + newSeparator;
+        } else {
+          newPhrase += this[i];
+        }
       }
     }
 
