6fb1aa3fd623a0f94e276de64f3df333ade86cb9
kshvetsova <katshvetsova@gmail.com>
2020-07-24 02:34:16 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,6 +5,26 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
+    let str = '';
+
+    if (this.length === 0) {
+      return str;
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (separator === undefined) {
+        str += this[i] + ',';
+      } else if (this[i] === null || this[i] === undefined) {
+        this[i] = separator;
+        str += this[i];
+      } else if (separator === null) {
+        str += this[i] + 'null';
+      } else {
+        str += this[i] + separator;
+      }
+    }
+
+    return (str + this[this.length - 1]);
     // write code here
   };
 }
