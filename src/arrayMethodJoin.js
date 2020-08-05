7412f9d755e53876167ed0954478595009cda8d3
Roman <romikkus@gmail.com>
2020-08-05 22:41:32 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,26 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    const array = [...this];
+
+    let copyOfSeparator = separator;
+
+    let str = '';
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === array[array.length - 1]) {
+        copyOfSeparator = '';
+      } else if (array[i] == null || array[i] === undefined) {
+        array[i] = '';
+      } else if (separator === undefined) {
+        copyOfSeparator = ',';
+      } else if (separator == null) {
+        copyOfSeparator = 'null';
+      }
+      str += array[i] + copyOfSeparator;
+    }
+
+    return str;
   };
 }
 
