34bff26c890d74b7331c0d43d4a9a74a38dfb99f
nznmvs <nznmvs@gmail.com>
2020-08-05 18:09:32 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,26 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let separatorNew = separator;
+
+    if (separator === undefined) {
+      separatorNew = ',';
+    }
+
+    for (let key of this) {
+      if (key === null || key === undefined) {
+        key = '';
+      }
+      result += key.toString() + separatorNew;
+    }
+
+    switch (true) {
+      case (separatorNew === null): return result.slice(0, -4);
+      case (result.length > this.length):
+        return result.slice(0, -separatorNew.toString().length);
+      default: return result;
+    }
   };
 }
 
