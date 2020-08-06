ed846dd559697d9cf6461c94cf7ad985a54533c4
Eduard Nazarenko <ferrarif40lemans@gmail.com>
2020-08-06 23:49:14 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,27 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let altSeparator = separator;
+
+    if (separator === undefined) {
+      altSeparator = ',';
+    }
+
+    let resultString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        resultString += '';
+      } else {
+        resultString += this[i];
+      }
+
+      if (i < this.length - 1) {
+        resultString += altSeparator;
+      }
+    }
+
+    return resultString;
   };
 }
 
