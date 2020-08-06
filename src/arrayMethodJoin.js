6aec1950bad8fb1272833551afd39bf563e60d4d
uni-8 <julia.zhol.a@gmail.com>
2020-08-06 18:04:28 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,26 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let rightSeparator = separator;
+
+    if (separator === undefined) {
+      rightSeparator = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        result += this[i];
+      } else {
+        result += '';
+      }
+
+      if (i < this.length - 1) {
+        result += rightSeparator;
+      }
+    }
+
+    return result;
   };
 }
 
