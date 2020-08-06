9bc2aa0d2a5690842a6c31630c695121f9ecc349
Roman <romikkus@gmail.com>
2020-08-06 16:32:37 +0300

    Fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,17 +6,18 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     // write code here
-    let copyOfSeparator = separator;
-
     let str = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === this[this.length - 1]) {
-        copyOfSeparator = '';
-      } else if (this[i] === null || this[i] === undefined) {
-        this[i] = '';
+      if (this[i] === null || this[i] === undefined) {
+        str += '';
+      } else {
+        str += this[i];
       }
-      str += this[i] + `${copyOfSeparator}`;
+
+      if (i !== this.length - 1) {
+        str += separator;
+      }
     }
 
     return str;
