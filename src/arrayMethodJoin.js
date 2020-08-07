f7f2364bcf7b43adc7596d14ec278bd1bf75b74f
movchko <lemberg.my.city@gmail.com>
2020-08-07 11:12:02 +0300

    done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,31 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let res = '';
+
+    if (this[0] === undefined || this[0] === null) {
+
+    } else {
+      res = this[0];
+    }
+
+    let sep = separator;
+
+    if (separator === null) {
+      sep = 'null';
+    } else if (separator === undefined) {
+      sep = ',';
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        res += sep;
+      } else {
+        res += sep + this[i];
+      }
+    }
+
+    return res.toString();
   };
 }
 
