d8c56c4776fa075bb4c28d2c4e79b9b5736f6cfd
Yurii Rysyn <zerocool_cs@ukr.net>
2020-08-07 17:35:50 +0300

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,20 +4,11 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
     let finalStr = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (separator === undefined) {
-        if (i === this.length - 1) {
-          finalStr += this[i];
-          break;
-        }
-        finalStr += this[i] + ',';
-        continue;
-      }
-
       if (this[i] === undefined || this[i] === null) {
         finalStr += '';
       } else {
