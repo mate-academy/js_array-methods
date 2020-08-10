129543c3c092dda4bf2f03ca794eac10e612a6be
Zarva <zarva6596@gmail.com>
2020-08-10 14:44:41 +0300

    first commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,31 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let k = '';
+
+    if (this.length === 1) {
+      k += this[0];
+
+      return k;
+    }
+
+    for (let a = 0; a < this.length; a++) {
+      if (this[a] === null || this[a] === undefined) {
+        k += separator;
+        continue;
+      }
+
+      k += this[a];
+
+      if (a === this.length - 1) {
+        break;
+      }
+
+      k += separator;
+    }
+
+    return k;
   };
 }
 
