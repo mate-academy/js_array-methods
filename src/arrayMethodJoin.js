589466b6b503243170280a7a166c583c8f6ac783
yuriimovchko <movchko.yuriy@gmail.com>
2020-08-13 21:37:10 +0300

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,11 +4,11 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let res = '';
 
     if (this[0] === undefined || this[0] === null) {
-
+      res = '';
     } else {
       res = this[0];
     }
@@ -17,10 +17,8 @@ function applyCustomJoin() {
 
     if (separator === null) {
       sep = 'null';
-    } else if (separator === undefined) {
-      sep = ',';
     }
-
+    
     for (let i = 1; i < this.length; i++) {
       if (this[i] === undefined || this[i] === null) {
         res += sep;
