2b1e3bbb0c3ec39b7af5e2194c691d4c10dce344
dmitry <akulinind@gmail.com>
2020-09-09 15:28:22 +0300

    Add join method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    function checkElem(elem) {
+      if (elem === null || elem === undefined) {
+        return '';
+      }
+
+      return elem;
+    }
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    let result = `${checkElem(this[0])}`;
+
+    for (let i = 1; i < this.length; i++) {
+      const elem = checkElem(this[i]);
+
+      result = `${result}${separator}${elem}`;
+    }
+
+    return result;
   };
 }
 
