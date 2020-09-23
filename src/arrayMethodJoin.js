fe36560cb56877f1bbc06ff95b508f2ada36c068
VoduDay <andriy.maniak@gmail.com>
2020-09-23 18:44:46 +0300

    join method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let resultString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const currentElement = this[i];
+
+      if (currentElement === null || currentElement === undefined) {
+        resultString += '';
+      } else {
+        resultString += currentElement;
+      }
+
+      if (i !== this.length - 1) {
+        resultString += separator;
+      }
+    }
+
+    return resultString;
   };
 }
 
