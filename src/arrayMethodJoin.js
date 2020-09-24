2aba1b7d0b6f642a2cdefd3a4b76a59d8832816a
OleksandrMykoliuk <69759907+OleksandrMykoliuk@users.noreply.github.com>
2020-09-24 21:30:57 +0300

    Solution PR

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,10 +7,6 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let string = '';
 
-    if (this.length === 0) {
-      return '';
-    }
-
     for (let element = 0; element < this.length; element++) {
       if (this[element] !== null && this[element] !== undefined) {
         string += this[element];
