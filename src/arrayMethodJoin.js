38cb90ab47b16396f452f7eb3673df1f399207c6
goncharukop <goncharukop@gmail.com>
2020-09-24 19:58:50 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,31 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let separatorValue = separator;
+
+    if (typeof separator === 'undefined') {
+      separatorValue = ',';
+    }
+
+    if (separator === null) {
+      separatorValue = 'null';
+    }
+
+    if (typeof this === 'object' && this.length > 0) {
+      for (let i = 0; i < this.length - 1; i++) {
+        if (this[i] === null) {
+          result += separatorValue;
+        } else if (this[i] === undefined) {
+          result += separatorValue;
+        } else {
+          result += this[i] + separatorValue;
+        }
+      }
+      result += this[this.length - 1];
+    }
+
+    return result;
   };
 }
 
