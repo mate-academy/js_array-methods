aacc21d01c6a2fed85751d0513645286d9a67b3e
Petro <petdavy@1991gmail.com>
2020-09-24 09:37:02 +0300

    refactoring

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,21 +6,15 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let joinedString = '';
-    let validSeparator = separator;
     const emptyValues = [null, undefined];
-    const arrayValues = Object.values(this);
 
-    if (validSeparator === undefined) {
-      validSeparator = ',';
-    }
-
-    for (let i = 0; i < arrayValues.length; i++) {
-      if (!emptyValues.includes(arrayValues[i])) {
-        joinedString += arrayValues[i];
+    for (let i = 0; i < this.length; i++) {
+      if (!emptyValues.includes(this[i])) {
+        joinedString += this[i];
       }
 
-      if (i < arrayValues.length - 1) {
-        joinedString += validSeparator;
+      if (i < this.length - 1) {
+        joinedString += separator;
       }
     }
 
