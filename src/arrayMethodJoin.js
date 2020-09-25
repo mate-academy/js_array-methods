31a89563e428c0fc3447c1e9ea599ec76e9e096e
Andrii Medziak <58214135+Andrii1996@users.noreply.github.com>
2020-09-25 11:40:53 +0300

    Change name

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let joinString = '';
+    let joinedString = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
-        joinString += this[i];
+        joinedString += this[i];
       }
 
       if (i < this.length - 1) {
-        joinString += separator;
+        joinedString += separator;
       }
     }
 
-    return joinString;
+    return joinedString;
   };
 }
 
