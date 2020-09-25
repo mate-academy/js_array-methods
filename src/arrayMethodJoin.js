22394625c499476989bcbed6734b33580a508cc3
Tania Omelko <tanyaomelko39@gmail.com>
2020-09-25 11:43:33 +0300

    change name of variable

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let string = '';
+    let joinedString = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
-        string += this[i];
+        joinedString += this[i];
       }
 
       if (i !== this.length - 1) {
-        string += separator;
+        joinedString += separator;
       }
     }
 
-    return string;
+    return joinedString;
   };
 }
 
