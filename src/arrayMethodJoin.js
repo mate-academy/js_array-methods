ee32a2f564bd640b00a9acc8e9db4a75aacd53c2
Anton <office.sklyarov@gmail.com>
2020-09-25 11:12:49 +0300

    corrected var names and separator usage

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,21 +5,21 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let newString = '';
+    let joinedString = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] === undefined || this[i] === null) {
-        newString += ',';
+        joinedString += separator;
       } else {
-        newString += this[i];
+        joinedString += this[i];
 
         if (i !== this.length - 1) {
-          newString += separator;
+          joinedString += separator;
         }
       }
     }
 
-    return newString;
+    return joinedString;
   };
 }
 
