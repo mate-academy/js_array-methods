d324eddb829660eef261e8fe7f7c793699a5225d
KirillLutsenko <mnrovski68@gmail.com>
2020-09-28 20:45:47 +0300

    Method-Join Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let newString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const currentElement = this[i];
+
+      if (currentElement !== null && currentElement !== undefined) {
+        newString += currentElement;
+      }
+
+      if (i !== this.length - 1) {
+        newString += separator;
+      }
+    }
+
+    return newString;
   };
 }
 
