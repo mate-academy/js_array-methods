e2ea7f7a46f371fb9aced369ee375b320420d1bc
Nikolay Filiuk <45862582+Feuerbach98@users.noreply.github.com>
2020-09-28 14:54:31 +0300

    Update src/arrayMethodJoin.js
    Co-authored-by: Vladimir <60401137+Vladimir-Beliy@users.noreply.github.com>

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,7 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let string = '';
+    let result = '';
 
     for (const element of this) {
       if (element === null || element === undefined) {
