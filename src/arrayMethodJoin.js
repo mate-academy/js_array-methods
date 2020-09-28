0f25162fe83a0d27193eed2fc6d19ceb5bc80205
Sofiia Kucherenko <kucherenko.sv13@gmail.com>
2020-09-28 17:46:22 +0300

    Change name of variable

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,19 +6,19 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     // write code here
-    let result = '';
+    let joinedArray = '';
 
     for (let element = 0; element < this.length; element++) {
       if (this[element] !== null && this[element] !== undefined) {
-        result += this[element];
+        joinedArray += this[element];
       }
 
       if (element !== this.length - 1) {
-        result += separator;
+        joinedArray += separator;
       }
     }
 
-    return result;
+    return joinedArray;
   };
 }
 
