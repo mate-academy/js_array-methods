b73ab553cb823f6d1d7859fc71edbe94eac231e3
Yuriy Putsan <putsan57@gmail.com>
2020-09-28 14:51:33 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,27 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let finalString = '';
+
+    for (let indexOfElement = 0;
+      indexOfElement < this.length;
+      indexOfElement++) {
+      if (this[indexOfElement] === undefined || this[indexOfElement] === null) {
+        finalString += '';
+      } else {
+        finalString += this[indexOfElement];
+      }
+
+      if (this.length - 1 !== indexOfElement) {
+        if (arguments[0] === undefined) {
+          finalString += ',';
+        } else {
+          finalString += arguments[0];
+        }
+      }
+    }
+
+    return finalString;
   };
 }
 
