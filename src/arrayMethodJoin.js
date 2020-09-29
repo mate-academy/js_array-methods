bbeb4621f38cabcbdb94559b2cc35dbfabccf33b
Vladyslav Pavlik <pavliklaw7@gmail.com>
2020-09-29 15:41:20 +0300

    name of variable fix

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let jointedStr = '';
+    let jointedString = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== undefined && this[i] !== null) {
-        jointedStr += this[i];
+        jointedString += this[i];
       }
 
       if (i !== this.length - 1) {
-        jointedStr += separator;
+        jointedString += separator;
       }
     }
 
-    return jointedStr;
+    return jointedString;
   };
 }
 
