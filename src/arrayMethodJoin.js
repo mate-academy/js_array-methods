7a832c354e59c0b47e33dbfcec602d05556971b4
Your Name <mogolgogol.90@gmail.com>
2020-10-01 00:01:49 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,23 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let joinedString = '';
+
+    if (separator === undefined) {
+      separator = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        joinedString += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        joinedString += separator;
+      }
+    }
+
+    return joinedString;
   };
 }
-
 module.exports = applyCustomJoin;
