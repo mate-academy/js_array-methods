f95d48d6395385043d33a22d8ebfe671d88d4aec
Olena kuzii <okuziy@gmail.com>
2020-09-30 17:39:05 +0300

    change if()

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,17 +7,16 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let joint = '';
 
-    if (this.length === 0) {
-      return '';
-    }
-
     for (let i = 0; i < this.length - 1; i++) {
       if ((this[i] !== null) && (this[i] !== undefined)) {
         joint += this[i];
       }
       joint += separator;
     }
-    joint += this[this.length - 1];
+
+    if (this.length !== 0) {
+      joint += this[this.length - 1];
+    }
 
     return joint;
   };
