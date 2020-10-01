2541fa30db929a471edd69d7cb8612ed29bc6b95
unknown <yurastt1@gmail.com>
2020-10-01 21:24:14 +0300

    result

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let res = '';
+    let result = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
-        res += this[i];
+        result += this[i];
       }
 
       if (i !== this.length - 1) {
-        res += separator;
+        result += separator;
       }
     }
 
-    return res;
+    return result;
   };
 }
 
