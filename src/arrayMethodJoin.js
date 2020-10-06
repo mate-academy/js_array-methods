c207fcdddddd6d48c7322c694ca47b2321ee5374
Volodymyr <volodymyr.mykhailiuk7@gmail.com>
2020-10-06 17:16:08 +0300

    Fix

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,23 +1,20 @@
 'use strict';
 
-/**
- * Implement method join
- */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let joinedArray = '';
+    let joinedString = '';
 
     for (let element = 0; element < this.length; element++) {
       if (this[element] !== null && this[element] !== undefined) {
-        joinedArray += this[element];
+        joinedString += this[element];
       }
 
       if (element !== this.length - 1) {
-        joinedArray += separator;
+        joinedString += separator;
       }
     }
 
-    return joinedArray;
+    return joinedString;
   };
 }
 
