2d93d3f2d16babc78fd8f361f1321d2ba3c50ff2
Igor Lisogor <illisogori@gmail.com>
2020-10-16 15:40:26 +0300

    applyCustomJoin added.

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      let element = this[i];
+
+      if (element === null || element === undefined) {
+        element = '';
+      }
+
+      if (i !== this.length - 1) {
+        joinString += element + (separator + '');
+      } else {
+        joinString += element;
+      }
+    }
+
+    return joinString;
   };
 }
 
