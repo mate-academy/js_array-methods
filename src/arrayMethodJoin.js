0f3da96b30b70661f9946b4d7491ec004d457ba4
OleksandrLisnevskyi <lisnevskyi@gmail.com>
2020-11-13 15:10:04 +0200

    Add solution join method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  // eslint-disable-next-line space-before-function-paren
+  [].__proto__.join2 = function (separator = ',') {
     // write code here
+
+    let res = '';
+
+    for (let el of this) {
+      if (el === null || el === undefined || el === []) {
+        el = '';
+      }
+
+      if (separator === null) {
+        res += el <= this.length - 2 ? el + 'null' : el;
+      } else {
+        res += el <= this.length - 2 ? el + separator : el;
+      }
+    }
+
+    return res;
   };
 }
 
