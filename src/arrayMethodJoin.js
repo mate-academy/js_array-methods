212798fc440f4518743bef5f727a5da586d9834e
AndrewKreshchenko <mail2andyk@gmail.com>
2020-11-24 18:02:40 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,35 +5,20 @@
  */
 function applyCustomJoin() {
   // Using __proto__ is not recommended. Instead, use Array.prototype
-  [].__proto__.join2 = function(separator) {
-    // Compute empty array and array with length 1
-    if (!this.length) {
-      return '';
-    }
-
-    if (!this.length === 1) {
-      return this[0];
-    }
-
-    // Join array with more than 1 elements
-    let sep = separator;
+  [].__proto__.join2 = function(separator = ',') {
     let str = '';
 
-    if (typeof sep === 'undefined') {
-      sep = ',';
-    } else if (sep === null) {
-      sep = 'null';
-    }
-
-    for (let i = 0; i < this.length - 1; i++) {
-      if (typeof this[i] === 'undefined' || this[i] === null) {
-        str += sep;
-      } else {
-        str += this[i] + sep;
+    for (let i = 0; i < this.length; i++) {
+      if (typeof this[i] !== 'undefined' && this[i] !== null) {
+        str += this[i];
       }
-    }
 
-    str += this[this.length - 1];
+      if (i === this.length - 1) {
+        break;
+      }
+
+      str += separator;
+    }
 
     return str;
   };
