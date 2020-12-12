7193939825265cf3fb759c8a3230b9adddc22cc3
Alex Lemeshchuk <alexyskr@gmail.com>
2020-12-12 15:51:27 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,35 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+
+    const testInput = (inputElement) => {
+      if (inputElement === null || inputElement === undefined) {
+        return '';
+      } else {
+        return inputElement;
+      }
+    };
+
+    const testSeparator = (separatorTest) => {
+      if (separatorTest === undefined) {
+        return ',';
+      } else if (separatorTest === null) {
+        return 'null';
+      } else {
+        return separatorTest;
+      }
+    };
+
+    for (let i = 0; i < this.length; i++) {
+      if (i < this.length - 1) {
+        result += testInput(this[i]) + testSeparator(separator);
+      } else {
+        result += testInput(this[i]);
+      }
+    }
+
+    return result;
   };
 }
 
