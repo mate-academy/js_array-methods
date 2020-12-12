56039f81dac7be4173e30f95ce629647f5dfe7f2
Serhii Alieksieievych <kajot1863@gmail.com>
2020-12-12 02:49:54 +0200

    completed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,34 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str;
+    let itemToPaste;
+
+    if (this[0] !== null && this[0] !== undefined) {
+      str = `${this[0]}`;
+    } else {
+      str = '';
+    }
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        itemToPaste = this[i];
+      } else {
+        itemToPaste = '';
+      }
+
+      if (separator !== undefined) {
+        str = str + separator + itemToPaste;
+      } else {
+        str = str + ',' + itemToPaste;
+      }
+    }
+
+    return str;
   };
 }
 
