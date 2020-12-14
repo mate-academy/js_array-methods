8b474d2727ae35d3b8686d7e4ab8e9a25d08443c
troospieler <troospieler1990@gmail.com>
2020-12-14 21:59:24 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,41 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let a = String(separator);
+
+    if (separator === undefined) {
+      a = ',';
+    }
+
+    const arr = [];
+    let firstString = '';
+    let resultString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        firstString += '';
+      } else {
+        firstString += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        firstString += a;
+      }
+    }
+
+    for (let i = 0; i < firstString.length; i++) {
+      arr[i] = firstString[i];
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === null || arr[i] === undefined) {
+        resultString += 24;
+      }
+
+      resultString += '' + arr[i];
+    }
+
+    return resultString;
   };
 }
 
