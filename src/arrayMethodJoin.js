63311461ad712b279abfe2ef3d914502316c8587
Tetiana Zotova <zotova.tetiana@gmail.com>
2020-12-16 00:52:08 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,34 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let str = '';
+    let separNew = separator;
+
+    if (arguments.length === 0 || separator === undefined) {
+      separNew = ',';
+    }
+
+    if (separator === null) {
+      separNew = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      let item = this[i];
+
+      if (this[i] === undefined || this[i] === null) {
+        item = '';
+      }
+
+      if (i === this.length - 1) {
+        str = str + item;
+      }
+
+      if (i !== this.length - 1) {
+        str = str + item + separNew;
+      }
+    }
+
+    return str;
   };
 }
 
