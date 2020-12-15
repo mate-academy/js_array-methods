6b4fa5edff0a9ecd38ccd5d6c160f6df623eaba2
Andrii Haidai <haidai.andriy@gmail.com>
2020-12-15 11:57:09 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,36 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+
+    function inputTest(input) {
+      if (input === null || input === undefined) {
+        return '';
+      } else {
+        return input;
+      }
+    };
+
+    function separatorTest(inputSeparator) {
+      if (inputSeparator === undefined) {
+        return ',';
+      } else if (inputSeparator === null) {
+        return 'null';
+      } else {
+        return inputSeparator;
+      }
+    };
+
+    for (let i = 0; i < this.length; i++) {
+      if (i < this.length - 1) {
+        result += inputTest(this[i]) + separatorTest(separator).toString();
+      } else {
+        result += this[i];
+      }
+    }
+    // console.log(result);
+
+    return result;
   };
 }
 
