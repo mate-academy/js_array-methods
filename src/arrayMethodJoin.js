8c966a99e455db5d05a401cbaf8866c43f7627ba
Olesia <olesia.tk@gmail.com>
2020-12-15 15:24:19 +0200

    Fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,17 +6,23 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     if (Array.isArray(this) && this.length > 1) {
+      let str;
+
       if (this[0] === null || this[0] === undefined) {
-        this[0] = '';
+        str = '';
+      } else {
+        str = `${this[0]}`;
       }
 
-      let str = `${this[0]}`;
+      let arrValue;
 
       for (let i = 1; i < this.length; i++) {
         if (this[i] === null || this[i] === undefined) {
-          this[i] = '';
+          arrValue = '';
+        } else {
+          arrValue = `${this[i]}`;
         }
-        str += `${separator}` + `${this[i]}`;
+        str += separator + arrValue;
       }
 
       return str;
