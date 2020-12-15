8ff1b06a64fc58b30c681c67b210bc65fd0e3d00
Dima Bond <neverpooh@gmail.com>
2020-12-15 16:50:09 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,32 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let string = '';
+    let input = separator;
+
+    if (!this || this.length === 0) {
+      return string;
+    }
+
+    if (input === undefined) {
+      input = ',';
+    }
+
+    input = '' + input;
+
+    const numbersMaxIndex = this.length - 1;
+
+    for (let i = 0; i < numbersMaxIndex; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        string += '' + input;
+      } else {
+        string += this[i] + input;
+      }
+    }
+
+    const result = string + this[numbersMaxIndex];
+
+    return result;
   };
 }
 
