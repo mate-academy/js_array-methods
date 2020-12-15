356ae45221eae36b473f2795e261d1632d7e9dfa
5Mountains <V.Piatyhor@gmail.com>
2020-12-15 17:07:14 +0200

    code improvement with separation

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,13 +4,8 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let joinMethod = '';
-    let splitter = separator;
-
-    if (splitter === undefined) {
-      splitter = ',';
-    }
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== undefined && this[i] !== null) {
@@ -18,7 +13,7 @@ function applyCustomJoin() {
       }
 
       if (i !== this.length - 1) {
-        joinMethod += splitter;
+        joinMethod += separator;
       }
     }
 
