f8c4cfedd485337c59063d59de76a47b98b99346
tamara ostapets <tamara.ostapec@gmail.com>
2020-12-15 16:42:04 +0200

    Array Method Join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,30 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let string = '';
+
+    if (this.length === 0) {
+      return string;
+    }
+
+    let value = separator;
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      value = `${this[i]}${separator}`;
+
+      if (i === this.length - 1) {
+        value = `${this[i]}`;
+      }
+
+      string += value;
+    }
+
+    return string;
   };
 }
 
