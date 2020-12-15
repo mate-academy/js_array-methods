92af2f3c3ac5a609942c431596b25262edbf0eac
Khrulenko Vitalii <khrulenko.v@gmail.com>
2020-12-15 20:09:04 +0200

    a bit more simple

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,28 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     if (this.length === 0) {
       return '';
     }
 
     let str = '';
-    let div = separator;
-
-    if (separator === undefined) {
-      div = ',';
-    }
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] === undefined || this[i] === null) {
-        str += div;
+        str += separator;
       } else {
         str += `${this[i]}`;
 
         if (i === this.length - 1) {
           return str;
         }
-        str += div;
+        str += separator;
       }
     }
   };
