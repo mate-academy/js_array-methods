c82cb19f31b649f044d0e5ed3ffd2c0eb7bc7be2
chri911 <delightsome@bigmir.net>
2020-12-15 21:47:09 +0200

    improve the code

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,40 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let str = '';
-
-    if (typeof separator !== 'undefined' && separator !== null) {
-      for (let i = 0; i < this.length; i++) {
-        if (i !== this.length - 1 && this.length !== 0) {
-          if (this[i] === undefined || this[i] === null) {
-            str += separator;
-          } else {
-            str += this[i] + separator;
-          }
-        } else {
-          str += this[i];
-        }
-      }
-    }
-
-    if (typeof separator === 'undefined') {
-      for (let i = 0; i < this.length; i++) {
-        if (i !== this.length - 1 && this.length !== 0) {
-          str += this[i] + ',';
-        } else {
-          str += this[i];
-        }
-      }
-    }
+    let connector = separator;
 
     if (separator === null) {
-      for (let i = 0; i < this.length; i++) {
-        if (i !== this.length - 1 && this.length !== 0) {
-          str += this[i] + 'null';
+      connector = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i !== this.length - 1 && this.length !== 0) {
+        if (this[i] === undefined || this[i] === null) {
+          str += connector;
         } else {
-          str += this[i];
+          str += this[i] + connector;
         }
+      } else {
+        str += this[i];
       }
     }
 
