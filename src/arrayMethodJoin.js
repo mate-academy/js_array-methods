dbc12deeb1c01e0e34b8b3b9cb574a6c298bd38b
Yurii Fitsyk <yuriy.fitsyk@gmail.com>
2020-12-16 13:42:54 +0200

    changes after review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,13 +8,11 @@ function applyCustomJoin() {
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (i !== this.length - 1) {
-        if (this[i] === null || this[i] === undefined) {
-          result += separator;
-        } else {
-          result += this[i];
-          result += separator;
-        }
+      if (this[i] === null || this[i] === undefined) {
+        result += separator;
+      } else if (i !== this.length - 1) {
+        result += this[i];
+        result += separator;
       } else {
         result += this[i];
       }
