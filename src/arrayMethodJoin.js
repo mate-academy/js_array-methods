e42f38313c1c67e4d0b3c135f91bad0e888eba2f
Alexandr Petrenko <alexandr.petrenko.fe@gmail.com>
2020-12-16 10:40:26 +0200

    array nome changing

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -9,7 +9,8 @@ function applyCustomJoin() {
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] === null || this[i] === undefined) {
-        this[i] = '';
+        str += separator;
+        continue;
       }
 
       if (i < this.length - 1) {
