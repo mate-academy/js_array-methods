91c381d13dabac10a083f98057680ee67a7d6efd
Baburin Eugene <58775463+Baburineu@users.noreply.github.com>
2020-12-29 01:41:54 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,27 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    const sep = separator === undefined
+      ? ','
+      : separator === null
+        ? 'null'
+        : separator;
+
+    if (this[0] == null || this[0] === undefined) {
+      this[0] = '';
+    }
+
+    let row = '' + this[0];
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] == null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      row += sep + this[i];
+    }
+
+    return row;
   };
 }
 
