0cb2249be812b4329a28adfdecd22dc5b7ad7bd3
Yevgeniy Podobreev <podobr_2018@ukr.net>
2020-12-29 19:55:55 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,7 +6,32 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let separatorCorrect;
+    let result = '';
+
+    if (separator === null) {
+      separatorCorrect = 'null';
+    } else if (separator === undefined) {
+      separatorCorrect = ',';
+    } else {
+      separatorCorrect = separator;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+        result += this[i];
+      }
+
+      if (i === this.length - 1) {
+        result += this[i];
+        continue;
+      }
+      result += this[i] + separatorCorrect;
+    }
+
+    return result;
   };
-}
+};
 
 module.exports = applyCustomJoin;
