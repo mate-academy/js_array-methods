9f046eee2631a6291b21b1e5c70f47918d092626
Eugene Khablenko <eugene.khablenko@gmail.com>
2021-01-14 12:06:15 +0200

    solutiion

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,38 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let result = '';
+    const x = this.length;
+
+    if (x === 0) {
+      result = '';
+
+      return result;
+    }
+
+    if (x === 1) {
+      result = this[x - 1];
+
+      return result.toString();
+    }
+
+    if (x > 1) {
+      for (let key of this) {
+        if (key === null || key === undefined) {
+          key = '';
+        }
+
+        result += String(key);
+
+        if (key < x - 1) {
+          result += separator;
+        }
+      }
+
+      return result;
+    };
   };
 }
 
