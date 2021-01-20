4c3c875273acf92c9402c6fc73516644ae1f6b9f
Olga Bordiug <obordiug.gmail.com>
2021-01-20 19:01:19 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,27 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let createdString = '';
+
+    if (this.length !== 0) {
+      for (let i = 0; i < this.length; i++) {
+        if (this[i] === null || this[i] === undefined) {
+          createdString += '';
+        } else {
+          createdString += this[i];
+        }
+
+        if (i < this.length - 1) {
+          if (separator !== undefined) {
+            createdString += separator;
+          } else {
+            createdString += ',';
+          }
+        }
+      }
+    }
+
+    return createdString;
   };
 }
 
