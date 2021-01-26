b97238a31d42f45fe9cedb715d7ed532c09a72ab
Artem Fedoseev <afedoseev359@gmail.com>
2021-01-26 13:48:29 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,31 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let output = '';
+    let useSeparator = separator;
+
+    if (separator === null) {
+      useSeparator = 'null';
+    };
+
+    for (let i = 0; i < this.length; i++) {
+      let item = this[i];
+
+      if (this[i] === null || this[i] === undefined) {
+        item = '';
+      };
+
+      if (i !== this.length - 1) {
+        // Add separator only if it's not the last item of array
+        output += item + useSeparator;
+      } else {
+        output += item;
+      };
+    }
+
+    return output;
   };
 }
 
