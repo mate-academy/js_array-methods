9f6f1f8e9e76a7e17ae79fd5a4bab23b0e78ec73
Александра Яковина <aleksandraakovina@MacBook-Pro-Aleksandra.local>
2021-02-04 01:11:30 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,26 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let str = '';
+    let spr = (separator != null)
+      ? separator.toString()
+      : ',';
+
+    if (separator === null) {
+      spr = 'null';
+    }
+
+    if (this.length) {
+      for (const [index, element] of this.entries()) {
+        const valueElement = !(element != null)
+          ? ''
+          : element.toString();
+
+        str += (index < this.length - 1) ? valueElement + spr : element;
+      }
+    }
+
+    return str;
   };
 }
 
