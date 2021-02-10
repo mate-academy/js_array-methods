899c193c8ae1b8b8e49fd50877a5636088e97780
AllaByshkina <alla.byshkin@gmail.com>
2021-02-10 18:23:35 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,31 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    if (this.length === 0) {
+      return '';
+    }
+
+    let sep = separator;
+
+    if (separator === undefined) {
+      sep = ',';
+    }
+
+    if (this.length) {
+      let str = '';
+
+      for (let i = 0; i < this.length; i++) {
+        if (this[i] !== null && this[i] !== undefined) {
+          str += this[i];
+        }
+
+        if (i < this.length - 1) {
+          str += sep;
+        }
+      };
+
+      return str;
+    };
   };
 }
 
