0ee841251ea53991cd6b25126aa7835f2be13e12
antoniakstepan <antoniakstepan1@gmail.com>
2021-02-11 13:08:36 +0200

    first commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,31 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let joinStr = '';
+
+    if (this.length === 1) {
+      joinStr += this;
+    }
+
+    if (this.length > 1) {
+      if (this[0] !== null) {
+        joinStr += this[0];
+      }
+
+      for (let i = 1; i < this.length; i++) {
+        if (separator === null) {
+          joinStr += null + this[i].toString();
+        } else if (this[i] === null || this[i] === undefined) {
+          joinStr += separator;
+        } else {
+          joinStr += separator + this[i];
+        }
+      }
+    }
+
+    return joinStr;
   };
 }
 
