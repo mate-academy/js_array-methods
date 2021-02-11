92adea71236e30920bfcb9f515ecb2037c2b31b6
morozoleh <olegmoroz93@gmail.com>
2021-02-11 14:48:01 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,33 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+
+    if (this.length) {
+      for (let i = 0; i < this.length - 1; i++) {
+        let value = this[i];
+        let symbol = separator;
+
+        if (this[i] === undefined
+          || this[i] === null) {
+          value = '';
+        }
+
+        if (separator === undefined) {
+          symbol = ',';
+        } else if (separator === null) {
+          symbol = 'null';
+        } else if (separator === '') {
+          symbol = '';
+        }
+
+        result += value + symbol;
+      }
+
+      result += this[this.length - 1];
+    };
+
+    return result;
   };
 }
 
