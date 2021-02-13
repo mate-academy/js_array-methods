c05b90ee4550a025c0b6578deb50d727b9a4adb7
Taras <seman6745@gmail.com>
2021-02-13 17:47:22 +0200

    first commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (this[0] !== null) {
+      result += this[0];
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if (separator === null) {
+        result += null + this[i].toString();
+      } else if (this[i] === null || this[i] === undefined) {
+        result += separator;
+      } else {
+        result += separator + this[i];
+      }
+    }
+
+    return result;
   };
 }
 
