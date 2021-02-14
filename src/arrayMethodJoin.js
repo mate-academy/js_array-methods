80f6f8bf2209842b7700f8bbf38be6001cf80160
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-02-14 20:55:06 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,36 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let string = '';
+
+    if (typeof (separator) === 'undefined') {
+      for (let i = 0; i < this.length; i++) {
+        this[i] = this[i].toString();
+
+        if (i === this.length - 1) {
+          string += this[i];
+        } else {
+          string += this[i] + ',';
+        }
+      }
+    }
+
+    if (typeof (separator) !== 'undefined') {
+      for (let i = 0; i < this.length; i++) {
+        if (this[i] === null || this[i] === undefined) {
+          this[i] = '';
+        }
+        this[i] = this[i].toString();
+
+        if (i === this.length - 1) {
+          string += this[i];
+        } else {
+          string += this[i] + separator;
+        }
+      }
+    }
+
+    return string;
   };
 }
 
