9f647f93a8360c2f9298ac43f8459bd5dffcc713
Olexandr Bieliaiev <Shurasava17@gmail.com>
2021-02-16 13:01:07 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joined = '';
+    let change;
+
+    for (let i = 0; i < this.length; i++) {
+      if (typeof this[i] === 'undefined' || this[i] === null) {
+        change = '';
+      } else {
+        change = this[i];
+      }
+
+      if (i !== this.length - 1) {
+        if (separator !== null) {
+          joined += change + separator;
+        } else {
+          joined += change + 'null';
+        }
+      } else {
+        joined += change;
+      }
+    }
+
+    return joined;
   };
 }
 
