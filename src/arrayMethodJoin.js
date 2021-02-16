891eb3c81839381681a3f7d230eb5454ee4ff443
Vasiliy Sergeev <the2producer@gmail.com>
2021-02-16 22:01:28 +0200

    join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,26 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinString = '';
+
+    if (this.length > 0) {
+      if (this[0] !== null) {
+        joinString += this[0];
+      }
+
+      for (let i = 1; i < this.length; i++) {
+        if (separator === null) {
+          joinString += null + this[i].toString();
+        } else if (this[i] === null || this[i] === undefined) {
+          joinString += separator;
+        } else {
+          joinString += separator + this[i];
+        }
+      }
+    }
+
+    return joinString;
   };
 }
 
