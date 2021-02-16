14213158caee505a1429802461beeb683c64db63
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-02-16 11:42:03 +0200

    first commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let counter = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && typeof this[i] !== 'undefined') {
+        counter += this[i];
+      }
+
+      if (!(i === this.length - 1)) {
+        counter += separator;
+      }
+    }
+
+    return counter;
   };
+
+  return this;
 }
 
 module.exports = applyCustomJoin;
