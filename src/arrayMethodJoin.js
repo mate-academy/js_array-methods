0640b36c4f97bee600830a85c1964dfdd46bb535
Lena Kravchenko <cupp.lenashapoval@gmail.com>
2021-02-17 11:37:15 +0200

    Fixes after review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -10,25 +10,17 @@ function applyCustomJoin() {
 
     if (this.length === 0) {
       return result;
-    } else {
-      for (let i = 0; i < this.length - 1; i++) {
-        if (
-          (!this[i]) && (this[i] !== 0)
-            && (this[i] !== false)
-            && (!isNaN(this[i]))
-        ) {
-          this[i] = '';
-        }
+    }
 
-        if (this[i] === undefined) {
-          this[i] = '';
-        }
-
-        result = result + this[i] + separator;
+    for (let i = 0; i < this.length - 1; i++) {
+      if ((this[i] === undefined) || (this[i] === null)) {
+        this[i] = '';
       }
 
-      result = result + valuesArray[valuesArray.length - 1];
+      result = result + this[i] + separator;
     }
+
+    result = result + valuesArray[valuesArray.length - 1];
 
     return result;
   };
