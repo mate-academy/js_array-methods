cc07a162b992b50ba6fc4a93d05ce39eb1fa5fe3
Bohdan Klius <bohdanklius@gmail.com>
2021-02-18 21:25:44 +0200

    Solution v2

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,23 +7,19 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let resultString = '';
 
-    if (this.length === 1) {
-      resultString += this;
+    if (this.length === 0) {
+      return resultString;
     }
 
-    if (this.length > 1) {
-      if (this[0] !== null) {
-        resultString += this[0];
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        resultString += '';
+      } else {
+        resultString += this[i];
       }
 
-      for (let i = 1; i < this.length; i++) {
-        if (separator === null) {
-          resultString += null + this[i].toString();
-        } else if (this[i] === null || this[i] === undefined) {
-          resultString += separator;
-        } else {
-          resultString += separator + this[i];
-        }
+      if (i < this.length - 1) {
+        resultString += separator;
       }
     }
 
