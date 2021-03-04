6e6c79c19712e15c02444b7bb6cef0fad00e6e1d
Onishchuk <onishchuk.danil@gmail.com>
2021-03-04 16:09:58 +0200

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,26 +5,25 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let result = '';
-
     if (this.length === 0) {
       return '';
     }
 
-    if ((this[0] === null) || (this[0] === undefined)) {
-    } else {
-      result = `${this[0]}`;
+    if (this[0] === null) {
+      this[0] = '';
     }
+
+    let result = this[0];
 
     for (let i = 1; i < this.length; i++) {
       if ((this[i] === null) || (this[i] === undefined)) {
-        result = result + separator + '';
+        result += separator + '';
       } else {
-        result = result + separator + this[i];
+        result += separator + this[i].toString();
       }
     }
 
-    return result;
+    return result.toString();
   };
 }
 
