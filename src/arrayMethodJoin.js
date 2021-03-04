2d8b9dcfa87b120c1999d9c67136caa68a733209
Inna Sorochynska <innasoro@gmail.com>
2021-03-04 21:04:55 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,29 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === null || this[i] === undefined || this[i] === '') {
+        if (separator === undefined) {
+          str += `,`;
+        } else {
+          str += `${separator}`;
+        }
+      } else if (separator === undefined) {
+        str += `${this[i]},`;
+      } else {
+        str += `${this[i]}${separator}`;
+      }
+    }
+
+    str += this[this.length - 1];
+
+    return str;
   };
 }
 
