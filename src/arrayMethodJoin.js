785ea0573057dc182e531a5afa6e0e0030025eeb
Andron <blackdog337.ab@gmail.com>
2021-03-05 14:45:28 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,28 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let joinedStr = '';
+    let sepCopy = separator;
+
+    for (let element of this) {
+      if (typeof sepCopy === 'undefined') {
+        sepCopy = ',';
+      };
+
+      if (typeof element === 'undefined') {
+        element = '';
+      } else if (`${element}` === 'null') {
+        element = '';
+      };
+      joinedStr += `${element}` + `${sepCopy}`;
+    };
+
+    if (sepCopy === '') {
+      return joinedStr;
+    };
+
+    return joinedStr.slice(0, -`${sepCopy}`.length);
   };
-}
+};
 
 module.exports = applyCustomJoin;
