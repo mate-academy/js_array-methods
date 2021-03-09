c44c097e58f536d291ba1d0508392468161bbbad
Inna Sorochynska <innasoro@gmail.com>
2021-03-09 17:24:00 +0200

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,7 +4,7 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let str = '';
 
     if (this.length === 0) {
@@ -13,13 +13,7 @@ function applyCustomJoin() {
 
     for (let i = 0; i < this.length - 1; i++) {
       if (this[i] === null || this[i] === undefined || this[i] === '') {
-        if (separator === undefined) {
-          str += `,`;
-        } else {
-          str += `${separator}`;
-        }
-      } else if (separator === undefined) {
-        str += `${this[i]},`;
+        str += `${separator}`;
       } else {
         str += `${this[i]}${separator}`;
       }
