b12117686f0186425235503dd32c1661a582be50
Vitalii Nepomiashchyi <nepomyaschyi1@gmail.com>
2021-03-10 13:55:28 +0200

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,15 +7,13 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let output = '';
 
-    if (this.length >= 1 && this[0] !== null) {
-      output += String(this[0]);
-    }
-
-    for (let i = 1; i < this.length; i++) {
-      output += String(separator);
-
+    for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
         output += String(this[i]);
+      }
+
+      if (i !== this.length - 1) {
+        output += String(separator);
       }
     }
 
