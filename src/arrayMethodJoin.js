6e22a9a8f98f9b0d1786f778570837c83516c459
Alexander Levchenko <a.levchenko603@gmail.com>
2021-03-10 13:44:07 +0200

    solution was fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,15 +7,10 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let string = '';
 
-    if (this.length === 0) {
-      return string;
-    }
-
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        this[i] = '';
+      if (this[i] !== null && this[i] !== undefined) {
+        string += this[i];
       }
-      string += this[i];
 
       if (i !== this.length - 1) {
         string += separator;
