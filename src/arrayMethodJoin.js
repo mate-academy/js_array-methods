3d1cddf6c7d6032464f0717ca63cc11d439491eb
Serhii-Nikitin <serg.niki@i.ua>
2021-03-10 12:38:50 +0200

    changes after review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,12 +8,12 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let string = '';
 
-    for (const i of this) {
-      if (i !== null && i !== undefined && i !== '') {
-        string += `${i}`;
+    for (const item of this) {
+      if (item !== null && item !== undefined && item !== '') {
+        string += `${item}`;
       }
 
-      if (this.indexOf(i) !== this.length - 1) {
+      if (this.indexOf(item) !== this.length - 1) {
         string += `${separator}`;
       }
     }
