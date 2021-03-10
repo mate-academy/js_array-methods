e3b4b8dfba312c77ca0df6787a7c1da72824dc2b
Iryna Haiduk <$ git config --global user.name Iryna>
2021-03-10 13:00:31 +0200

    Fixed bugs after review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,27 +4,19 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
     let str = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (separator === undefined) {
-        if (this[i] === null || this[i] === undefined) {
-          str += ',';
-        } else if (i !== this.length - 1) {
-          str += `${this[i]},`;
-        } else {
-          str += `${this[i]}`;
-        }
+      if (i === 0 && this[i] === null) {
+        continue;
+      } else if (this[i] === null || this[i] === undefined) {
+        str += `${separator}`;
+      } else if (i === 0) {
+        str += `${this[i]}`;
       } else {
-        if (this[i] === null || this[i] === undefined) {
-          str += `${separator}`;
-        } else if (i !== this.length - 1) {
-          str += `${this[i]}${separator}`;
-        } else {
-          str += `${this[i]}`;
-        }
+        str += `${separator}${this[i]}`;
       }
     }
 
