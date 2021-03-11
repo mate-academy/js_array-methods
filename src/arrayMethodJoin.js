899a18eadf3b330877fc0250c5927ee899a47bab
Kostrica <romiorw@ukr.net>
2021-03-11 18:40:21 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,43 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+
+    if (this.length) {
+      for (let i = 0; i < this.length; i++) {
+        if (i !== this.length - 1) {
+          if (separator === undefined) {
+            if (this[i] == null) {
+              str += '' + ',';
+            } else {
+              str += this[i] + ',';
+            }
+          } else if (separator == null) {
+            if (this[i] == null) {
+              str += '' + 'null';
+            } else {
+              str += this[i] + 'null';
+            }
+          } else {
+            if (this[i] == null) {
+              str += '' + separator;
+            } else {
+              str += this[i] + separator;
+            }
+          }
+        }
+
+        if (i === this.length - 1) {
+          if (this[i] == null) {
+            str += '';
+          } else {
+            str += this[i];
+          }
+        }
+      }
+    }
+
+    return str;
   };
 }
 
