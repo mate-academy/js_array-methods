a39024d36ea53d24205b6a877d25b377124b7bc5
Andriy Fesych <andriyhimself@gmail.com>
2021-03-14 18:17:09 +0200

    Solution_0.1

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,36 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let separ = separator;
+
+    if (separ === undefined) {
+      separ = ',';
+    }
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (this.length === 1) {
+      return String(this);
+    }
+
+    let res = String(this[0]);
+
+    if ((this[0] === null) || (this[0] === undefined)) {
+      res = '';
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      let a = String(this[i]);
+
+      if ((a === 'null') || (a === 'undefined')) {
+        a = '';
+      }
+      res += String(separ) + a;
+    }
+
+    return res;
   };
 }
 
