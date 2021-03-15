c7841fc084f65c735916446ad1817027a02e5fec
Olga Vasylevskaya <olgavasylevskaya@ukr.net>
2021-03-15 17:08:16 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,29 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === null || typeof this[i] === 'undefined') {
+        this[i] = '';
+      }
+
+      if (typeof separator === 'undefined') {
+        result += this[i] + ',';
+      } else if (!separator && separator !== '') {
+        result += this[i] + 'null';
+      } else {
+        result += this[i] + separator;
+      }
+    }
+
+    result += this[this.length - 1];
+
+    return result;
   };
 }
 
