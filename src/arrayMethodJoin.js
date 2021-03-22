aca2a60f3d1ee76fb41dc74854064d3357c3c0d7
Dmytro Prontenko <dprontenko@gmail.com>
2021-03-22 16:46:47 +0200

    test

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,25 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let result = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i <= this.length; i++) {
+      if (this[i] === null || typeof this[i] === 'undefined') {
+        this[i] = '';
+      }
+
+      if (typeof separator === 'undefined') {
+        result = this[i] + ',';
+      } else {
+        result = this[i] + separator;
+      }
+    }
+
+    return result;
   };
 }
 
