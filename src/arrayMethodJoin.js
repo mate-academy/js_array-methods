496deeb8404721742934b972692ad8f2e3fd8543
Valentyna Makarycheva <valentina.kravch14@gmail.com>
2021-03-29 11:16:13 +0300

    Solution array Method Join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,35 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let strElements = '';
+    let separatorNew = separator;
+
+    if (this.length === 0) {
+      return strElements;
+    }
+
+    if (separator === undefined) {
+      separatorNew = ',';
+    }
+
+    if (this.length > 1) {
+      for (let i = 0; i <= this.length - 2; i++) {
+        if (this[i] !== null && this[i] !== undefined) {
+          strElements += this[i];
+        }
+
+        strElements += `${separatorNew}`;
+      }
+
+      strElements += this[this.length - 1];
+
+      return `${strElements}`;
+    } else {
+      strElements = this[0];
+
+      return `${strElements}`;
+    }
   };
-}
+};
 
 module.exports = applyCustomJoin;
