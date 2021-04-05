a8db3c639f1f722acb7bf7c2c1899614607795d8
=Roma <roma20299@gmail.com>
2021-04-05 12:11:49 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,33 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let res = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === null || typeof this[i] === 'undefined') {
+        this[i] = '';
+      }
+
+      if (separator === null) {
+        res += this[i] + 'null';
+      }
+
+      if (typeof separator === 'undefined') {
+        res += this[i] + ',';
+      }
+
+      if (typeof separator !== 'undefined' && separator !== null) {
+        res += this[i] + separator;
+      }
+    }
+
+    res += this[this.length - 1];
+
+    return res;
   };
 }
 
