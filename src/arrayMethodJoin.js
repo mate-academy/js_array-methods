64e04d5b05c00ac73f867f5ed98e4fdf53e692ac
Maksym_Starodubtsev_FE_May_21 <maksym.starodubtsev@gmaill.com>
2021-04-23 18:49:43 +0300

    Confirm

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+    let yy = separator;
+
+    for (const char of this) {
+      if (char === null || char === undefined) {
+        str += ',';
+      } else if (this[this.length - 1] === char) {
+        str += char;
+      } else {
+        if (yy === null) {
+          yy = 'null';
+        }
+        str += char + yy;
+      }
+    }
+
+    return str;
   };
 }
 
