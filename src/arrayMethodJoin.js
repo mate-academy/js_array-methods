1020195e057cff57fef761d407f980ba0809832c
Andrey Dorohov <andrey.dorohov8@gmail.com>
2021-04-24 14:52:08 +0300

    done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,33 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+    let sep = separator;
+
+    if (this.length === 0) {
+      return str;
+    }
+
+    if (sep === null) {
+      sep = `${separator}`;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === 0) {
+        if (!(this[i] === null || this[i] === undefined)) {
+          str = `${this[0]}`;
+        }
+      } else {
+        if (this[i] === null || this[i] === undefined) {
+          str += sep;
+        } else {
+          str += sep + this[i];
+        }
+      }
+    }
+
+    return str;
   };
 }
 
