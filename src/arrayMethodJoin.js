813d0bbd234bd145634a9143aa1e17a2740c57bf
victor <victorarytionowmail@gmail.com>
2021-04-24 14:56:10 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,34 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+    const separatorStr = separator + '';
+
+    if (separatorStr !== 'undefined') {
+      for (const i of this) {
+        if (i !== undefined && i !== null) {
+          str += i + separatorStr;
+        } else {
+          str += ',';
+        }
+      }
+    } else {
+      for (const i of this) {
+        if (i !== undefined && i !== null) {
+          str += i + ',';
+        } else {
+          str += ',';
+        }
+      }
+    }
+
+    if (separatorStr === 'undefined') {
+      str = str.substring(0, str.length - 1);
+    } else {
+      str = str.substring(0, str.length - separatorStr.length);
+    }
+
+    return str;
   };
 }
 
