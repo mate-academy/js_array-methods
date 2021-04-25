c47b71fb52815cf25e9206af87dd2e5aeba0c6b4
Bulaev Denis <bulaevdenis7@gmail.com>
2021-04-26 01:27:43 +0300

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,40 +6,28 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     // write code here
-    const joinedArr = [''];
+    let joinedArr = '';
     const length = this.length;
 
     const sep = typeof (separator) === 'object'
       ? String(separator)
       : separator;
 
-    if (length < 1) {
-      return '';
-    }
-
     for (let i = 0; i < length; i++) {
       let element;
 
-      switch (true) {
-        case typeof (this[i]) === 'undefined':
-          element = '';
-
-          break;
-
-        case typeof (this[i]) === 'object' && !this[i] === true:
-          element = '';
-
-          break;
-
-        default:
-          element = this[i];
+      if (this[i] === undefined || this[i] === null) {
+        element = '';
+      } else {
+        element = this[i];
       }
-      joinedArr.push(joinedArr[i] + element + sep);
+
+      joinedArr += element + sep;
     }
 
     return sep
-      ? joinedArr[length].slice(0, -sep.length)
-      : joinedArr[length];
+      ? joinedArr.slice(0, -sep.length)
+      : joinedArr;
   };
 }
 
