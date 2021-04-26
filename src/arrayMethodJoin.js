638734e2c3178666a9986817a1620f5276b1b739
ViPiven <vitalijjpiven777@gmail.com>
2021-04-26 16:48:31 +0300

    'Solution'

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        this[i] = '';
+      }
+
+      if (i === 0) {
+        result += this[i];
+      } else {
+        result += `${separator}${this[i]}`;
+      }
+    }
+
+    return result;
   };
 }
 
