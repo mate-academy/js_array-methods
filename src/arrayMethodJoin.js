da342b415acba41f26e441fefc4c20cfc5e70553
Vitaliy Buchincik <v.buchinchik@gmail.com>
2021-04-26 16:28:07 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,52 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let resultString = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (separator === '') {
+      for (let i = 0; i < this.length; i++) {
+        resultString += this[i];
+      }
+
+      return resultString;
+    }
+
+    if (separator === null) {
+      for (let i = 0; i < this.length - 1; i++) {
+        if (this[i] === undefined || this[i] === null) {
+          resultString += '' + 'null';
+          continue;
+        }
+        resultString += `${this[i]}null`;
+      }
+      resultString += `${this[this.length - 1]}`;
+
+      return resultString;
+    }
+
+    if (!separator) {
+      for (let i = 0; i < this.length - 1; i++) {
+        resultString += `${this[i]},`;
+      }
+      resultString += `${this[this.length - 1]}`;
+    }
+
+    if (separator) {
+      for (let i = 0; i < this.length - 1; i++) {
+        if (this[i] === undefined || this[i] === null) {
+          resultString += '' + separator;
+          continue;
+        }
+        resultString += `${this[i]}${separator}`;
+      }
+      resultString += `${this[this.length - 1]}`;
+    }
+
+    return resultString;
   };
 }
 
