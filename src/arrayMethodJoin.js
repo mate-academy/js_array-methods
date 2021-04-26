7561da7c6c5a7f3be3a25b88bb205525eaf27609
Serhii Kolesnyk <kolesniks203@gmail.com>
2021-04-26 22:02:49 +0300

    after fixing

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,13 +4,8 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    let separ = separator;
-
-    if (separator === undefined) {
-      separ = ',';
-    }
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] === undefined || this[i] === null) {
@@ -20,7 +15,7 @@ function applyCustomJoin() {
       }
 
       if (i !== this.length - 1) {
-        result += separ;
+        result += separator;
       }
     }
 
