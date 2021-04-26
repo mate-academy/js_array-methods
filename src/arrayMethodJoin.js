db0e368fed6355a394f6ec8921bb42836ad82a55
Ruslana Podshyvalova <lanataylorpersonal@gmail.com>
2021-04-26 23:33:00 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,45 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let strFromArr = '';
+    const separatorSign = checkSeparator(separator);
+
+    if (this.length === 0) {
+      return strFromArr;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      const item = checkItem(this[i]);
+
+      if (i !== this.length - 1) {
+        strFromArr += item + separatorSign;
+      } else {
+        strFromArr += item;
+      }
+    }
+
+    return strFromArr;
   };
+}
+
+function checkSeparator(separator) {
+  if (separator === undefined) {
+    return ',';
+  }
+
+  if (separator === null) {
+    return 'null';
+  }
+
+  return separator;
+}
+
+function checkItem(item) {
+  if (item === undefined || item === null) {
+    return '';
+  }
+
+  return item;
 }
 
 module.exports = applyCustomJoin;
