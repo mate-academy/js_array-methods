13d28179125a0517c5ef9c5f17a93481621b1eda
victor <victorarytionowmail@gmail.com>
2021-04-26 13:47:53 +0300

    set comma as a default value, optimize code

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,32 +4,18 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let str = '';
-    const separatorStr = separator + '';
 
-    if (separatorStr !== 'undefined') {
+    if (separator !== undefined) {
       for (const i of this) {
         if (i !== undefined && i !== null) {
-          str += i + separatorStr;
+          str += i + '' + separator;
         } else {
           str += ',';
         }
       }
-    } else {
-      for (const i of this) {
-        if (i !== undefined && i !== null) {
-          str += i + ',';
-        } else {
-          str += ',';
-        }
-      }
-    }
-
-    if (separatorStr === 'undefined') {
-      str = str.substring(0, str.length - 1);
-    } else {
-      str = str.substring(0, str.length - separatorStr.length);
+      str = str.substring(0, str.length - (separator + '').length);
     }
 
     return str;
