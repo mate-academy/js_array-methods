72529bf91c73ea5e9bb6576c651a7856ce8a333d
”Ivan <ivasylenko@ukr.net>
2021-04-26 12:52:28 +0300

    Solution 4

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -10,13 +10,11 @@ function applyCustomJoin() {
     for (let i = 0; i < this.length; i++) {
       if (i === (this.length - 1)) {
         result += this[i];
+      } else if (this[i] === undefined || this[i] === null) {
+        result += separator;
       } else {
-        if (this[i] === undefined || this[i] === null) {
-          result += separator;
-        } else {
-          result += this[i];
-          result += separator;
-        }
+        result += this[i];
+        result += separator;
       }
     }
 
