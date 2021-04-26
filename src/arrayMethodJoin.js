43dcfd70a9cd6cd3efa81d405a2687e3c7d045e0
Vitalii Siedykh <siedykh.v@gmail.com>
2021-04-26 18:17:05 +0300

    small_fix

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,10 +7,6 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
 
-    if (this.length === 0) {
-      return result;
-    }
-
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== undefined && this[i] !== null) {
         result += this[i];
