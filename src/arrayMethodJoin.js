41ffc0ebe3b9840f04cb29d6cbae48b51e4a780c
victor <victorarytionowmail@gmail.com>
2021-04-26 14:08:28 +0300

    delete excess code

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,16 +7,14 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let str = '';
 
-    if (separator !== undefined) {
-      for (const i of this) {
-        if (i !== undefined && i !== null) {
-          str += i + '' + separator;
-        } else {
-          str += ',';
-        }
+    for (const i of this) {
+      if (i !== undefined && i !== null) {
+        str += i + '' + separator;
+      } else {
+        str += ',';
       }
-      str = str.substring(0, str.length - (separator + '').length);
     }
+    str = str.substring(0, str.length - (separator + '').length);
 
     return str;
   };
