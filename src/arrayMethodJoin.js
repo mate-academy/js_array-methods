be1f8e735d4a1dcc2b482d40603a7f9e932f5392
Pavel Kuzmenko <pashokkuzmenko@gmail.com>
2021-04-26 19:58:49 +0300

    v0.2

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,19 +7,14 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let joinResult = '';
 
-    if (this.length === 0) {
-      return joinResult;
-    }
-
-    if (this[0] !== null) {
-      joinResult += this[0];
-    }
-
-    for (let i = 1; i < this.length; i++) {
-      if ((this[i] === null) || (this[i] === undefined)) {
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
         joinResult += `${separator}`;
+      } else
+      if (i === this.length - 1) {
+        joinResult += this[i];
       } else {
-        joinResult += `${separator}` + this[i];
+        joinResult += this[i] + `${separator}`;
       }
     }
 
