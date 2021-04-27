f794dc560afe3015b598bbdac4ea6405a8ea4886
Bogdan Kotsupey <80522234+Bogdan-Kotsupey@users.noreply.github.com>
2021-04-27 21:22:42 +0300

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,94 +6,22 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    let lastIndex;
+    const string = `${separator}`;
 
-    for (let i = 0; i < this.length; i++) {
-      while (this.length - 1 > i) {
-        if (this[i] === null) {
-          result += separator;
-          break;
-        }
-
-        if (this[i] === undefined) {
-          result += separator;
-          break;
-        }
-        result += this[i] + separator;
-        break;
+    for (const word of this) {
+      if (word === null || word === undefined) {
+        result += string;
+        continue;
       }
 
-      lastIndex = this[i];
-    }
-
-    result += lastIndex;
-
-    if (this.length === 0) {
-      return '';
+      result += word + string;
     }
 
     if (separator === '') {
-      result = '';
-
-      for (let i = 0; i < this.length; i++) {
-        if (this[i] === null) {
-          result += separator;
-          continue;
-        }
-
-        if (this[i] === undefined) {
-          result += separator;
-          continue;
-        }
-        result += this[i];
-      }
+      return result.slice(0, result.length);
+    } else {
+      return result.slice(0, result.length - string.length);
     }
-
-    if (typeof separator === 'object') {
-      result = '';
-
-      for (let i = 0; i < this.length; i++) {
-        while (this.length - 1 > i) {
-          if (this[i] === null) {
-            result += separator;
-          }
-
-          if (this[i] === undefined) {
-            result += separator;
-          }
-          result += this[i] + separator;
-          break;
-        }
-
-        lastIndex = this[i];
-      }
-
-      result += lastIndex;
-    }
-
-    if (separator === null) {
-      result = '';
-
-      for (let i = 0; i < this.length; i++) {
-        while (this.length - 1 > i) {
-          if (this[i] === null) {
-            result += 'null';
-          }
-
-          if (this[i] === undefined) {
-            result += 'null';
-          }
-          result += this[i] + 'null';
-          break;
-        }
-
-        lastIndex = this[i];
-      }
-
-      result += lastIndex;
-    }
-
-    return result;
   };
 }
 
