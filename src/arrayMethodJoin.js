bb7e82f79bd026d9cc1888a19a6615d6839eece3
Vitaliy Buchincik <v.buchinchik@gmail.com>
2021-04-27 11:55:59 +0300

    Fixes after code review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,20 +6,20 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let resultString = '';
-    const lastElement = this[this.length - 1];
 
     if (this.length === 0) {
       return '';
     }
 
-    for (let i = 0; i < this.length - 1; i++) {
+    for (let i = 0; i < this.length; i++) {
       if (this[i] === undefined || this[i] === null) {
-        resultString += '' + separator;
-        continue;
+        resultString += String(separator);
+      } else if (i !== this.length - 1) {
+        resultString += `${this[i]}${separator}`;
+      } else {
+        resultString += this[this.length - 1];
       }
-      resultString += `${this[i]}${separator}`;
     }
-    resultString += lastElement;
 
     return resultString;
   };
