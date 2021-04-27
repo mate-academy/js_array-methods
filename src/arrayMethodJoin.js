1075d56a65fe663a470d1078c07555fc9e87a7d5
ched88xtc <ched88official@gmail.com>
2021-04-27 11:50:11 +0300

    another solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,15 +7,17 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
 
+    if (this.length <= 1) {
+      return `${this}`;
+    }
+
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        this[i] = '';
+      if (this[i] !== null && this[i] !== undefined) {
+        result += this[i];
       }
 
-      if (i === 0) {
-        result += this[i];
-      } else {
-        result += `${separator}${this[i]}`;
+      if (i !== this.length - 1) {
+        result += separator;
       }
     }
 
