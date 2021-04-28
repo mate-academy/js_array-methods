adc99bff1a550fa0f64e794b76ac892c0ce81e98
Yaroslav <gso218695@gmail.com>
2021-04-28 17:44:46 +0300

    after student review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,33 +5,13 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    // let string = '';
-
-    // if (this.length === 0) {
-    //   return '';
-    // }
-
-    // for (const item of this) {
-    //   if (this[item] === null || this[item] === undefined) {
-    //     this[item] = '';
-    //   }
-
-    //   if (item < this.length - 1) {
-    //     string += separator;
-    //   }
-    // }
-
-    // return string;
-
     let string = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] !== undefined && this[i] !== null) {
-        string += this[i];
-      }
-
-      if (this[i] !== undefined || this[i] !== null) {
+      if (this[i] === undefined || this[i] === null) {
         string += '';
+      } else {
+        string += this[i];
       }
 
       if (i < this.length - 1) {
