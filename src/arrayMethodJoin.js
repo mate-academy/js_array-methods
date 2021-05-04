75dd16f9cd73663949ca0901e2d48674ca6258ef
SergiyKhomchenko <39374976+SergiyKhomchenko@users.noreply.github.com>
2021-05-04 20:01:08 +0300

    Update arrayMethodJoin.js

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,26 +6,23 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     // write code here
-    let result = '';
-    const resultArray = [];
+    let stringArr = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        resultArray[resultArray.length] = '';
-      } else {
-        resultArray[resultArray.length] = this[i];
+      let elem = this[i];
+
+      if (elem === null || elem === undefined) {
+        elem = '';
+      }
+
+      stringArr += elem;
+
+      if (i !== this.length - 1) {
+        stringArr += separator;
       }
     }
 
-    for (let i = 0; i < resultArray.length; i++) {
-      result += resultArray[i];
-
-      if (i !== resultArray.length - 1) {
-        result += separator;
-      }
-    }
-
-    return result;
+    return stringArr;
   };
 }
 
