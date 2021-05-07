12cccf3883b9f2879d5a5d010aefb6d499e5c959
VasylPerepichai <vperepichay@gmail.com>
2021-05-07 19:00:55 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,38 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let delim = separator;
+    let resString = '';
+
+    function getNullUndefined(value) {
+      let string;
+
+      if (value === undefined || value === null) {
+        string = '';
+
+        return string;
+      }
+
+      return value;
+    }
+
+    if (delim === undefined) {
+      delim = ',';
+    }
+
+    if (delim === null) {
+      delim = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i !== this.length - 1) {
+        resString += getNullUndefined(this[i]) + delim;
+      } else {
+        resString += getNullUndefined(this[i]);
+      }
+    }
+
+    return resString;
   };
 }
 
