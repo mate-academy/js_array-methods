bb8b4d6591b0dce78ccd5c6b60618894a81a7627
KostyaPenyavskiy <kornelius9898@gmail.com>
2021-05-19 19:38:21 +0300

    done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,27 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+
+    switch (this.length) {
+      case 0:
+        return '';
+
+      case 1:
+        return `${this[0]}`;
+
+      default:
+        for (let i = 0; i < this.length; i++) {
+          const e = this[i] === undefined || this[i] === null ? '' : this[i];
+
+          i === this.length - 1
+            ? str += `${e}`
+            : str += `${e}` + separator;
+        }
+    }
+
+    return str;
   };
 }
 
