8853e5d1f2fe74b60ade3cfc41b903000bd1dc83
MaximTsyhanenko <maksym.tsyganenko85@gmail.com>
2021-05-26 16:56:02 +0300

    solution v.1

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,38 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+
+    let delimiter;
+
+    switch (separator) {
+      case undefined :
+        delimiter = ',';
+        break;
+
+      case null :
+        delimiter = 'null';
+        break;
+
+      default :
+        delimiter = '' + separator;
+    }
+
+    const numberOfSliceSymbols = delimiter.length;
+
+    for (const i of this) {
+      if (i == null || i === undefined) {
+        str += delimiter;
+      } else {
+        str += i + delimiter;
+      }
+    }
+
+    if (delimiter === '') {
+      return str;
+    } else {
+      return str.slice(0, str.length - numberOfSliceSymbols);
+    }
   };
 }
 
