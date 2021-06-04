9de54f2c8d87eca2b35b89b11f9670d0d071b5e1
Ihor <igor.pyliak@techmagic.co>
2021-06-04 10:50:07 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,34 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    if (!this.length) {
+      return '';
+    }
+
+    function checkType(value) {
+      const whiteList = ['number', 'object', 'boolean', 'string'];
+
+      return whiteList.includes(typeof value) && value !== null;
+    }
+
+    let concated = checkType(this[0]) ? this[0] : '';
+
+    for (let i = 1; i < this.length; i++) {
+      const value = checkType(this[i]) ? this[i] : '';
+
+      switch (separator) {
+        case '':
+          concated = concated + '' + value;
+          break;
+        case undefined:
+          concated = concated + ',' + value;
+          break;
+        default:
+          concated = concated + `${separator}` + value;
+      }
+    }
+
+    return concated.toString();
   };
 }
 
