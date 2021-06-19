efcdea4b5f35a9d13e89c15d866f8235e44814ab
Oleksii Lymonov <alex25_92@ukr.net>
2021-06-19 15:10:37 +0300

    first_try

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let stringOfArray = `${this[0]}`;
+
+    if (this[0] === null || this[0] === undefined) {
+      stringOfArray = '';
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        stringOfArray += `${separator}${''}`;
+        continue;
+      }
+      stringOfArray += `${separator}${String(this[i])}`;
+    }
+
+    return stringOfArray;
   };
 }
 
