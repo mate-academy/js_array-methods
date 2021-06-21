80b4d48ab815b6b54ce292bd876b0f3473c7d735
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-06-21 08:31:10 +0300

    feat: completed task join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,26 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let resultStr = '';
+    let unifier = separator;
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (separator === undefined) {
+      unifier = ',';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        this[i] = '';
+      }
+      resultStr += `${this[i]}${unifier}`;
+    }
+    resultStr += this[this.length - 1];
+
+    return resultStr;
   };
 }
 
