436d40b22bfe6807c61a11cbafc8d14eb8468f67
vdzhyranov <v.dzhyranov@gmail.com>
2021-06-22 17:24:47 +0300

    fix

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,19 +6,19 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     // write code here
-    let str = '';
+    let joinedString = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
-        str += this[i];
+        joinedString += this[i];
       }
 
       if (i !== this.length - 1) {
-        str += separator;
+        joinedString += separator;
       }
     }
 
-    return str;
+    return joinedString;
   };
 }
 
