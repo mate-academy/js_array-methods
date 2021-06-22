79838de4e76a7906d62657958ac5ba96ba8d140a
VolodymyrKlepykov <volodymyr.klepykov@gmail.com>
2021-06-22 13:13:46 +0300

    fix: add default value separator

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,23 +4,18 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let resultStr = '';
-    let unifier = separator;
 
     if (this.length === 0) {
       return '';
-    }
-
-    if (separator === undefined) {
-      unifier = ',';
     }
 
     for (let i = 0; i < this.length - 1; i++) {
       if (this[i] === undefined || this[i] === null) {
         this[i] = '';
       }
-      resultStr += `${this[i]}${unifier}`;
+      resultStr += `${this[i]}${separator}`;
     }
     resultStr += this[this.length - 1];
 
