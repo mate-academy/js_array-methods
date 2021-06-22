59f8602ba1945c75438f9c61fa68984c74045de9
Oleksii Lymonov <alex25_92@ukr.net>
2021-06-22 11:48:29 +0300

    one_condition

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,21 +6,27 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     // write code here
-    let stringOfArray = `${this[0]}`;
+    let joinedSeparator = '';
 
-    if (this[0] === null || this[0] === undefined) {
-      stringOfArray = '';
-    }
-
-    for (let i = 1; i < this.length; i++) {
+    for (let i = 0; i < this.length; i++) {
       if (this[i] === null || this[i] === undefined) {
-        stringOfArray += `${separator}${''}`;
-        continue;
+        if (i === 0) {
+          joinedSeparator = '';
+          continue;
+        } else {
+          joinedSeparator += `${separator}${''}`;
+          continue;
+        }
+      } else {
+        if (i === 0) {
+          joinedSeparator = `${this[0]}`;
+          continue;
+        }
+        joinedSeparator += `${separator}${this[i]}`;
       }
-      stringOfArray += `${separator}${String(this[i])}`;
     }
 
-    return stringOfArray;
+    return joinedSeparator;
   };
 }
 
