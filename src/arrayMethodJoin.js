d2c199370a50d2d18ae02379271a072d3a4bd31a
Artem Yemelyanov <ae1666528@gmail.com>
2021-06-23 13:57:40 +0300

    Implemented join method for arrays

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,27 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let joinedItems = '';
+    let separatorChar = separator;
+
+    if (typeof separatorChar === 'undefined') {
+      separatorChar = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined
+        || this[i] === null) {
+        joinedItems += '';
+      } else {
+        joinedItems += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        joinedItems += separatorChar;
+      }
+    }
+
+    return joinedItems;
   };
 }
 
