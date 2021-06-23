c23ee1c87f392949207189cb84f9a9a7aa67f57e
Bishko <yurik322412@gmail.com>
2021-06-23 15:52:48 +0300

    code refactor

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,30 +5,20 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let result = '';
+    let joinedElements = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        this[i] = '';
+      if (this[i] !== undefined
+        && this[i] !== null) {
+        joinedElements += this[i];
       }
 
-      if (result) {
-        result += separator;
+      if (i !== this.length - 1) {
+        joinedElements += separator;
       }
-
-      result += this[i];
     }
 
-    if (
-      !this[0]
-      && this[0] !== false
-      && this[0] !== 0
-      && this.length !== 0
-    ) {
-      return `,,${result}`;
-    }
-
-    return result;
+    return joinedElements;
   };
 }
 
