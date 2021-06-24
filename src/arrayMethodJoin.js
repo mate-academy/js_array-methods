78733d8fa284ba5c8a0564d1fa55952c86b26e5f
Artem Krulykovskyi <krulikovskiy.a@gmail.com>
2021-06-24 18:56:34 +0300

    I have some questions........

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,36 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length === 0) {
+      return '';
+    }
+
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null) {
+        this[i] = '';
+      } else if (this[i] === true) {
+        this[i] = 'true';
+      } else if (this[i] === false) {
+        this[i] = 'false';
+      } else if (this[i] === undefined) {
+        this[i] = '';
+      } else if (this[i] === 'null') {
+        this[i] = 'null';
+      } else if (isNaN(this[i])) {
+        this[i] = 'NaN';
+      }
+
+      result += this[i];
+
+      if (i !== this.length - 1) {
+        result += separator;
+      }
+    }
+
+    return result;
   };
 }
 
