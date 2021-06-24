d7d6440eec25a9fcc0020bdf2d6e3a4f5cfb5ef2
David Dohru <tronsmartmy@gmail.com>
2021-06-24 21:28:18 +0300

    join done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let resOfJoinMethod = '';
+
+    if (typeof (this) === 'object') {
+      for (const key of this) {
+        if (key !== undefined && key !== null) {
+          resOfJoinMethod += key;
+        }
+
+        if (key !== this[this.length - 1]) {
+          resOfJoinMethod += separator;
+        }
+      }
+    }
+
+    return resOfJoinMethod;
   };
 }
 
