f4d94ae20e5816a964607de6ead8adfbbe3ebd05
David Dohru <tronsmartmy@gmail.com>
2021-06-25 09:50:03 +0300

    fixed name for variable

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,21 +5,21 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let resOfJoinMethod = '';
+    let resultOfJoinMethod = '';
 
     if (typeof (this) === 'object') {
       for (const key of this) {
         if (key !== undefined && key !== null) {
-          resOfJoinMethod += key;
+          resultOfJoinMethod += key;
         }
 
         if (key !== this[this.length - 1]) {
-          resOfJoinMethod += separator;
+          resultOfJoinMethod += separator;
         }
       }
     }
 
-    return resOfJoinMethod;
+    return resultOfJoinMethod;
   };
 }
 
