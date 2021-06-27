13515d31b96c96c263ddf94c5d2c91671fc5e732
NYaremchuk <nazarmails@gmail.com>
2021-06-27 23:40:51 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let stringfFromArray = '';
+    const sep = String(separator);
+
+    for (const char of this) {
+      if (char !== null && char !== undefined) {
+        stringfFromArray += char + sep;
+      } else {
+        stringfFromArray += sep;
+      }
+    }
+
+    if (sep) {
+      let stringWithoutLastChar = '';
+
+      for (let i = 0; i < stringfFromArray.length - sep.length; i++) {
+        stringWithoutLastChar += stringfFromArray[i];
+      }
+
+      return stringWithoutLastChar;
+    }
+
+    return stringfFromArray;
   };
 }
 
