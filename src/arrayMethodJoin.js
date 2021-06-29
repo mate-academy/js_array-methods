da01c1eb9c02a35bdbe0ec6f8cfda65e3a0d8dad
Aleksandr Semerenko <sanya96088475@gmail.com>
2021-06-29 12:46:38 +0300

    join method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,26 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedValues = '';
+
+    if (this.length === 0) {
+      return joinedValues;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        this[i] = '';
+      }
+
+      if (i === this.length - 1) {
+        joinedValues += `${this[i]}`;
+      } else {
+        joinedValues += `${this[i]}${separator}`;
+      }
+    }
+
+    return joinedValues;
   };
 }
 
