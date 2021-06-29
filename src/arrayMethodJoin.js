a663275b82c6285d2eb51eadf769b7c709c4b99f
Yaroslav <gso218695@gmail.com>
2021-06-29 15:18:36 +0300

    test

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,21 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let string = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        string += '';
+      }
+
+      string += this[i];
+
+      if (i < this.length - 1) {
+        string += separator;
+      }
+
+      return string;
+    }
   };
 }
 
