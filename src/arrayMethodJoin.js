542e6f3faf6286cd728a68f110ea81ff9f3287fa
Sviatoslav Kishka <sviatoslavkishka@gmail.com>
2021-06-29 12:10:20 +0300

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,19 +6,19 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     // write code here
-    let string = '';
+    let result = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
-        string += this[i];
+        result += this[i];
       }
 
       if (i !== this.length - 1) {
-        string += separator;
+        result += separator;
       }
     }
 
-    return string;
+    return result;
   };
 }
 
