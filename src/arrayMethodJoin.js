1dcdff6909e9f2d761e50f910d9c073db1ab010b
Pavlo Starynskyi <starinskiy@gmail.com>
2021-06-29 15:09:32 +0300

    first commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,21 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let string = '';
+
+    for (const value of this) {
+      if (value !== undefined && value !== null) {
+        string += value;
+      }
+
+      if (value !== this[this.length - 1]) {
+        string += separator;
+      }
+    }
+
+    return string;
   };
-}
+};
 
 module.exports = applyCustomJoin;
