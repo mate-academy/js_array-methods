5b10dbbb1a364399aa0ff5d099f3ca0ffb702d2a
NYaremchuk <nazarmails@gmail.com>
2021-06-29 17:47:14 +0300

    Fixed mistakes

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,24 +6,15 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let stringfFromArray = '';
-    const sep = String(separator);
 
-    for (const char of this) {
-      if (char !== null && char !== undefined) {
-        stringfFromArray += char + sep;
-      } else {
-        stringfFromArray += sep;
-      }
-    }
-
-    if (sep) {
-      let stringWithoutLastChar = '';
-
-      for (let i = 0; i < stringfFromArray.length - sep.length; i++) {
-        stringWithoutLastChar += stringfFromArray[i];
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        stringfFromArray += this[i];
       }
 
-      return stringWithoutLastChar;
+      if (this.length - i !== 1) {
+        stringfFromArray += separator;
+      }
     }
 
     return stringfFromArray;
