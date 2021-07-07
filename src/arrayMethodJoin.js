a86beb2e98c06d361fad1d6930d1bb631270d6cd
Oleksandr Hrachov <ol.grachov@gmail.com>
2021-07-07 10:38:37 +0300

    SOLUTION

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,46 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let resultStr = '';
+
+    if (separator === '') {
+      for (const word of this) {
+        resultStr += word;
+      }
+    }
+
+    if (separator) {
+      const arr = [];
+
+      for (const word of this) {
+        arr.push(word);
+        arr.push(separator);
+      }
+
+      for (let i = 0; i < arr.length - 1; i++) {
+        if (arr[i] === null || arr[i] === undefined) {
+          resultStr += '';
+        } else {
+          resultStr += arr[i];
+        }
+      }
+    }
+
+    if (separator === null) {
+      const arr = [];
+
+      for (const word of this) {
+        arr.push(word);
+        arr.push(separator);
+      }
+
+      for (let i = 0; i < arr.length - 1; i++) {
+        resultStr += arr[i];
+      }
+    }
+
+    return resultStr;
   };
 }
 
