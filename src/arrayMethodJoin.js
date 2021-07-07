d667b1c7dc965fc1853f75d167f214c76d8e8dfd
Oleksandr Hrachov <ol.grachov@gmail.com>
2021-07-07 13:42:26 +0300

    2-nd commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,20 +7,20 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let resultStr = '';
 
-    if (separator === '') {
-      for (const word of this) {
-        resultStr += word;
+    const arr = [];
+
+    for (const word of this) {
+      arr.push(word);
+      arr.push(separator);
+    }
+
+    if (separator === null || separator === undefined) {
+      for (let i = 0; i < arr.length - 1; i++) {
+        resultStr += arr[i];
       }
     }
 
     if (separator) {
-      const arr = [];
-
-      for (const word of this) {
-        arr.push(word);
-        arr.push(separator);
-      }
-
       for (let i = 0; i < arr.length - 1; i++) {
         if (arr[i] === null || arr[i] === undefined) {
           resultStr += '';
@@ -30,16 +30,9 @@ function applyCustomJoin() {
       }
     }
 
-    if (separator === null) {
-      const arr = [];
-
+    if (separator === '') {
       for (const word of this) {
-        arr.push(word);
-        arr.push(separator);
-      }
-
-      for (let i = 0; i < arr.length - 1; i++) {
-        resultStr += arr[i];
+        resultStr += word;
       }
     }
 
