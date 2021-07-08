f0aded57ca4e45a72fba57d0f89ec43590bb1973
Oleksandr Hrachov <ol.grachov@gmail.com>
2021-07-08 15:51:15 +0300

    3-rd commit without using array methods

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -9,12 +9,15 @@ function applyCustomJoin() {
 
     const arr = [];
 
+    let k = 0;
+
     for (const word of this) {
-      arr.push(word);
-      arr.push(separator);
+      arr[k] = word;
+      arr[k + 1] = separator;
+      k += 2;
     }
 
-    if (separator === null || separator === undefined) {
+    if (separator === null || separator === undefined || separator === '') {
       for (let i = 0; i < arr.length - 1; i++) {
         resultStr += arr[i];
       }
@@ -27,12 +30,6 @@ function applyCustomJoin() {
         } else {
           resultStr += arr[i];
         }
-      }
-    }
-
-    if (separator === '') {
-      for (const word of this) {
-        resultStr += word;
       }
     }
 
