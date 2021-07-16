00aa77bcfc8a9844babc504ceaecff52d270a483
Roman Prokopiv <rprokopiv@gmail.com>
2021-07-16 19:38:21 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,30 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let separatorLocal = separator;
+
+    if (!isFinite(separatorLocal)
+    && (separatorLocal !== '-')
+    && typeof separatorLocal !== 'object') {
+      separatorLocal = ',';
+    }
+
+    for (let i = 0; i <= this.length - 1; i++) {
+      let word = this[i];
+
+      if (word === null || typeof word === 'undefined') {
+        word = '';
+      };
+
+      result += word;
+
+      if (i !== this.length - 1) {
+        result += separatorLocal;
+      }
+    };
+
+    return result;
   };
 }
 
