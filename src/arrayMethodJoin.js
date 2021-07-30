92cfac2e8612670e5f7e111797506120dd2960fa
darynakravchuk <darinakravchuk@gmail.com>
2021-07-30 15:42:13 +0300

    join method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let arr = '';
+
+    if (this.length === 1) {
+      return `${this}`;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (typeof this[i] !== 'undefined' && this[i] !== null) {
+        arr += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        arr += separator;
+      }
+    }
+
+    return arr;
   };
 }
 
