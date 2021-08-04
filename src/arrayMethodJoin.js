1ab2243da4da2ef8246a752c5c10a23ece39b657
MaxStets <weawer458@gmail.com>
2021-08-04 14:01:18 +0300

    Solution -- arrayMethodJoin

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    const length = this.length;
+
+    if (length === 0) {
+      return '';
+    }
+
+    return this.reduce((string, current, index) => {
+      let currentInner = current;
+      const typeofVal = typeof currentInner;
+
+      if (typeofVal === 'undefined' || typeofVal === 'object') {
+        currentInner = '';
+      }
+
+      if (index === length - 1) {
+        return string + currentInner;
+      }
+
+      return string + currentInner + separator;
+    }, '');
   };
 }
 
