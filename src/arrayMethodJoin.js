8485b8c98ca53e5597fcfc8b1ea69f1eff0d26a9
Alexandr Somov <aleksandr.somov2011@gmail.com>
2021-08-04 23:56:58 +0300

    'Solution'

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let resStr = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        resStr += separator;
+        continue;
+      }
+      resStr += this[i];
+
+      if (i !== this.length - 1) {
+        resStr += separator;
+      }
+    }
+
+    return resStr;
   };
 }
 
