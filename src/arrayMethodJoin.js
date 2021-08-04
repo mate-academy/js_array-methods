4105766ee36ecd654588f14e5c10f87f6fb74e03
Illia Vovchenko <ilya.vovchenko@gmail.com>
2021-08-04 15:19:07 +0300

    remade first if block cause i was changing starting array

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,11 +8,9 @@ function applyCustomJoin() {
     let joinedString = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        this[i] = '';
+      if (this[i] !== null && this[i] !== undefined) {
+        joinedString += this[i];
       }
-
-      joinedString += this[i];
 
       if (i !== this.length - 1) {
         joinedString += separator;
