8405ed7728a5d90f1d1d8dd3fa572b7d18c229b1
Alex Hlotov <alexhlotov67@gmail.com>
2021-08-04 16:10:45 +0300

    Code was made shorter

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,20 +5,18 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    // write code here
     let joinedArray = '';
 
-    if (this[0] === null || this[0] === undefined) {
-      joinedArray = '';
-    } else {
-      joinedArray += this[0];
-    }
-
-    for (let i = 1; i < this.length; i++) {
+    for (let i = 0; i < this.length; i++) {
       if (this[i] === null || this[i] === undefined) {
         joinedArray += separator;
-      } else {
-        joinedArray += separator + '' + this[i];
+        continue;
+      }
+
+      joinedArray += this[i];
+
+      if (i !== this.length - 1) {
+        joinedArray += separator;
       }
     }
 
