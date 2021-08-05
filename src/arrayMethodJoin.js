6cc836f41ba8b460ad1a4ed9636a1b5da825fdd5
Volodymyr Khimiak <vv.himiak@gmail.com>
2021-08-05 17:23:45 +0300

    first commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,25 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let separator2 = separator;
+
+    if (typeof separator === 'undefined') {
+      separator2 = ',';
+    }
+
+    let testString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (typeof this[i] === 'object' || typeof this[i] === 'undefined') {
+        testString += `${separator2}`;
+      } else if (i === this.length - 1) {
+        testString += `${this[i]}`;
+      } else {
+        testString += `${this[i]}${separator2}`;
+      }
+    };
+
+    return testString;
   };
 }
 
