3df1f0f918ca59e43a79384153f907b2897036ee
Taras Holiuk <taras.holiuk@gmail.com"dsveagj 3rm>
2021-08-05 17:01:41 +0300

    initial commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,33 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinResultString = '';
+
+    if (this.length < 0) {
+      return joinResultString;
+    }
+
+    let workSeparator = separator;
+
+    if (workSeparator === null) {
+      workSeparator = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      let element = this[i];
+
+      if (element === null || element === undefined) {
+        element = '';
+      }
+
+      if (separator !== undefined && this.length > 1 && i !== this.length - 1) {
+        element += workSeparator;
+      }
+      joinResultString += element;
+    }
+
+    return joinResultString;
   };
 }
 
