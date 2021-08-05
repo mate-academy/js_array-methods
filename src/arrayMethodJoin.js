b0905859eb216cdf0c5f304619395158b2416c8c
lera-yaskevych <redrum.back@gmail.com>
2021-08-05 17:10:25 +0300

    basic version

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,28 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let string = '';
+    let symbol = separator;
+
+    if (symbol === undefined) {
+      symbol = ',';
+    } else if (symbol === null) {
+      symbol = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1) {
+        string += this[i];
+      } else {
+        if (this[i] === null || this[i] === undefined) {
+          string += '' + symbol;
+        } else {
+          string += this[i] + symbol;
+        }
+      }
+    }
+
+    return string;
   };
 }
 
