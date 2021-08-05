23299ec8df25937b580da50a69ed27c9c74a7ea1
sgw8 <ionart3737@gmail.com>
2021-08-05 18:02:29 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,23 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
-  };
+    let newSerarator = separator
+    let res = ''
+  
+    if(!separator) {
+      newSerarator = ','
+    }
+  
+    for(let i = 0; i < arr.length; i++) {
+      if(arr[i+1]){
+        res += arr[i] + newSerarator
+      } else {
+        res += arr[i] 
+      }
+     
+    }
+    return res
+  }
 }
 
 module.exports = applyCustomJoin;
