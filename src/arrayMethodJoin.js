09f6910bc82d3372fbc85f0900172f7bc43f494f
lera-yaskevych <redrum.back@gmail.com>
2021-08-05 17:43:17 +0300

    small reduce

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,25 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let string = '';
     let symbol = separator;
 
-    if (symbol === undefined) {
-      symbol = ',';
-    } else if (symbol === null) {
+    if (symbol === null) {
       symbol = 'null';
     }
 
     for (let i = 0; i < this.length; i++) {
-      if (i === this.length - 1) {
-        string += this[i];
-      } else {
+      if (i !== this.length - 1) {
         if (this[i] === null || this[i] === undefined) {
           string += '' + symbol;
         } else {
           string += this[i] + symbol;
         }
+      } else {
+        string += this[i];
       }
     }
 
