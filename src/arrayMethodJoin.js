4a9cbb9d29506f2f32b62f679b11de4cc1849346
Andrii Serhovetc <serhovetc88@gmail.com>
2021-08-06 13:51:30 +0300

    fix after review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,21 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let joinArray = '';
+    let resultString = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        joinArray += separator;
-        continue;
+      if (this[i] !== null && this[i] !== undefined) {
+        resultString += this[i];
       }
-      joinArray += this[i];
 
       if (i !== this.length - 1) {
-        joinArray += separator;
+        resultString += separator;
       }
     }
 
-    return joinArray;
+    return resultString;
   };
 }
 
