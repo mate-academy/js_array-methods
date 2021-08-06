fa2b7b2062b7c1847dbd29567f65ae670c7d694e
unknown <hado.roman.ihorovych@gmail.com>
2021-08-06 14:31:51 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,37 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let totalJoin = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] == null) {
+        totalJoin += separator;
+
+        continue;
+      }
+
+      if (i === this.length - 1) {
+        totalJoin += this[i];
+
+        return totalJoin;
+      }
+
+      if (separator === undefined) {
+        totalJoin += this[i] + ',';
+
+        continue;
+      }
+
+      if (separator == null) {
+        totalJoin += this[i] + 'null';
+
+        continue;
+      }
+
+      totalJoin += this[i] + separator;
+    }
+
+    return totalJoin;
   };
 }
 
