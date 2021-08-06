5770b8e7d93f5b3477909b782df66b7e81434e55
kristof22122 <kristf44@gmail.com>
2021-08-06 11:18:47 +0300

    changed for loop

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,17 +7,21 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     // write code here
     let joined = '';
+    let iterationCount = 0;
 
     if (this.length === 0) {
       return '';
     }
 
-    for (let i = 0; i < this.length - 1; i++) {
-      if (this[i] !== undefined && this[i] !== null) {
-        joined += `${this[i]}${separator}`;
-      } else {
-        joined += separator;
+    for (const item of this) {
+      if (iterationCount < this.length - 1) {
+        if (item !== undefined && item !== null) {
+          joined += `${item}${separator}`;
+        } else {
+          joined += separator;
+        }
       }
+      iterationCount++;
     }
 
     return joined + this[this.length - 1];
