665387b58409bbd67213a362fcb9787050b181e1
Andrii-Pinchuk <altkobalt4@gmail.com>
2021-08-06 08:47:13 +0300

    Method-join implemented

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,36 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    if (this.length <= 1) {
+      return this.toString();
+    }
+
+    let newStr = '';
+    let newSeparator = separator;
+
+    if (separator === undefined || separator === 'null') {
+      newSeparator = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i < this.length - 1
+        && this[i] !== null
+        && this[i] !== undefined) {
+        newStr += this[i];
+        newStr += newSeparator;
+      }
+
+      if (i < this.length - 1
+        && (this[i] === null || this[i] === undefined)) {
+        newStr += newSeparator;
+      }
+
+      if (i === this.length - 1) {
+        newStr += this[i];
+      }
+    }
+
+    return newStr;
   };
 }
 
