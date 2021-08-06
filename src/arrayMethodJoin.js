fe253e4eae4fc81e7b399a90f3a7bea6dedd6fcf
lera-yaskevych <redrum.back@gmail.com>
2021-08-06 11:04:23 +0300

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,15 +7,15 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let string = '';
 
-    for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
+    for (const element of this) {
+      if (element === null || element === undefined) {
         string += separator;
         continue;
       }
 
-      string += this[i];
+      string += element;
 
-      if (i !== this.length - 1) {
+      if (this.indexOf(element) !== this.length - 1) {
         string += separator;
       }
     }
