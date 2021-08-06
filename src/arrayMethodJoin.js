e7a44232650ee20c5588ec0a24b875bf55d07f83
Clon <clonvhs@gmail.com>
2021-08-06 16:00:12 +0300

    Solution
    v1.0

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,32 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedArray = '';
+
+    for (const item of this) {
+      if (item === null || item === undefined) {
+        joinedArray += '' + separator;
+      } else {
+        if (item !== this[this.length - 1]) {
+        }
+        joinedArray += item + '' + separator;
+      }
+    }
+
+    if (joinedArray[joinedArray.length - 1] === separator) {
+      joinedArray = joinedArray.slice(0, -1);
+    }
+
+    if (separator === null) {
+      joinedArray = joinedArray.slice(0, -4);
+    }
+
+    if (typeof separator === 'object' && separator !== null) {
+      joinedArray = joinedArray.slice(0, -15);
+    }
+
+    return joinedArray;
   };
 }
 
