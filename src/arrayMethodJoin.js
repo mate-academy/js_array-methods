4baef9d780706e2626243b6f023ae5118bb036f1
Mykhailo Ostapysko <mp4clips@gmail.com>
2021-08-06 15:07:03 +0300

    version 1.0

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,44 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    if (this.length === 0) {
+      return '';
+    }
+
+    let separatorCopy = separator;
+
+    if (separator === null) {
+      separatorCopy = 'null';
+    }
+
+    if (separator === undefined) {
+      separatorCopy = 'undefined';
+    }
+
+    if (typeof separatorCopy === 'object') {
+      separatorCopy = '[object Object]';
+    }
+
+    if (this.length > 1) {
+      let output = '';
+
+      for (let item of this) {
+        if (item === null || item === undefined) {
+          item = '';
+        }
+        output += item + separatorCopy;
+      }
+
+      if (separator === '') {
+        return output;
+      }
+
+      return output.slice(0, -separatorCopy.length);
+    }
+
+    return String(this[0]);
   };
 }
 
