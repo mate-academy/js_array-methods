aba9c51f8b93f56dbf6115d3195396a0170f49c5
Taras Holiuk <taras.holiuk@gmail.com"dsveagj 3rm>
2021-08-06 13:13:03 +0300

    bags from comments fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,27 +7,17 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let joinResultString = '';
 
-    if (this.length < 0) {
-      return joinResultString;
-    }
-
-    let workSeparator = separator;
-
-    if (workSeparator === null) {
-      workSeparator = 'null';
-    }
-
     for (let i = 0; i < this.length; i++) {
       let element = this[i];
 
       if (element === null || element === undefined) {
         element = '';
       }
-
-      if (separator !== undefined && this.length > 1 && i !== this.length - 1) {
-        element += workSeparator;
-      }
       joinResultString += element;
+
+      if (i !== this.length - 1) {
+        joinResultString += separator;
+      }
     }
 
     return joinResultString;
