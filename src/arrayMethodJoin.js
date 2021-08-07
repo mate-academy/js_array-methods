003d9c84686b6e30014db2b1fb2cc3ab52f6bbd3
Yevhen Pecherskyi <gekape4erskii@gmail.com>
2021-08-07 14:03:38 +0300

    join done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,25 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    const input = [...this];
+    const inputToString = `${input}`;
+    let result = '';
+
+    if (input.length === 0) {
+      return '';
+    } else if (input.length === 1 || typeof separator === 'undefined') {
+      return `${input}`;
+    }
+
+    for (let i = 0; i < inputToString.length; i++) {
+      if (inputToString[i] === ',') {
+        result += `${separator}`;
+      } else {
+        result += `${inputToString[i]}`;
+      }
+    }
+
+    return result;
   };
 }
 
