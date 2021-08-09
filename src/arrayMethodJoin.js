f1b60269c9610b0217db6345a7731132d372d645
Mykhailo Ostapysko <mp4clips@gmail.com>
2021-08-09 12:06:53 +0300

    version 4.0

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,22 +7,18 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let output = '';
 
-    for (const item of this) {
-      if (item !== null && item !== undefined) {
-        if (this.indexOf(item) !== this.length - 1) {
-          output += `${item}${separator}`;
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        if (i !== this.length - 1) {
+          output += `${this[i]}${separator}`;
         } else {
-          output += item;
+          output += this[i];
         }
       } else {
-        if (this.indexOf(item) !== this.length - 1) {
+        if (i !== this.length - 1) {
           output += `${separator}`;
         }
       }
-    }
-
-    if (separator === '') {
-      return output;
     }
 
     return output;
