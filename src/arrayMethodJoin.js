60ff1cef4365551ef2323e716fae498011b73ccb
Andrii-Kalaida <harleyarkhem@gmail.com>
2021-08-09 13:04:41 +0300

    finish task

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined) {
+        result = result + separator;
+        continue;
+      }
+
+      if (this[i] === null) {
+        result = result + separator;
+        continue;
+      }
+
+      result += this[i];
+
+      if (i !== this.length - 1) {
+        result += separator;
+      }
+    }
+
+    return result;
   };
 }
 
