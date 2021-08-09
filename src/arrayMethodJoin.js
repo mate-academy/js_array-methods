470899f5289c488bb95a2eab2605f6c82415ab87
Clon <clonvhs@gmail.com>
2021-08-09 13:02:27 +0300

    Solution after pull request review and looking few other solutions
    v1.1

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,35 +1,24 @@
 'use strict';
 
-/**
- * Implement method join
- */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let joinedArray = '';
+    let joinedArr = '';
 
-    for (const item of this) {
-      if (item === null || item === undefined) {
-        joinedArray += '' + separator;
-      } else {
-        if (item !== this[this.length - 1]) {
-        }
-        joinedArray += item + '' + separator;
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null && this[i] !== []) {
+        joinedArr += this[i];
+      }
+
+      if (i < this.length - 1) {
+        joinedArr += separator;
       }
     }
 
-    if (joinedArray[joinedArray.length - 1] === separator) {
-      joinedArray = joinedArray.slice(0, -1);
-    }
-
-    if (separator === null) {
-      joinedArray = joinedArray.slice(0, -4);
-    }
-
-    if (typeof separator === 'object' && separator !== null) {
-      joinedArray = joinedArray.slice(0, -15);
-    }
-
-    return joinedArray;
+    return joinedArr;
   };
 }
 
