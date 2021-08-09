8033a8881d9602fc62c1384a9a20874c604d596f
Vsevolod Maslak <vsevolodmaslak@gmail.com>
2021-08-09 09:46:57 +0300

    final

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,26 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joined = '';
+    let iterationCount = 0;
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (const item of this) {
+      if (iterationCount < this.length - 1) {
+        if (item !== undefined && item !== null) {
+          joined += `${item}${separator}`;
+        } else {
+          joined += separator;
+        }
+      }
+      iterationCount++;
+    }
+
+    return joined + this[this.length - 1];
   };
 }
 
