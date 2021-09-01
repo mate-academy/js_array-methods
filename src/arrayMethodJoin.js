6dd2af4f452a89ac084a0901346fffe43a7e1261
Katerina Bovdyr <katok5673966@gmail.com>
2021-09-01 13:19:16 +0300

    commit-1

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let retStr = '';
+
+    if (typeof separator === 'string'
+       || typeof separator === 'object'
+       || separator == null) {
+      for (let i = 0; i < this.length; i++) {
+        if (this[i] !== null && this[i] !== undefined) {
+          retStr += this[i];
+        }
+
+        if (i !== this.length - 1) {
+          retStr += separator;
+        }
+      }
+    };
+
+    return retStr;
   };
 }
 
