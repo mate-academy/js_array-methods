1460c078ea49bc6701be9714ceb19d14e5db8398
Serhii Dvorets <dvorets.sergey@gmail.com>
2021-09-06 13:20:25 +0300

    1

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const arr = [...this];
+    let result = '';
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === undefined || arr[i] === null) {
+        arr[i] = '';
+      }
+
+      if (i === arr.length - 1) {
+        result += arr[i].toString();
+      } else {
+        result += arr[i].toString();
+        result += separator;
+      }
+    }
+
+    return result;
   };
 }
 
