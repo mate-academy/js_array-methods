d5587da3b02b942576f9328e10abb290a17382b3
Rak Oleg <rakole86@gmail.com>
2021-09-15 14:35:54 +0300

    js_array-method-join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,40 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (separator === null) {
+      // eslint-disable-next-line no-param-reassign
+      separator = 'null';
+
+      for (const key of this) {
+        str += `${key}${separator}`;
+      }
+
+      return (str.substr(0, str.length - separator.length));
+    }
+
+    if (typeof (separator) === 'object') {
+      // eslint-disable-next-line no-param-reassign
+      separator = '[object Object]';
+
+      for (const key of this) {
+        str += `${key}${separator}`;
+      }
+
+      return str.substr(0, str.length - separator.length);
+    }
+
+    for (const key of this) {
+      str += `${[key]}${[separator]}`;
+    }
+
+    return str.substr(0, str.length - separator.length);
   };
 }
 
