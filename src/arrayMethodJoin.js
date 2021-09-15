324687169569be8cf987fd9c88b9e7f76a5575b8
bohdan_kochetov <bodja1208@gmail.com>
2021-09-15 14:19:23 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,79 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+
+    if (separator === undefined) {
+      for (let i = 0; i < this.length; i++) {
+        if (i === this.length - 1) {
+          str = str + this[i];
+          continue;
+        }
+
+        if (this[i] === null || this[i] === undefined) {
+          str = str + ',' + ',';
+          continue;
+        }
+        str = str + this[i] + ',';
+      }
+
+      return str;
+    }
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (separator === null) {
+      for (let i = 0; i < this.length; i++) {
+        if (i === this.length - 1) {
+          str = str + this[i];
+          continue;
+        }
+
+        if (this[i] === null || this[i] === undefined) {
+          str = str + ',' + null;
+          continue;
+        }
+        str = str + this[i] + null;
+      }
+
+      return str;
+    }
+
+    if (separator) {
+      for (let i = 0; i < this.length; i++) {
+        if (i === this.length - 1) {
+          str = str + this[i];
+          continue;
+        }
+
+        if (this[i] === null || this[i] === undefined) {
+          str = str + ',';
+          continue;
+        }
+        str = str + this[i] + separator;
+      }
+
+      return str;
+    }
+
+    if (separator === '') {
+      for (let i = 0; i < this.length; i++) {
+        if (i === this.length - 1) {
+          str = str + this[i];
+          continue;
+        }
+
+        if (this[i] === null || this[i] === undefined) {
+          str = str + ',';
+          continue;
+        }
+        str = str + this[i];
+      }
+
+      return str;
+    }
   };
 }
 
