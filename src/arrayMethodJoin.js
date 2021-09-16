ad18cb4ade8057ce2035c79625c07aa190c78144
bohdan_kochetov <bodja1208@gmail.com>
2021-09-16 13:34:41 +0300

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,78 +7,98 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     let str = '';
 
-    if (separator === undefined) {
-      for (let i = 0; i < this.length; i++) {
-        if (i === this.length - 1) {
-          str = str + this[i];
-          continue;
-        }
-
-        if (this[i] === null || this[i] === undefined) {
-          str = str + ',' + ',';
-          continue;
-        }
-        str = str + this[i] + ',';
-      }
-
-      return str;
-    }
-
     if (this.length === 0) {
       return '';
     }
 
-    if (separator === null) {
-      for (let i = 0; i < this.length; i++) {
-        if (i === this.length - 1) {
-          str = str + this[i];
-          continue;
-        }
-
-        if (this[i] === null || this[i] === undefined) {
-          str = str + ',' + null;
-          continue;
-        }
-        str = str + this[i] + null;
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === this[this.length - 1]) {
+        str += this[i];
+      } else if (this[i] === null || this[i] === undefined) {
+        str += ',';
+      } else if (separator === null) {
+        str += this[i] + 'null';
+      } else if (separator === undefined) {
+        str += this[i] + ',';
+      } else {
+        str += this[i] + separator;
       }
-
-      return str;
     }
 
-    if (separator) {
-      for (let i = 0; i < this.length; i++) {
-        if (i === this.length - 1) {
-          str = str + this[i];
-          continue;
-        }
-
-        if (this[i] === null || this[i] === undefined) {
-          str = str + ',';
-          continue;
-        }
-        str = str + this[i] + separator;
-      }
-
-      return str;
-    }
-
-    if (separator === '') {
-      for (let i = 0; i < this.length; i++) {
-        if (i === this.length - 1) {
-          str = str + this[i];
-          continue;
-        }
-
-        if (this[i] === null || this[i] === undefined) {
-          str = str + ',';
-          continue;
-        }
-        str = str + this[i];
-      }
-
-      return str;
-    }
+    return str;
   };
 }
 
 module.exports = applyCustomJoin;
+
+// if (separator === undefined) {
+//   for (let i = 0; i < this.length; i++) {
+//     if (i === this.length - 1) {
+//       str = str + this[i];
+//       continue;
+//     }
+
+//     if (this[i] === null || this[i] === undefined) {
+//       str = str + ',' + ',';
+//       continue;
+//     }
+//     str = str + this[i] + ',';
+//   }
+
+//   return str;
+// }
+
+// if (this.length === 0) {
+//   return '';
+// }
+
+// if (separator === null) {
+//   for (let i = 0; i < this.length; i++) {
+//     if (i === this.length - 1) {
+//       str = str + this[i];
+//       continue;
+//     }
+
+//     if (this[i] === null || this[i] === undefined) {
+//       str = str + ',' + null;
+//       continue;
+//     }
+//     str = str + this[i] + null;
+//   }
+
+//   return str;
+// }
+
+// if (separator) {
+//   for (let i = 0; i < this.length; i++) {
+//     if (i === this.length - 1) {
+//       str = str + this[i];
+//       continue;
+//     }
+
+//     if (this[i] === null || this[i] === undefined) {
+//       str = str + ',';
+//       continue;
+//     }
+//     str = str + this[i] + separator;
+//   }
+
+//   return str;
+// }
+
+// if (separator === '') {
+//   for (let i = 0; i < this.length; i++) {
+//     if (i === this.length - 1) {
+//       str = str + this[i];
+//       continue;
+//     }
+
+//     if (this[i] === null || this[i] === undefined) {
+//       str = str + ',';
+//       continue;
+//     }
+//     str = str + this[i];
+//   }
+
+//   return str;
+// }
