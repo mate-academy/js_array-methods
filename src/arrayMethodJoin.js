0a584b5c895f2662ddc10fa94a27c45cd220e22d
Anastasiia Afanasieva <anastasiiaafanasieva@Anastasiias-MacBook-Air.local>
2021-09-16 13:55:20 +0300

    solution js_array-method-join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,30 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let input = separator;
+    let result = '';
+
+    if (input === undefined) {
+      input = ',';
+    }
+
+    if (input === null) {
+      input = String(input);
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      if (i === this.length - 1) {
+        input = '';
+      }
+
+      result += this[i] + input;
+    }
+
+    return result;
   };
 }
 
