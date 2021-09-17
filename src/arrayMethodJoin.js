4603c7e15ea3adc616fab7c462b5126fc20ee364
Anastasiia Afanasieva <anastasiiaafanasieva@Anastasiias-MacBook-Air.local>
2021-09-17 13:17:31 +0300

    added default value for separator and changed logic

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,28 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let input = separator;
     let result = '';
-
-    if (input === undefined) {
-      input = ',';
-    }
 
     if (input === null) {
       input = String(input);
     }
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        this[i] = '';
-      }
-
       if (i === this.length - 1) {
         input = '';
       }
 
-      result += this[i] + input;
+      if (this[i] === null || this[i] === undefined) {
+        result += input;
+      } else {
+        result += this[i] + input;
+      }
     }
 
     return result;
