e47b696e91111cf2a35ba5ba629e2a8aaa209822
PresidentComanch <ap.dissembler@gmail.com>
2021-09-17 22:15:17 +0300

    --done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,29 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+    let sep;
+
+    if (this.length === 0) {
+      return str;
+    }
+
+    if (typeof separator === `undefined`) {
+      sep = ',';
+    } else {
+      sep = separator;
+    }
+
+    for (let el = 0; el < this.length - 1; el++) {
+      if (this[el] === null || this[el] === undefined) {
+        this[el] = '';
+      }
+      str = `${str}${this[el]}${sep}`;
+    }
+
+    str = str + this[this.length - 1];
+
+    return str;
   };
 }
 
