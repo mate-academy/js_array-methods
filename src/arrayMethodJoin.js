61fbedcd9c3dd84a71f92b33be8d70a33ac85c08
caelum29 <caelum29@gmail.com>
2021-09-20 22:40:32 +0300

    changed according to review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,14 +5,6 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    if (this.length === 1) {
-      if (this[0] === null || this[0] === undefined) {
-        return '';
-      }
-
-      return this[0] + '';
-    }
-
     let result = '';
 
     for (let index = 0; index < this.length; index++) {
