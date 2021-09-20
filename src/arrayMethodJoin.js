7d3d61739fb3d1be304211f5f0b47ac69c467b19
Loza Marusa <marusa.loza@gmail.com>
2021-09-20 11:48:16 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,30 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+    let separate = separator;
+
+    if (separate === undefined) {
+      separate = ',';
+    }
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        str += '';
+      } else {
+        str += this[i];
+      };
+
+      if ([i] < this.length - 1) {
+        str += separate;
+      }
+    };
+
+    return str;
   };
 }
 
