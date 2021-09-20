a91f2e0f9e38bf1d45d225876ceee5fc5345ea21
Roman Toritsyn <romantoritsyn@gmail.com>
2021-09-20 15:26:45 +0300

    done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        result = `${result}${separator}`;
+        continue;
+      }
+
+      if (i === this.length - 1) {
+        result = `${result}${this[i]}`;
+        continue;
+      }
+      result = `${result}${this[i]}${separator}`;
+    }
+
+    return result;
   };
 }
 
