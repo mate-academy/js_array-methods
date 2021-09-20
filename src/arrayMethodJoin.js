2f4cb07accf6fc69cdf26c7f25e10f1156e1e117
AVronskyi <vronskyi.a@gmail.com>
2021-09-20 16:50:19 +0300

    add default value for separator

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,12 +4,8 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    if (separator !== undefined) {
-      return this.toString().replace(/,/g, separator);
-    } else {
-      return this.toString();
-    };
+  [].__proto__.join2 = function(separator = ',') {
+    return this.toString().replace(/,/g, separator);
   };
 }
 
