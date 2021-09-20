e510df94b130fc0de8452a026c3095001ce9ceda
anastasiia <theyashkindzhus@gmail.com>
2021-09-20 12:08:26 +0300

    done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,28 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let str = '';
+    let normilizeSeparator = separator;
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (normilizeSeparator === undefined) {
+      normilizeSeparator = ',';
+    } else if (normilizeSeparator === null) {
+      normilizeSeparator = 'null';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        str += '' + normilizeSeparator;
+      } else {
+        str += this[i] + normilizeSeparator;
+      }
+    }
+
+    return str + this[this.length - 1];
   };
 }
 
