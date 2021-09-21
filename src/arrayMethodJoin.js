205c1df9eb0e9a88981f6938ce34f66fe9ae4ea3
Taras Ulko <internalpeace10@outlook.com>
2021-09-21 13:59:49 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,25 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let string = '';
+    const sep = separator === undefined
+      ? ','
+      : separator;
+
+    for (const item of this) {
+      if (item === this[this.length - 1]) {
+        string += `${item}`;
+        continue;
+      }
+
+      if (item === null || item === undefined) {
+        string += `${sep}`;
+      } else {
+        string += `${item}${sep}`;
+      }
+    }
+
+    return string;
   };
 }
 
