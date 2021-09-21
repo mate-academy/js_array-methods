d63df4594f22609d05a6205bf478a991d9065b30
Sviat Torba <torba.svyatoslav.fox@gmail.com>
2021-09-22 00:58:32 +0300

    fix and rework

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,24 +4,16 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    let beetw = separator;
-
-    if (separator === undefined) {
-      beetw = ',';
-    }
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === undefined || this[i] === null) {
-        this[i] = '';
+      if (this[i] !== undefined && this[i] !== null) {
+        result += this[i];
       }
 
-      if (i === this.length - 1) {
-        result += this[i];
-      } else {
-        result += this[i];
-        result += beetw;
+      if (i !== this.length - 1) {
+        result += separator;
       }
     }
 
