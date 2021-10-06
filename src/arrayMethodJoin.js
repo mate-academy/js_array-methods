32b830823f8e5bc0a3bcfcfa1de667bf33a82389
Jana Tretjak <tretiachechka@gmail.com>
2021-10-06 15:07:21 +0200

    ver01

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,39 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const result = new Array(0);
+    let result1 = '';
+
+    if (this.length === 0) {
+      return '';
+    } else {
+      for (let i = 0; i < this.length - 1; i++) {
+        if (this === []) {
+          return '';
+        } else if (separator === null) {
+          result[i] = this[i].toString() + 'null';
+        } else if (separator === undefined) {
+          result[i] = this[i].toString() + ',';
+        } else if (this[i] === null) {
+          this[i] = '';
+          result[i] = this[i].toString() + separator.toString();
+        } else if (this[i] === undefined) {
+          this[i] = '';
+          result[i] = this[i].toString() + separator.toString();
+        } else {
+          result[i] = this[i].toString() + separator.toString();
+        }
+      }
+
+      result[result.length] = this[this.length - 1];
+
+      for (const elem of result) {
+        result1 += elem.toString();
+      }
+
+      return result1;
+    }
   };
 }
 
