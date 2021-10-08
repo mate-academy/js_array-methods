ec6d1e4daa38a78ae8c29deec5264b9d3f77b757
Denys Stryzhalko <samu3lkacis@gmail.com>
2021-10-09 00:58:58 +0300

    method join v1

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] == null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      if (i !== this.length - 1) {
+        result += this[i] + `${separator}`;
+      } else {
+        result += `${this[i]}`;
+      }
+    }
+
+    return result;
   };
 }
 
