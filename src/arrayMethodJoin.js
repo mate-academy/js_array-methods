07c4adf29eb1e55d318648a2e6cfb16e46b1662e
Jana Tretjak <tretiachechka@gmail.com>
2021-10-08 11:58:47 +0200

    ver02

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,39 +5,28 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    const result = new Array(0);
-    let result1 = '';
-
-    if (this.length === 0) {
+    if (!this.length) {
       return '';
-    } else {
-      for (let i = 0; i < this.length - 1; i++) {
-        if (this === []) {
-          return '';
-        } else if (separator === null) {
-          result[i] = this[i].toString() + 'null';
-        } else if (separator === undefined) {
-          result[i] = this[i].toString() + ',';
-        } else if (this[i] === null) {
-          this[i] = '';
-          result[i] = this[i].toString() + separator.toString();
-        } else if (this[i] === undefined) {
-          this[i] = '';
-          result[i] = this[i].toString() + separator.toString();
-        } else {
-          result[i] = this[i].toString() + separator.toString();
-        }
-      }
-
-      result[result.length] = this[this.length - 1];
-
-      for (const elem of result) {
-        result1 += elem.toString();
-      }
-
-      return result1;
     }
+
+    let result = String(this[0]);
+
+    if (this[0] === null || this[0] === undefined) {
+      result = '';
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if (separator === undefined) {
+        result += ',' + String(this[i]);
+      } else if (this[i] === null || this[i] === undefined) {
+        result += (String(separator) + '');
+      } else {
+        result += (String(separator) + String(this[i]));
+      }
+    }
+
+    return result;
   };
-}
+};
 
 module.exports = applyCustomJoin;
