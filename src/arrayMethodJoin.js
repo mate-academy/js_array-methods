4f4123a0c90dcf8d697e321ea2808a644c047218
Misha Rosa <misharosa.lviv@gmail.com>
2021-10-09 03:18:52 +0300

    join was created

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    // eslint-disable-next-line no-unused-vars
+    let word = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        word += `${separator}`;
+      } else {
+        word += this[i] + `${separator}`;
+      }
+    }
+
+    return word + `${this[this.length - 1]}`;
   };
 }
 
