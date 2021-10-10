9163629832495d7b17b3ad4e486f25eebc83e426
Taras Lynets <linetstg@gmail.com>
2021-10-10 19:13:59 +0300

    solutions

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,22 +5,22 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let result = '';
+    let string = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] === null || this[i] === undefined) {
-        result += `${separator}`;
+        string += `${separator}`;
         continue;
       }
 
       if (i === this.length - 1) {
-        result += `${this[i]}`;
+        string += `${this[i]}`;
       } else {
-        result += `${this[i]}${separator}`;
+        string += `${this[i]}${separator}`;
       }
     }
 
-    return result;
+    return string;
   };
 }
 
