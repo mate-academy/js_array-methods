2954bad231f55790510b3ebb8436ddd87a6c45ef
Max <gorodtira51@gmail.com>
2021-10-10 20:57:04 +0300

    done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let x = '';
+
+    for (let i = 0; i < this.length; i++) {
+
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      };
+
+      x += this[i];
+
+      if (i !== this.length - 1) {
+        x += separator;
+      }
+    }
+
+    return x;
   };
 }
 
