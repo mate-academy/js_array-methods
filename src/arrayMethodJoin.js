e9c4b269842a7573dcfe958e96f7ae4c10b26631
Taras Kozak <taraskozakmail@gmail.com>
2021-10-11 10:26:18 +0300

    refactored

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,27 +5,22 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    const incomingData = [...this];
+    let result = '';
 
-    if (incomingData.length > 0) {
-      let result = '';
+    for (let i = 0; i < this.length; i++) {
+      let element = this[i];
 
-      for (let i = 0; i < incomingData.length; i++) {
-        let element = incomingData[i];
-
-        if (element === null || element === undefined) {
-          element = '';
-        }
-      
-        if (i === incomingData.length - 1) {
-          result += element;
-          return result;
-        }
-
-        result += `${element}`+ separator;
+      if (element === null || element === undefined) {
+        element = '';
       }
+
+      if (i === this.length - 1) {
+        result += element;
+        return result;
+      }
+      result += `${element}` + separator;
     }
-    return '';
+    return result;
   };
 }
 
