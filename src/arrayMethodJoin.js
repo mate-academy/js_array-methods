274ee698fc18dc67db9d6934095289fc775a6342
Irka Shemchuk <ilysbbk89@yahoo.com>
2021-10-11 20:06:27 +0300

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,12 +8,7 @@ function applyCustomJoin() {
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (typeof this[i] === typeof null) {
-        result += `${separator}`;
-        continue;
-      }
-
-      if (typeof this[i] === typeof undefined) {
+      if (this[i] === null || this[i] === undefined) {
         result += `${separator}`;
         continue;
       }
