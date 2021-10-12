944cde138e92536cd97e7826b682f1b489d0ad9f
Dmytro Brushnivskyi <dimalvivatc@gmail.com>
2021-10-12 20:12:33 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,43 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let newString = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (separator === null) {
+      // eslint-disable-next-line no-param-reassign
+      separator = 'null';
+
+      for (const el of this) {
+        newString += this[el];
+        newString += separator;
+      }
+
+      return newString.substr(0, newString.length - separator.length);
+    }
+
+    if (typeof separator === 'object') {
+      // eslint-disable-next-line no-param-reassign
+      separator = '[object Object]';
+
+      for (const el of this) {
+        newString += this[el];
+        newString += separator;
+      }
+
+      return newString.substr(0, newString.length - separator.length);
+    }
+
+    for (let el = 0; el < this.length; el++) {
+      newString += [this[el]];
+      newString += [separator];
+    }
+
+    return newString.substr(0, newString.length - separator.length);
   };
 }
 
