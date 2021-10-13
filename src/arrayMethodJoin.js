c1af9dc6444f3d4ec3c301639de5631af66fdcb2
Dmytro Hubych <dh.hubych@gmail.com>
2021-10-13 13:46:57 +0300

    fixed2

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,20 +6,19 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let string = '';
-    const input = [...this];
 
-    if (input.length === 0) {
+    if (this.length === 0) {
       return '';
     }
 
-    for (let i = 0; i < input.length; i++) {
-      if (input[i] === null || input[i] === undefined) {
-        input[i] = '';
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
       }
 
-      string += input[i];
+      string += this[i];
 
-      if (i === input.length - 1) {
+      if (i === this.length - 1) {
         break;
       }
 
