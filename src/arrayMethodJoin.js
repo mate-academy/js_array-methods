089264a253bda8f5c0d73a4708405ef416bd00a6
Dmytro Brushnivskyi <dimalvivatc@gmail.com>
2021-10-18 17:55:29 +0300

    Code is refactored, only with one for loop and two if conditions

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,39 +6,21 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let newString = '';
+    const lastItem = this[this.length - 1]; // last item of array
 
-    if (this.length === 0 || this.length === 1) {
-      for (const el of this) {
-        newString += el;
+    for (let i = 0; i < this.length; i++) {
+      // if current item is not equal to the values
+      if ((this[i] !== undefined) && (this[i] !== null)) {
+        // add this item to the string
+        newString += this[i];
       }
 
-      /* if length is zero return empty str, if length is one
-      return str without a separator
-      */
-      return '' || newString;
+      // if current item is not last in the array
+      if (this[i] !== lastItem) {
+        // add separator to the string
+        newString += separator;
+      }
     }
-
-    for (let i = 0; i < this.length - 1; i++) {
-      /* eslint valid-typeof: ["error", { "requireStringLiterals": true}]
-      */
-      if ((this[i] === undefined || this[i] === null)) {
-        this[i] = '';
-      }
-
-      if (this[this.length - 1] === undefined) {
-        this[this.length - 1] = '';
-      }
-
-      if (separator === null) {
-        // eslint-disable-next-line no-param-reassign
-        separator = 'null';
-      }
-
-      newString += this[i] + separator;
-    }
-
-    /* remove separator after the last element in the string */
-    newString += this[this.length - 1];
 
     return newString;
   };
