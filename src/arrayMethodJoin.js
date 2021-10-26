6236511e3613274ffa0df62c774408b79cfe38b0
Anna Tovkailo <babylike24@gmail.com>
2021-10-27 00:24:18 +0300

    Fixed after code review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,54 +5,18 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    // empty array
-    if (this.length === 0) {
-      return '';
-    }
+    let concatString = '';
 
-    // array of one
-    if (this.length === 1) {
-      return ('' + this[0]);
-    }
-
-    // empty separator or null or empty string
-    let thisSeparator;
-
-    if (separator === null) {
-      thisSeparator = 'null';
-    } else if (separator === '') {
-      thisSeparator = '';
-    } else if (!separator) {
-      thisSeparator = ',';
-    } else {
-      thisSeparator = separator;
-    }
-
-    // first element undefined or null
-    let firstItem;
-
-    if (this[0] === null) {
-      firstItem = '';
-    } else if (typeof this[0] === 'undefined') {
-      firstItem = '';
-    } else {
-      firstItem = this[0];
-    }
-
-    let concatString = firstItem;
-
-    for (let i = 1; i < this.length; i++) {
-      let item;
-
-      // element undefined or null
-      if (this[i] === null) {
-        item = '';
-      } else if (typeof this[i] === 'undefined') {
-        item = '';
+    for (let i = 0; i < this.length; i++) {
+      if ((this[i] !== null) && (typeof this[i] !== 'undefined')) {
+        concatString += this[i];
       } else {
-        item = this[i];
+        concatString += '';
       }
-      concatString = concatString + thisSeparator + item;
+
+      if (i < this.length - 1) {
+        concatString += separator;
+      }
     }
 
     return concatString;
