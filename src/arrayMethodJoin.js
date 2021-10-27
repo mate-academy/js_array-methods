90d792c39acf9ca8514c39298cf8e7a596b180a6
Illia Ostrovskiy <aleanrenuar@gmail.com>
2021-10-27 17:29:37 +0300

    Initial commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,26 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length) {
+      let joinedString = '';
+
+      for (let element = 0; element < this.length; element++) {
+        if (this[element] == null) {
+          this[element] = '';
+        }
+
+        if (element + 1 === this.length) {
+          joinedString += this[element];
+
+          return joinedString;
+        }
+
+        joinedString += this[element] + `${separator}`;
+      }
+    }
+
+    return '';
   };
 }
 
