d411d048eca086bb92c18f882f4c5b5cfd3d8b71
Vasiliy <voronvasia@gmail.com>
2021-10-31 20:26:43 +0200

    Join method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,83 @@
 'use strict';
 
-/**
- * Implement method join
- */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let sep = separator;
+    let string = '';
+    let count = 0;
+
+    if (this.length === 1) {
+      string = this[0].toString();
+
+      return string;
+    }
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (sep === ',') {
+      for (let i = 0; i < this.length; i++) {
+        count++;
+
+        if (this[i] === null || this[i] === undefined) {
+          this[i] = '';
+        }
+
+        string += this[i];
+
+        if (count === this.length) {
+          break;
+        }
+
+        string += sep;
+      }
+
+      return string;
+    }
+
+    if (sep === null) {
+      sep = 'null';
+    }
+
+    if (sep === '') {
+      for (let i = 0; i < this.length; i++) {
+        string += this[i];
+      }
+
+      return string;
+    }
+
+    if (!sep) {
+      count = 0;
+
+      for (let i = 0; i < this.length; i++) {
+        count++;
+        string += this[i];
+
+        if (count === this.length) {
+          break;
+        }
+
+        string += ',';
+      }
+    }
+
+    if (typeof sep === 'string' || typeof sep === 'object') {
+      count = 0;
+
+      for (let i = 0; i < this.length; i++) {
+        count++;
+        string += this[i];
+
+        if (count === this.length) {
+          break;
+        }
+        string += sep;
+      }
+    }
+
+    return string;
   };
 }
 
