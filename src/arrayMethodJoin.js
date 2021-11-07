724fcc60fbfcf417f675c63d418480bef143b529
Vitaly <vitaly.pasechnik.11@gmail.com>
2021-11-07 03:35:54 +0200

    last saved

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,33 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let separators = separator;
+    const arr = [...this];
+    let str = '';
+
+    if (separators === null) {
+      separators = 'null';
+    }
+
+    if (arr.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === null || arr[i] === undefined) {
+        str += separators;
+        continue;
+      }
+
+      if (i === arr.length - 1) {
+        str += arr[i];
+        continue;
+      }
+      str += arr[i] + separators;
+    }
+
+    return str;
   };
 }
 
