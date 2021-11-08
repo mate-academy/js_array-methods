1b213bfd915c2230141bd8b829f04532ec5a5bcf
Ievgen Volkov <neoodessa86@gmail.com>
2021-11-08 21:50:14 +0200

    Solution 2

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -12,7 +12,7 @@ function applyCustomJoin() {
         string += this[i];
       }
 
-      if (i !== this.length - 1) {
+      if (i < this.length - 1) {
         string += separator;
       }
     }
