fcb9a3048ac00a7543be1415594ddd3e115017f7
Dmitriy Hrytsak <hrytsakmail@gmail.com>
2021-11-09 17:01:27 +0200

    Create custom join method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,21 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const splitter = typeof separator === 'string' ? separator : separator + '';
+    let joined = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const element = this[i] === null || this[i] === undefined ? '' : this[i];
+
+      joined += element;
+
+      if (i !== this.length - 1) {
+        joined += splitter;
+      }
+    }
+
+    return joined;
   };
 }
 
