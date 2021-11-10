29872541fbf6641e6f2be5674d13d4bb4ab6f800
Vitaliy Voytovich <voitovichatque@gmail.com>
2021-11-10 12:26:37 +0200

    Join solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,31 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedString = '';
+    let workSep = separator;
+
+    if (workSep === null) {
+      workSep = 'null';
+    }
+
+    if (this.length) {
+      for (let i = 0; i < this.length; i++) {
+        if (this[i] === null || this[i] === undefined) {
+          this[i] = '';
+        }
+
+        if (i === this.length - 1) {
+          return joinedString + this[i];
+        }
+
+        joinedString += this[i] + workSep;
+      }
+
+      return joinedString;
+    }
+
+    return '';
   };
 }
 
