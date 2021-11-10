882f5affdb5d16b2b0c594a990758c93f009cadf
WorkAccCom <work.acc.com@gmail.com>
2021-11-10 22:05:22 +0200

    First solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,30 @@
 /**
  * Implement method join
  */
+
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length === 0) {
+      return '';
+    }
+
+    let resultStr;
+
+    if (this[0] !== null) {
+      resultStr = `${this[0]}`;
+    } else {
+      resultStr = '';
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        resultStr += separator;
+      } else {
+        resultStr += separator + `${this[i]}`;
+      }
+    }
+
+    return resultStr;
   };
 }
 
