82121e857163358aab9f8157ba5a2c962aee0c38
Mariia Bila <freiheit.maria@gmail.com>
2021-11-10 20:11:13 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,35 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let devider = separator;
+    let string = '';
+
+    switch (devider) {
+      case undefined:
+        devider = ',';
+        break;
+      case null:
+        devider = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      switch (this[i]) {
+        case null:
+          string += '';
+          break;
+        case undefined:
+          string += '';
+          break;
+        default:
+          string += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        string += devider;
+      }
+    }
+
+    return string;
   };
 }
 
