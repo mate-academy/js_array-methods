016ba22c340e5e1e04ab4269ea1900a333e078db
Ivan Stanko <ivan_stanko@ukr.net>
2021-11-11 08:29:21 +0200

    add method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const char = this[i];
+      const isValid = char !== undefined && char !== null;
+
+      if (isValid) {
+        result += char;
+      }
+
+      if (i !== this.length - 1) {
+        result += separator;
+      }
+    }
+
+    return result;
   };
 }
 
