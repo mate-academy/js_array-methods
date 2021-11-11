bd7c56b400bb323721abd673a9123f4c94d604ce
Artem Skachkov <erocezz@gmail.com>
2021-11-11 23:33:50 +0200

    Join method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinMethod = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        joinMethod += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        joinMethod += separator;
+      }
+    }
+
+    return joinMethod;
   };
 }
 
