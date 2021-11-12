c65988b8cbda563c30a9147577a2193b9c9cf51e
Artem Skachkov <92296409+skachkov23@users.noreply.github.com>
2021-11-12 16:31:42 +0200

    Update src/arrayMethodJoin.js
    Co-authored-by: Alona Ionova <68023426+aionova@users.noreply.github.com>

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,7 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let joinMethod = '';
+    let joinedString = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
