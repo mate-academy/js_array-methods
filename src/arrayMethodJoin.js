fff32823a1a644599a6b099ce5b4b788cd7765ce
Valeriia Dlugash <lera.dlugash@gmail.com>
2021-11-14 00:51:34 +0200

    Solution with 14 tests

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,37 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    if (this.length === 1) {
+      return this.toString();
+    }
+
+    let newStr = '';
+
+    for (let n = 0; n < this.length; n++) {
+      if (this[n] === '') {
+        newStr += '';
+      } else if (this[n] === 'null') {
+        newStr += 'null';
+      } else if (this[n] === null || this[n] === undefined) {
+        newStr += '';
+      } else {
+        const value = JSON.stringify(this[n]);
+
+        newStr += value;
+      }
+
+      if (n < this.length - 1) {
+        if (separator === undefined) {
+          newStr += ',';
+        } else if (separator === '') {
+          newStr += '';
+        } else {
+          newStr += separator;
+        }
+      }
+    }
+
+    return newStr;
   };
 }
 
