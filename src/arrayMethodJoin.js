80806049753127346ea1afe1053a25d677e3b12e
Vitalii Lytvyn <vitalii.lytvyn01@gmail.com>
2021-11-14 18:37:01 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,27 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separationString = ',') {
+    let joinedString = '';
+    const separator = String(separationString);
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1) {
+        if (this[i] === null || this[i] === undefined) {
+          break;
+        }
+        joinedString += this[i];
+        break;
+      }
+
+      if (this[i] !== null && this[i] !== undefined) {
+        joinedString += this[i] + separator;
+      } else {
+        joinedString += separator;
+      }
+    }
+
+    return joinedString;
   };
 }
 
