3e112027e5f5f4517860eb38714d2b498378c881
nadiaH <nadiahrb79@gmailcom>
2021-11-15 11:56:26 +0200

    fix separator and if

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,26 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    let s = separator;
-
-    if (separator === null) {
-      s = 'null';
-    }
-
-    if (separator === undefined) {
-      s = ',';
-    }
+  [].__proto__.join2 = function(separator = ',') {
+    // const s = separator;
 
     let string = '';
 
     for (let i = 0; i < this.length; i++) {
-      string += this[i] === null || this[i] === undefined
-        ? ''
-        : this[i];
+      if (this[i] === null || this[i] === undefined) {
+        string += '';
+      } else {
+        string += this[i];
+      }
 
       if (i !== this.length - 1) {
-        string += s;
+        string += separator;
       }
     }
 
