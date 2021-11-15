5e4b3bf4e18bc694d9967d24e480596e8ffc87a5
lanebx <havchikos@gmail.com>
2021-11-15 06:37:46 +0200

    changed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,11 +5,10 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    const newArr = [...this];
     let newStr = '';
 
-    for (const i of newArr) {
-      if (i === newArr[newArr.length - 1]) {
+    for (const i of this) {
+      if (i === this[this.length - 1]) {
         if (i != null) {
           newStr += String(i);
         }
