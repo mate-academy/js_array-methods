7ace8debfe4068de7e436cde0dcdbe199deb8616
Valeriia Dlugash <lera.dlugash@gmail.com>
2021-11-15 19:35:30 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,41 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    if (this.length === 1) {
-      return this.toString();
-    }
+  [].__proto__.join2 = function(separator = ',') {
+    let res = '';
 
-    let newStr = '';
-
-    for (let n = 0; n < this.length; n++) {
-      if (this[n] === '') {
-        newStr += '';
-      } else if (this[n] === 'null') {
-        newStr += 'null';
-      } else if (Number.isNaN(this[n])) {
-        newStr += 'NaN';
-      } else if (this[n] === null || this[n] === undefined) {
-        newStr += '';
-      } else {
-        const value = JSON.stringify(this[n]);
-
-        newStr += value;
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        res += this[i];
       }
 
-      if (n < this.length - 1) {
-        if (separator === undefined) {
-          newStr += ',';
-        } else if (separator === '') {
-          newStr += '';
-        } else {
-          newStr += separator;
-        }
+      if (i !== this.length - 1) {
+        res += separator;
       }
     }
 
-    return newStr;
+    return res;
   };
 }
-
 module.exports = applyCustomJoin;
