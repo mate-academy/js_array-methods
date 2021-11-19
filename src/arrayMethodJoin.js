5381b9d01ffe48dc12087bdcfde9c2656bc9af16
Pavlo Khashchevskyi <PikiMapo92@gmail.com>
2021-11-19 14:39:42 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,35 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    let newSaparator = separator;
+    const newArr = this;
+    const notAdd = [null, undefined];
+
+    if (newSaparator === null) {
+      newSaparator = 'null';
+    }
+
+    for (let i = 0; i < newArr.length; i++) {
+      const cell = newArr[i];
+
+      if (notAdd.includes(cell)) {
+        if (i + 1 !== newArr.length) {
+          result += newSaparator;
+        }
+        continue;
+      }
+
+      if (i + 1 === newArr.length) {
+        result += cell;
+        continue;
+      }
+
+      result += cell + newSaparator;
+    }
+
+    return result;
   };
 }
 
