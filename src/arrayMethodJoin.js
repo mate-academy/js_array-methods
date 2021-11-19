5daa105848d40c743a00569036e95e163b04c776
xpromt <xpromt@gmail.com>
2021-11-19 12:05:38 +0200

    Update arrayMethodJoin.js

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
-  };
+     let joinedString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        joinedString += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        joinedString += separator;
+      }
+    }
+
+    return joinedString;
 }
 
 module.exports = applyCustomJoin;
