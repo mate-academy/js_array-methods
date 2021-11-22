5db3855e7fe11b6e8e50e751c6665e1dce2003b6
Danylo Onishchuk <78209611+danylo-onishchuk@users.noreply.github.com>
2021-11-22 14:50:52 +0200

    Update arrayMethodJoin.js

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,25 +6,6 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    let sep = separator;
-
-    if (sep === null) {
-      sep = 'null';
-    }
-
-    for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        this[i] = '';
-      }
-
-      if (i === this.length - 1) {
-        result += this[i];
-      } else {
-        result += this[i] + sep;
-      }
-    }
-
-    return result;
   };
 }
 
