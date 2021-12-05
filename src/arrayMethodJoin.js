92665575c82519781450379d2b69d5b2d4033396
Ihor Barban <barbanihor@gmial.com>
2021-12-05 14:01:11 +0200

    Join method done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let strJoin = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        strJoin += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        strJoin += separator;
+      }
+    }
+
+    return strJoin;
   };
 }
-
 module.exports = applyCustomJoin;
