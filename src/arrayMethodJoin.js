7ed2019753e5d14e1f96892fd1cb38f4b35f1272
Katy Mozharovska <katy.mozharovska@gmail.com>
2021-12-09 13:31:06 +0200

    Fixed after review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,23 +4,19 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
     if (!this.length) {
       return '';
     }
 
-    const divider = (separator
-      || separator === ''
-      || separator === null) ? separator : ',';
-
     let string = (this[0] === null) ? '' : `${this[0]}`;
 
     for (let i = 1; i < this.length; i++) {
       if (this[i] === null || this[i] === undefined) {
-        string += `${divider}`;
+        string += `${separator}`;
       } else {
-        string += `${divider}${this[i]}`;
+        string += `${separator}${this[i]}`;
       }
     }
 
