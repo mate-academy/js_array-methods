f48ed675a1381057dadd626a044395d207f807c4
Andrey Aleksandrov <a.s.aleksandrov89@gmail.com>
2021-12-13 19:15:23 +0100

    Create join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,34 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let joined = '';
+    let separ = ',';
+
+    if (!(separator === undefined)) {
+      separ = separator + '';
+    }
+
+    if (!(this[0] === null || this[0] === undefined)) {
+      joined = this[0] + '';
+    }
+
+    if (this.length === 1) {
+      return joined;
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      let nextItem = '';
+
+      if (this[i] === null || this[i] === undefined) {
+        nextItem = '';
+      } else {
+        nextItem = this[i] + '';
+      }
+
+      joined = joined + separ + nextItem;
+    }
+
+    return joined;
   };
 }
 
