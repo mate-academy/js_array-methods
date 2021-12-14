481943b8accf692b55e81adfed2373bd6774dbb2
KHTRE <KHTREE@gmail.com>
2021-12-14 15:45:53 +0200

    works

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length < 1) {
+      return '';
+    }
+
+    let result = this[0] + '';
+
+    if (this[0] === null) {
+      result = '';
+    }
+
+    const joiner = separator + '';
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        result += joiner;
+      } else {
+        result += joiner + this[i];
+      }
+    }
+
+    return result;
   };
 }
 
