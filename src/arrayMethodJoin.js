e64c155f2b23222b8607b970efd3ac338d3ecf8e
Maksym Leshok <max6leshok@gmail.com>
2021-12-14 15:43:01 +0200

    Done in 15 rows

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,39 +7,13 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let str = '';
 
-    if (this.length === 0) {
-      return '';
-    }
-
-    if (separator !== null) {
-      for (let i = 0; i < this.length - 1; i++) {
-        if (this[i] === undefined || this[i] === null) {
-          str += '' + separator;
-        } else {
-          str += this[i] + separator;
-        }
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        str += this[i];
       }
 
-      if (this[this.length - 1] === undefined
-        || this[this.length - 1] === null) {
-        str += '';
-      } else {
-        str += this[this.length - 1];
-      }
-    } else {
-      for (let j = 0; j < this.length - 1; j++) {
-        if (this[j] === undefined || this[j] === null) {
-          str += '' + 'null';
-        } else {
-          str += this[j] + 'null';
-        }
-      }
-
-      if (this[this.length - 1] === undefined
-        || this[this.length - 1] === null) {
-        str += '';
-      } else {
-        str += this[this.length - 1];
+      if (i < this.length - 1) {
+        str += separator;
       }
     }
 
