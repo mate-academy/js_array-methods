08b8278deb6cd45a92f542ef46cc2267fce59c7f
Niki <tes.kolia@gmail.com>
2021-12-15 19:23:24 +0200

    done without using nested if statements

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,33 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let str = '';
+
+    for (let i = 0; i < this.length; i++) {
+      switch (this[i]) {
+        case (this.length - 1 && ''):
+          str += '';
+          break;
+        case this.length - 1:
+          str += this[i];
+          break;
+
+        default:
+          if (separator === null) {
+            str += this[i] + 'null';
+          } else if (this[i] === null || this[i] === undefined) {
+            str += separator;
+          } else if (this.length === 1) {
+            str += this[i];
+          } else {
+            str += this[i] + separator;
+          }
+      }
+    }
+
+    return str;
   };
 }
 
