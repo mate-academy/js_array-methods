2a22eac958b66cc0e7066e4026392140d5ae958d
Volodymyr Oleksiienko <oleksienkovladimir50@gmail.com>
2021-12-16 17:29:22 +0200

    done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,29 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+    let innerSeparator = separator;
+
+    if (separator === undefined) {
+      innerSeparator = ',';
+    }
+
+    for (const key of this) {
+      if (key === undefined || key === null) {
+        if (this.indexOf(key) !== (this.length) - 1) {
+          str += innerSeparator;
+        }
+        continue;
+      } else {
+        str += key;
+
+        if (this.indexOf(key) !== (this.length) - 1) {
+          str += innerSeparator;
+        }
+      }
+    }
+
+    return str;
   };
 }
 
