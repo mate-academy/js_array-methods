f48ae04ba09d3da5d0a4092b812603e1fb645371
Serhii Haidukov <sergeyglazgo@gmail.com>
2021-12-16 18:29:23 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,33 @@
 'use strict';
+/* eslint-disable no-console */
 
 /**
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let x = 0; x < this.length; x++) {
+      if (x === this.length - 1) {
+        if (this[x] === undefined) {
+          result += '';
+        } else {
+          result += `${this[x]}`;
+        }
+
+        return result;
+      }
+
+      if (this[x] === undefined || this[x] === null) {
+        result += '' + separator;
+        continue;
+      }
+
+      result += `${this[x]}` + separator;
+    }
+
+    return result;
   };
 }
 
