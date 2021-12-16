b07c7d345f0b3b39b5d594918103d1e39cec729f
Taras Zherebetkiy <tarasj76@gmail.com>
2021-12-16 16:51:33 +0200

    join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+    const newSeperator = '' + separator;
+
+    for (let i = 0; i < this.length; i++) {
+      let item = this[i];
+
+      if (item === undefined) {
+        item = '';
+      }
+
+      if (item === null) {
+        item = '';
+      }
+
+      if (i === this.length - 1) {
+        str += item;
+        break;
+      }
+      str += item + newSeperator;
+    }
+
+    return str;
   };
 }
 
