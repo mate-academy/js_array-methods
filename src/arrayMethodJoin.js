29a8c83c8e5e79d28e76abade1a573dde43753a7
Yulia Koshkina <julyasav4uk@gmail.com>
2021-12-16 17:44:33 +0200

    first commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,38 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let join = separator;
+
+    if (separator === undefined) {
+      join = ',';
+    }
+
+    if (separator === null) {
+      join = 'null';
+    }
+
+    if (this.length === 0) {
+      result = '';
+    }
+
+    if (this.length === 1) {
+      return this[0].toString();
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      if (i !== this.length - 1) {
+        result += this[i] + join;
+      } else {
+        result += this[i];
+      }
+    }
+
+    return result.toString();
   };
 }
 
