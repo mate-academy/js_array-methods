af0e28a4915f21e846e25403d7893344cbbcb68e
SavelovSerhii <savelov12111@gmail.com>
2021-12-16 12:47:03 +0200

    First version

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let resultStr = '';
+    const notAllowed = [null, undefined];
+
+    if (!notAllowed.includes(this[0])) {
+      resultStr = `${this[0]}`;
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if (!notAllowed.includes(this[i])) {
+        resultStr += `${separator}` + `${this[i]}`;
+      } else {
+        resultStr += `${separator}`;
+      }
+    }
+
+    return resultStr;
   };
 }
 
