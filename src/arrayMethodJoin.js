85086f815f846256625e9abc0875b2a58976ae92
Oleksandr Ostrovskyi <o.ostrovskiy62@gmail.com>
2021-12-18 00:32:56 +0200

    join solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,26 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        str += `${this[i]}${separator}`;
+      } else {
+        str += `${separator}`;
+      }
+    }
+
+    if (this[this.length - 1] !== undefined && this[this.length - 1] !== null) {
+      str += `${this[this.length - 1]}`;
+    }
+
+    return str;
   };
 }
 
