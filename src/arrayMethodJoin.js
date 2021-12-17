6c8835de764d4754a0a0a079bde0298ae8e11518
Volodymyr Oleksiienko <oleksienkovladimir50@gmail.com>
2021-12-17 12:42:06 +0200

    version 2

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,21 +8,6 @@ function applyCustomJoin() {
     let str = '';
     const innerSeparator = separator;
 
-    // for (const key of this) {
-    //   if (key === undefined || key === null) {
-    //     if (this.indexOf(key) !== (this.length) - 1) {
-    //       str += innerSeparator;
-    //     }
-    //     continue;
-    //   } else {
-    //     str += key;
-
-    //     if (this.indexOf(key) !== (this.length) - 1) {
-    //       str += innerSeparator;
-    //     }
-    //   }
-    // }
-
     for (let i = 0; i < this.length; i++) {
       if (this[i] === undefined || this[i] === null) {
         if (i !== (this.length) - 1) {
