2b315575475221d46ae0e1d8d915bf8f0fb00dce
Niki <tes.kolia@gmail.com>
2021-12-17 13:13:49 +0200

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,31 +6,19 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     // write code here
-    let str = '';
+    let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      switch (this[i]) {
-        case (this.length - 1 && ''):
-          str += '';
-          break;
-        case this.length - 1:
-          str += this[i];
-          break;
+      if (this[i] !== undefined && this[i] !== null) {
+        result += this[i];
+      }
 
-        default:
-          if (separator === null) {
-            str += this[i] + 'null';
-          } else if (this[i] === null || this[i] === undefined) {
-            str += separator;
-          } else if (this.length === 1) {
-            str += this[i];
-          } else {
-            str += this[i] + separator;
-          }
+      if (this.length - 1 !== i) {
+        result += separator;
       }
     }
 
-    return str;
+    return result;
   };
 }
 
