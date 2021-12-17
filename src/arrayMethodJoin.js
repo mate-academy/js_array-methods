4e500a6c7c9b14db642034f9251dddfb9e7690bc
Bohdan Maksymiuk <bohdan.max@ukr.net>
2021-12-17 12:44:41 +0200

    simplier check

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,12 +8,9 @@ function applyCustomJoin() {
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      let item = this[i];
-
-      if (this[i] === undefined || this[i] === null) {
-        item = '';
+      if (this[i] !== undefined && this[i] !== null) {
+        result += `${this[i]}`;
       }
-      result += item;
 
       if (i < this.length - 1) {
         result += separator;
