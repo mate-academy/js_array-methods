ae2cca398234b34f73d8ac5de569906d3ef6becd
Volodymyr Oleksiienko <oleksienkovladimir50@gmail.com>
2021-12-17 15:04:41 +0200

    changed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,20 +6,14 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let str = '';
-    const innerSeparator = separator;
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === undefined || this[i] === null) {
-        if (i !== (this.length) - 1) {
-          str += innerSeparator;
-        }
-        continue;
-      } else {
+      if (this[i] !== undefined && this[i] !== null) {
         str += this[i];
+      }
 
-        if (i !== (this.length) - 1) {
-          str += innerSeparator;
-        }
+      if (i < this.length - 1) {
+        str += separator;
       }
     }
 
