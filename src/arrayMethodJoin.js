8077f5eba32d2c25296f60ee81eb4299c07198a5
michael valiahmetov <michael.valiahmetov@gmail.com>
2021-12-17 14:56:14 +0200

    Method-join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,27 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    if (this.length === 0) {
+      return '';
+    };
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if ((this[i] === null) || (this[i] === undefined)) {
+        this[i] = '';
+      };
+
+      result += this[i] + '' + separator;
+    };
+
+    if ((this[this.length - 1] === null)
+      || (this[this.length - 1] === undefined)) {
+      this[this.length - 1] = '';
+    };
+
+    return result + this[this.length - 1];
   };
 }
 
