6790ac823afa988810c73f45cd53be3955f54948
Yevhenii Rozdorozhnii <rozdorozhnii@ukr.net>
2021-12-18 21:37:03 +0200

    solution_v1.0

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,26 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+
+    if (this.length === 0) {
+      return str;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      const expression = (this[i] === null || this[i] === undefined)
+        ? ''
+        : this[i];
+
+      if (i !== this.length - 1) {
+        str += expression + String(separator);
+      } else {
+        str += expression;
+      }
+    }
+
+    return str;
   };
 }
 
