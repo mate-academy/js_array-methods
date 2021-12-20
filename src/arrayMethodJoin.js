ed37f893998adf9f778688bad147ec95472159e8
Iryna Zolko <koko_irina@ukr.net>
2021-12-20 09:02:45 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,31 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    if (this.length === 0) {
+      return '';
+    }
+
+    let sep = separator;
+    const lastIndex = this.length - 1;
+
+    if (sep === undefined) {
+      sep = ',';
+    }
+
+    let result = '';
+
+    for (let i = 0; i < lastIndex; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        result += this[i];
+      }
+      result += sep;
+    }
+
+    if (this[lastIndex] !== null && this[lastIndex] !== undefined) {
+      result += this[lastIndex];
+    }
+
+    return result;
   };
 }
 
