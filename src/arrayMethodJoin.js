82682eb99fbcc5c701ee31dc07825a9b3ff14c14
Roman Shevchuk <14rom.shevchuk@gmail.com>
2021-12-20 15:28:35 +0200

    join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,33 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let element = separator;
+
+    if (this.length === 0) {
+      return result;
+    }
+
+    if (element === undefined) {
+      element = ',';
+    } else {
+      element = separator + '';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        this[i] = '';
+      }
+
+      if (i === this.length - 1) {
+        result += this[i];
+
+        return result;
+      }
+
+      result += this[i] + element;
+    }
   };
-}
+};
 
 module.exports = applyCustomJoin;
