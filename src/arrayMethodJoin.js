96694953a0cb23a32ba2c9d82339796abe35829d
Roman Shevchuk <14rom.shevchuk@gmail.com>
2021-12-21 09:58:02 +0200

    fix join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,33 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    let element = separator;
-
-    if (this.length === 0) {
-      return result;
-    }
-
-    if (element === undefined) {
-      element = ',';
-    } else {
-      element = separator + '';
-    }
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === undefined || this[i] === null) {
-        this[i] = '';
-      }
-
-      if (i === this.length - 1) {
+      if (this[i] !== undefined && this[i] !== null) {
         result += this[i];
-
-        return result;
       }
 
-      result += this[i] + element;
+      if (i !== this.length - 1) {
+        result += separator;
+      }
     }
+
+    return result;
   };
 };
 
