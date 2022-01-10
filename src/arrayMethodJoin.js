c42b3176650379572c75ab36a204949f31dc5c0e
Ladyha Roman <ladrom.ua@gmail.com>
2022-01-10 13:21:19 +0200

    solution3

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,19 +6,15 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let row = '';
-    let sep = String(separator);
 
     for (let i = 0; i < this.length; i++) {
-      if (i === this.length - 1) {
-        sep = '';
-      };
+      if (this[i] !== null && this[i] !== undefined) {
+        row += this[i];
+      }
 
-      if (typeof this[i] === 'undefined' || typeof this[i] === 'object') {
-        row += '' + sep;
-        continue;
-      };
-
-      row += this[i] + sep;
+      if (i !== this.length - 1) {
+        row += separator;
+      }
     }
 
     return row;
