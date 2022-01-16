59ea72593b1ec7074a98e02ce909d5fc45b0b045
yuliia <yulia.shyshka@gmail.com>
2022-01-16 10:08:36 +0200

    implementation

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let row = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        row += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        row += separator;
+      }
+    }
+
+    return row;
   };
 }
 
