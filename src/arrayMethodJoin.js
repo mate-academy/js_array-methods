0cac60409abbee10880c9c583b6034c2777a3f1a
Roman Kovalchuk <roma220702@gmail.com>
2022-01-19 15:53:28 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,45 +4,19 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let str = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (separator === undefined) {
-        if (this[i] === this[i - 1]) {
-          str += '';
-        } else {
-          if (this[i] === undefined || this[i] === null) {
-            str += ',';
-          } else {
-            if (i === this.length - 1) {
-              str += this[i];
-            } else {
-              str += `${this[i]},`;
-            }
-          }
-        }
-      } else if (separator === '') {
-        if (this[i] === undefined || this[i] === null) {
-          str += '';
-        } else {
-          str += this[i];
-        }
+      if (this[i] === undefined || this[i] === null) {
+        str += separator;
       } else {
-        if (this[i] === undefined || this[i] === null) {
-          str += separator;
-        } else {
-          if (i === this.length - 1) {
-            str += this[i];
-          } else {
-            str += `${this[i]}${separator}`;
-          }
-        }
+        str += this[i];
       }
-    }
 
-    if (str[str.length - 1] === str[str.length - 2]) {
-      str = str.substr(0, str.length - 1);
+      if (i < this.length - 1) {
+        str += separator;
+      }
     }
 
     return str;
