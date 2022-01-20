2af6e832a5b238cfc1366aaa14d1861be1cf8602
Roman Kovalchuk <roma220702@gmail.com>
2022-01-20 10:08:00 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -9,7 +9,7 @@ function applyCustomJoin() {
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] === undefined || this[i] === null) {
-        str += separator;
+        str += '';
       } else {
         str += this[i];
       }
