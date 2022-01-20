c6b8430747db27fa41cb4fb3d92a950880e55670
Дима Краснобокий <krasnoboky.dima@gmail.com>
2022-01-20 09:47:19 +0200

    done1

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let separ = separator;
+    let result = '';
+
+    if (separator === null) {
+      separ = 'null';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      let x = this[i];
+
+      if (x === undefined || x === null) {
+        x = '';
+      }
+      result += x + separ;
+    }
+
+    if (this[this.length - 1]) {
+      result += this[this.length - 1];
+    }
+
+    return result;
   };
 }
 
