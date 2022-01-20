37994da28657b029468d7afcf83f450741a193d0
Dmytro Dsypiuk <dimich578ua@gmail.com>
2022-01-20 12:57:04 +0200

    some mistakes

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,24 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let sep = '';
+    let newWord = '';
+
+    if (separator.length > 0) {
+      sep = separator;
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      const element = this[i];
+
+      if (i !== this.length - 1) {
+        newWord += element + sep;
+        continue;
+      }
+      newWord += element;
+    }
+
+    return newWord;
   };
 }
 
