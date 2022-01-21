410c1eb3251cb3065ebe45e9b07bcef78ed3fd2b
Yevhenii Kriuchek <evhenii.kriuchek@gmail.com>
2022-01-21 16:23:54 +0200

    hm

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let res = '';
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        res += this[i].toString() + separator;
+      } else {
+        res += '' + separator;
+      }
+    }
+
+    res += this[this.length - 1] === undefined ? '' : this[this.length - 1];
+
+    return res;
   };
 }
 
