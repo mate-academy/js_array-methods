e829bf911afb77fd2b2f5643b333aa37fe8f2189
Oleksii Pinchuk <oleksii.pin4uk@gmail.com>
2022-01-21 15:25:06 +0300

    join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,38 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedElements = '';
+    let delimiter = separator;
+
+    switch (delimiter) {
+      case undefined:
+        delimiter = ',';
+        break;
+
+      case null:
+        delimiter = 'null';
+        break;
+
+      default:
+        break;
+    }
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null && this[i] !== []) {
+        joinedElements += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        joinedElements += delimiter;
+      }
+    }
+
+    return joinedElements;
   };
 }
 
