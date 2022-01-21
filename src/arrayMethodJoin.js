38a417b510c3d606157a08242882af8b98e62593
trtskvalerie <trtsk.tea.valerie@gmail.com>
2022-01-21 15:19:45 +0200

    join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let arrJoined = '';
+    let tempElement = '';
+
+    if (this[0] !== undefined && this[0] !== null) {
+      arrJoined = this[0].toString();
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      tempElement = '';
+
+      if (this[i] !== undefined && this[i] !== null) {
+        tempElement = this[i].toString();
+      }
+
+      arrJoined += separator + tempElement;
+    }
+
+    return arrJoined;
   };
 }
 
