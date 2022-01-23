06221b63482890355834509cb1344c493137dabb
Mykhailo Ianush <yanushmisha@gmail.com>
2022-01-23 14:06:47 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,31 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let stringResult = '';
+    let separatorElem = ',';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (separator !== undefined) {
+      separatorElem = separator === null ? 'null' : separator;
+    }
+
+    if ((this[0] !== null) && (this[0] !== undefined)) {
+      stringResult += this[0];
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if ((this[i] === null) || (this[i] === undefined)) {
+        stringResult += separatorElem + '';
+        continue;
+      }
+
+      stringResult += separatorElem + this[i];
+    }
+
+    return stringResult;
   };
 }
 
