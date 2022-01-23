adee3892ae57670b3d4325720b223094ac4c65cd
Kostya Kovbel <dovkerdota@gmail.com>
2022-01-23 15:20:04 +0200

    sol

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,31 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let elementSeparator = separator;
+
+    if (separator === null) {
+      elementSeparator = 'null';
+    }
+
+    let stringAfterJoin = '';
+    const indexOfLast = this.length - 1;
+
+    for (let i = 0; i < indexOfLast; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        stringAfterJoin += '' + elementSeparator;
+      } else {
+        stringAfterJoin += this[i] + elementSeparator;
+      }
+    }
+
+    if (this[indexOfLast] === null || this[indexOfLast] === undefined) {
+      stringAfterJoin += '';
+    } else {
+      stringAfterJoin += this[indexOfLast];
+    }
+
+    return stringAfterJoin;
   };
 }
 
