439f3a20fc3f4b31b3206b720f15db6f36a90895
Misha Novikov <novikov1807@gmail.com>
2022-01-24 18:23:03 +0200

    Complite

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,31 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let result = '';
+    let separatorSymbol = ',';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (separator !== undefined) {
+      separatorSymbol = (separator === null) ? 'null' : separator;
+    }
+
+    if ((this[0] !== null) && (this[0] !== undefined)) {
+      result += this[0];
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if ((this[i] === null) || (this[i] === undefined)) {
+        result += separatorSymbol;
+        continue;
+      }
+
+      result += separatorSymbol + this[i];
+    }
+
+    return result;
   };
 }
 
