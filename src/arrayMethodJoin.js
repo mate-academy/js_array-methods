e532eb87da436188f46458e558ad7e62e5ea0896
DanyloLipar <danilo.lipar2016@gmail.com>
2022-01-25 18:46:34 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,7 +6,41 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let result = '';
+    let item = separator;
+    let letter;
+
+    if (typeof item === 'undefined') {
+      item = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      letter = this[i];
+
+      if (typeof letter === 'number') {
+        String(letter);
+      }
+
+      if (letter === this[this.length - 1] && letter === undefined) {
+        return result;
+      }
+
+      if (letter === this[this.length - 1] && letter === null) {
+        return result;
+      }
+
+      if (letter === null || letter === undefined) {
+        letter = ',';
+      }
+
+      if (letter === this[this.length - 1] || letter === ',') {
+        result += letter;
+      } else {
+        result = result + letter + item;
+      }
+    }
+
+    return result;
   };
 }
-
 module.exports = applyCustomJoin;
