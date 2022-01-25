96d488f72ec1388390cb7500c1539fdbef8dc145
Олег Майборода <xa12850446@student.karazin.ua>
2022-01-25 08:30:54 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let modifiedSeparator = separator;
+    let joinedElement = '';
+
+    if (separator === null) {
+      modifiedSeparator = null + '';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      let item = this[i];
+
+      if ([undefined, null].includes(item)) {
+        item = '';
+      }
+
+      if (i !== this.length - 1) {
+        joinedElement += item + modifiedSeparator;
+      } else {
+        joinedElement += item;
+      }
+    }
+
+    return joinedElement;
   };
 }
 
