f58e97a2f5f3a53373a2907678c47aab25155cf5
Dorosh13 <dorosh.d.13@gmail.com>
2022-01-25 16:06:02 +0200

    created array-method-join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let arrJoin = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        arrJoin += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        arrJoin += separator;
+      }
+    }
+
+    return arrJoin;
   };
 }
 
