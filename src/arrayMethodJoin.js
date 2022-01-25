25027b314d36cc34ecb74c63b80320331206d3e7
Mykhailo Ianush <yanushmisha@gmail.com>
2022-01-25 15:24:22 +0200

    fix all

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,30 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
     let stringResult = '';
-    let separatorElem = ',';
 
     if (this.length === 0) {
       return '';
     }
 
-    if (separator !== undefined) {
-      separatorElem = separator === null ? 'null' : separator;
-    }
-
-    if ((this[0] !== null) && (this[0] !== undefined)) {
-      stringResult += this[0];
-    }
-
-    for (let i = 1; i < this.length; i++) {
-      if ((this[i] === null) || (this[i] === undefined)) {
-        stringResult += separatorElem + '';
-        continue;
+    for (let i = 0; i < this.length; i++) {
+      if ((this[i] !== null) && (this[i] !== undefined)) {
+        stringResult += this[i];
       }
 
-      stringResult += separatorElem + this[i];
+      if (i !== this.length - 1) {
+        stringResult += separator === null ? 'null' : separator;
+      }
     }
 
     return stringResult;
