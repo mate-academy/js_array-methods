e495bd8853ed08e1ed9c22340b6234367987822b
Roma Boreiko <roma.boreiko@gmail.com>
2022-01-26 19:56:36 +0200

    solution after code review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,7 +8,7 @@ function applyCustomJoin() {
     let resultString = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
+      if (this[i] !== null && this[i] !== undefined) {
         resultString += this[i];
       }
 
