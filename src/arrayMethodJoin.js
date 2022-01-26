a1f8a92c673f0bfc3a7d42da8d17e68f9f22d1e9
unknown <m23a09x96@gmail.com>
2022-01-26 11:30:01 +0200

    more fixes

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,21 +6,16 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let resultOfJoin = '';
-    let sepAsist = separator;
-
-    if (separator === null) {
-      sepAsist = 'null';
-    }
 
     for (let i = 0; i < this.length; i++) {
-      let item = this[i];
+      const item = this[i];
 
       if (item !== undefined && item !== null) {
         resultOfJoin += item;
       }
 
       if (i !== this.length - 1) {
-        resultOfJoin += sepAsist;
+        resultOfJoin += separator;
       }
     }
 
