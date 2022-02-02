57b18d394f4b58e0e1e2a1db25c59735a56faeb4
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-02-02 19:04:20 +0200

    Join method was implemented

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,33 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let resultString = '';
+    let sep;
+
+    if (separator === null) {
+      sep = 'null';
+    } else {
+      sep = separator;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1) {
+        if (this[i] === null || this[i] === undefined) {
+          resultString += '';
+        } else {
+          resultString += this[i];
+        }
+      } else {
+        if (this[i] === null || this[i] === undefined) {
+          resultString += '' + sep;
+        } else {
+          resultString += this[i] + sep;
+        }
+      }
+    }
+
+    return resultString;
   };
 }
 
