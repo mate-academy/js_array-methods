d91945b7cc367a94d9bcc11ab7a808a7d7247370
Misha Ivchenko <ivchenkomykhail@meta.ua>
2022-02-03 14:40:25 +0200

    Method join was fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,27 +6,16 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let resultString = '';
-    let sep;
-
-    if (separator === null) {
-      sep = 'null';
-    } else {
-      sep = separator;
-    }
 
     for (let i = 0; i < this.length; i++) {
-      if (i === this.length - 1) {
-        if (this[i] === null || this[i] === undefined) {
-          resultString += '';
-        } else {
-          resultString += this[i];
-        }
+      if (this[i] === null || this[i] === undefined) {
+        resultString += '';
       } else {
-        if (this[i] === null || this[i] === undefined) {
-          resultString += '' + sep;
-        } else {
-          resultString += this[i] + sep;
-        }
+        resultString += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        resultString += separator;
       }
     }
 
