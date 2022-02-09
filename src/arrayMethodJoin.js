ccbcf63bc704bb810505a650bd5dcde63d0d782f
Yaroslav Rehulskyi <barsinvest84@gmail.com>
2022-02-09 19:49:44 +0200

    Many ifs. How to simplify it?

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (i !== this.length - 1) {
+        if (this[i] !== null && this[i] !== undefined) {
+          str = str + this[i] + separator;
+        } else {
+          str += separator;
+        }
+      } else if (this[i] !== null && this[i] !== undefined) {
+        str += this[i];
+      }
+    }
+
+    return str;
   };
 }
 
