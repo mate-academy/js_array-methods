b2af5104d058d858e916547b16b91d8f81a50700
krolich500grn <edu.izaytsev@gmail.com>
2022-02-10 16:40:46 +0200

    solution array methon join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joined = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        joined += this[i];
+      }
+
+      if (i === this.length - 1) {
+        break;
+      }
+
+      joined += separator;
+    }
+
+    return joined;
   };
 }
 
