0f7bce934b21d63f5c5ae9c70d6c9382b76a36b9
Oleksandr Bratashchuk <olbratashchuk03@gmail.com>
2022-02-10 19:29:06 +0200

    waiting for reject))

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1) {
+        if (this[i] === null || this[i] === undefined) {
+          break;
+        }
+        str += this[i].toString();
+        break;
+      }
+
+      if (this[i] === null || this[i] === undefined) {
+        str += separator;
+        continue;
+      }
+
+      str += this[i].toString();
+      str += separator;
+    }
+
+    return str;
   };
 }
 
