3b04e2027d2c761500c4af813adb1e61312f9884
Anton Hanetskyi <antonganetskiy@gmail.com>
2022-02-12 19:39:37 +0200

    comments was fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,19 +7,14 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
     let symbal = '';
-    let currentSep = separator;
-
-    if (separator === null) {
-      currentSep = 'null';
-    }
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== undefined && this[i] !== null) {
-        symbal = this[i];
+        symbal = `${this[i]}`;
       }
 
       if (i < this.length - 1) {
-        result += symbal + currentSep;
+        result += symbal + separator;
       } else {
         result += symbal;
       }
