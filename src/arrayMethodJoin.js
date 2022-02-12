835f2f17e9184eb25efe8576ef27ae6fd8d90dfd
Anton Hanetskyi <antonganetskiy@gmail.com>
2022-02-12 18:11:44 +0200

    join method was implement

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,30 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    let symbal = '';
+    let currentSep = separator;
+
+    if (separator === null) {
+      currentSep = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        symbal = this[i];
+      }
+
+      if (i < this.length - 1) {
+        result += symbal + currentSep;
+      } else {
+        result += symbal;
+      }
+
+      symbal = '';
+    }
+
+    return result;
   };
 }
 
