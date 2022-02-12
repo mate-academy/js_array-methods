0e311699a912c97c2df0fa070be841f0c24a2e69
Oleksandr Bratashchuk <olbratashchuk03@gmail.com>
2022-02-12 19:10:10 +0200

    fully rewrote (inspired by Anton Hanetskiy idea)

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,23 +6,20 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let str = '';
+    let toAdd = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (i === this.length - 1) {
-        if (this[i] === null || this[i] === undefined) {
-          break;
-        }
-        str += this[i].toString();
-        break;
-      }
+      toAdd = `${this[i]}`;
 
       if (this[i] === null || this[i] === undefined) {
-        str += separator;
-        continue;
+        toAdd = '';
       }
 
-      str += this[i].toString();
-      str += separator;
+      if (i < this.length - 1) {
+        str += toAdd + separator;
+      } else {
+        str += toAdd;
+      }
     }
 
     return str;
