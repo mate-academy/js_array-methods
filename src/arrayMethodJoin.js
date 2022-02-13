588739073640cfd0ef15fb860ee77a61572e0cca
Alexandr Trubin-Koshyk <alexandr.koshyk@gmail.com>
2022-02-13 12:10:22 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,43 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let sep = ',';
+    let checkValue = 0;
+    const inputLength = this.length;
+
+    if (separator === null) {
+      sep = 'null';
+    }
+
+    if (separator || separator === '') {
+      sep = separator.toString();
+    }
+
+    for (let i = 0; i < inputLength; i++) {
+      checkValue = this[i];
+
+      switch (checkValue) {
+        case null:
+          result += sep;
+          break;
+
+        case undefined:
+          if (i !== inputLength - 1) {
+            result += sep;
+          }
+          break;
+
+        default:
+          if (i !== inputLength - 1) {
+            result += checkValue + sep;
+          } else {
+            result += checkValue;
+          }
+      }
+    }
+
+    return result;
   };
 }
 
