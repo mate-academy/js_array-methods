610269e206ffaa2ade2161f73abecd2a88fa64db
Batick3737 <antonandriy@ukr.net>
2022-02-18 11:18:31 +0200

    changed code2

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,21 +5,23 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let masive = '';
+    let result = '';
 
-    for (let i = 0; i < this.length - 1; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        masive += `` + separator;
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined && this[i] === this[this.length - 1]) {
+        return `${result}`;
+      }
+
+      if (this[i] === this[this.length - 1]) {
+        result += `${this[i]}` + '';
+      } else if (this[i] === null || this[i] === undefined) {
+        result += `` + separator;
       } else {
-        masive += `${this[i]}` + separator;
+        result += `${this[i]}` + separator;
       }
     }
 
-    if (this[this.length - 1] === undefined) {
-      return `${masive}`;
-    }
-
-    return `${masive}` + `${this[this.length - 1]}`;
+    return `${result}`;
   };
 };
 
