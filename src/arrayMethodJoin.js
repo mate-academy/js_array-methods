4716aa9505ca724654fce8cca90db687af491755
Alex Nastychenko <nastychenko1993@gmail.com>
2022-02-21 01:11:49 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,31 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length <= 0) {
+      return '';
+    }
+
+    let universalSeparator = separator;
+    let str = '';
+
+    if (universalSeparator === null) {
+      universalSeparator = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+    };
+
+    for (let i = 0; i < this.length - 1; i++) {
+      str += this[i] + universalSeparator;
+    };
+
+    str += this[this.length - 1];
+
+    return str;
   };
 }
 
