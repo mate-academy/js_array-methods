51e99599443dd4f47552ce47abe96a991c5004d7
dant0n <dmibuc@gmail.com>
2022-02-21 22:01:04 +0200

    Finish hw.

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,28 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let internalSeparator;
+    let resultString = '';
+
+    if (separator === undefined) {
+      internalSeparator = ',';
+    } else {
+      internalSeparator = separator;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i > 0) {
+        resultString += internalSeparator;
+      }
+
+      if (this[i] === undefined || this[i] === null) {
+        resultString += '';
+      } else {
+        resultString += this[i].toString();
+      }
+    }
+
+    return resultString;
   };
 }
 
