b52581b63037749fb570b3121cd0ad1c86c1bf55
dant0n <dmibuc@gmail.com>
2022-02-22 20:52:54 +0200

    Add default parameter, minor refactor

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,24 +4,16 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    let internalSeparator;
+  [].__proto__.join2 = function(separator = ',') {
+    const internalSeparator = separator;
     let resultString = '';
-
-    if (separator === undefined) {
-      internalSeparator = ',';
-    } else {
-      internalSeparator = separator;
-    }
 
     for (let i = 0; i < this.length; i++) {
       if (i > 0) {
         resultString += internalSeparator;
       }
 
-      if (this[i] === undefined || this[i] === null) {
-        resultString += '';
-      } else {
+      if (this[i] !== undefined && this[i] !== null) {
         resultString += this[i].toString();
       }
     }
