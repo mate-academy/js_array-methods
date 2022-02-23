813559bac785557977d43b52256058f14acb3ba7
dant0n <dmibuc@gmail.com>
2022-02-23 21:00:33 +0200

    Remove internalSeparator variable.

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,12 +5,11 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    const internalSeparator = separator;
     let resultString = '';
 
     for (let i = 0; i < this.length; i++) {
       if (i > 0) {
-        resultString += internalSeparator;
+        resultString += separator;
       }
 
       if (this[i] !== undefined && this[i] !== null) {
