80339e5b7ca074551b24457987e4f382e8cacbd3
Yuriy Shvorob <yuriyshvorob@ip-10-10-0-221.eu-central-1.compute.internal>
2022-02-23 12:15:05 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,27 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    const modifiedSeparator = `${separator}`;
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1 && this[i] !== undefined) {
+        result += this[i];
+        break;
+      } else if (i === this.length - 1 && this[i] === undefined) {
+        result += '';
+        break;
+      }
+
+      if (this[i] !== null && this[i] !== undefined) {
+        result += this[i] + modifiedSeparator;
+      } else {
+        result += modifiedSeparator;
+      }
+    }
+
+    return result;
   };
 }
 
