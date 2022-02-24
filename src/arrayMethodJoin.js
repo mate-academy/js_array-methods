d237e7956e6cfc83cfe566e63c1a528629e4ae9f
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2022-02-24 20:26:55 +0200

    Solution for task

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+
+    if (this.length > 0) {
+      let part = '';
+
+      if (this[0] !== undefined && this[0] !== null) {
+        part = this[0];
+      }
+      str += part;
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      let part = '';
+
+      if (this[i] !== undefined && this[i] !== null) {
+        part = this[i];
+      }
+      str += String(separator) + part;
+    }
+
+    return str;
   };
 }
 
