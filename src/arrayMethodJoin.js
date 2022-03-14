2599d6da98a151eb5b99b42100e81919e7bbcb24
Roman Nikitenko <Lomkaaa@ukr.net>
2022-03-14 10:59:08 +0200

    Task complete

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    this.forEach((str, i) => {
+      let s = str;
+
+      if (s === null || s === undefined) {
+        s = '';
+      }
+      result += `${s}`;
+
+      if (this.length > i + 1) {
+        result += `${separator}`;
+      }
+    });
+
+    return result;
   };
 }
 
