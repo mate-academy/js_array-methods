e0b34281031d0637aa64d3c24849812a0346bca7
Serhii Pastukhov <pastuhovsergey@gmail.com>
2022-03-23 11:00:31 +0200

    Solution

--- readme.md
+++ readme.md
@@ -5,3 +5,4 @@
 [Guideline](https://github.com/mate-academy/js_task-guideline/blob/master/README.md)
 
 Read more about [Array.prototype.join()](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/join)
+
--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,31 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str_ = '';
+
+    for (const x of this) {
+      if (x === null || x === undefined) {
+        str_ += ',';
+        continue;
+      }
+      str_ += x;
+
+      if (separator === undefined) {
+        str_ += ',';
+      } else if (separator !== undefined) {
+        str_ += separator;
+      }
+    }
+
+    if (separator === null) {
+      str_ = str_.slice(0, -4);
+    } else if (typeof separator === 'object') {
+      str_ = str_.slice(0, -15);
+    } else if (separator !== '') {
+      str_ = str_.slice(0, -1);
+    }
+
+    return str_;
   };
 }
 
