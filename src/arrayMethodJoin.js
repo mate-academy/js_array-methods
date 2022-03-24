03406bbdd648f2556e857c0dcd604cc9edc385c6
Olexandr Istomin <gn.53@ukr.net>
2022-03-24 19:19:02 +0200

    Solution-2

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,31 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let newStr = ``;
 
-    if (this[0] === undefined || this[0] === null || this[0] === '') {
-      newStr += '';
-    } else {
+    if (this[0] !== undefined && this[0] !== null) {
       newStr += `${this[0]}`;
     }
 
     let el;
 
     for (let i = 1; i < this.length; i++) {
-      if (this[i] === undefined || this[i] === null || this[i] === '') {
-        el = '';
-      } else {
+      if (this[i] !== undefined && this[i] !== null) {
         el = this[i];
+      } else {
+        el = '';
       }
 
-      if (separator === undefined) {
-        newStr += ',' + el;
-      } else if (separator === '') {
-        newStr += el;
-      } else {
-        newStr += `${separator}` + el;
-      }
+      newStr += `${separator}` + el;
     }
 
     return newStr;
