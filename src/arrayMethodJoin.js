e1c8a1a4c2237faf74fa3399be855c5e6a0a6596
Denys Lipin <lipin.transstream@gmail.com>
2022-03-29 13:46:42 +0300

    'Solution'

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let string = '';
+
+    for (const element of this) {
+      if (element === null || element === undefined) {
+        string += String(separator);
+      } else {
+        string += String(element) + String(separator);
+      }
+    }
+
+    const end = string.length - String(separator).length;
+
+    return string.slice(0, end);
   };
 }
 
