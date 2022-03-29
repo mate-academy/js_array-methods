4b1a674b6b06a4dcbe1860a80d08d1b0d40ca9f8
Vlad Skoromnyi <VectraSton@gmail.com>
2022-03-29 16:04:53 +0300

    method join was implemented

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,31 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let string = '';
+    let s = separator;
+
+    if (separator === null) {
+      s = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        if (i === this.length - 1) {
+          string += this[i];
+        } else {
+          string += this[i] + s;
+        }
+      } else {
+        if (i === this.length - 1) {
+          string += '';
+        } else {
+          string += '' + s;
+        }
+      }
+    }
+
+    return string;
   };
 }
 
