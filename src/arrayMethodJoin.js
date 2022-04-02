a16af5d17b4416e5f317725ad1d5aa6d02974b7f
Ivan Kadykalo <ivan.kadykalo@gmail.com>
2022-04-02 17:40:22 +0300

    Create solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,42 @@
 /**
  * Implement method join
  */
+
+function separatorFilter(divider) {
+  switch (divider) {
+    case null: {
+      return 'null';
+    }
+
+    case undefined: {
+      return 'undefined';
+    }
+
+    default: {
+      return divider;
+    }
+  }
+}
+
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(userSeparator = ',') {
+    let resultString = '';
+    let separator = separatorFilter(userSeparator);
+
+    for (const part of this) {
+      if (this.indexOf(part) === this.length - 1) {
+        separator = '';
+      }
+
+      if (part === null || part === undefined) {
+        resultString += separator;
+        continue;
+      }
+
+      resultString += part + separator;
+    }
+
+    return resultString;
   };
 }
 
