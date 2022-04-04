d13da3fb54a545f0d300a7599efac1b6cffc623e
Serhii Naumenko <s.naumenko.miy@gmail.com>
2022-04-04 19:07:36 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,34 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    if (this.length === 0 || this === undefined) {
+      return '';
+    }
+
+    let separ = separator;
+
+    if (separator === undefined) {
+      separ = ',';
+    }
+
+    if (separator === null) {
+      separ = 'null';
+    }
+
+    let newString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        if (i !== this.length - 1) {
+          newString += ',';
+        }
+      } else {
+        i === this.length - 1 ? newString += this[i]
+          : newString += this[i] + separ;
+      }
+    }
+
+    return newString;
   };
 }
 
