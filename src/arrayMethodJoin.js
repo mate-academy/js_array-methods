56fd06493ae4cd3aeab800cc1bbae573148cb679
Vlad Skoromnyi <VectraSton@gmail.com>
2022-04-04 17:40:50 +0300

    remaked

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,25 +6,14 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let string = '';
-    let s = separator;
-
-    if (separator === null) {
-      s = 'null';
-    }
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
-        if (i === this.length - 1) {
-          string += this[i];
-        } else {
-          string += this[i] + s;
-        }
-      } else {
-        if (i === this.length - 1) {
-          string += '';
-        } else {
-          string += '' + s;
-        }
+        string += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        string += separator;
       }
     }
 
