9dcc97ee5d0a8207ad5649ecdf334f315e321b25
Nikita Prepelytsia <prep.nikita@gmail.com>
2022-04-05 08:32:17 +0300

    done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,25 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let trueSeparator = separator;
+
+    if (separator === undefined) {
+      trueSeparator = ',';
+    }
+
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        result += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        result += trueSeparator;
+      }
+    }
+
+    return result;
   };
 }
 
