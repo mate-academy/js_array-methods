a8e2f4683c098393d957591971073b74ccfc93b4
unknown <avicostov2@gmail.com>
2022-04-05 22:25:05 +0200

    frrfr

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let newStr = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        newStr += `${separator}`;
+        continue;
+      }
+
+      newStr += `${this[i]}${separator}`;
+    }
+
+    if (this[this.length - 1] !== undefined && this[this.length - 1] !== null) {
+      newStr += this[this.length - 1];
+    }
+
+    return newStr;
   };
 }
 
