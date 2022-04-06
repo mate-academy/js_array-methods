71c714c1d4492947520835dc1b21a7308f0e022b
Tetiana Koval <tetiana.koval.bit@gmail.com>
2022-04-06 12:26:53 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let newString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      if (i < this.length - 1) {
+        newString = newString + this[i] + separator;
+      } else {
+        newString += this[i];
+      }
+    };
+
+    return newString;
   };
 }
 
