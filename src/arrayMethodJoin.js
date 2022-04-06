16cb7937090de6c3174e1302de5860f9452a33d9
Anatolii Vlasiuk <vlasyuk_a@ukr.net>
2022-04-06 14:41:16 +0300

    Without replace

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,19 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    let str;
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
 
-    if (separator !== undefined) {
-      str = this.toString().replace(/,/g, separator);
+    for (let i = 0; i < this.length; i++) {
+      if (this.length > 0 && this[i] !== null && this[i] !== undefined) {
+        str += this[i];
+      }
 
-      return str;
-    } else {
-      str = this.toString();
-
-      return str;
+      if (i !== this.length - 1) {
+        str += separator;
+      }
     }
-  };
-};
 
+    return str;
+  };
+}
 module.exports = applyCustomJoin;
