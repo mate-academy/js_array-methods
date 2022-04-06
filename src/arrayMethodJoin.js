941209b681bc36b72e2fcc02ac3f3b0a5298c3cd
Andriy Shvets <PiratesQ@i.ua>
2022-04-06 13:55:56 +0300

    variable changed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let str = '';
+    let result = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== undefined && this[i] !== null) {
-        str += this[i];
+        result += this[i];
       }
 
       if (i !== this.length - 1) {
-        str += separator;
+        result += separator;
       }
     }
 
-    return str;
+    return result;
   };
 }
 
