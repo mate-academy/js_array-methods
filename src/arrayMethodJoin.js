1d8cf6bccef13b7dfcfa3a338e1a8a590f180609
Pavlo Stasiv <99363993+StasivPavlo@users.noreply.github.com>
2022-04-06 20:31:05 +0200

    arrayMethodJoin

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+    const CloneSeparator = separator;
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        this[i] = '';
+      }
+
+      if (!i) {
+        str += `${this[i]}`;
+        continue;
+      }
+
+      str += `${CloneSeparator}${this[i]}`;
+    }
+
+    return str;
   };
 }
 
