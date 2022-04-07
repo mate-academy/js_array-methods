dd097d543f2990a2eaa6de5f6690085a0cc3b766
Kateryna Gurienkova <gurienkova@gmail.com>
2022-04-07 11:47:50 +0300

    fix

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,10 +7,6 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let string = '';
 
-    if (this === 0 || this === undefined || this === []) {
-      return string;
-    }
-
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== undefined
         && this[i] !== null) {
