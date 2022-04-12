d1b110199c34041685f2df0100b358354400a36d
Владимир Салли <vovazalli@gmail.com>
2022-04-12 13:28:52 +0300

    Solution v2

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,12 +6,11 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
+    const adjustedSeparator = separator === null ? 'null' : separator;
 
     for (let i = 0; i < this.length; i++) {
       const currentElement = this[i] !== null && this[i] !== undefined
         ? this[i] : '';
-
-      const adjustedSeparator = separator === null ? 'null' : separator;
 
       if (i === this.length - 1) {
         result += currentElement;
