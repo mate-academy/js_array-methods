b5964d237da7eab42d90fb1ca3c97e6f2fe470ff
Kostiantyn <havryshkostya@gmail.com>
2022-04-14 17:41:24 +0400

    nevermind the 1st commit it was a cat, check this out

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,15 +7,6 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let str = '';
 
-    if (this.length === 0) {
-      return '';
-    }
-
-    if (separator === null) {
-      // eslint-disable-next-line no-param-reassign
-      separator = 'null';
-    }
-
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
         str += this[i];
