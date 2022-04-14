13bae80a20ebaed74a471cb4ef18dd7ef9e7595f
vladkoval <kickgames675@gmail.com>
2022-04-14 23:54:43 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,28 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let separators = separator;
+
+    if (separator === undefined) {
+      separators = ',';
+    }
+
+    let result = '';
+    const str = String(separators);
+    const cut = str.length;
+
+    for (let i of this) {
+      if (i === undefined || i === null) {
+        i = '';
+      }
+      result += i + str;
+    }
+
+    if (cut === 0) {
+      return (result);
+    }
+
+    return result.slice(0, -cut);
   };
 }
 
