7712f314a9a6b148ff179d69185d65d8d506778f
Kostiantyn <havryshkostya@gmail.com>
2022-04-14 17:39:43 +0400

    if you dont like 16th line, offer a solution pls

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (separator === null) {
+      // eslint-disable-next-line no-param-reassign
+      separator = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        str += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        str += separator;
+      }
+    }
+
+    return str;
   };
 }
 
