5b431d77d7b87190ccfa23a3b4f23a8343203683
efdalyalcin <efdalyalcin@hotmail.com>
2022-04-25 15:32:29 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,29 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    const length = this.length;
+
+    let seperUsed = separator;
+
+    if ('' + seperUsed === 'undefined') {
+      seperUsed = ',';
+    }
+
+    for (let i = 0; i < length; i++) {
+      if (typeof this[i] === 'undefined'
+       || this[i] === null) {
+        ; // if equal to just pass, not equal to doesn't work
+      } else {
+        result += this[i];
+      }
+
+      if (i !== length - 1) {
+        result += '' + seperUsed;
+      }
+    }
+
+    return result;
   };
 }
 
