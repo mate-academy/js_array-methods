3728952966f4ab4699fbae9a7447f0894cfb7f57
Anatolii Vlasiuk <vlasyuk_a@ukr.net>
2022-04-27 15:51:56 +0300

    Join method done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+
+    for (let i = 0; i < this.length; i++) {
+      switch (this[i]) {
+        case null:
+        case undefined:
+          str += '';
+          break;
+        default:
+          str += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        str += separator;
+      }
+    }
+
+    return str;
   };
 }
 
