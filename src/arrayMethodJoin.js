4b3298e04aa9b072b3e88b3934a98fe932d05eea
Swazi Kunene <kuneneswazi18@gmail.com>
2022-04-27 09:42:43 +0200

    used default parameter instead

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,13 +4,9 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    let newSeparator = separator;
-
-    if (separator === undefined) {
-      newSeparator = ',';
-    }
+    const newSeparator = separator;
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
