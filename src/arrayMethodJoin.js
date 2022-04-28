b191cefac7628ebc4ad74dd19b25146d4ac7e399
elkhayate <melkhayate61@gmail.com>
2022-04-28 22:19:35 +0000

    done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,25 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const elm = this[i];
+
+      if (elm !== undefined && elm !== null) {
+        result += elm;
+      }
+
+      if (i + 1 < this.length) {
+        if (separator === undefined) {
+          result += ',';
+        } else {
+          result += separator;
+        }
+      }
+    }
+
+    return result;
   };
 }
 
