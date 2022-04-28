a990bac715b93c08fcf3145a0b18f2b97da2e6fb
Zubov Illia <contactduodiamonds@gmail.com>
2022-04-28 20:20:56 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,27 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let word = '';
+    let sep = separator;
+
+    if (separator === undefined) {
+      sep = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1) {
+        if (this[i] === undefined) {
+        } else {
+          word += `${this[i]}`;
+        }
+      } else if (this[i] === null || this[i] === undefined) {
+        word += `${sep}`;
+      } else {
+        word += `${this[i]}${sep}`;
+      }
+    }
+
+    return word;
   };
 }
 
