6f87a1ed385e08b87fced7593013271edc286285
Andriyk0 <andriy22221@gmail.com>
2022-04-29 16:56:54 +0300

    create method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+    const sep = '' + separator;
+
+    for (let i = 0; i < this.length; i++) {
+      if (typeof (this[i]) === 'undefined' || typeof (this[i]) === 'object') {
+        if (i !== this.length - 1) {
+          str += '' + sep;
+        }
+      } else if (i !== this.length - 1) {
+        str += this[i] + sep;
+      } else {
+        str += this[i];
+      }
+    }
+
+    return str;
   };
 }
 
