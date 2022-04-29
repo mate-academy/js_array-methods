b234a7290ba96a923176fa8d8ab298fba47187a9
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2022-04-29 16:54:16 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,37 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let separ = separator;
+
+    switch (separ) {
+      case undefined:
+        separ = ',';
+        break;
+
+      case null:
+        separ = 'null';
+        break;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1 && this[i] === undefined) {
+        continue;
+      }
+
+      if (this[i] === undefined || this[i] === null) {
+        result += ',';
+        continue;
+      }
+
+      if (i === this.length - 1) {
+        result += String(this[i]);
+      } else {
+        result += String(this[i]) + separ;
+      }
+    }
+
+    return result;
   };
 }
 
