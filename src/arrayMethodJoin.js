ad62e2ef4d076e3d06133e17d24b741414e1d6cf
Uliton <x3m@Oleksandrs-MacBook-Pro.local>
2022-04-30 17:49:05 +0200

    make JOIN method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,57 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    const separatorValue = separator;
+    let str = '';
+
+    if (separatorValue === undefined) {
+      str += this[0];
+
+      for (let i = 1; i < this.length; i++) {
+        str += ',' + this[i];
+      };
+
+      return str;
+    }
+
+    if (separatorValue === '') {
+      str += this[0];
+
+      for (let i = 1; i < this.length; i++) {
+        str += this[i];
+      }
+
+      return str;
+    }
+
+    if (separatorValue) {
+      if (this.length === 0) {
+        return str;
+      } else if (this[0] === null) {
+      } else {
+        str += this[0];
+      }
+
+      for (let i = 1; i < this.length; i++) {
+        if (this[i] === undefined || this[i] === null) {
+          str += separatorValue;
+        } else {
+          str += separatorValue + this[i];
+        }
+      }
+
+      return str;
+    }
+
+    if (separatorValue === null) {
+      str += this[0];
+
+      for (let i = 1; i < this.length; i++) {
+        str += String(separatorValue) + this[i];
+      };
+
+      return str;
+    };
   };
 }
 
