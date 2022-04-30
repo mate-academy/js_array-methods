6a3120ebc8d429150f002f754cb0e5e19ffca01e
Andrii Yelnikov <whiteyelnikov@gmail.com>
2022-04-30 11:48:25 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,29 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let res = '';
+    let separ = separator;
+
+    if (separator === undefined) {
+      separ = ',';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        res += separ;
+      }
+
+      if (this[i] !== null && typeof (this[i]) !== 'undefined') {
+        res += this[i];
+        res += separ;
+      }
+    }
+
+    if (this[this.length - 1] !== undefined) {
+      res += this[this.length - 1];
+    }
+
+    return res;
   };
 }
-
 module.exports = applyCustomJoin;
