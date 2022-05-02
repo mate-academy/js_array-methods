d4703c2eae36f9ce3c14e8c7fcc3044263c0fe1f
Vicky Miziukanova <enrikadej@gmail.com>
2022-05-02 20:39:01 +0300

    tasl is solved

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let string = '';
+
+    if (this[0] !== undefined && this[0] !== null) {
+      string += this[0];
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      string += separator;
+
+      if (this[i + 1] !== undefined && this[i + 1] !== null) {
+        string += this[i + 1];
+      }
+    }
+
+    return string;
   };
 }
 
