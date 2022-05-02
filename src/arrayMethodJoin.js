cdc01ea99676ee210575c6b66bcd213af2b0e281
Uktam19980416 <rakhmatullayev2019@mail.ru>
2022-05-02 03:29:56 +0200

    join method is ready

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,21 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function (separator = ',') {
     // write code here
+    let joinMethod = '';
+
+    for (let idx = 0; idx < this.length; idx++) {
+      if (this[idx] !== undefined && this[idx] !== null) {
+        joinMethod += this[idx];
+      }
+
+      if (idx !== this.length - 1) {
+        joinMethod += separator;
+      }
+    }
+
+    return joinMethod;
   };
 }
 
