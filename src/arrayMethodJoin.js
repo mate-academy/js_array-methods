70ce3b70f14f0a81357ae9d4a164693c65200c61
Natalya Tsybulko <tsybulko.natalya395@gmail.com>
2022-05-03 20:50:57 +0300

    fix array mutation bug

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,12 +8,10 @@ function applyCustomJoin() {
     let resultString = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === undefined
-        || this[i] === null) {
-        this[i] = '';
+      if (this[i] !== undefined
+        && this[i] !== null) {
+        resultString += this[i];
       }
-
-      resultString += this[i];
 
       if (i !== this.length - 1) {
         resultString += separator;
