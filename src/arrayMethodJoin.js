15fa738b00ab0857421b7f6cff7af71c50e30671
Oleksandr Voloshyn <voloshyn244gmail.com>
2022-05-03 13:49:56 +0200

    arrayMethodSolution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,21 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let resultString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        resultString += this[i];
+      }
+
+      if (i + 1 !== this.length) {
+        resultString += separator;
+      }
+    }
+
+    return resultString;
   };
 }
 
