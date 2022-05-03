e01abeb2c59af4bb13c6a430def0a79cf25604c4
Vicky Miziukanova <enrikadej@gmail.com>
2022-05-03 14:56:35 +0300

    cleaned redundant condition

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,7 +7,7 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let string = '';
 
-    for (let i = 0; i < this.length - 1; i++) {
+    for (let i = 0; i <= this.length - 1; i++) {
       if (this[i] !== undefined && this[i] !== null) {
         string += this[i];
       }
