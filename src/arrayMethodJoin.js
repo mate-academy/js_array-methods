38c92687b354c3d7a7111b130bd59b453282eebc
Daryna Chekmariova <dashachekmariova@gmail.com>
2022-05-04 15:54:35 +0300

    method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        result += `${separator}`;
+      } else {
+        result += `${this[i]}${separator}`;
+      }
+    }
+
+    if (this[this.length - 1] !== undefined) {
+      result += this[this.length - 1];
+    }
+
+    return result;
   };
 }
 
