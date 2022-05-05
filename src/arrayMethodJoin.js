ecd03c50b0a2009f9093ab5dfb43d08be6f18755
Volodymyr <lesksv2000@gmail.com>
2022-05-06 00:04:29 +0300

    join fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,11 +6,12 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     // write code here
-    let result = '';
 
     if (this.length === 0) {
       return '';
     }
+
+    let result = '';
 
     for (let i = 0; i < this.length; i++) {
       if ((this[i] !== null) && (this[i] !== undefined)) {
