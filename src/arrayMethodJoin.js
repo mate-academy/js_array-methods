4bc4b7459e87a8e2f4ece5500216b1a755e4c0c9
Ihor Pavlenko <pavlenkoigor3@gmail.com>
2022-05-07 11:37:17 +0300

    method join solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let sum = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if ((this[i] !== null) && (this[i] !== undefined)) {
+        sum = sum + this[i];
+      }
+
+      if (i !== this.length - 1) {
+        sum = sum + separator;
+      }
+    }
+
+    return sum;
   };
 }
 
