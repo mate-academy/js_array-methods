3849290ef82b5956fa2cec13196dd3beaede2886
Iryna Derenivska <iryna.derenivska@gmail.com>
2022-05-13 14:57:20 +0300

    Fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -16,26 +16,11 @@ function applyCustomJoin() {
       result += this[0];
     }
 
-    switch (separator) {
-      case '': {
-        for (let i = 1; i < this.length; i++) {
-          if ((this[i] === null) || (this[i] === undefined)) {
-            result += '';
-          } else {
-            result += this[i];
-          }
-        }
-        break;
-      }
-
-      default: {
-        for (let i = 1; i < this.length; i++) {
-          if ((this[i] === null) || (this[i] === undefined)) {
-            result = result + separator + '';
-          } else {
-            result = result + separator + this[i];
-          }
-        }
+    for (let i = 1; i < this.length; i++) {
+      if ((this[i] === null) || (this[i] === undefined)) {
+        result = result + separator + '';
+      } else {
+        result = result + separator + this[i];
       }
     }
 
