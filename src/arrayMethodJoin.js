79f788bae3ea6b38578e99abf357cd1677ef2ff8
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-05-19 18:27:02 +0300

    develop

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null) {
+        result += `${separator}`;
+      } else if (typeof this[i] === 'undefined' && i === this.length - 1) {
+        continue;
+      } else if (typeof this[i] === 'undefined') {
+        result += `${separator}`;
+      } else if (i === this.length - 1) {
+        result += `${this[i]}`;
+      } else {
+        result += `${this[i]}${separator}`;
+      }
+    }
+
+    return result;
   };
 }
 
