90b0491375d9af417924163c5cd8621d8890cb40
Volodumur Vitinskiy <vovahunter1989@gmail.com>
2022-05-19 12:49:53 +0300

    first try

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    let separ = separator;
+
+    if (separator === null) {
+      separ = 'null';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        result += separ;
+      } else {
+        result += this[i] + separ;
+      }
+    }
+
+    if (this[this.length - 1] !== null
+      && this[this.length - 1] !== undefined) {
+      result += this[this.length - 1];
+    }
+
+    return result;
   };
 }
 
