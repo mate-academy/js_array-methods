c59c56435a2d62ca571014ca63d892a2b326a45f
Kamil Witek <kamilprogramuje@gmail.com>
2022-05-22 11:20:15 +0200

    all tests passed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,33 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let result = '';
+    let newSeperator = separator;
+
+    if (separator === null) {
+      newSeperator = 'null';
+    }
+
+    for (const key of this) {
+      switch (key) {
+        case null:
+        case undefined:
+          result += newSeperator + '';
+          break;
+        default:
+          result += newSeperator + key;
+          break;
+      }
+    }
+
+    // remove first separator
+    if (result.includes(separator)) {
+      result = result.replace(separator, '');
+    }
+
+    return result;
   };
 }
 
