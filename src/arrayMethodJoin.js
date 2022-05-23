c6314ed2170fe47e003e8f93a1cf0d29ee4647b1
Oleh Kalynovskyi <oleh.kalynovskyi@nure.ua>
2022-05-23 14:07:33 +0300

    fix

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,16 +8,12 @@ function applyCustomJoin() {
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null) {
-        result += `${separator}`;
-      } else if (typeof this[i] === 'undefined' && i === this.length - 1) {
-        continue;
-      } else if (typeof this[i] === 'undefined') {
-        result += `${separator}`;
-      } else if (i === this.length - 1) {
-        result += `${this[i]}`;
-      } else {
-        result += `${this[i]}${separator}`;
+      result += (this[i] === undefined || this[i] === null)
+        ? ''
+        : this[i];
+
+      if (i < this.length - 1) {
+        result += separator;
       }
     }
 
