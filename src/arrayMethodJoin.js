2583782d961f54107d40aba293f94194c6acee28
Pavlo Pokryvets <pavelpokryvets@gmail.com>
2022-05-27 12:31:11 +0300

    First_solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,38 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let copySeparator = separator;
+    let firstElement = this[0];
+
+    if (separator === null) {
+      copySeparator = 'null';
+    }
+
+    if (firstElement === null) {
+      firstElement = '';
+    }
+
+    let str = '' + firstElement;
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      if (isNaN(this[i]) && this[i] !== 'null') {
+        this[i] = 'NaN';
+      }
+
+      str += copySeparator + this[i];
+    }
+
+    return str;
   };
 }
 
