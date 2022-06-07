49203c0f1b10f3d4fa67ea712a92f4ed06e3722b
Nikita Karbazin <karbazinnikita@gmail.com>
2022-06-07 22:44:22 +0300

    join method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,30 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const length = this.length;
+    let resultJoin = '';
+    let counter = 0;
+
+    while (counter < length) {
+      if (counter > 0) {
+        resultJoin += `${separator}`;
+      }
+
+      let next = '';
+      const elem = this[counter];
+
+      next = elem === null || elem === undefined ? '' : elem;
+      resultJoin += `${next}`;
+      counter++;
+    }
+
+    return resultJoin;
   };
 }
+
+const source = [0, 1, 2, 3];
+
+source.join(null);
 
 module.exports = applyCustomJoin;
