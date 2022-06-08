3a837df80e7d2bcb5ab6cef4054f336f01c3aa7f
Denys Halopa <galyopa@gmail.com>
2022-06-08 20:08:42 +0300

    changed to for ... of

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,12 +7,12 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let joinedStr = '';
 
-    for (let i = 0; i < this.length; i++) {
-      if (this[i] !== undefined && this[i] !== null) {
-        joinedStr += this[i];
+    for (const value of this) {
+      if (value !== undefined && value !== null) {
+        joinedStr += value;
       }
 
-      if (i !== this.length - 1) {
+      if (this.indexOf(value) !== this.length - 1) {
         joinedStr += separator;
       }
     }
