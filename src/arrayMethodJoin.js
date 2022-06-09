8b04a13d29995883d529ce30bffe1e1c325b404d
Maksym Stupin <masik1850@gmail.com>
2022-06-10 01:56:56 +0300

    done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let string = '';
+
+    for (let x = 0; x < this.length; x++) {
+      if (this[x] !== null && this[x] !== undefined) {
+        string += this[x];
+      }
+
+      if (x !== this.length - 1) {
+        string += separator;
+      }
+    }
+
+    return string;
   };
 }
 
