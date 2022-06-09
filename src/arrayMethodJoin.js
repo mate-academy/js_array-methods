adba9b8125fcfb0bd917eadf6abb4a605160f637
Volodymyr Shymanskyi <v.shimanskyi@bnb.net.ua>
2022-06-09 17:57:21 +0300

    1-st commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,33 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let sep;
+    let x;
+
+    if (separator !== undefined) {
+      sep = separator;
+    } else {
+      sep = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        x = '';
+      } else {
+        x = this[i];
+      }
+
+      if (x !== undefined) {
+        if (i < this.length - 1) {
+          result = result + x + sep;
+        } else {
+          result = result + x;
+        }
+      }
+    }
+
+    return result;
   };
 }
 
