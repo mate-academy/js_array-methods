19d1a3c493246b89a801e3c27c0fc0da743d48ad
Ihor Rybalko <garik.rybalko@gmail.com>
2022-06-09 19:53:29 +0300

    Join method solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let string = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const element = this[i];
+
+      if (element !== null && element !== undefined) {
+        string += element;
+      }
+
+      if (i < this.length - 1) {
+        string += separator;
+      }
+    }
+
+    return string;
   };
 }
 
