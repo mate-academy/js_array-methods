67b018a97170f45572d1be0a3dd6734dcd28148a
Stanislav Topikha <stanislav.topikha@gmail.com>
2022-06-09 13:06:12 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,17 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let string = '';
+    const paste = (separator === undefined) ? ',' : `${separator}`;
+
+    for (let i = 0; i < this.length; i++) {
+      let el = this[i];
+
+      el = (el === null || el === undefined) ? '' : `${el}`;
+      string += (i === this.length - 1) ? el : el + paste;
+    }
+
+    return string;
   };
 }
 
