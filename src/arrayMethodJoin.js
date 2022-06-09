21a5a6151cac417f1cc14fc58bed8a799dc09136
Stanislav Topikha <stanislav.topikha@gmail.com>
2022-06-09 22:31:31 +0300

    Simplified

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,11 +8,15 @@ function applyCustomJoin() {
     let string = '';
 
     for (let i = 0; i < this.length; i++) {
-      let element = this[i];
+      const element = this[i];
 
-      element = (element === null || element === undefined) ? '' : `${element}`;
+      string += (element === null || element === undefined)
+        ? ''
+        : element;
 
-      string += (i === this.length - 1) ? element : element + separator;
+      if (i !== this.length - 1) {
+        string += separator;
+      }
     }
 
     return string;
