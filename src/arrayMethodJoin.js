6694b0322443c3a489ce5e8e52eb771caa6bb981
SAMAEV <andreysamaev@gmail.com>
2022-06-09 17:40:06 +0300

    after s review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -9,15 +9,11 @@ function applyCustomJoin() {
 
     for (let i = 0; i < this.length; i++) {
       if (typeof this[i] !== 'undefined' && this[i] !== null) {
-        if (i !== this.length - 1) {
-          str = str + this[i] + separator;
-        } else {
-          str = str + this[i];
-        }
-      } else if (typeof this[i] === 'undefined' && i === this.length - 1) {
-        return str;
-      } else {
-        str = str + ',';
+        str += this[i];
+      }
+
+      if (i < this.length - 1) {
+        str += separator;
       }
     }
 
