d9589c34ed6566f3ea41f4664e9f9e1b908905ad
Kate Khon <katerynakhon@gmail.com>
2022-06-09 21:26:52 +0300

    initial commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedArr = '';
+
+    for (const element of this) {
+      if (typeof element === 'number'
+        || typeof element === 'string'
+        || typeof element === 'boolean') {
+        joinedArr += element;
+      }
+
+      joinedArr += separator;
+    }
+
+    const lastSeparator = joinedArr.lastIndexOf(separator);
+
+    return joinedArr.slice(0, lastSeparator);
   };
 }
 
