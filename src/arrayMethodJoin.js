d7676fcc0da47058a9ce8be3594e4f5d08af64c0
Roman Marunkevich <vinnibea@gmail.com>
2022-06-09 16:23:26 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -12,9 +12,7 @@ function applyCustomJoin() {
         result += separator;
       }
 
-      if (this[i] === null || typeof this[i] === 'undefined') {
-        result += '';
-      } else {
+      if (this[i] !== null && typeof this[i] !== 'undefined') {
         result += this[i];
       }
     }
