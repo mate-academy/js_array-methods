bde60a63f5baa9da0782ba34c23cf2afd11f209a
Lesya Savka <lesyasimple@gmail.com>
2022-06-09 16:21:33 +0300

    mistake were corrected

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let newSring = '';
+    let newString = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== undefined && this[i] !== null) {
-        newSring += this[i];
+        newString += this[i];
       }
 
       if (i !== this.length - 1) {
-        newSring += separator;
+        newString += separator;
       }
     }
 
-    return newSring;
+    return newString;
   };
 }
 
