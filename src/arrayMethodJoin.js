09bbd929a1c4cd98c1d8bcc1e2117c733717c159
Serhii Vorona <seregavorona94@gmail.com>
2022-06-10 09:01:13 +0300

    joinMethod

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,24 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let resultStr = '';
+    let sep = separator;
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        resultStr += this[i];
+      }
+
+      if (sep === undefined) {
+        sep = ',';
+      }
+
+      if (i !== this.length - 1) {
+        resultStr += sep;
+      }
+    }
+
+    return resultStr;
   };
 }
 
