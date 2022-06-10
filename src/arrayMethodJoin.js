c96ad458b1c6caf68d1629820f7213b7fae4580d
Alina Komashchuk <akomashchuk24@gmail.com>
2022-06-10 16:28:34 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,25 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    const checkSep = separator === undefined ? ',' : String(separator);
+
+    for (let i = 0; i < this.length; i++) {
+      const item = checkIfAcceptableValue(this[i]) ? this[i] : '';
+
+      if (i === this.length - 1) {
+        result += item;
+      } else {
+        result += item + checkSep;
+      }
+    }
+
+    return result;
   };
+};
+
+function checkIfAcceptableValue(value) {
+  return value !== undefined && value !== null;
 }
 
 module.exports = applyCustomJoin;
