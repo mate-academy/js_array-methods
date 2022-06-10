1e7556272c7388858666ca149364efbcaaebf7de
Andriy Hamernyk <0986758477and@gmail.com>
2022-06-10 11:28:34 +0300

    Method join created

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,32 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let divider = separator;
+    let string = '';
+
+    if (this.length === 0) {
+      return string;
+    }
+
+    if (divider === undefined) {
+      divider = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        if (i !== this.length - 1) {
+          string += divider;
+        }
+      } else if (this[i] !== undefined || this[i] !== null) {
+        string += `${this[i]}`;
+
+        if (i !== this.length - 1) {
+          string += divider;
+        }
+      }
+    }
+
+    return string;
   };
 }
 
