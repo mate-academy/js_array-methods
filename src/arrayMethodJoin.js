faad7abc053b859010dbeaccb1cdf32bf5694443
Ivan Grabovsky <ivan.grabovsky.ua@gmail.com>
2022-06-10 19:35:29 +0300

    optimized solution a little bit v2

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,17 +5,12 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    if (this.length === 0) {
-      return '';
-    }
-
     let returnString = '';
-    const copyArray = this;
     const parseSeparator = separator === null ? 'null' : separator;
 
-    for (let i = 0; i < copyArray.length; i++) {
-      if (copyArray[i] !== undefined && copyArray[i] !== null) {
-        returnString += copyArray[i];
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        returnString += this[i];
       }
 
       if (this.length - 1 !== i) {
