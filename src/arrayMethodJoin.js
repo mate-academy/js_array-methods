0d7cafbfa6bf1f71d26c2c77220db1a2fea15138
Andrii Nychaiuk <anichajuk@gmail.com>
2022-06-10 08:58:26 +0300

    logic was simplified

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,21 +7,17 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let connected = '';
 
-    for (let i = 0; i < this.length - 1; i++) {
-      if (separator === null) {
-        connected += `${this[i]}${null}`;
-      } else if (this[i] === null || this[i] === undefined) {
-        connected += '' + separator;
-      } else {
-        connected += this[i] + separator;
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        connected += this[i];
+      }
+
+      if (this.length - 1 !== i) {
+        connected += separator;
       }
     }
 
-    if (this[this.length - 1] === undefined) {
-      return connected;
-    }
-
-    return connected + this[this.length - 1];
+    return connected;
   };
 }
 
