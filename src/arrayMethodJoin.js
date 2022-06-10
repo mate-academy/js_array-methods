5e232360d7a5707a4c3d4bb8a8a37f4d19702b10
Dmitrii Pirohov <dmitriy_pirogov@live.com>
2022-06-10 17:12:37 +0300

    first version

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,25 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    const arr = this;
+    let sepCopy = separator;
+    let strResult = '';
+
+    if (sepCopy === undefined) {
+      sepCopy = ',';
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== undefined && arr[i] !== null) {
+        strResult += arr[i];
+      }
+
+      if (i !== this.length - 1) {
+        strResult += sepCopy;
+      }
+    }
+
+    return strResult;
   };
 }
 
