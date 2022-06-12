9412f4018ca2712b39b628002d137a6e85bd4108
Dmitrii Pirohov <dmitriy_pirogov@live.com>
2022-06-12 22:03:47 +0300

    second version

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,23 +4,19 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    const arr = this;
-    let sepCopy = separator;
+  [].__proto__.join2 = function(separator = ',') {
     let strResult = '';
 
-    if (sepCopy === undefined) {
-      sepCopy = ',';
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        strResult += this[i];
+      }
+
+      strResult += separator;
     }
 
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] !== undefined && arr[i] !== null) {
-        strResult += arr[i];
-      }
-
-      if (i !== this.length - 1) {
-        strResult += sepCopy;
-      }
+    if (this[this.length - 1] !== null && this[this.length - 1] !== undefined) {
+      strResult += this[this.length - 1];
     }
 
     return strResult;
