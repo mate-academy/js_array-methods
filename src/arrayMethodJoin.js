917ebe4e6c92e04433a171fad6a4ef3848ff1fad
Denys Politov <dpolitov2001@gmail.com>
2022-06-12 20:32:49 +0300

    Completed the task

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,30 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedStr = '';
+
+    if (separator === null) {
+      // eslint-disable-next-line no-param-reassign
+      separator = 'null';
+    }
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        joinedStr += this[i];
+      }
+      joinedStr += separator;
+    }
+
+    if (this[this.length - 1] !== null && this[this.length - 1] !== undefined) {
+      joinedStr += this[this.length - 1];
+    }
+
+    return joinedStr;
   };
 }
 
