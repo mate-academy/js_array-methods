404a3bd5c357d0c5fab5d9d7bda8fec260239de7
Yaroslav Baiduk <byaa99@gmail.com>
2022-06-13 14:26:22 +0300

    Compleated

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedArray = '';
+
+    for (let q = 0; q < this.length; q++) {
+      if (this[q] !== undefined & this[q] !== null) {
+        joinedArray += this[q];
+      }
+
+      if (this[q] !== this[this.length - 1]) {
+        joinedArray += separator;
+      }
+    }
+
+    return joinedArray;
   };
 }
 
