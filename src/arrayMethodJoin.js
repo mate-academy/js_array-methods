a0c16162af002bbde704ed23bd7d468402bf08fa
Ihor Shynkar <aedricson@gmail.com>
2022-06-14 12:32:28 +0300

    more complex solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,24 +5,24 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let result = '';
+    let joinedString = '';
+    const lastElement = this[this.length - 1];
 
-    for (let i = 0; i < this.length; i++) {
+    for (let i = 0; i < this.length - 1; i++) {
       let currentElement = this[i];
 
       if (currentElement === null || currentElement === undefined) {
         currentElement = '';
       }
 
-      if (i === this.length - 1) {
-        result += currentElement;
-        break;
-      }
-
-      result += `${currentElement}` + `${separator}`;
+      joinedString += `${currentElement}${separator}`;
     }
 
-    return result;
+    if (lastElement !== undefined) {
+      joinedString += lastElement;
+    }
+
+    return joinedString;
   };
 }
 
