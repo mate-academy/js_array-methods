3702ca4509aa460b6e99be4cdb170198d837f314
Andriy Hamernyk <0986758477and@gmail.com>
2022-06-14 21:10:40 +0300

    Reworked

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,29 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    let divider = separator;
+  [].__proto__.join2 = function(separator = ',') {
     let string = '';
 
     if (this.length === 0) {
       return string;
     }
 
-    if (divider === undefined) {
-      divider = ',';
-    }
-
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === undefined || this[i] === null) {
-        if (i !== this.length - 1) {
-          string += divider;
-        }
-      } else if (this[i] !== undefined || this[i] !== null) {
-        string += `${this[i]}`;
+      if (this[i] !== undefined && this[i] !== null) {
+        string += this[i];
+      }
 
-        if (i !== this.length - 1) {
-          string += divider;
-        }
+      if (this.indexOf(this[i]) !== this.length - 1) {
+        string += separator;
       }
     }
 
