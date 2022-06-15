1e05fc6f95dec6667d7b28185f363195081293de
PaulRodriguez2049 <paul.rodriguez.2049@gmail.com>
2022-06-15 22:04:52 +0300

    first commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,28 @@
 /**
  * Implement method join
  */
+
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let string = '';
+    let insert = '';
+
+    for (const el of this) {
+      if (el !== null && el !== undefined) {
+        insert = el;
+      }
+
+      if (el === this[this.length - 1]) {
+        string += '' + insert;
+      } else {
+        string += '' + insert + separator;
+      }
+
+      insert = '';
+    }
+
+    return string;
   };
 }
 
