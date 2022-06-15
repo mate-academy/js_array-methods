298b8f5c8786174f1ed370042b8e8b8e20f33534
Katia Zhurenko <87322698+Katia-zz@users.noreply.github.com>
2022-06-15 21:40:46 +0200

    first\

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let res = '';
+
+
+    for (const word of this) {
+      if (word !== undefined && word !== null) {
+        res += `${word}`;
+
+        if (res.length === (this.length) * 2 - 1) {
+          break;
+        }
+        res += `${separator}`;
+      }
+    }
+
+    return res;
   };
 }
 
