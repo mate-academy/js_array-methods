c1da500e65b09f729d849cacd0e81f454927051d
Alex Burkivskyi <bur4ik003@gmail.com>
2022-06-18 09:48:30 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,21 +7,17 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     const length = this.length;
     let resultJoin = '';
-    let counter = 0;
 
-    while (counter < length) {
-      if (counter > 0) {
-        resultJoin += separator;
+    for (let i = 0; i < length; i++) {
+      const current = this[i];
+
+      if (current !== undefined && current !== null) {
+        resultJoin += current;
       }
 
-      const elem = this[counter];
-
-      const next = elem === null || elem === undefined
-        ? ''
-        : elem;
-
-      resultJoin += next;
-      counter++;
+      if (i < length - 1) {
+        resultJoin += separator;
+      }
     }
 
     return resultJoin;
