4636399a4f50a71faf75e7c68251b6ea145236ca
Zykhor Andrii <zuxoran@gmail.com>
2022-06-27 11:30:12 +0300

    task done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,31 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length === 0) {
+      return '';
+    }
+
+    let result = '';
+
+    if (typeof this[0] === 'object' || typeof this[0] === 'undefined') {
+    } else {
+      result = String(this[0]);
+    }
+
+    if (this.length === 1) {
+      return result;
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if (typeof this[i] === 'object' || typeof this[i] === 'undefined') {
+        result += separator;
+      } else {
+        result += `${separator}${this[i]}`;
+      }
+    }
+
+    return result;
   };
 }
 
