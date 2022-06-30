fe617008ee192ddf97cf9b96270f861945e05716
Vladimir Yurkov <vladimir.yurkov.91@gmail.com>
2022-06-30 14:47:37 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,35 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+
+    if (this.length === 0) {
+      return result;
+    }
+
+    let delimiter = '';
+
+    switch (separator) {
+      case undefined:
+        delimiter = ',';
+        break;
+      case null:
+        delimiter = 'null';
+        break;
+      default:
+        delimiter = separator.toString();
+        break;
+    }
+
+    for (const item of this) {
+      const itemValue = item === undefined || item === null ? '' : item;
+
+      result += itemValue + delimiter;
+    }
+
+    result = result.substring(0, result.length - delimiter.length);
+
+    return result;
   };
 }
 
