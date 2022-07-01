df89774979e0fbae75fd1b5383be6b063a8774c1
Anton Chetveryk <chetverykantonma@gmail.com>
2022-07-01 19:18:05 +0300

    fixed after review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,39 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    let str = '';
-
-    if (this.length === 1) {
-      return this.toString();
-    }
+    let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      const cur = this[i] === null || this[i] === undefined ? '' : this[i];
+      if (this[i] !== undefined && this[i] !== null) {
+        result += `${this[i]}`;
+      }
 
-      if (separator) {
-        str += cur + separator;
-      } else {
-        if (separator === '') {
-          str += cur;
-        } else if (separator === null) {
-          str += cur + 'null';
-        } else {
-          str += cur + ',';
-        }
+      if (i < this.length - 1) {
+        result += separator;
       }
     }
 
-    if (separator !== '') {
-      if (separator === null) {
-        str = str.slice(0, -4);
-      } else if (typeof separator === 'object') {
-        str = str.slice(0, -15);
-      } else {
-        str = str.slice(0, -1);
-      };
-    }
-
-    return str;
+    return result;
   };
 }
 
