d18a4f41c34e3349f91c6f5aaee5a0f60ddac438
Sergii Maniurov <maniurov_sv@ukr.net>
2022-07-04 20:36:25 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,18 +7,14 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let string = '';
 
-    if (this.length !== 0) {
-      if (this[0] !== undefined && this[0] !== null) {
-        string += this[0];
-      }
+    if (this.length > 0) {
+      for (let i = 0; i < this.length; i++) {
+        if (this[i] !== undefined && this[i] !== null) {
+          string += this[i];
+        }
 
-      if (this.length > 1) {
-        for (let i = 1; i < this.length; i++) {
-          if (this[i] !== undefined && this[i] !== null) {
-            string += String(separator) + this[i];
-          } else {
-            string += String(separator) + '';
-          }
+        if (i !== this.length - 1) {
+          string += String(separator);
         }
       }
     }
