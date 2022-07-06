37e5aaaecc551acba4ff1c6e72bcdf10010940d5
Oleksandr-Pustovit <Pustovit.o2020@gmail.com>
2022-07-06 11:44:25 +0300

    added join function

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,31 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      switch (this[i]) {
+        case undefined:
+          break;
+
+        case null:
+          break;
+
+        case false:
+          result += 'false';
+          break;
+
+        default:
+          result += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        result += separator;
+      }
+    }
+
+    return result;
   };
 }
 
