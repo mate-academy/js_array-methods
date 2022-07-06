e597b81b82feeac6ecee00d08bc19542cccfd053
DashaPoliazz <dasha.poliazzz@gmail.com>
2022-07-06 15:42:42 +0300

    solution 2

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,58 +4,34 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
-    if (typeof separator === 'number' && !isNaN(separator)) {
-      return this;
-    }
+    let result = '';
+    const lastElement = this[this.length - 1];
 
-    const stringConsctuctor = (separatorSymbol) => {
-      let result = '';
+    for (let i = 0; i < this.length - 1; i++) {
+      const word = this[i];
 
-      for (let i = 0; i < this.length; i++) {
-        const currentElement = this[i];
-        const isLastElement = i === this.length - 1;
-        const isElementValid = currentElement === undefined
-          || currentElement === null
-          || currentElement === '';
-
-        if (isLastElement) {
-          result += `${currentElement}`;
-        } else if (isElementValid) {
-          result += `INVALID_PROPERTY${separatorSymbol}`;
-        } else {
-          result += `${currentElement}${separatorSymbol}`;
-        }
+      if (word !== null && word !== undefined) {
+        result += `${this[i]}`;
       }
 
-      const splittedResult = result.split(separatorSymbol);
-
-      for (let i = 0; i < splittedResult.length; i++) {
-        const current = splittedResult[i];
-
-        if (current === 'INVALID_PROPERTY') {
-          result = result.replace('INVALID_PROPERTY', '');
-        }
-      }
-
-      if (result.includes('undefined')) {
-        result = result.replace('undefined', '');
-      }
-
-      return result;
-    };
-
-    if (separator === undefined) {
-      return stringConsctuctor(',');
+      result += `${separator}`;
     }
 
-    if (separator === null) {
-      return stringConsctuctor('null');
+    if (!this.length) {
+      return '';
     }
 
-    return stringConsctuctor(separator);
+    if (lastElement !== null && lastElement !== undefined) {
+      result += `${lastElement}`;
+    }
+
+    return result;
   };
 }
+
+applyCustomJoin();
+[0, 1, 2, 3].join2();
 
 module.exports = applyCustomJoin;
