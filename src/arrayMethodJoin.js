2336665641478a0c1b635994a05baebc6c663ec9
Leoned Pustovit <lrerednawl@gmail.com>
2022-07-06 11:58:22 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,44 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length === 0) {
+      return '';
+    }
+
+    let divider = separator;
+    let joinedString = '';
+
+    if (separator === null) {
+      divider = 'null';
+    }
+
+    if (this[0] !== null) {
+      joinedString = '' + this[0];
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      joinedString += '' + divider;
+
+      switch (this[i]) {
+        case undefined:
+
+          break;
+
+        case NaN:
+          joinedString += 'NaN';
+          break;
+
+        case null:
+          break;
+
+        default:
+          joinedString += '' + this[i];
+          break;
+      }
+    }
+
+    return '' + joinedString;
   };
 }
 
