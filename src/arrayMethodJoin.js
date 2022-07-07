38a28b491136b8143f0401b39dc7e187e86b8cea
Olena Rodiontseva <elena.rodiontseva@gmail.com>
2022-07-07 15:49:17 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,33 @@
 /**
  * Implement method join
  */
+
+//подставить новые сепараторы
+//перебрать массив
+//если эл ок - добавить в новую строку
+//соследний эл доб без сапаратора
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let joinedItems = '';
+    let divider = separator;
+
+    if (separator === undefined) {
+      divider = ',';
+    } else if (separator === null) {
+      divider = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        joinedItems += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        joinedItems += divider;
+      }
+    }
+
+    return joinedItems;
   };
 }
 
