b3ab219560ca1d392875b63c055024bdf84e88d5
Oleksiy Uchaev <corob.dir@gmail.com>
2022-07-07 14:29:42 +0300

    join method ready for use :)

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,10 +3,24 @@
 /**
  * Implement method join
  */
-function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+function applyCustomJoin(...data) {
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedIntoString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        joinedIntoString += '';
+      } else if (this[i] !== ' ') {
+        joinedIntoString += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        joinedIntoString += separator;
+      }
+    }
+
+    return joinedIntoString;
   };
-}
+};
 
 module.exports = applyCustomJoin;
