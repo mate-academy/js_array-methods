99da3f70f82fffc9f8a17923f086a6bf96d5282f
Ruslan Petrenko <ruslanpetrenko2203@gmail.com>
2022-07-08 00:59:42 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,33 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+
+    if (this.length) {
+      for (let i = 0; i < this.length; i++) {
+        let arrEl = this[i];
+
+        if (this[i] === undefined || this[i] === null) {
+          arrEl = '';
+        }
+
+        if (separator === '' || separator || separator === null) {
+          if (this.length - 1 === i) {
+            result += arrEl;
+          } else {
+            result += arrEl + '' + separator;
+          }
+        } else {
+          if (this.length - 1 === i) {
+            result += arrEl;
+          } else {
+            result += arrEl + ',';
+          }
+        }
+      }
+    };
+
+    return result;
   };
 }
 
