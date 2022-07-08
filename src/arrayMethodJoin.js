f035a7ab7dfe2a3eaf1181270703b298802a7230
lentyaeev <daniilshaliuk@gmail.com>
2022-07-08 20:28:15 +0300

    little fix

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,23 +6,23 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let string = '';
+    let separatorToAdd = separator;
+
+    if (separator === null) {
+      separatorToAdd = 'null';
+    }
 
     this.forEach((element, index) => {
       let elementToAdd = element;
-      let separatorToAdd = separator;
-
-      if (separator === null) {
-        separatorToAdd = 'null';
-      }
 
       if (elementToAdd === undefined || elementToAdd === null) {
         elementToAdd = '';
       }
 
-      if (index === this.length - 1) {
-        string += elementToAdd;
-      } else {
-        string += elementToAdd + separatorToAdd;
+      string += elementToAdd;
+
+      if (index !== this.length - 1) {
+        string += separatorToAdd;
       }
     });
 
