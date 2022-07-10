8f5d8006fc3109994e604e78bb973b2826f62d5f
Yuliia Dronova <106536680+yuliia-dronova@users.noreply.github.com>
2022-07-10 12:59:08 +0300

    Join method was applied

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joined = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (this.length === 1) {
+      return this[0].toString();
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        this[i] = '';
+      }
+      joined += this[i].toString() + separator;
+    }
+
+    if (this[this.length - 1] === undefined || this[this.length - 1] === null) {
+      this[this.length - 1] = '';
+    }
+
+    return joined + this[this.length - 1];
   };
 }
 
