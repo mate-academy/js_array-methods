01c3f1bf428a817933db0ea9e60fbbb6fb72aae8
Yuliia Dronova <106536680+yuliia-dronova@users.noreply.github.com>
2022-07-10 13:19:03 +0300

    one if stetement was replaced to cycle for optimisation

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -15,15 +15,15 @@ function applyCustomJoin() {
       return this[0].toString();
     }
 
-    for (let i = 0; i < this.length - 1; i++) {
+    for (let i = 0; i < this.length; i++) {
       if (this[i] === undefined || this[i] === null) {
         this[i] = '';
       }
-      joined += this[i].toString() + separator;
-    }
 
-    if (this[this.length - 1] === undefined || this[this.length - 1] === null) {
-      this[this.length - 1] = '';
+      if (i === this.length - 1) {
+        break;
+      }
+      joined += this[i].toString() + separator;
     }
 
     return joined + this[this.length - 1];
