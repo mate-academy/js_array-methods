1ab7ed5988be38ecbc384bed53fac629df6c65f2
Roman Romanenko <romanenko1996@meta.ua>
2022-07-10 15:31:56 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,23 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let joinText = '';
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (typeof (separator) === 'undefined') {
+        joinText += this[i] + ',';
+      } else if (this[i] === null || typeof (this[i]) === 'undefined') {
+        joinText += ',';
+      } else {
+        joinText += this[i] + String(separator);
+      }
+    }
+
+    if (typeof (this[this.length - 1]) !== 'undefined') {
+      joinText += this[this.length - 1];
+    }
+
+    return joinText;
   };
 }
 
