7d5d042b83aab68444ce8dd4cc1987b03e74b2cc
PavloMarkov <r9markovpn@gmail.com>
2022-07-11 12:20:52 +0300

    all checks were placed in one loop

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,48 +5,22 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let sep = separator;
     let joinedText = '';
 
-    if (separator === null) {
-      sep = 'null';
-    }
-
-    if (this.length === 1) {
-      joinedText += this[0];
-
-      return joinedText;
-    } else if (this.length > 1) {
-      switch (this[0]) {
+    for (let i = 0; i < this.length; i++) {
+      switch (this[i]) {
         case null:
+        case undefined:
           joinedText += '';
           break;
 
         default:
-          joinedText += this[0];
+          joinedText += this[i];
           break;
       }
-    }
 
-    if (this.length > 1) {
-      for (let i = 1; i < this.length; i++) {
-        let elem = this[i];
-
-        switch (this[i]) {
-          case null:
-            elem = '';
-            break;
-
-          case undefined:
-            elem = '';
-            break;
-
-          default:
-            elem = this[i];
-            break;
-        }
-        joinedText += (sep + elem);
-        elem = '';
+      if (i !== this.length - 1) {
+        joinedText += separator;
       }
     }
 
