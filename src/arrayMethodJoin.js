5850a6bbdeff7ffcf5880ea14d137658ea48b589
Roman Romanenko <romanenko1996@meta.ua>
2022-07-11 15:12:48 +0300

    fixed problems after review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,20 +4,18 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let joinText = '';
 
     for (let i = 0; i < this.length - 1; i++) {
-      if (typeof (separator) === 'undefined') {
-        joinText += this[i] + ',';
-      } else if (this[i] === null || typeof (this[i]) === 'undefined') {
-        joinText += ',';
-      } else {
-        joinText += this[i] + String(separator);
+      if (this[i] != null) {
+        joinText += `${this[i]}`;
       }
+
+      joinText += `${separator}`;
     }
 
-    if (typeof (this[this.length - 1]) !== 'undefined') {
+    if (this.length > 0 && this[this.length - 1] != null) {
       joinText += this[this.length - 1];
     }
 
