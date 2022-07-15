8cbbd9ec5dcd8b50dbbc62b665bb6606faa548a8
Anastasiya <105457299+Anastasiya145@users.noreply.github.com>
2022-07-15 10:36:01 +0200

    the method join was created

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,25 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if ((this[i] === null) || (this[i] === undefined)) {
+        result += '';
+      } else {
+        result += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        if (separator === undefined) {
+          result += ',';
+        } else {
+          result += separator;
+        }
+      }
+    }
+
+    return result;
   };
 }
 
