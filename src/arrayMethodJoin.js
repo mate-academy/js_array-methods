a646067aad2c2cfa05925f88de26778eb3765149
Bohdan Tron <ikwiczzts@gmail.com>
2022-07-19 16:53:33 +0300

    array method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,26 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length === 0) {
+      return '';
+    }
+
+    let join = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        join += '';
+      } else {
+        join += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        join += separator;
+      }
+    }
+
+    return join;
   };
 }
 
