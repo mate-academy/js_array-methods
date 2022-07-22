6d26c14c80647c2583bf64139bd8ea3c7f2a7601
Denys Topchyi <miliarder97@gmail.com>
2022-07-22 14:32:04 +0300

    method joined

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1 && this[i] === undefined) {
+        continue;
+      }
+
+      if (this[i] === null || this[i] === undefined) {
+        str += separator;
+      } else if (i === this.length - 1) {
+        str += this[i];
+      } else {
+        str = str + this[i] + separator;
+      }
+    }
+
+    return str;
   };
 }
 
