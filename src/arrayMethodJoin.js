e6229ef9398b91e5f83625e45a738bce0ecade72
Maria Bramnik <mariabramnik@gmail.com>
2022-07-23 00:26:21 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,58 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let stringResult = '';
+    let sep;
+
+    if (this.length === 0) {
+      return stringResult;
+    }
+
+    if (this.length === 1) {
+      stringResult += this[0];
+
+      return stringResult;
+    }
+
+    if (arguments.length === 0 || arguments[0] === undefined) {
+      sep = ',';
+    } else if (arguments[0] === null) {
+      sep = 'null';
+    } else {
+      sep = arguments[0];
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined) {
+        if (i !== this.length - 1) {
+          stringResult += sep;
+        }
+        continue;
+      } else if (this[i] === null) {
+        stringResult += sep;
+        continue;
+      } else if (this[i] === 'null') {
+        stringResult += null;
+        stringResult += sep;
+        continue;
+      } else if (isNaN(this[i])) {
+        stringResult += 'NaN';
+        stringResult += sep;
+        continue;
+      } else if (this[i] === false) {
+        stringResult += 'false';
+        stringResult += sep;
+        continue;
+      }
+
+      stringResult += this[i];
+
+      if (i !== this.length - 1) {
+        stringResult += sep;
+      }
+    }
+
+    return stringResult;
   };
 }
 
