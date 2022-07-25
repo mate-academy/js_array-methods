93f74fc32594349e96b833d2764aea4e68de9068
Mark Shapka <mark.shapka1@gmail.com>
2022-07-25 23:36:50 +0300

    Task_arrayMethodJoin

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,30 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let string = '';
+    let sep = ',';
+
+    if (separator !== undefined) {
+      sep = separator + '';
+    }
+
+    if (this.length === 0) {
+      return string;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if ((this[i] === null || this[i] === undefined)) {
+        if (i !== 0) {
+          string += sep;
+        }
+      } else if (i === 0) {
+        string += this[i];
+      } else {
+        string += (sep + this[i]);
+      }
+    }
+
+    return string;
   };
 }
 
