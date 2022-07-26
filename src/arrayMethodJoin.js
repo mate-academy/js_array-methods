50dec4e77a1250920d7fac2517933b594f3a5ba2
Dmytro Sheremetiev <dmytriy.sheremetiev@gmail.com>
2022-07-26 07:53:15 +0300

    First commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,36 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let newStr = '';
+    let sep = separator;
+
+    switch (this.length) {
+      case 0:
+        return '';
+
+      case 1:
+        return `${this[0]}`;
+    };
+
+    if (separator === null) {
+      sep = 'null';
+    }
+
+    for (let el = 0; el < this.length - 1; el++) {
+      if ((this[el] === undefined || this[el] === null)) {
+        newStr += sep;
+        continue;
+      }
+
+      newStr += this[el] + sep;
+    }
+
+    if (this[this.length - 1] !== undefined) {
+      newStr += this[this.length - 1];
+    }
+
+    return newStr;
   };
 }
 
