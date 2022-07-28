356796a2d557a0c62846ff10d569f8b4c60fb49c
Yevhen Pavlovskyi <pavlovskiy07@gmail.com>
2022-07-28 12:12:00 +0300

    all done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,21 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let x = 0; x < this.length; x++) {
+      if (this[x] !== undefined && this[x] !== null) {
+        result += this[x];
+      }
+
+      if (x + 1 !== this.length) {
+        result += separator;
+      }
+    }
+
+    return result;
   };
-}
+};
 
 module.exports = applyCustomJoin;
