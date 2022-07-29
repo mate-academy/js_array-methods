4a091ef83595e4e537cf3556776104d2e4f29cc6
p-omogit-e <polinakotyrlo@gmail.com>
2022-07-29 14:26:24 +0300

    method join done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let rez = '';
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        rez += '';
+      } else {
+        rez += this[i];
+      }
+      rez += separator;
+    }
+
+    if (this[this.length - 1] === null || this[this.length - 1] === undefined) {
+      rez += '';
+    } else {
+      rez += this[this.length - 1];
+    }
+
+    return rez;
   };
 }
 
