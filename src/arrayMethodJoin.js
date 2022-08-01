06921059ce4db22b22168c2472b2c672e18ef0d9
Pelahiia Khalishkhova <pelahiia.kh@gmail.com>
2022-08-01 14:22:42 +0200

    second version

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,26 +7,18 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
 
-    if (this.length < 1) {
-      result += this;
-
-      return result;
-    }
-
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === undefined || this[i] === null) {
-        this[i] = '';
+      if (this[i] !== undefined && this[i] !== null) {
+        result += this[i];
       }
 
-      if (i === this.length - 1) {
-        result += this[i];
-      } else {
-        result += this[i] + String(separator);
+      if (i !== this.length - 1) {
+        result += separator;
       }
     }
 
     return result;
   };
-}
+};
 
 module.exports = applyCustomJoin;
