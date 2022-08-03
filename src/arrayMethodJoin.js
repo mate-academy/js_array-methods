559ddb47ed9a1cba3f6d5fa707e24a6d060f6134
sergii-nosachenko <s.nosachenko@gmail.com>
2022-08-03 11:48:00 +0300

    Task solution code

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,18 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+
+    for (let i = 0; i < this.length; i++) {
+      str += this[i] != null ? this[i] : '';
+
+      if (i !== this.length - 1) {
+        str += separator;
+      }
+    }
+
+    return str;
   };
 }
 
