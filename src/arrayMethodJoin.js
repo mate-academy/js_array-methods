3bb094499957ef00f8d2f54a552dcc96a536fd9b
Maria Bramnik <mariabramnik@gmail.com>
2022-08-06 18:09:18 +0300

    Fixes after code review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,60 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
-    let stringResult = '';
-    let sep;
-
-    if (this.length === 0) {
-      return stringResult;
-    }
-
-    if (this.length === 1) {
-      stringResult += this[0];
-
-      return stringResult;
-    }
-
-    if (arguments.length === 0 || arguments[0] === undefined) {
-      sep = ',';
-    } else if (arguments[0] === null) {
-      sep = 'null';
-    } else {
-      sep = arguments[0];
-    }
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === undefined) {
-        if (i !== this.length - 1) {
-          stringResult += sep;
-        }
-        continue;
-      } else if (this[i] === null) {
-        stringResult += sep;
-        continue;
-      } else if (this[i] === 'null') {
-        stringResult += null;
-        stringResult += sep;
-        continue;
-      } else if (isNaN(this[i])) {
-        stringResult += 'NaN';
-        stringResult += sep;
-        continue;
-      } else if (this[i] === false) {
-        stringResult += 'false';
-        stringResult += sep;
-        continue;
+      if (this[i] !== undefined && this[i] !== null) {
+        result += `${this[i]}`;
       }
 
-      stringResult += this[i];
-
-      if (i !== this.length - 1) {
-        stringResult += sep;
+      if (i < this.length - 1) {
+        result += separator;
       }
     }
 
-    return stringResult;
+    return result;
   };
 }
 
