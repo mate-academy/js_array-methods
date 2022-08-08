ebca7fb1b229e4ad84c8199993eaa0bfaad7762d
Volodymyr Chyuko <vol.chuyko@gmail.com>
2022-08-08 16:26:05 +0300

    Optimize code after review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,24 +4,18 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let str = '';
-    let currentSeparator;
+    let currentSeparator = '';
 
-    if (separator === undefined) {
-      currentSeparator = ',';
-    } else if (separator === null) {
-      currentSeparator = 'null';
-    } else {
-      currentSeparator = separator.toString();
-    }
+    currentSeparator += separator;
 
     for (const element of this) {
-      if (element === undefined || element === null) {
-        str += currentSeparator;
-      } else {
-        str += element + currentSeparator;
+      if (element !== undefined && element !== null) {
+        str += element;
       }
+
+      str += currentSeparator;
     }
 
     if (currentSeparator) {
