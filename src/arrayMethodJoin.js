4e86840153a3e25d5a6918ee36fcdff6a464dfe3
Kostya Kosyuk <80011474+Kostya7337@users.noreply.github.com>
2022-08-10 01:05:44 +0300

    done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,31 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let joinedString = '';
+    let defaultSeparator = ',';
+    const length = this.length;
+
+    if (typeof (separator) !== 'undefined') {
+      defaultSeparator = '' + separator;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if ((this[i] === undefined || this[i] === null) && i !== length - 1) {
+        joinedString += defaultSeparator;
+        continue;
+      } else if (this[i] === undefined && i === length - 1) {
+        continue;
+      }
+
+      if (this[i] !== this[this.length - 1]) {
+        joinedString += this[i] + defaultSeparator;
+      } else {
+        joinedString += this[i];
+      }
+    }
+
+    return joinedString;
   };
-}
+};
 
 module.exports = applyCustomJoin;
