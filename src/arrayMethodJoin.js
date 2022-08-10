35db9a54925a6b9b7c7f0329450d62c220ca006a
NataliyaAn <otchet333@gmail.com>
2022-08-10 21:41:46 +0300

    Join- solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,28 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    const signSepar = (!separator && separator !== null && separator !== '')
+      ? ','
+      : separator;
+
+    if (this.length > 0) {
+      for (let i = 0; i < this.length - 1; i++) {
+        if (this[i] !== undefined
+          && this[i] !== null) {
+          result += this[i] + String(signSepar);
+        } else {
+          result += String(signSepar);
+        }
+      }
+
+      if (this[this.length - 1] !== undefined
+        && this[this.length - 1] !== null) {
+        result += this[this.length - 1];
+      }
+    }
+
+    return result;
   };
 }
 
