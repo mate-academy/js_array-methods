ad5b205217cc6d730f38703ab5b7be5d70b72b21
Roman Boiar <rmboiar@gmai.com>
2022-08-10 15:28:47 +0200

    task solution code

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,21 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let stringOutcome = '';
+    const lastItem = this.length - 1;
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        stringOutcome += this[i];
+      }
+
+      if (i !== lastItem) {
+        stringOutcome += separator;
+      }
+    }
+
+    return stringOutcome;
   };
 }
 
