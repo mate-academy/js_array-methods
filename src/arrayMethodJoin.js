87dd4aacd84232b049f8cf6eab6660db47343fd1
Kostya Kosyuk <80011474+Kostya7337@users.noreply.github.com>
2022-08-11 15:36:56 +0300

    SIMPLE DONE

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,28 +4,19 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let joinedString = '';
-    let defaultSeparator = ',';
-    const length = this.length;
-
-    if (typeof (separator) !== 'undefined') {
-      defaultSeparator = '' + separator;
-    }
 
     for (let i = 0; i < this.length; i++) {
-      if ((this[i] === undefined || this[i] === null) && i !== length - 1) {
-        joinedString += defaultSeparator;
-        continue;
-      } else if (this[i] === undefined && i === length - 1) {
+      if (this[i] !== undefined && this[i] !== null) {
+        joinedString += this[i];
+      }
+
+      if (i === this.length - 1) {
         continue;
       }
 
-      if (this[i] !== this[this.length - 1]) {
-        joinedString += this[i] + defaultSeparator;
-      } else {
-        joinedString += this[i];
-      }
+      joinedString += separator;
     }
 
     return joinedString;
