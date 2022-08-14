33dfef5d0395f17f63662a20ff9e34297ef6cf8d
Oleksandr Babmyndra <alphamonik1@gmail.com>
2022-08-14 17:05:17 +0300

    Alternative method join was created

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,31 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length === 0) {
+      return '';
+    } else if (this.length === 1) {
+      return String(this[0]);
+    }
+
+    let result;
+    const separatorToString = String(separator);
+
+    if (this[0] === undefined || this[0] === null) {
+      result = '';
+    } else {
+      result = this[0];
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        result += separatorToString;
+        continue;
+      }
+      result += separatorToString + String(this[i]);
+    }
+
+    return result;
   };
 }
 
