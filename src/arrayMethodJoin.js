ae159c5a0059cca1ba5602f90aabf45159215440
Vadls <vadim_tanasienko54541@ukr.net>
2022-08-14 13:48:59 +0300

    Comments removed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,22 +6,15 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let arr = '';
-    const separatorCopy = separator + '';
-    // console.log(this);
+    const separatorString = separator + '';
 
     for (let i = 0; i < this.length; i++) {
       arr += (this[i] === undefined || this[i] === null ? '' : this[i])
-      + (i < this.length - 1 ? separatorCopy : '');
-    // console.log(this[i]);
+      + (i < this.length - 1 ? separatorString : '');
     }
-    // console.log(arr);
 
     return arr;
   };
 }
-
-// const source = [1, 2, 3, 4];
-
-// [false, null, 1, undefined, 0, NaN, ''].join2();
 
 module.exports = applyCustomJoin;
