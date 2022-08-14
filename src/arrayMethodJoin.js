5a3c8842224c1723f77fb455b09ed642131c4d89
Dima Kobzar <kobzar7dima@gmail.com>
2022-08-14 15:30:10 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,15 +5,10 @@
  */
 
 function applyCustomJoin() {
-  // let str = '';
-  // let t = [0, 1, 2, 3];
-  // let t = [false, null, 1, undefined, 0, NaN];
-
   [].__proto__.join2 = function(separator = ',') {
     let str = '';
 
     for (let i = 0; i < this.length; i++) {
-      // console.log(t[i]);
       if (typeof this[i] === 'undefined' || this[i] === null) {
         str += '';
       } else {
@@ -21,37 +16,12 @@ function applyCustomJoin() {
       }
 
       if (i !== this.length - 1) {
-      //  console.log("object");
         str += separator;
       }
     }
 
     return str;
   };
-  // console.log(t.join2());
-  // return str;
 }
 
-// console.log(applyCustomJoin());
-// console.log(t);
-
 module.exports = applyCustomJoin;
-
-// function applyCustomJoin() {
-//   // let str = '';
-//   // let t = [0, 1, 2, 3];
-
-//   [].__proto__.join2 = function(separator = ',') {
-//     // if (this.length === 0) {
-//     //   return '';
-//     // }
-//     // if (!this.length) {
-//     //   return '';
-//     // }
-
-//     let str = '';
-//     // return str;
-//   };
-//   // console.log(t.join2());
-//   // return str;
-// }
