883fef234c28495606167b1d4aa9b00907604080
Maxim Gumeniuk <gumeniukmaks0510@gmail.com>
2022-08-14 12:59:54 +0300

    done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,26 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length <= 1) {
+      return this.toString();
+    };
+
+    const separation = separator;
+
+    let str = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (typeof this[i] !== 'undefined' && this[i] !== null) {
+        str += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        str += separation;
+      }
+    }
+
+    return str;
   };
 }
 
