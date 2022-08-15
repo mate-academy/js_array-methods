413e96e728a7864374fb7b1c8b6f32df321b768c
Oleksii Pustovit <91pustovit91@gmail.com>
2022-08-15 12:28:45 +0300

    solution of method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -10,11 +10,11 @@ function applyCustomJoin() {
     for (let i = 0; i < this.length; i++) {
       if (typeof this[i] !== 'undefined' && this[i] !== null) {
         result += this[i];
-      }
+      };
 
       if (i !== this.length - 1) {
         result += separator;
-      }
+      };
     }
 
     return result;
