91f14840e8de0f0df5f16bdce516b825a4a804d0
Volodymir Tymtsias <vovanuch2007@gmail.com>
2022-08-15 14:06:14 +0300

    Solution join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,29 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let newSeparator = separator;
+
+    if (this.length > 0) {
+      if (separator === undefined) {
+        newSeparator = ',';
+      }
+
+      for (let i = 0; i < this.length - 1; i++) {
+        if (this[i] !== undefined && this[i] !== null) {
+          result = result + this[i] + newSeparator;
+        } else {
+          result = result + newSeparator;
+        }
+      }
+
+      if (this[this.length - 1] !== undefined
+        && this[this.length - 1] !== null) {
+        result = result + this[this.length - 1];
+      }
+    }
+
+    return result;
   };
 }
 
