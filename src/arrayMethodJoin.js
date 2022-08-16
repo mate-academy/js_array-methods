849347e9b76d0eb63fffaec512558c4f5451fc3c
Oleksandr Sobchuk <osobchuk1@gmail.com>
2022-08-16 21:48:33 +0300

    applyCustomJoin

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let newString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const element = this[i];
+
+      if (element !== undefined && element !== null) {
+        newString += `${element}`;
+      }
+
+      if (i !== this.length - 1) {
+        newString += `${separator}`;
+      }
+    }
+
+    return newString;
   };
 }
 
