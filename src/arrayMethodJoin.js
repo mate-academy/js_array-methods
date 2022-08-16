17efb5cae5af1aa981a3a7890291d137b86a618a
Yevheniia Danylchuk <shenijad1@gmail.com>
2022-08-16 13:18:20 +0200

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,11 +4,21 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    const result = this.toString();
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
 
-    if (separator !== undefined) {
-      return result.replace(/,/g, separator);
+    if (this === undefined || this === null || this.length === 0) {
+      return '';
+    }
+
+    for (const i of this) {
+      if (i !== undefined && i !== null) {
+        result += i;
+      }
+
+      if (i !== this[this.length - 1]) {
+        result += separator;
+      }
     }
 
     return result;
