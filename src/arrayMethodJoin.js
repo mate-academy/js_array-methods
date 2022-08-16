db45f50ee8e1ff43ab0cc0667ad3f66d8ed96dab
NataliyaAn <otchet333@gmail.com>
2022-08-16 11:37:41 +0300

    solution was fixed3

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,7 +6,6 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    const separatorString = String(separator);
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== undefined
@@ -15,7 +14,7 @@ function applyCustomJoin() {
       }
 
       if (i !== this.length - 1) {
-        result += separatorString;
+        result += String(separator);
       }
     }
 
