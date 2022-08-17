1a0f1be312f2e020f1817a8c52e7f52620c21e12
Oleksandr Babmyndra <alphamonik1@gmail.com>
2022-08-17 10:35:58 +0300

    Remove "cumbersomeness"

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,25 +7,18 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     if (this.length === 0) {
       return '';
-    } else if (this.length === 1) {
-      return String(this[0]);
     }
 
-    let result;
-    const separatorToString = String(separator);
-
-    if (this[0] === undefined || this[0] === null) {
-      result = '';
-    } else {
-      result = this[0];
-    }
+    let result = (this[0] === undefined || this[0] === null)
+      ? ''
+      : String(this[0]);
 
     for (let i = 1; i < this.length; i++) {
       if (this[i] === undefined || this[i] === null) {
-        result += separatorToString;
+        result += separator;
         continue;
       }
-      result += separatorToString + String(this[i]);
+      result += separator + String(this[i]);
     }
 
     return result;
