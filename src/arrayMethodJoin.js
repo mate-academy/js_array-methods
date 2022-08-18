270d2bcd69675eca6eaa92340cc3d78c92e2c147
Oleksandr Malyshev <sanyokmalyshev@gmail.com>
2022-08-18 13:49:29 +0800

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,10 +3,51 @@
 /**
  * Implement method join
  */
+
+const source = [0, 1, 2, 3];
+
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let sep = separator;
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+    }
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (this.length === 1) {
+      sep = '';
+    }
+
+    if (separator === null) {
+      sep = 'null';
+    }
+
+    if (sep === undefined) {
+      sep = ',';
+    }
+
+    let result = '';
+
+    for (let i = 1; i <= this.length; i++) {
+      if (i === this.length) {
+        result += this[i - 1];
+        break;
+      }
+      result += this[i - 1] + sep;
+    }
+
+    return result;
+    // console.log(result);
   };
+
+  source.join2({});
 }
 
+applyCustomJoin();
 module.exports = applyCustomJoin;
