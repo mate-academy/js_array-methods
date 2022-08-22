2146ee4cb3e820dcd8497e79bf290a95abbf441c
Nazar Vorona <voron1765@gmail.com>
2022-08-22 23:57:31 +0300

    All done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,62 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+
+    if (this.length >= 0) {
+      for (let i = 0; i < this.length; i++) {
+        if (this.length === 1) {
+          str += this[i];
+          break;
+        }
+
+        if (separator === null) {
+          if (this.length - 1 === i) {
+            str += this[i];
+            break;
+          }
+          str += this[i] + 'null';
+          continue;
+        }
+
+        if (separator === '') {
+          str += this[i];
+          continue;
+        }
+
+        if (separator === undefined) {
+          if (this.length - 1 === i) {
+            str += this[i];
+            break;
+          }
+          str += this[i] + ',';
+          continue;
+        }
+
+        if (!separator) {
+          str += this[i] + ',';
+          continue;
+        }
+
+        if (this[i] === null || this[i] === undefined) {
+          if (this.length - 1 === i) {
+            if (this[i] === undefined) {
+              break;
+            }
+          }
+          str += separator;
+          continue;
+        }
+
+        if (this.length - 1 === i) {
+          str += this[i];
+          break;
+        }
+        str += this[i] + separator;
+      }
+    }
+
+    return str;
   };
 }
 
