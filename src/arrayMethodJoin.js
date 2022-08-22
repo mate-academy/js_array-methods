29c5e9175e303c903ceb244ae11cc00d71402495
Vladislav Gogol <gogolsimon95@gmail.com>
2022-08-22 20:30:23 +0300

    Solution join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,25 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let resultString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        resultString += '';
+      } else {
+        resultString += this[i];
+      }
+
+      if (i === (this.length - 1)) {
+        resultString += '';
+      } else if (separator === undefined) {
+        resultString += ',';
+      } else {
+        resultString += separator;
+      }
+    }
+
+    return resultString;
   };
 }
 
