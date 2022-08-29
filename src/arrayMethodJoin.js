1cb3d4e8b5b042509d294ca10215ab5d9310ceff
Kate Novak <novak5683@gmail.com>
2022-08-29 15:11:19 +0200

    Solution_arr_join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,51 @@
 /**
  * Implement method join
  */
+
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    const newArr = Array.from(this);
+    let result = '';
+    let newSep = separator;
+    const exp = separator;
+
+    switch (exp) {
+      case undefined:
+        newSep = ',';
+        break;
+
+      case '':
+        newSep = '';
+        break;
+
+      case null:
+        newSep = 'null';
+        break;
+    }
+
+    for (let item of newArr) {
+      if (newArr.length === 1) {
+        result += item;
+
+        return result;
+      }
+
+      if (item === null || item === undefined) {
+        item = '';
+      }
+
+      if (typeof (newSep) === 'object') {
+        newSep = newSep.toString();
+      }
+
+      result += item + newSep;
+    }
+
+    if (newSep.length !== 0 && typeof (newSep) !== 'object') {
+      result = result.slice(0, -1 * newSep.length);
+    }
+
+    return result;
   };
 }
 
