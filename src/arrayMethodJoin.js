c0593d5cceb68b31445c9bdaec49cf3c69179cc7
Nick_Radchuk <mukola.radchuk@gmail.com>
2022-08-30 14:31:38 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,30 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let combinedString = '';
+    let separateString;
+
+    switch (separator) {
+      case undefined:
+        separateString = ',';
+        break;
+      case null:
+        separateString = 'null';
+        break;
+      default:
+        separateString = separator.toString();
+    }
+
+    for (const element of this) {
+      if (element === null || element === undefined) {
+        combinedString += separateString;
+      } else {
+        combinedString += element + separateString;
+      }
+    }
+
+    return combinedString
+      .substring(0, combinedString.length - separateString.length);
   };
 }
 
