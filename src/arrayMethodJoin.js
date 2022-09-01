870945d6819976bc60990e20fff418458767e9bf
Maksym Avramenko <nonste@outlook.com>
2022-09-01 16:40:49 +0300

    fixed as required

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,16 +8,12 @@ function applyCustomJoin() {
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === undefined || this[i] === null) {
-        this[i] = '';
+      if (this[i] !== undefined && this[i] !== null) {
+        result += this[i];
       }
 
-      if (result.length === 0 && this.length !== 1) {
-        result = `${this[i]}${separator}`;
-      } else if (i + 1 !== this.length) {
-        result = `${result}${this[i]}${separator}`;
-      } else {
-        result = `${result}${this[i]}`;
+      if (i < this.length - 1) {
+        result += separator;
       }
     }
 
