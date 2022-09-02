ec08c612783fc159298db2fbb01b166708760941
Sergey Vorobey <sergey88nik@gmail.com>
2022-09-02 15:52:46 +0300

    task solved

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,27 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let sep = separator;
+    let result = '';
+
+    for (let i = 0; i < this.length - 1; i++) {
+      result += chek(this[i]);
+      result += sep;
+    }
+
+    result += chek(this[this.length - 1]);
+
+    return result;
   };
+}
+
+function chek(element) {
+  if (element === undefined || element == null) {
+    return '';
+  }
+
+  return element;
 }
 
 module.exports = applyCustomJoin;
