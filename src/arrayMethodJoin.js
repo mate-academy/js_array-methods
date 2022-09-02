01947798d2db975c1cbf66c5fb0ce7b772bf8818
Taras Nakonechnyi <nakonechnyi.taras@gmail.com>
2022-09-02 17:03:46 +0300

    Solution v2.0

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,24 +7,37 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     let finalString = '';
     let newSeparator = separator;
-    let counter = 0;
 
     if (separator === undefined) {
       newSeparator = ',';
     }
 
-    for (let key of this) {
-      if (key === null || key === undefined) {
-        key = '';
+    for (let x = 0; x < (this.length - 1); x++) {
+      if (this[x] === null || this[x] === undefined) {
+        this[x] = '';
       }
 
-      if ((this.length - 1) === counter) {
-        finalString += `${key}`;
-      } else {
-        finalString += `${key}${newSeparator}`;
-        counter++;
-      }
+      finalString += `${this[x]}${newSeparator}`;
     }
+
+    if (this[this.length - 1] === null || this[this.length - 1] === undefined) {
+      finalString += '';
+    } else {
+      finalString += `${this[this.length - 1]}`;
+    }
+
+    // for (let key of this) {
+    //   if (key === null || key === undefined) {
+    //     key = '';
+    //   }
+
+    //   if ((this.length - 1) === counter) {
+    //     finalString += `${key}`;
+    //   } else {
+    //     finalString += `${key}${newSeparator}`;
+    //     counter++;
+    //   }
+    // }
 
     return finalString;
   };
