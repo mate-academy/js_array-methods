3ba87772f823c909e528abd4072f2d8db8aeeb24
Mitriy Che <mitriyche@gmail>
2022-09-02 17:36:07 +0300

    method Join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,36 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let sep = separator;
+    let result = '';
+    const copySource = [...this];
+
+    if (copySource.length === 0) {
+      return '';
+    }
+
+    if (copySource.length === 1) {
+      sep = '';
+    }
+
+    if (sep === null) {
+      sep = 'null';
+    }
+
+    for (let i = 0; i < copySource.length; i++) {
+      if (copySource[i] === null || copySource[i] === undefined) {
+        copySource[i] = '';
+      }
+
+      if (i === copySource.length - 1) {
+        result += copySource[i];
+        break;
+      }
+      result += copySource[i] + sep;
+    }
+
+    return result;
   };
 }
 
