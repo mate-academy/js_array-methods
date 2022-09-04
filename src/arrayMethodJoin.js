7090b8d7be2cb9685e5a795310826363adc98512
Nazar Vorona <voron1765@gmail.com>
2022-09-05 01:05:14 +0300

    ASK

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,59 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let str = '';
 
     if (this.length >= 0) {
       for (let i = 0; i < this.length; i++) {
-        if (this.length === 1) {
-          str += this[i];
-          break;
-        }
-
-        if (separator === null) {
-          if (this.length - 1 === i) {
-            str += this[i];
-            break;
-          }
+        if (separator === null && i !== this.length - 1) {
           str += this[i] + 'null';
           continue;
         }
 
-        if (separator === '') {
-          str += this[i];
-          continue;
-        }
-
-        if (separator === undefined) {
-          if (this.length - 1 === i) {
-            str += this[i];
-            break;
-          }
-          str += this[i] + ',';
-          continue;
-        }
-
-        if (!separator) {
-          str += this[i] + ',';
-          continue;
-        }
-
         if (this[i] === null || this[i] === undefined) {
-          if (this.length - 1 === i) {
-            if (this[i] === undefined) {
-              break;
-            }
+          if (i !== this.length - 1) {
+            str += separator;
           }
-          str += separator;
           continue;
         }
 
-        if (this.length - 1 === i) {
-          str += this[i];
-          break;
+        if (i !== this.length - 1 && this[i] !== undefined) {
+          str += this[i] + separator;
+          continue;
         }
-        str += this[i] + separator;
+
+        str += this[i];
       }
     }
 
