282458b8b4c4c123763b59b8e4449c35a5ee61de
Max Bovtun <maxbovtun@gmail.com>
2022-09-06 14:07:34 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,40 @@
 /**
  * Implement method join
  */
+
+// - There is kinda an easy solution for
+// |-- this task but with some weird tests
+// |-- like null could be a separator
+// |-- but not an undefined, or NaN. XD
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let res = '';
+    let symbol = separator;
+
+    if (separator === null) {
+      symbol = 'null';
+    } else if (separator === '') {
+      symbol = '';
+    } else if (!separator) {
+      symbol = ',';
+    }
+
+    // Main loop:
+    for (let i = 0; i < this.length; i++) {
+      let item = this[i];
+
+      if (item === null || item === undefined) {
+        item = '';
+      }
+
+      if (i !== this.length - 1) {
+        res += item + symbol;
+      } else {
+        res += item;
+      }
+    }
+
+    return res;
   };
 }
 
