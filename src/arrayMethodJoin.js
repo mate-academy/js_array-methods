c284475da5d6526923e5b166b5b2ae3749c0727d
Nazar Vorona <voron1765@gmail.com>
2022-09-08 22:08:19 +0300

    delete coments

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -24,33 +24,3 @@ function applyCustomJoin() {
 }
 
 module.exports = applyCustomJoin;
-// function applyCustomJoin() {
-//   [].__proto__.join2 = function(separator = ',') {
-//     let str = '';
-
-//     if (this.length >= 0) {
-//       for (let i = 0; i < this.length; i++) {
-//         if (separator === null && i !== this.length - 1) {
-//           str += this[i] + 'null';
-//           continue;
-//         }
-
-//         if (this[i] === null || this[i] === undefined) {
-//           if (i !== this.length - 1) {
-//             str += separator;
-//           }
-//           continue;
-//         }
-
-//         if (i !== this.length - 1 && this[i] !== undefined) {
-//           str += this[i] + separator;
-//           continue;
-//         }
-
-//         str += this[i];
-//       }
-//     }
-
-//     return str;
-//   };
-// }
