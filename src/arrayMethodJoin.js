9c376237b20f3216981cd780a28f0e1a72ef4eb8
Nazar Vorona <voron1765@gmail.com>
2022-09-08 22:07:34 +0300

    Finish

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,33 +5,52 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let str = '';
+    let result = '';
 
     if (this.length >= 0) {
       for (let i = 0; i < this.length; i++) {
-        if (separator === null && i !== this.length - 1) {
-          str += this[i] + 'null';
-          continue;
+        if (this[i] !== undefined && this[i] !== null) {
+          result += `${this[i]}`;
         }
 
-        if (this[i] === null || this[i] === undefined) {
-          if (i !== this.length - 1) {
-            str += separator;
-          }
-          continue;
+        if (i < this.length - 1) {
+          result += separator;
         }
-
-        if (i !== this.length - 1 && this[i] !== undefined) {
-          str += this[i] + separator;
-          continue;
-        }
-
-        str += this[i];
       }
     }
 
-    return str;
+    return result;
   };
 }
 
 module.exports = applyCustomJoin;
+// function applyCustomJoin() {
+//   [].__proto__.join2 = function(separator = ',') {
+//     let str = '';
+
+//     if (this.length >= 0) {
+//       for (let i = 0; i < this.length; i++) {
+//         if (separator === null && i !== this.length - 1) {
+//           str += this[i] + 'null';
+//           continue;
+//         }
+
+//         if (this[i] === null || this[i] === undefined) {
+//           if (i !== this.length - 1) {
+//             str += separator;
+//           }
+//           continue;
+//         }
+
+//         if (i !== this.length - 1 && this[i] !== undefined) {
+//           str += this[i] + separator;
+//           continue;
+//         }
+
+//         str += this[i];
+//       }
+//     }
+
+//     return str;
+//   };
+// }
