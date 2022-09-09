78574b3c524b4171ac120659195c8e97b4604dab
Ruslan Nedaiborshch <rnedaiborshch@gmail.com>
2022-09-09 12:15:37 +0300

    alternativ method-join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,38 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let rezult = '';
+    const long = this.length;
+
+    for (let i = 0; i < this.length; i++) {
+      switch (long > 0) {
+        case this[this.length - 1] === undefined && i === long - 1:
+          rezult += '';
+          break;
+
+        case (long - 1) === i || long <= 1:
+          rezult += this[i];
+          break;
+
+        case separator === undefined:
+          rezult += this[i] + ',';
+          break;
+
+        case separator === null:
+          rezult += this[i] + 'null';
+          break;
+
+        case this[i] === null || this[i] === undefined:
+          rezult += ',';
+          break;
+
+        default:
+          rezult += this[i] + separator;
+          break;
+      }
+    }
+
+    return rezult;
   };
 }
 
