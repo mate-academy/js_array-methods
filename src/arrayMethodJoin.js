a36dc460949b7ce452b1510978aa4d5ffb7db9b5
Anastasia Titarenko <titarenkoap123@gmail.com>
2022-09-09 14:49:34 +0300

    function arrayMethodJoin done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,26 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        if (i !== this.length - 1) {
+          str += `${separator}`;
+        }
+      }
+
+      if (this[i] !== undefined && this[i] !== null) {
+        if (i !== this.length - 1) {
+          str += `${this[i]}${separator}`;
+        } else {
+          str += `${this[i]}`;
+        }
+      }
+    }
+
+    return str;
   };
 }
 
