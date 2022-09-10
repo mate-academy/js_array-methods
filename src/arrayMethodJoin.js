451d8dcf655b4142b21e9cabeffc07799ff97d9f
Anna Grechana <grechanaanna@gmial.com>
2022-09-10 23:37:41 +0300

    1st version

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-param-reassign */
 'use strict';
 
 /**
@@ -5,7 +6,36 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let resalt = '';
+    let step;
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      if (separator === null) {
+        separator = 'null';
+      }
+
+      if (separator === undefined) {
+        separator = ',';
+      }
+
+      if (this.length === 1) {
+        resalt = resalt + this[i];
+      } else {
+        if (i < this.length - 1) {
+          step = this[i] + separator;
+        } else {
+          step = this[i];
+        }
+
+        resalt = resalt + step;
+      }
+    }
+
+    return resalt;
   };
 }
 
