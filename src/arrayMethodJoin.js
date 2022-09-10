4e6d8a4ecc409f8c4d01623001e139afdc3f1bd9
Ruslan Nedaiborshch <rnedaiborshch@gmail.com>
2022-09-10 11:22:30 +0300

    fixes after review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,35 +4,16 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let rezult = '';
-    const long = this.length;
 
     for (let i = 0; i < this.length; i++) {
-      switch (long > 0) {
-        case this[this.length - 1] === undefined && i === long - 1:
-          rezult += '';
-          break;
+      if (this[i] !== undefined && this[i] !== null) {
+        rezult += this[i];
+      }
 
-        case (long - 1) === i || long <= 1:
-          rezult += this[i];
-          break;
-
-        case separator === undefined:
-          rezult += this[i] + ',';
-          break;
-
-        case separator === null:
-          rezult += this[i] + 'null';
-          break;
-
-        case this[i] === null || this[i] === undefined:
-          rezult += ',';
-          break;
-
-        default:
-          rezult += this[i] + separator;
-          break;
+      if (i !== this.length - 1) {
+        rezult += separator;
       }
     }
 
