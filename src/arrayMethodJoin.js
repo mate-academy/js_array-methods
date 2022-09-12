f97d3c67508ad8f56a1ebb98f487bc73f7e1703a
Andriy Andrieiev <proph7000@gmail.com>
2022-09-12 17:03:45 +0300

    Fixes after code review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,12 +6,27 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    const sep = (separator === null) ? 'null' : separator;
+    let sep;
+    let arg;
+
+    if (separator === null) {
+      sep = 'null';
+    } else {
+      sep = separator;
+    }
 
     for (let i = 0; i < this.length; i++) {
-      const arg = (this[i] === null || this[i] === undefined) ? '' : this[i];
+      if (this[i] === null || this[i] === undefined) {
+        arg = '';
+      } else {
+        arg = this[i];
+      }
 
-      result += (i < this.length - 1) ? arg + sep : arg;
+      if (i < this.length - 1) {
+        result += arg + sep;
+      } else {
+        result += arg;
+      }
     }
 
     return result;
