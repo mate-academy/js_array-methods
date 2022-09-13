d2c6364c829bcf1868e8f1009606c7d7bbbe48cf
grafit-off <graf.off.it@gmail.com>
2022-09-13 13:54:43 +0300

    feat: solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function (separator = ',') {
     // write code here
+    let str = ``;
+
+    for (let i = 0; i < this.length; i++) {
+      const item = this[i];
+      const itemIsNullUndefine = (item === null || item === undefined);
+      const elToJoin = itemIsNullUndefine ? '' : item;
+
+      str += `${elToJoin}`;
+
+      if (i !== this.length - 1) {
+        str += separator;
+      }
+    }
+
+    return str;
   };
 }
 
