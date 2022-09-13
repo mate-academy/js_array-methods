514a110a943cb32800c882a02cf82bb973ecc234
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-09-13 11:58:52 +0300

    short solution without classic if

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,17 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    const between = separator === undefined ? ',' : separator;
+    const initialLength = this.length;
+
+    let newStr = '';
+
+    for (let i = 0; i < initialLength; i++) {
+      newStr += (this[i] === undefined || this[i] === null) ? '' : this[i];
+      newStr += i !== initialLength - 1 ? between : '';
+    }
+
+    return newStr;
   };
 }
 
