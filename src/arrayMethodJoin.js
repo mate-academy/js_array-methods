f469998fe27a193d8e40e01124dd0726e62960f9
Kolya Movchan <kmovchan261@gmail.com>
2022-09-13 12:52:52 +0300

    js_array-method-join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinString = '';
+    let modifiedSeparator = separator;
+
+    if (separator === null) {
+      modifiedSeparator = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      if (i === this.length - 1) {
+        joinString += this[i];
+        break;
+      }
+
+      joinString += this[i] + modifiedSeparator;
+    }
+
+    return joinString;
   };
 }
 
