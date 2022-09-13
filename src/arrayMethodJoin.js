e577d6cbca47957330dbbb434ad5508824ae360b
grafit-off <graf.off.it@gmail.com>
2022-09-13 14:06:15 +0300

    fix: removed unnecessary space

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,7 +4,7 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function (separator = ',') {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
     let str = ``;
 
