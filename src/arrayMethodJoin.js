ef445d677faca78acefc77a27f2ca24ccfe344da
Volodymyr Nosachenko <vovanosachenko@meta.ua>
2022-09-14 17:55:16 +0300

    some renames

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,16 +5,18 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    const initialLength = this.length;
+    const arrayLength = this.length;
 
-    let newStr = '';
+    let convertedString = '';
 
-    for (let i = 0; i < initialLength; i++) {
-      newStr += (this[i] === undefined || this[i] === null) ? '' : this[i];
-      newStr += i !== initialLength - 1 ? separator : '';
+    for (let i = 0; i < arrayLength; i++) {
+      convertedString += (this[i] === undefined || this[i] === null)
+        ? ''
+        : this[i];
+      convertedString += i !== arrayLength - 1 ? separator : '';
     }
 
-    return newStr;
+    return convertedString;
   };
 }
 
