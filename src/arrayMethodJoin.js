464a51405736aee52a745858252540b5726aaeaa
Taras Nakonechnyi <nakonechnyi.taras@gmail.com>
2022-09-14 17:20:27 +0300

    Amendments from Sanya Bratashchuk 2022-09-14

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,20 +4,15 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let finalString = '';
-    let newSeparator = separator;
-
-    if (separator === undefined) {
-      newSeparator = ',';
-    }
 
     for (let x = 0; x < (this.length - 1); x++) {
       if (this[x] === null || this[x] === undefined) {
         this[x] = '';
       }
 
-      finalString += `${this[x]}${newSeparator}`;
+      finalString += `${this[x]}${separator}`;
     }
 
     if (this[this.length - 1] === null || this[this.length - 1] === undefined) {
@@ -25,19 +20,6 @@ function applyCustomJoin() {
     } else {
       finalString += `${this[this.length - 1]}`;
     }
-
-    // for (let key of this) {
-    //   if (key === null || key === undefined) {
-    //     key = '';
-    //   }
-
-    //   if ((this.length - 1) === counter) {
-    //     finalString += `${key}`;
-    //   } else {
-    //     finalString += `${key}${newSeparator}`;
-    //     counter++;
-    //   }
-    // }
 
     return finalString;
   };
