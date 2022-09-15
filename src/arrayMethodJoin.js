b6f6c58c467d53a3612854e93aad823507ecf24b
Tilak Kumar <mailtotilakkumar@gmail.com>
2022-09-15 23:50:31 +0530

    simplified

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,37 +6,21 @@
 
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    const len = this.length;
-
     let result = '';
-    let count = 0;
 
-    if (len === 0) {
-      return '';
-    }
-
-    for (const i of this) {
-      if (count === len - 1) {
-        break;
+    for (let i = 0; i < this.length; i++) {
+      
+      if (this[i] !== undefined && this[i] !== null) {
+        result += this[i];
       }
 
-      if (i === null || i === undefined) {
-        result = result + separator;
+      if (i < this.length - 1) {
+        result += separator;
       }
-
-      if (i !== null && i !== undefined) {
-        result = result + i + separator;
-      }
-
-      count++;
-    }
-
-    if (this[len - 1] !== null && this[len - 1] !== undefined) {
-      result = result + this[len - 1];
     }
 
     return result;
-  };
+  }
 }
 
 module.exports = applyCustomJoin;
