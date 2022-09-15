6312102662964fa4414b1e8d93e6637de224351d
Volodymyr Klymkiv <volodymyr.klymkiv@gmail.com>
2022-09-15 20:24:17 +0300

    solution js_task-join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinResult = '';
+    const joinSeparator = String(separator);
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        this[i] = '';
+      }
+      joinResult += this[i] + joinSeparator;
+    }
+
+    if (joinSeparator !== '') {
+      return joinResult.substring(0, joinResult.length - joinSeparator.length);
+    }
+
+    return joinResult;
   };
 }
-
 module.exports = applyCustomJoin;
