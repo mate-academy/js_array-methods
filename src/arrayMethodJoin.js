e560ef927dc10473edefbd7bcb1280e920a956e1
Myroslav Diiak <diiakm23819@gmail.com>
2022-09-15 20:13:51 +0300

    fixed comments

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,17 +4,11 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
-
-    let separate = separator;
 
     if (this.length === 0) {
       return '';
-    }
-
-    if (separator === undefined) {
-      separate = ',';
     }
 
     let result = String(this[0]);
@@ -25,9 +19,9 @@ function applyCustomJoin() {
 
     for (let i = 1; i < this.length; i++) {
       if (this[i] === null || this[i] === undefined) {
-        result += separate + '';
+        result += separator + '';
       } else {
-        result += separate + String(this[i]);
+        result += separator + String(this[i]);
       }
     }
 
