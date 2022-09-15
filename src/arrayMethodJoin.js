30c256038c6cc0126aafbe3d8e85c61eaff22be2
Ihor Andrianov <andrianoia@gmail.com>
2022-09-15 12:49:19 +0300

    feat: func realisation

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length === 0) {
+      return '';
+    }
+
+    let index = 0;
+    let concatenated = '';
+    let next;
+
+    while (index < this.length) {
+      if (this[index] === undefined || this[index] === null) {
+        next = '';
+      } else {
+        next = this[index];
+      }
+
+      concatenated += (index < this.length - 1)
+        ? next + String(separator)
+        : next;
+      index++;
+    }
+
+    return concatenated;
   };
 }
 
