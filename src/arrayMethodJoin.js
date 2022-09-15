e052320e39bd30f94c180d5a2053560ee8421d57
Tilak Kumar <mailtotilakkumar@gmail.com>
2022-09-15 23:39:46 +0530

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,39 @@
 /**
  * Implement method join
  */
+
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const len = this.length;
+
+    let result = '';
+    let count = 0;
+
+    if (len === 0) {
+      return '';
+    }
+
+    for (const i of this) {
+      if (count === len - 1) {
+        break;
+      }
+
+      if (i === null || i === undefined) {
+        result = result + separator;
+      }
+
+      if (i !== null && i !== undefined) {
+        result = result + i + separator;
+      }
+
+      count++;
+    }
+
+    if (this[len - 1] !== null && this[len - 1] !== undefined) {
+      result = result + this[len - 1];
+    }
+
+    return result;
   };
 }
 
