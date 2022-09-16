f80fce4d32eae5637427bf38ebdbeb8216e77dad
Oleh Chaikin <xxoleg1990xx@gmail.com>
2022-09-17 00:13:42 +0300

    join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,21 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const length = this.length;
+    let str = '';
+
+    for (let i = 0; i < length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        str += this[i];
+      }
+
+      if (length - 1 !== i) {
+        str += separator;
+      }
+    }
+
+    return str;
   };
 }
 
