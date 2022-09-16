5d01f11d9fd529209170c220dd70264e6e0c8afe
Killdex <kdcompany2018@gmail.com>
2022-09-16 12:04:41 +0300

    removed unnecessary if statment

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,19 +7,17 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
 
-    if (this.length) {
-      for (let i = 0; i < this.length; i++) {
-        if (this[i] === null || this[i] === undefined) {
-          this[i] = '';
-        }
-
-        result += `${this[i]}`;
-
-        if (i !== this.length - 1) {
-          result += `${separator}`;
-        }
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
       }
-    };
+
+      result += `${this[i]}`;
+
+      if (i !== this.length - 1) {
+        result += `${separator}`;
+      }
+    }
 
     return result;
   };
