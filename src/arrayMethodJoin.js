e25b37a35f20245457815ec4b756d0d4a98957c7
Anton Vaida <tohan1.ua@gmail.com>
2022-09-16 15:05:48 +0300

    remove if

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,10 +7,6 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
 
-    if (this.length < 1) {
-      return '';
-    }
-
     for (let i = 0; i < this.length; i++) {
       if (this[i] === null || this[i] === undefined) {
         this[i] = '';
