cbd116351efacf9e3c49c3fba234110591ef5053
Serhii Romaniuk <deepspark@gmail.com>
2022-09-16 14:34:40 +0300

    implemented the method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const copyThis = [...this];
+    const copySeparator = separator;
+    let strJoin = '';
+
+    for (let i = 0; i < copyThis.length; i++) {
+      if (copyThis[i] === null || copyThis[i] === undefined) {
+        strJoin += '';
+      } else {
+        strJoin += copyThis[i];
+      }
+
+      if (i !== copyThis.length - 1) {
+        strJoin += copySeparator;
+      }
+    }
+
+    return strJoin;
   };
 }
 
