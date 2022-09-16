7d77ccf3e40fc45a35927c54b04d6ca802770338
fatihsamur <a.fatihsamur@gmail.com>
2022-09-16 03:27:27 +0300

    it will be refactored

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,33 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separatorChar = ',') {
+    if (this.length === 0) {
+      return '';
+    }
+
+    let joinedString = '';
+    let separator = separatorChar;
+
+    if (separatorChar === null) {
+      separator = 'null';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      let currentElement = this[i];
+
+      if (currentElement === null || currentElement === undefined) {
+        currentElement = '';
+      }
+
+      joinedString += currentElement + separator;
+    }
+
+    if (this[this.length - 1]) {
+      joinedString += this[this.length - 1];
+    }
+
+    return joinedString;
   };
 }
 
