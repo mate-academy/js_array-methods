8990c659677a7a64ac817185f855cff09d9632b3
Maksym845 <mzuravel845@gmail.com>
2022-09-16 11:27:46 +0300

    solution was added

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,32 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let res = '';
+    let sep = separator;
+
+    if (sep === undefined || isNaN(sep)) {
+      sep = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1 && this[i] === undefined) {
+        break;
+      }
+
+      if (i === this.length - 1) {
+        res += this[i];
+        break;
+      }
+
+      if (this[i] === null || this[i] === undefined) {
+        res += sep;
+        continue;
+      }
+
+      res += `${this[i]}${sep}`;
+    }
+
+    return res;
   };
 }
 
