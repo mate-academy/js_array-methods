4f3d37f5e29264f174414660c82b6a6d63d8e9e7
Nikita Ovchinnikov <nikitaovch99@gmail.com>
2022-09-16 15:00:00 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,31 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = (',')) {
+    let result = '';
+
+    if (!this.length) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1) {
+        result += nullCheck(this[i]);
+      } else {
+        result += nullCheck(this[i]) + separator;
+      }
+    }
+
+    return result;
   };
+
+  function nullCheck(value) {
+    if (value === null || value === undefined) {
+      return '';
+    }
+
+    return value.toString();
+  }
 }
 
 module.exports = applyCustomJoin;
