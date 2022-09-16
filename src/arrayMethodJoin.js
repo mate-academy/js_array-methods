46c7bfe4b0e7e6a1e869a253ecf7c2483ef7719c
Anastasiia Nahaiska <nahaiskaanastasiia@gmail.com>
2022-09-16 22:43:43 +0300

    Changed the variable name

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let string = '';
+    let result = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== undefined && this[i] !== null) {
-        string += this[i];
+        result += this[i];
       }
 
       if (i !== this.length - 1) {
-        string += separator;
+        result += separator;
       }
     }
 
-    return string;
+    return result;
   };
 }
 
