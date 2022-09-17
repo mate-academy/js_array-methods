0b1f83cf6d66e27ea149b92cd96b3321218736fd
Andrii Zhabynets <a.zhabynets03@gmail.com>
2022-09-17 23:28:40 +0300

    result

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length === 0) {
+      return '';
+    }
+
+    let firstEl = this[0];
+
+    if (firstEl === null || firstEl === undefined) {
+      firstEl = '';
+    }
+
+    let joined = `${firstEl}`;
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      joined += `${separator}` + this[i];
+    }
+
+    return joined;
   };
 }
 
