0b31350d4b6b0fd871284e6e7078b7ab3e527166
Nikita Ovchinnikov <nikitaovch99@gmail.com>
2022-09-17 14:05:18 +0300

    fixes after st review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,15 +7,11 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = (',')) {
     let result = '';
 
-    if (!this.length) {
-      return '';
-    }
-
     for (let i = 0; i < this.length; i++) {
-      if (i === this.length - 1) {
-        result += nullCheck(this[i]);
-      } else {
-        result += nullCheck(this[i]) + separator;
+      result += nullCheck(this[i]);
+
+      if (i !== this.length - 1) {
+        result += separator;
       }
     }
 
