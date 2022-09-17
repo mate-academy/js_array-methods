1a090aa3daa501ac79597acf5ea5421c033d4c42
Arsen-Shpak <a.shpakovich2000@gmail.com>
2022-09-17 20:11:20 +0300

    join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,21 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let str = '';
+
+    for (let item = 0; item < this.length; item++) {
+      if (this[item] !== null && this[item] !== undefined) {
+        str += this[item];
+      }
+
+      if (item !== this.length - 1) {
+        str += separator;
+      }
+    }
+
+    return str;
   };
 }
 
