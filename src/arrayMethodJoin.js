37f4b88527f50f62dfc1c1ad4b4195ff624fa7ca
Killdex <kdcompany2018@gmail.com>
2022-09-19 20:32:56 +0300

    corrected issue

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,11 +8,9 @@ function applyCustomJoin() {
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        this[i] = '';
+      if (this[i] !== null && this[i] !== undefined) {
+        result += this[i];
       }
-
-      result += this[i];
 
       if (i !== this.length - 1) {
         result += separator;
