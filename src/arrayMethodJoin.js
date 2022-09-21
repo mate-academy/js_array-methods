52c10b83fd09654f0c49acfb929b847de6059efc
BuildTools <unconfigured@null.spigotmc.org>
2022-09-21 14:10:08 +0300

    task-done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length === 0) {
+      return '';
+    }
+
+    const arr = this.map(e => e === null || e === undefined ? '' : e);
+
+    // eslint-disable-next-line no-var
+    var res = '';
+
+    for (let i = 0; i < arr.length - 1; i++) {
+      res += arr[i];
+      res += separator;
+    }
+
+    res += arr[arr.length - 1];
+
+    return res;
   };
 }
 
