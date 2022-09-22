20f1d66e3fee6a58023688cffff782515f010a12
Nhlanhla Thubana <nm.thubana@gmail.com>
2022-09-22 20:15:00 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,34 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+    let newStr = '';
+    let separatorAsString = '';
+
+    if (separator === null) {
+      separatorAsString = 'null';
+    } else {
+      separatorAsString = separator.toString();
+    }
+
+    for (const item of this) {
+      if (item === undefined || item === null) {
+        str += '';
+      } else {
+        str += item.toString();
+      }
+
+      str += separatorAsString;
+    }
+
+    if (separatorAsString !== '') {
+      newStr = str.slice(0, -separatorAsString.length);
+    } else {
+      newStr = str;
+    }
+
+    return newStr;
   };
 }
 
