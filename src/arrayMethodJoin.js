0e1537e54c35155c1520b3db6cd26193235fc196
Anastasiia Tilikina <anastasiia.tilikina@gmail.com>
2022-09-22 21:52:51 +0300

    Implement join method function

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    const joinSymbol = String(separator);
+
+    for (let i = 0; i < this.length; i++) {
+      let element = String(this[i]);
+
+      if (this[i] === null || this[i] === undefined) {
+        element = '';
+      }
+
+      if (i === this.length - 1) {
+        result += element;
+      } else {
+        result += (element + joinSymbol);
+      }
+    }
+
+    return result;
   };
 }
 
