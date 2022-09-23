aabfed4ba488684499052c464984bc906e896df0
Danil <dasha8854567@gmail.com>
2022-09-23 12:29:59 +0300

    commit1

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,32 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    let UsedSeparator = separator;
+
+    if (separator === undefined) {
+      UsedSeparator = ',';
+    }
+
+    for (let key = 0; key < this.length; key++) {
+      if (key === this.length - 1) {
+        // result = result + this[key];
+
+        if (this[key] === undefined || this[key] === null) {} else {
+          result = result + this[key];
+        }
+        break;
+      }
+
+      if (this[key] === undefined || this[key] === null) {
+        result = result + `${UsedSeparator}`;
+      } else {
+        result = result + this[key] + `${UsedSeparator}`;
+      }
+    }
+
+    return result;
   };
 }
 
