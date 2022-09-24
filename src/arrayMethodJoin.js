fae8f0bdec056dd3a0e07cf780de1425935e73d1
Anna Grechana <grechanaanna@gmial.com>
2022-09-24 14:55:43 +0300

    4th  version

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -9,13 +9,11 @@ function applyCustomJoin() {
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        result += '';
-      } else {
+      if (this[i] !== null && this[i] !== undefined) {
         result += this[i];
       }
 
-      if (!(i === this.length - 1)) {
+      if ((i !== this.length - 1)) {
         result += separator;
       }
     }
