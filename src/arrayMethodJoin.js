245a335334031f266daae265af8db4813d8e7fb7
Oleksii Pishchuhin <aliliolek@gmail.com>
2022-09-25 19:50:54 +0300

    Solution 1.0

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,60 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let newString = '';
+    let sepSign = '';
+    let element = '';
+
+    if (separator) {
+      sepSign = separator;
+    } else if (separator === undefined) {
+      sepSign = ',';
+    } else if (separator === null) {
+      sepSign = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] || this[i] === 0) {
+        element = this[i];
+      } else if (this[i] === false) {
+        element = false;
+      } else if (isNaN(this[i]) && this[i] !== undefined) {
+        element = NaN;
+      } else if (this[i] === null) {
+        element = '';
+      } else if (this[i] === undefined) {
+        element = '';
+      } else if (this[i] === '') {
+        element = '';
+      }
+
+      // if (isNaN(this[i]) && this[i] !== undefined) {
+      //   element = NaN;
+      // } else {
+      //   switch (this[i]) {
+      //     case '':
+      //     case undefined:
+      //     case null:
+      //       element = '';
+      //       break;
+      //     case false:
+      //       element = false;
+      //       break;
+      //     case true:
+      //     case 0:
+      //       element = this[i];
+      //       break;
+      //   }
+      // }
+
+      if (i === (this.length - 1)) {
+        newString += element;
+      } else {
+        newString += element + sepSign;
+      }
+    }
+
+    return newString;
   };
 }
 
