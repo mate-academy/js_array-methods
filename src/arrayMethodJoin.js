f7efca40bae37cd717b85da2dc617afaec76a905
Mykola Brodyuk <nick.brodyuk@gmail.com>
2022-09-27 20:49:51 +0300

    restructured code

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,26 +5,15 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let separatorValue = separator;
     let resultString = '';
 
-    if (separator === null) {
-      separatorValue = 'null';
-    } else if (separator === '') {
-      separatorValue = '';
-    }
-
     for (let i = 0; i < this.length; i++) {
-      let element = this[i];
-
-      if (this[i] === undefined || this[i] === null) {
-        element = '';
+      if (this[i] !== null && this[i] !== undefined) {
+        resultString += this[i];
       }
 
-      if (i === 0) {
-        resultString += element;
-      } else {
-        resultString += separatorValue + element;
+      if (i !== this.length - 1) {
+        resultString += separator;
       }
     }
 
