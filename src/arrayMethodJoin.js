31090bac454ef9235bb3d309b6508337eda184f3
Denis Cherevaschenko <deniska.cherevaschenko@gmail.com>
2022-09-27 16:23:43 +0300

    fixed remarks

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,29 +4,16 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(...args) {
-    const separator = args;
+  [].__proto__.join2 = function(separator = ',') {
     let result = '';
-
-    for (let i = 0; i < args.length; i++) {
-      if (separator[i] === undefined) {
-        separator[i] = ',';
-      } else if (separator[i] === null) {
-        separator[i] = 'null';
-      }
-    }
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
         result += this[i];
       }
 
-      if (i < this.length - 1) {
+      if (i !== this.length - 1) {
         result += separator;
-
-        if (!args.length) {
-          result += ',';
-        }
       }
     }
 
