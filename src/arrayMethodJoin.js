3ac9e61bd6888a85a459761856a95c661c4e41e7
metin <metinbicaksiz@gmail.com>
2022-10-02 13:41:29 -0700

    join method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,31 @@
 /**
  * Implement method join
  */
+
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = `,`) {
     // write code here
+
+    let result = '';
+    const sep = separator;
+
+    if (this.length === 0) {
+      return result;
+    }
+
+    for (let item = 0; item < this.length; item++) {
+      if (this[item] === null || this[item] === undefined) {
+        this[item] = ``;
+      }
+
+      if (item === this.length - 1) {
+        result += `${this[item]}`;
+        break;
+      }
+      result += `${this[item]}${sep}`;
+    }
+
+    return result;
   };
 }
 
