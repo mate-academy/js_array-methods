632cd67de0f35e6f5214a089b5b360edc9c6ae5e
Hanna Zaitseva <anyazajceva@gmail.com>
2022-10-09 15:12:03 +0300

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,52 +4,17 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    if (!this.length) {
-      return '';
-    }
+  [].__proto__.join2 = function(separator = ',') {
+    let string = '';
 
-    let separ;
-
-    switch (separator) {
-      case undefined:
-        separ = ',';
-        break;
-
-      case '':
-        separ = '';
-        break;
-
-      case null:
-        separ = 'null';
-        break;
-
-      default:
-        separ = separator;
-    }
-
-    let string;
-
-    if (this[0] === null || this[0] === undefined) {
-      string = '';
-    } else {
-      string = this[0].toString();
-    }
-
-    if (this.length === 1) {
-      return string;
-    }
-
-    let item = '';
-
-    for (let i = 1; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        item = separ;
-      } else {
-        item = separ + this[i].toString();
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        string += this[i].toString();
       }
 
-      string += item;
+      if (i !== this.length - 1) {
+        string += separator;
+      }
     }
 
     return string;
