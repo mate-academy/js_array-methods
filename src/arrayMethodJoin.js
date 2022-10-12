9f5a6c2425d84a552e3ad6313982a7e451468534
Mykola Yakymiv <mykola8426@gmail.com>
2022-10-12 23:56:27 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,20 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let string = this[0] === null || typeof this[0]
+    === 'undefined' ? '' : String(this[0]);
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === null || typeof this[i] === 'undefined') {
+        string += separator + '';
+      } else {
+        string = `${string}${typeof separator === 'undefined'
+          ? ','
+          : separator}${this[i]}`;
+      }
+    }
+
+    return string;
   };
 }
 
