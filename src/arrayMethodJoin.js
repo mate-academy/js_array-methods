6ad3e3434fd76f28cc512e7e3d619717acd5f76b
bokorzita <104830913+bokorzita@users.noreply.github.com>
2022-10-14 07:24:27 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,38 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    const joinedArr = [];
+    let j = 0;
+    let newSeparator = separator;
+
+    if (newSeparator === undefined) {
+      newSeparator = ',';
+    }
+
+    for (let i = 0; i < (this.length - 1) * 2 - 1; i++) {
+      if (this[j] === null || this[j] === undefined) {
+        joinedArr[i] = '';
+      } else {
+        joinedArr[i] = this[j];
+      }
+      joinedArr[i + 1] = newSeparator;
+      i++;
+      j++;
+    }
+
+    if (this[this.length - 1] === null || this[this.length - 1] === undefined) {
+      joinedArr[joinedArr.length] = '';
+    } else {
+      joinedArr[joinedArr.length] = this[this.length - 1];
+    }
+
+    let str = '';
+
+    for (const e of joinedArr) {
+      str += e;
+    }
+
+    return str;
   };
 }
 
