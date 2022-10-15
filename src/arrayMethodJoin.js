f9d75c0d669d9d478eef7920adf391f32af02174
Oleksandr Chernikov <pjatochkin2013@gmail.com>
2022-10-15 12:29:20 +0300

    commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,24 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
-  };
+    let string = "";
+    let division = separator;
+    for (let i = 0; i < this.length; i++) {
+      if (division === undefined) {
+        division = ',';
+      }
+      if (division === null) {
+        division = 'null';
+      }
+      if (this[i] !== undefined && this[i] !== null) {
+        string += this[i];
+      }
+      if (this[i] !== this.length -1 && this.length > 1 && this[i] !== '') {
+        string += division;
+      }
+    }
+    return string;
+  }
 }
 
 module.exports = applyCustomJoin;
