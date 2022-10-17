bb27b99488ecfe1704d971c2e02d88243f7315dd
Maks <comanderius@gmail.com>
2022-10-17 21:27:10 +0200

    Method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined && i === this.length - 1) {
+        result += '';
+      } else if (this[i] === null || this[i] === undefined) {
+        result += '';
+        result += separator;
+      } else if (i === this.length - 1) {
+        result += this[i];
+      } else {
+        result += this[i];
+        result += separator;
+      }
+    }
+
+    return result;
   };
 }
 
