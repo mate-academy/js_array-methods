76200b4765d72e6d6c7dc27754960bc44f98a47e
Stepan Prokopyev <stepkapro2002@gmail.com>
2022-10-24 11:57:37 +0300

    Solution was Created

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedString = '';
+    const separatorToString = String(separator);
+
+    for (const item of this) {
+      if (item === null || item === undefined) {
+        joinedString += '' + separatorToString;
+      } else {
+        joinedString += item + separatorToString;
+      }
+    }
+
+    // eslint-disable-next-line max-len
+    return joinedString.slice(0, joinedString.length - separatorToString.length);
   };
 }
 
