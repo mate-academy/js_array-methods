4d8e29cd6c1addc5c62bea4fe279e693dea00d0b
Svitlana Yudina <osbka31@gmail.com>
2022-10-24 16:24:37 +0300

    first commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,22 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+    let sep = String(separator);
+
+    if (separator === undefined) {
+      sep = ',';
+    }
+
+    for (const ch of this) {
+      if (ch === null || ch === undefined) {
+        str += '' + sep;
+      } else {
+        str += String(ch) + sep;
+      }
+    }
+
+    return str.slice(0, str.length - sep.length);
   };
 }
 
