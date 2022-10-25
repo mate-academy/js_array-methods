b135f7d369f0c53590587b59f6cceb8470ec0167
Spirit <baolt.king@gmail.com>
2022-10-25 17:21:47 +0300

    complete

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,23 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let res = '';
+    const stringSep = typeof (separator) === typeof (undefined)
+      ? ','
+      : String(separator);
+
+    for (let x = 0; x < this.length; x++) {
+      if (this[x] === undefined || this[x] === null) {
+        this[x] = '';
+      }
+
+      res += this[x]
+      + (x === this.length - 1
+        ? ''
+        : stringSep);
+    }
+
+    return res;
   };
 }
 
