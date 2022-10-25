3508ba70a1858eddf94b317a345e7f512fc3505e
alpopkov <zenf@MacBook-Pro-zenf.local>
2022-10-25 16:12:23 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1 && this[i] === undefined) {
+        break;
+      }
+
+      if (i === this.length - 1) {
+        result += this[i];
+        break;
+      }
+
+      if (this[i] === null || this[i] === undefined) {
+        result += separator;
+        continue;
+      }
+
+      result += `${this[i]}${separator}`;
+    }
+
+    return result;
   };
 }
 
