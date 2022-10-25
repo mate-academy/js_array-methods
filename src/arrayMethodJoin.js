3b3c763566bc9ce4d03a45119f459e48a494fbe6
Artem66 <melodpi@gmail.com>
2022-10-25 12:44:18 +0100

    implemented method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let separatedString = '';
+    const lastIndex = this.length - 1;
+
+    for (let index = 0; index < this.length; index++) {
+      if (index === lastIndex && this[index] === null) {
+        separatedString += ``;
+      } else if (index === lastIndex && this[index] === undefined) {
+        separatedString += ``;
+      } else if (index === lastIndex) {
+        separatedString += `${this[index]}`;
+      } else if (this[index] === null || this[index] === undefined) {
+        separatedString += `${separator}`;
+      } else {
+        separatedString += `${this[index]}${separator}`;
+      }
+    }
+
+    return separatedString;
   };
 }
 
