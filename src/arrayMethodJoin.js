3db43908207a94150de40dfd00c2369b374835ab
Bohdan Stepanets <number1xy@gmail.com>
2022-10-25 20:23:44 +0300

    Solution with fixes

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let stringFromArray = '';
+    let resultOfJoin = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
-        stringFromArray += (this[i]);
+        resultOfJoin += this[i];
       }
 
       if (i < this.length - 1) {
-        stringFromArray += separator;
+        resultOfJoin += separator;
       }
     }
 
-    return stringFromArray;
+    return resultOfJoin;
   };
 }
 
