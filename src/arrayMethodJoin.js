73a5bac37419cfd348aaca28e1bfa13359a8f1f8
Roman Maksymuk <romamaksymuk@gmail.com>
2022-10-26 00:03:55 +0300

    done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let resultStr = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        this[i] = '';
+      }
+
+      if (i === 0) {
+        resultStr += this[i];
+      } else {
+        resultStr += `${separator}${this[i]}`;
+      }
+    }
+
+    return resultStr;
   };
 }
 
