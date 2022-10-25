583a78d7a8aa54e62b37f156db68e5b52fcb1af7
Anastasiia Smirnova <anastasia.smirnova96@gmail.com>
2022-10-25 19:27:28 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,31 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let string = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === null || typeof this[i] === 'undefined') {
+        string += '';
+        string += separator;
+      } else {
+        string += this[i];
+        string += separator;
+      }
+    }
+
+    if (this[this.length - 1] === null
+      || typeof this[this.length - 1] === 'undefined') {
+      string += '';
+    } else {
+      string += this[this.length - 1];
+    }
+
+    return string;
   };
 }
 
