8794219bb5455d7e6036c8e255175d155c71bc2e
Vadym Baranov <vadym.baranov@hotmail.com>
2022-10-26 14:49:42 +0300

    Task completed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let newStr = '';
+    const lastItem = this.length - 1;
+
+    for (const i of this) {
+      if (i === null || i === undefined) {
+        newStr += '';
+      } else {
+        newStr += i;
+      }
+
+      if (i !== this[lastItem]) {
+        newStr += separator;
+      }
+    }
+
+    return newStr;
   };
 }
 
