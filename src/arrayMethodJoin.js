fcd89d0b919b3f6421b7ccf908ec6bea0817061c
Sergey Kovyazin <mr.roland1989@gmail.com>
2022-10-26 04:28:34 +0300

    join method is ready

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,33 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    const modifiedThis = [...this];
+    const newArray = [];
+    let result = '';
+
+    for (let i = 0; i < modifiedThis.length; i++) {
+      if (modifiedThis[i] === null || modifiedThis[i] === undefined) {
+        modifiedThis[i] = '';
+      }
+    }
+
+    for (let i = 0; i < modifiedThis.length; i++) {
+      newArray[i * 2] = modifiedThis[i];
+    }
+
+    for (let i = 1; i < newArray.length; i = i + 2) {
+      newArray[i] = separator;
+
+      if (separator === undefined) {
+        newArray[i] = ',';
+      }
+    }
+
+    for (const element of newArray) {
+      result += element;
+    }
+
+    return result;
   };
 }
 
