cdc5a7a611d6a38d8c3195a747b00817ce093d2b
Oleksandr Stryzhavchuk <112485525+Oleksandr-Stryzhavchuk@users.noreply.github.com>
2022-10-26 10:48:51 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const arrLength = this.length;
+    let resultStr = '';
+
+    for (let i = 0; i < arrLength; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        resultStr += '';
+      } else {
+        resultStr += this[i];
+      }
+
+      if (i !== arrLength - 1) {
+        resultStr += separator;
+      }
+    }
+
+    return resultStr;
   };
 }
 
