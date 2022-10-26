db9f247a1181781e8a11cf3709631a28889d7c84
AndreyChernysh <achernysh744@gmail.com>
2022-10-26 15:19:03 +0300

    fix after students review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let result = '';
+    let joinString = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== undefined && this[i] !== null) {
-        result += this[i];
+        joinString += this[i];
       }
 
       if (i !== this.length - 1) {
-        result += separator;
+        joinString += separator;
       }
     }
 
-    return result;
+    return joinString;
   };
 }
 
