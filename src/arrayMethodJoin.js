9363d66f4aa6b5e20fe4cde66b1b178ce22cc646
Kovalskyi Semen <esceyp249>
2022-10-26 19:22:30 +0300

    Fixed naming

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let str = '';
+    let joinedString = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== undefined && this[i] !== null) {
-        str += this[i];
+        joinedString += this[i];
       }
 
       if (i !== this.length - 1) {
-        str += separator;
+        joinedString += separator;
       }
     }
 
-    return str;
+    return joinedString;
   };
 }
 
