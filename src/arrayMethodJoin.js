090df35c064e416d76729ad579cdff7ee49a8c7d
Bohdan Kovalenko <bog7807@gmail.com>
2022-10-27 18:12:48 +0300

    Solution git commit -am Solution git commit -am Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,26 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    const lastElement = this[this.length - 1];
+
+    for (let i = 0; i < this.length; i++) {
+      if (this.length === 1) {
+        result += this[i];
+        break;
+      }
+
+      if (this[i] !== null && this[i] !== undefined) {
+        result += this[i];
+      }
+
+      if (this[i] !== lastElement) {
+        result += separator;
+      }
+    }
+
+    return result;
   };
 }
 
