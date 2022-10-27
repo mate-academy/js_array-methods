64d9b99160157715990584ff8c9e0e8b978e482a
Sergey Kovyazin <mr.roland1989@gmail.com>
2022-10-27 04:55:04 +0300

    one loop solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,30 +5,20 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    const modifiedThis = [...this];
-    const newArray = [];
     let result = '';
 
-    for (let i = 0; i < modifiedThis.length; i++) {
-      if (modifiedThis[i] === null || modifiedThis[i] === undefined) {
-        modifiedThis[i] = '';
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        result += this[i];
       }
-    }
 
-    for (let i = 0; i < modifiedThis.length; i++) {
-      newArray[i * 2] = modifiedThis[i];
-    }
-
-    for (let i = 1; i < newArray.length; i = i + 2) {
-      newArray[i] = separator;
-
-      if (separator === undefined) {
-        newArray[i] = ',';
+      if (i !== this.length - 1) {
+        if (separator === undefined) {
+          result += ',';
+        } else {
+          result += separator;
+        }
       }
-    }
-
-    for (const element of newArray) {
-      result += element;
     }
 
     return result;
