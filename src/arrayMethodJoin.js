1a2e03ee1b13ed84f98a51bb93be987301cf1f87
dmitriy-kovalenko <kovalenkodmitrij@gmail.com>
2022-11-04 15:04:12 +0200

    test Ok

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,28 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let resultString = '';
+    let sepInsert = separator;
+
+    if (separator === undefined) {
+      sepInsert = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      let element = this[i];
+
+      if (element === undefined || element == null) {
+        element = '';
+      }
+
+      if (i !== this.length - 1) {
+        resultString += `${element}${sepInsert}`;
+      } else {
+        resultString += `${element}`;
+      }
+    }
+
+    return resultString;
   };
 }
 
