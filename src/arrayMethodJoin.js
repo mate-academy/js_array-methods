203c7686f70644ceaffd4bc96bd105d2a2e8e557
Viktoriia Stetsenko <stetsenko.viktoriia@gmail.com>
2022-11-11 17:43:07 +0200

    method join was implemented

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,24 @@
 'use strict';
 
-/**
- * Implement method join
- */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null) {
+        if (this[i] !== undefined) {
+          result += this[i];
+        }
+      }
+
+      if (i !== this.length - 1) {
+        if (separator !== undefined) {
+          result += separator;
+        }
+      }
+    }
+
+    return result;
   };
 }
 
