6df395f54ed48e9d45729a04cf1b85de0b73b8ff
anton-sosonskyi <anton.sosonskyi@gmail.com>
2022-11-14 20:13:14 +0200

    Fixed nesting

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,28 +4,17 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
     let resultStr = '';
-    let joincharacter = separator;
-
-    if (separator === '') {
-      joincharacter = '';
-    } else if (separator === null) {
-      joincharacter = 'null';
-    } else if (separator === undefined) {
-      joincharacter = ',';
-    }
 
     for (let i = 0; i < this.length; i++) {
-      if (i === this.length - 1 && this[i] === undefined) {
-        continue;
-      } else if (i === this.length - 1) {
+      if (this[i] !== null && this[i] !== undefined) {
         resultStr += this[i];
-      } else if (this[i] === null || this[i] === undefined) {
-        resultStr += '' + joincharacter;
-      } else {
-        resultStr += this[i] + joincharacter;
+      }
+
+      if (i !== this.length - 1) {
+        resultStr += separator;
       }
     }
 
