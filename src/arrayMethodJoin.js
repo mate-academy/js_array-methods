b79357ac8e897c7ffb81b8104869418e221400ec
Nick Busarhin <busarginkolia@gmail.com>
2022-11-14 12:27:12 +0200

    changedJoinMethod

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,23 +7,15 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let rowOfSymbols = '';
 
-    if (this[0] !== null) {
-      rowOfSymbols = rowOfSymbols + this[0];
-    } else {
-      rowOfSymbols = '';
-    }
-
-    if (this.length === 0) {
-      rowOfSymbols = '';
-    }
-
-    for (let i = 1; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        rowOfSymbols += ',';
-      } else {
-        rowOfSymbols += separator + String(this[i]);
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        rowOfSymbols += this[i];
       }
-    };
+
+      if (i !== this.length - 1) {
+        rowOfSymbols += separator;
+      }
+    }
 
     return rowOfSymbols;
   };
