1964bc589b94eabe22067c8f28df0fdcdd667320
GabrielDeveloper <89942450+GabrielMaksimovich@users.noreply.github.com>
2022-11-15 20:44:55 +0200

    crazy solution, that takes me 3 days

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,30 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let string = '';
+    const getEffectiveValue = function(value) {
+      if (value === 'null') {
+        return value;
+      }
+
+      const imValue = String(value);
+
+      return (imValue === 'null' || typeof value === 'undefined') ? ''
+        : imValue;
+    };
+
+    if (this.length <= 0) {
+      return string;
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      const effectiveValue = getEffectiveValue(this[i]);
+
+      string += effectiveValue + (separator + '');
+    }
+
+    return string + getEffectiveValue(this[this.length - 1]);
   };
 }
 
