08d45cb098cfd04644c5fb775990700b4dba163c
Marina TORONYI <marina.toronyi>
2022-11-15 20:38:28 +0100

    'Solution'

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,36 @@
 'use strict';
 
-/**
- * Implement method join
- */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let newString = '';
+
+    if (this[0] === null) {
+      newString += '';
+    } else {
+      newString += this[0];
+    };
+
+    if (this.length === 0) {
+      return '';
+    };
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        if (separator === null) {
+          newString += 'null';
+        } else {
+          newString += separator;
+        };
+      } else {
+        if (separator === null) {
+          newString += ('null' + this[i]);
+        } else {
+          newString += (separator + this[i]);
+        };
+      }
+    };
+
+    return newString;
   };
 }
 
