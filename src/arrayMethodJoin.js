0df34e01a9164ad173d982e4ed0978e2025ad5ed
Yurii Filiushkin <yurii.filiushkin@gmail.com>
2022-11-16 14:11:25 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,41 +6,15 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let newString = '';
-    let newSeparator = separator;
-    const copyArray = [...this];
 
-    for (let i = 0; i < copyArray.length; i++) {
-      if (copyArray[i] === null) {
-        copyArray[i] = '';
-      } else if (copyArray[i] === undefined) {
-        copyArray[i] = '';
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        newString += this[i];
       }
-    }
 
-    switch (separator) {
-      case null:
-        newSeparator = 'null';
-        break;
-
-      case undefined:
-        newSeparator = ',';
-        break;
-    }
-
-    switch (copyArray.length) {
-      case 0:
-        break;
-
-      case 1:
-        newString = copyArray[0].toString();
-        break;
-
-      default:
-        newString = copyArray[0].toString();
-
-        for (let index = 1; index < copyArray.length; index++) {
-          newString += newSeparator.toString() + copyArray[index];
-        }
+      if (i !== this.length - 1) {
+        newString += separator;
+      }
     }
 
     return newString;
