9c5e7f735bd2aca51b177674cff9b0905070b1ee
Oselskyi Oleksandr <oselskyi3@gmail.com>
2022-11-20 09:20:26 +0200

    fix remarks

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -9,25 +9,15 @@ function applyCustomJoin() {
     let str = '';
 
     for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        str += this[i];
+      }
+    
       if (i !== this.length - 1) {
-        if (this[i] === null) {
-          this[i] = '';
-        } else if (this[i] === undefined) {
-          this[i] = '';
-        }
-
-        if (separator === undefined) {
-          str += this[i] + ',';
-        } else if (separator === null) {
-          str += this[i] + 'null';
+        if(separator === undefined) {
+          str += ',';
         } else {
-          str += this[i] + separator;
-        }
-      } else {
-        if (this[i] === undefined) {
-          str += '';
-        } else {
-          str += this[i];
+          str += separator
         }
       }
     }
