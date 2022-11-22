fd5bcae6fe20ba6a32597b89e04423d6310e6899
Demut <uddarendeli1@gmail.com>
2022-11-23 00:23:12 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,32 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    let sep;
+
+    // in case the passed separator is null
+    if (separator === null) {
+      sep = 'null';
+    } else {
+      sep = separator;
+    }
+
+    // joining the array excluding the last element
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        result += this[i];
+      }
+      result += sep;
+    }
+
+    // adding the last element
+    if (this[this.length - 1] !== null
+        && this[this.length - 1] !== undefined) {
+      result += this[this.length - 1];
+    }
+
+    return result;
   };
 }
 
