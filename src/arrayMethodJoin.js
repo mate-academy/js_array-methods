87d745b877e5c35677ea392e80a04625a01957ec
Sergey Mironenko <serg1prime@gmail.com>
2022-11-23 00:59:50 +0200

    Task was done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    let step;
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        step = '';
+      } else {
+        step = this[i];
+      }
+
+      if (i === this.length - 1) {
+        result += `${step}`;
+      } else {
+        result += `${step}` + `${separator}`;
+      }
+    }
+
+    return result;
   };
 }
 
