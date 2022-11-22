7d9152256fab152580e46fa1b101007e1b362a74
Saleh BUD <saleh.7734@gmail.com>
2022-11-22 14:31:05 +0200

    All Done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,23 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let res = '';
+    const sep = separator !== undefined ? separator : ',';
+    const arr = [...this];
+
+    for (let i = 0; i < arr.length; i++) {
+      if (
+        (arr[i] || arr[i] === 0 || isNaN(arr[i]) || arr[i] === false)
+        && arr[i] !== undefined
+      ) {
+        res += String(arr[i]);
+      }
+      res += sep;
+    }
+
+    res = res.substring(0, res.length - String(sep).length);
+
+    return res;
   };
 }
 
