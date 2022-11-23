a7658dd80fb20ea9080bace8d0d67ea7116b13e2
mr-valentos <nerpavlad@gmail.com>
2022-11-23 20:56:45 +0200

    join method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,26 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const newSeparator = String(separator);
+
+    let joinString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      let checkedElement = this[i];
+
+      if (checkedElement === null || checkedElement === undefined) {
+        checkedElement = '';
+      }
+
+      if (i === 0) {
+        joinString += checkedElement;
+      } else {
+        joinString += newSeparator + checkedElement;
+      }
+    }
+
+    return joinString;
   };
 }
 
