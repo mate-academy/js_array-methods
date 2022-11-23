14bb0f12c19b694a382ab5d82628e98a3177a8ae
ysrckr <dyt.yasarcakir@gmail.com>
2022-11-23 19:33:12 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,34 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
-  };
-}
+    let result = '';
+    let argPlaceholder = separator;
 
+    if (separator === undefined) {
+      argPlaceholder = ',';
+    } else if (separator === null) {
+      argPlaceholder = 'null';
+    } else if (typeof separator === 'object') {
+      argPlaceholder = separator.toString();
+    }
+
+    if (argPlaceholder === '') {
+      for (let i = 0; i < this.length; i++) {
+        result += this[i];
+      }
+
+      return result;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        result += argPlaceholder;
+      } else {
+        result += this[i] + argPlaceholder;
+      }
+    }
+
+    return result.slice(0, -argPlaceholder.length);
+  };
+};
 module.exports = applyCustomJoin;
