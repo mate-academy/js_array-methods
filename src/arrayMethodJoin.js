819f3249c6e30686fb92aa0648d35d78bc8c9607
Levente Krotos <krotos.levi@gmail.com>
2022-11-24 19:28:18 +0100

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    const checkSep = (separator === null) ? 'null' : separator;
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        result += '' + checkSep;
+      } else {
+        result += this[i] + checkSep;
+      }
+    }
+
+    return (this[this.length - 1] === undefined)
+      ? result : result + this[this.length - 1];
   };
 }
 
