188ec0edc16a7410c23c8ff5b726919020a1d8c9
mykolachupyak <chupyak@gmail.com>
2022-11-26 21:04:50 +0200

    improved solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,18 +5,17 @@
  */
 
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    const newSeparator = separator === undefined ? ',' : separator;
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
         result = result + this[i];
       }
-      result = result + newSeparator;
+      result = result + separator;
     }
 
-    return result.substring(0, result.length - String(newSeparator).length);
+    return result.substring(0, result.length - String(separator).length);
   };
 }
 
