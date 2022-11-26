c840027859f78f3f483be239315f6ec2932713bc
mykolachupyak <chupyak@gmail.com>
2022-11-26 15:54:48 +0200

    All tests ara passed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,17 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    const newSeparator = separator === undefined ? ',' : separator;
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        result = result + this[i];
+      }
+      result = result + newSeparator;
+    }
+
+    return result.substring(0, result.length - String(newSeparator).length);
   };
 }
 
