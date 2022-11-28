09fb387aece633b352ca49f88ca7e2c2ff17143c
Kate Buchinska <katrishka18@gmail.com>
2022-11-28 23:09:08 +0400

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,22 +5,22 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let resultString = '';
+    let changedString = '';
     const lastInArray = this[this.length - 1];
 
     for (let i = 0; i < this.length - 1; i++) {
       if (this[i] === null || this[i] === undefined) {
-        resultString += `${separator}`;
+        changedString += `${separator}`;
       } else {
-        resultString += this[i] + `${separator}`;
+        changedString += this[i] + `${separator}`;
       }
     }
 
     if (lastInArray !== null && lastInArray !== undefined) {
-      resultString += lastInArray;
+      changedString += lastInArray;
     }
 
-    return resultString;
+    return changedString;
   };
 }
 
