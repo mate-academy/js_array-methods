ee257b9e3f1da77f18f1e95626721341d35654d0
Alireza Mohammadhossein <mh.alireza1993@gmail.com>
2022-11-28 22:56:16 +0100

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,26 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let newSeparator = separator;
+
+    if (separator === undefined) {
+      newSeparator = ',';
+    }
+
+    let list = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        list += this[i];
+      }
+
+      if (this.length - 1 !== i) {
+        list += newSeparator;
+      }
+    }
+
+    return list;
   };
-}
+};
 
 module.exports = applyCustomJoin;
