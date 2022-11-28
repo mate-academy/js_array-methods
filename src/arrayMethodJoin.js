9ac7de67c7e57b7db189a2166350bae986c0ec86
Dima Kolesnyk <kolesnyk.dmytro.victorovych@gmail.com>
2022-11-28 22:12:54 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,25 @@
 /**
  * Implement method join
  */
+
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let arrayToString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (typeof this[i] === 'string' || typeof this[i] === 'number'
+      || typeof this[i] === 'boolean') {
+        arrayToString += this[i];
+      } else {
+        arrayToString += '';
+      }
+
+      if (i !== this.length - 1) {
+        arrayToString += separator;
+      }
+    }
+
+    return arrayToString;
   };
 }
 
