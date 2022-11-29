03259c928f61f037625585f1ccbc7c5f76b9403d
Mohamed Dahi <mohamedjane362@gmail.com>
2022-11-29 15:18:40 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,35 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      if (separator === undefined) {
+        if (i === this.length - 1) {
+          str += this[i];
+        } else {
+          str += this[i] + ',';
+        }
+      } else if (separator === null) {
+        if (i === this.length - 1) {
+          str += this[i];
+        } else {
+          str += this[i] + 'null';
+        }
+      } else {
+        if (i === this.length - 1) {
+          str += this[i];
+        } else {
+          str += this[i] + separator;
+        }
+      }
+    }
+
+    return str;
   };
 }
 
