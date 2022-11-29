9afb9a198dd133813c824e5e83c6d6e5f9032145
Roman Korchovskiy <korchovskiy@MacBook-Pro-Roman.local>
2022-11-29 10:35:19 +0200

    array method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let joinedStr = '';
+
+    for (let i = 0; i < this.length; i++) {
+      let currentElement = this[i];
+
+      if (currentElement === null || currentElement === undefined) {
+        currentElement = '';
+      }
+
+      joinedStr += currentElement;
+
+      if (i !== this.length - 1) {
+        joinedStr += separator;
+      }
+    }
+
+    return joinedStr;
   };
 }
 
