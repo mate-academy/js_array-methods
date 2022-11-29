27e3abed685fe5f049ca8392d53e85074f3e2123
Diana Demkiv <dianademkiv789@gmail.com>
2022-11-30 00:35:30 +0200

    method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const separatorToString = String(separator);
+    let newJoin = '';
+    const lastSymbol = this[this.length - 1];
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        newJoin += this[i] + separatorToString;
+      } else {
+        newJoin += separatorToString;
+      }
+    }
+
+    if (lastSymbol !== null && lastSymbol !== undefined) {
+      newJoin += lastSymbol;
+    }
+
+    return newJoin;
   };
 }
 
