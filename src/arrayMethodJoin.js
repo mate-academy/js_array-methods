7c1eef123a84127f531b53cca3999cccf2132f58
Anton Martynes <martynes1997@gmail.com>
2022-11-29 15:54:27 +0200

    join method realised

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const separatorStr = `${separator}`;
+    let joinedString = '';
+
+    const length = this.length;
+
+    for (let i = 0; i < length; i++) {
+      let element = this[i];
+
+      if (element === null || element === undefined) {
+        element = '';
+      }
+
+      if (i === length - 1) {
+        joinedString += element;
+        continue;
+      }
+
+      joinedString += element + separatorStr;
+    }
+
+    return joinedString;
   };
 }
 
