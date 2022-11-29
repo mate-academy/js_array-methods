5527f575f0a04abd0ede4d0416e720f833e2d789
Daryna Zinchenko <zinchenko.daryna@gmail.com>
2022-11-29 16:56:46 +0100

    join created

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,35 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedStr = '';
+    let sep = separator;
+
+    if (separator === null) {
+      sep = 'null';
+    }
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (this.length === 1) {
+      return `${this[0]}`;
+    }
+
+    function value(val) {
+      if ((val === undefined) || (val === null)) {
+        return '';
+      }
+
+      return val;
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      joinedStr += value(this[i]) + sep;
+    }
+
+    return joinedStr + value(this[this.length - 1]);
   };
 }
 
