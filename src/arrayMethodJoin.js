665b472be00dc7d7973450fea698e1a3720d1f2e
Jack Ashkar <jackelashkar@gmail.com>
2022-11-30 20:06:55 +0300

    finished

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedString = '';
+
+    for (let i = 0; i < this.length; i += 1) {
+      if (this[i] !== undefined && this[i] !== null) {
+        joinedString += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        joinedString += separator;
+      }
+    }
+
+    return joinedString;
   };
 }
 
