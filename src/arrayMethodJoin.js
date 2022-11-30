9c10a292d926e6156f0ad6d091619a482d40591e
Ivan Melnychuk <jacksmilereacher@gmail.com>
2022-12-01 00:22:55 +0200

    implemented join method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let resultString = (this[0] === null || this[0] === undefined)
+      ? ''
+      : String(this[0]);
+    const elementSerarator = separator === null
+      ? 'null'
+      : separator;
+
+    for (let i = 1; i < this.length; i++) {
+      const element = (this[i] === null || this[i] === undefined)
+        ? ''
+        : this[i];
+
+      resultString += elementSerarator + element;
+    }
+
+    return resultString;
   };
 }
 
