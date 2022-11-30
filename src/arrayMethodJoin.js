ea05df0f8e826b892612063e4b1494ea08da5e5c
Anastasiia Bilska <nastya.bilskaya@gmail.com>
2022-11-30 20:52:00 +0200

    mistakes were fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,22 +5,20 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let whole = '';
+    let joinedStr = '';
     const maxIndex = this.length - 1;
 
     for (let i = 0; i <= maxIndex; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        whole += '';
-      } else {
-        whole += this[i];
+      if (this[i] !== null && this[i] !== undefined) {
+        joinedStr += this[i];
       }
 
       if (i !== maxIndex) {
-        whole += separator;
+        joinedStr += separator;
       }
     }
 
-    return whole;
+    return joinedStr;
   };
 }
 
