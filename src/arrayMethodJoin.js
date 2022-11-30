2027014c8ec4490d98bab626880991d9d1c82509
mr-valentos <nerpavlad@gmail.com>
2022-11-30 11:45:59 +0200

    mentor review fix

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,21 +6,21 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     const newSeparator = String(separator);
+    let joinString;
 
-    let joinString = '';
+    if (this[0] === null || this[0] === undefined) {
+      joinString = '';
+    } else {
+      joinString = String(this[0]);
+    }
 
-    for (let i = 0; i < this.length; i++) {
+    for (let i = 1; i < this.length; i++) {
       let checkedElement = this[i];
 
       if (checkedElement === null || checkedElement === undefined) {
         checkedElement = '';
       }
-
-      if (i === 0) {
-        joinString += checkedElement;
-      } else {
-        joinString += newSeparator + checkedElement;
-      }
+      joinString += newSeparator + checkedElement;
     }
 
     return joinString;
