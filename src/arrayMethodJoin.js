5365ce74c976c562337a3df0c369c2ff7197c981
Vasya Rymarenko <vrima60@gmail.com>
2022-11-30 16:18:09 +0200

    Variable name changed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,21 +5,21 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let res = '';
+    let resultOfJoin = '';
     const separatorToString = String(separator);
 
     for (let i = 0; i < this.length - 1; i++) {
       if (this[i] === undefined || this[i] === null) {
         this[i] = '';
       }
-      res += `${this[i]}${separatorToString}`;
+      resultOfJoin += `${this[i]}${separatorToString}`;
     }
 
     if (this[this.length - 1] !== undefined) {
-      res += `${this[this.length - 1]}`;
+      resultOfJoin += `${this[this.length - 1]}`;
     }
 
-    return res;
+    return resultOfJoin;
   };
 }
 
