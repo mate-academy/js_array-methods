ed0de0ce974aa70ddd1529cbcf930a68c4a43ab5
Romeo Kiss <kiss.romeo.richard@gmail.com>
2022-11-30 20:21:32 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,27 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let string = '';
+    let normalizedSeparator = separator;
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null | this[i] === undefined) {
+        this[i] = '';
+      }
+
+      if (separator === null) {
+        normalizedSeparator = 'null';
+      }
+
+      if (i < this.length - 1) {
+        string += this[i] + normalizedSeparator;
+      } else {
+        string += this[i];
+      }
+    }
+
+    return string;
   };
 }
 
