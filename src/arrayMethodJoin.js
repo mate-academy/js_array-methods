eb7c87503ebf4efb69bde09a608669fb6048dd9b
Sveta Kryukova <kryukova.sv.13@gmail.com>
2022-12-01 08:43:17 +0200

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,18 +4,8 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    let string = '';
-
-    if (separator !== undefined) {
-      string = this.toString().replace(/,/g, separator);
-
-      return (string);
-    } else {
-      string = this.toString();
-
-      return (string);
-    }
+  [].__proto__.join2 = function(separator = ',') {
+    return this.toString().replace(/,/g, separator);
   };
 };
 
