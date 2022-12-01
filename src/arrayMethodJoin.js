ee3bb7f04c122233bdcacf6dc5b08dc6e80d7b54
Oleksandr Dehtiarov <retabo.kainrou@gmail.com>
2022-12-01 02:00:54 +0200

    Solution for array task

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const separatorAsString = String(separator);
+    const lastItem = this[this.length - 1];
+    let result = '';
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        result += separatorAsString;
+      } else {
+        result += this[i] + separatorAsString;
+      }
+    }
+
+    if (lastItem !== undefined) {
+      result += lastItem;
+    }
+
+    return result;
   };
 }
 
