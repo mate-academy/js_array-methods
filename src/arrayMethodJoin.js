900419a2c15e75fc7e40dbc7c550836151d77f1e
Olena Yanovska <alena.yn.adw@gmail.com>
2022-12-02 02:03:15 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,21 @@
 /**
  * Implement method join
  */
-function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+function applyCustomJoin(input) {
+  [].__proto__.join2 = function(separator = ',') {
+    let newString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        newString += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        newString += separator;
+      }
+    }
+
+    return newString.trim();
   };
 }
 
