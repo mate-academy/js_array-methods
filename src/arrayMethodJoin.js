171f67d3074eee749ae20ddd79d59a477b008296
Vadym Prydatok <vadymprydatok@gmail.com>
2022-12-06 14:51:52 +0200

    MethodJoin 2.0

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,39 +4,16 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let newString = '';
-    let cloneSeparator = separator;
-
-    if (cloneSeparator === undefined) {
-      cloneSeparator = ',';
-    }
-
-    if (cloneSeparator === null) {
-      cloneSeparator = 'null';
-    }
 
     for (let i = 0; i < this.length; i++) {
-      if (i === this.length - 1) {
-        if (this[i] === undefined) {
-          break;
-        }
+      if (this[i] !== undefined && this[i] !== null) {
         newString += this[i];
-        break;
       }
 
-      if (Number.isNaN(this[i])) {
-        newString += 'NaN' + cloneSeparator;
-      }
-
-      if (this[i] === undefined || this[i] === null) {
-        newString += cloneSeparator;
-        continue;
-      }
-
-      if (this[i + 1] || this[i + 1] === undefined || this[i + 1] === null
-        || isNaN(this[i + 1])) {
-        newString += this[i] + cloneSeparator;
+      if (i !== this.length - 1) {
+        newString += separator;
       }
     };
 
