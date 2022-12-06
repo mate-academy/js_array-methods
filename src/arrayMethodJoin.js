edac9edc497cc04f1c1b951eb30e5c7deafe862a
Anatolii <batstolya@gmail.com>
2022-12-06 13:29:47 +0200

    another solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,28 +5,21 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    const separatorToString = separator + '';
     let resultString = '';
 
     for (let i = 0; i < this.length; i++) {
       const element = this[i];
 
-      if (element === undefined || element === null) {
-        resultString += separatorToString;
-      } else {
-        resultString += element + separatorToString;
+      if (element !== null && element !== undefined) {
+        resultString += element;
+      }
+
+      if (i !== this.length - 1) {
+        resultString += separator;
       }
     }
 
-    const lengthSeparator = separatorToString.length;
-
-    if (lengthSeparator === 0) {
-      return resultString;
-    }
-
-    const stringWithoutLastElement = resultString.slice(0, -lengthSeparator);
-
-    return stringWithoutLastElement;
+    return resultString;
   };
 }
 
