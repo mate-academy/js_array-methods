bfe48f10764a183457e5124ca3e26811f0fe5e86
Vadym Prydatok <vadymprydatok@gmail.com>
2022-12-06 14:38:11 +0200

    MethodJoin

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,42 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let newString = '';
+    let cloneSeparator = separator;
+
+    if (cloneSeparator === undefined) {
+      cloneSeparator = ',';
+    }
+
+    if (cloneSeparator === null) {
+      cloneSeparator = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1) {
+        if (this[i] === undefined) {
+          break;
+        }
+        newString += this[i];
+        break;
+      }
+
+      if (Number.isNaN(this[i])) {
+        newString += 'NaN' + cloneSeparator;
+      }
+
+      if (this[i] === undefined || this[i] === null) {
+        newString += cloneSeparator;
+        continue;
+      }
+
+      if (this[i + 1] || this[i + 1] === undefined || this[i + 1] === null
+        || isNaN(this[i + 1])) {
+        newString += this[i] + cloneSeparator;
+      }
+    };
+
+    return newString;
   };
 }
 
