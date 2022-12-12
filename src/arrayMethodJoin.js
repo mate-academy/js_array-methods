c35b3669b087ec94fde6d63d65195d6cdd69cec5
ihorivna007 <emmakorovnik@gmail.com>
2022-12-12 14:48:39 +0200

    initial commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    if (this[0] != null) {
+      result += this[0];
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      let str = '';
+
+      if (this[i] != null) {
+        str = this[i];
+      }
+      result += `${separator}${str}`;
+    }
+
+    return result;
   };
 }
 
