bdd44cab149d17b7c386d8810f3d25063aab2f70
Oleksandr Maslo <98667890az@gmail.com>
2022-12-21 14:18:24 +0200

    joinFunction

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,27 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    let copySeparator = separator;
+
+    if (copySeparator === null) {
+      copySeparator = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      if (i < this.length - 1) {
+        result += this[i] + copySeparator;
+      } else {
+        result += this[i];
+      }
+    }
+
+    return result;
   };
 }
 
