6048a87b2cede273d1cb2a4e46cb00acc09685ab
Taras Baran <baranprivat22@gmail.com>
2022-12-22 00:44:27 +0200

    finished

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1) {
+        result += this[i] || '';
+
+        return result;
+      }
+
+      switch (this[i]) {
+        case undefined:
+        case null:
+          result += '' + `${separator}`;
+
+          break;
+        default:
+          result += this[i] + `${separator}`;
+      }
+    }
+
+    return result;
   };
 }
 
