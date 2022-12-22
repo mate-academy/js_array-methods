0679ded6dff87b833bdf13c49d8dceed25f47eb7
drhtka <drhtka@gmil.com>
2022-12-22 12:42:43 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let tmpString = '';
+    const lastKey = this.length - 1;
+
+    for (const key of this) {
+      if (key === null || key === undefined) {
+        tmpString += '';
+      } else {
+        tmpString += key;
+      }
+
+      if (key !== this[lastKey]) {
+        tmpString += separator;
+      }
+    }
+
+    return tmpString;
   };
 }
 
