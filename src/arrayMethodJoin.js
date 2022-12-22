5acfda86206f5ee25cc1026f41836f371f71052a
Karadzha Mykola <nicholaskaradzha@gmail.com>
2022-12-22 10:06:08 +0000

    custom join method completed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,32 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let newSeparator = separator;
+    const newArray = [];
+    let string = '';
+
+    if (!separator) {
+      newSeparator = `${separator}`;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        newArray.push('');
+      } else {
+        newArray.push(this[i]);
+      }
+    }
+
+    for (let i = 0; i < newArray.length; i++) {
+      if (i === newArray.length - 1) {
+        string += newArray[i];
+      } else {
+        string += newArray[i] + newSeparator;
+      }
+    }
+
+    return string;
   };
 }
 
