6b28f2746dd521b7b4a8e0aadafafa9bc7650ef1
Sergey Mironenko <serg1prime@gmail.com>
2022-12-27 23:46:39 +0200

    Task was fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,19 +6,14 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    let step;
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        step = '';
-      } else {
-        step = this[i];
+      if (this[i] !== null && this[i] !== undefined) {
+        result += this[i];
       }
 
-      if (i === this.length - 1) {
-        result += `${step}`;
-      } else {
-        result += `${step}` + `${separator}`;
+      if (i !== this.length - 1) {
+        result += separator;
       }
     }
 
