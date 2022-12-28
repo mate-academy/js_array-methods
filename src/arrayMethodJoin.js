2d52036f595ef713b9caa3ca1f4a1617f0155e63
Sergey Mironenko <serg1prime@gmail.com>
2022-12-28 21:55:51 +0200

    Method join third commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let result = '';
+    let joinedString = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
-        result += this[i];
+        joinedString += this[i];
       }
 
       if (i !== this.length - 1) {
-        result += separator;
+        joinedString += separator;
       }
     }
 
-    return result;
+    return joinedString;
   };
 }
 
