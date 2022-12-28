0cb5d120cda44e4e06242b023a45428b1decc752
Alex Vahnovan <sonyaxent@gmail.com>
2022-12-29 00:00:41 +0100

    Created join2 function

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let join2 = '';
+
+    for (let i = 0; i < this.length; i++) {
+      let element = this[i];
+
+      if (element === null || element === undefined) {
+        element = '';
+      }
+
+      join2 += element;
+
+      if (i !== this.length - 1) {
+        join2 += separator;
+      }
+    }
+
+    return join2;
   };
 }
 
