a830e53d832d16b15109362e5d4b2d889a767071
Iryna Pysanko <malva37@gmail.com>
2022-12-30 11:35:15 +0200

    delete

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,10 +7,6 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let newString = '';
 
-    if (this.length === 1) {
-      return '1';
-    }
-
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== undefined && this[i] !== null) {
         newString += this[i];
