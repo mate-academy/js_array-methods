250b4adb65e3a8425815de545b1cd76f001df704
Andrey Berezyanskiy <andrey.berezyanskiy@gmail.com>
2022-12-30 17:03:10 +0200

    v-01

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,29 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    const copyArray = [...this];
+    let joinString = '';
+    let stringSeparator = '';
+
+    if (separator === undefined) {
+      stringSeparator = ',';
+    } else {
+      stringSeparator = '' + separator;
+    }
+
+    for (let i = 0; i < copyArray.length; i++) {
+      if (copyArray[i] === null || copyArray[i] === undefined) {
+        copyArray[i] = '';
+      }
+
+      if (i !== copyArray.length -1) {
+        joinString += copyArray[i] + stringSeparator;
+      } else {
+        joinString += copyArray[i];
+      }
+    }
+
+    return joinString;
   };
 }
-
 module.exports = applyCustomJoin;
