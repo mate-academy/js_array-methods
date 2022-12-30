ba9807ce90b87b0a763da6a6792317dabf9ea674
Ihor Bondarenko <garryrocksmanjs@gmail.com>
2022-12-30 17:47:25 +0200

    Custom implementation of Array.prototype.join()

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joined = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || typeof this[i] === 'undefined') {
+        this[i] = '';
+      }
+
+      if (i < this.length - 1) {
+        joined += this[i]
+          + (separator !== null ? separator.toString() : 'null');
+      } else {
+        joined += this[i];
+      }
+    }
+
+    return joined;
   };
 }
 
