a26d50dcbb3fb8449cf9575d44f0a54cc0e125fd
Sasha <privetloch@gmail.com>
2022-12-31 23:35:59 +0200

    ez

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,39 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    // const arr = [...this];
+    let res = '';
+    let separatorCopy = separator;
+
+    for (let i = 0; i < this.length; i++) {
+      // if (isNaN(this[i])) {
+      //   this[i] = '';
+      // }
+
+      if (this[i] === undefined) {
+        this[i] = '';
+      }
+
+      if (this[i] === null) {
+        this[i] = '';
+      }
+
+      if (separatorCopy === undefined) {
+        separatorCopy = ',';
+      }
+
+      if (separatorCopy === null) {
+        separatorCopy = 'null';
+      }
+
+      if (i !== this.length - 1) {
+        res += this[i] + separatorCopy;
+      } else {
+        res += this[i];
+      }
+    }
+
+    return res;
   };
 }
 
