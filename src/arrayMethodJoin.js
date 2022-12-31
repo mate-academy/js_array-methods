b5b6b49a17221776c7be12eedbe309a717daf2c5
svitjojo <kornotavr@gmail.com>
2022-12-31 12:54:30 +0200

    Result

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,29 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let string = '';
+    let copySeparator = `${separator}`;
+
+    if (typeof separator === 'undefined') {
+      copySeparator = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      switch (typeof this[i]) {
+        case 'object':
+          string += copySeparator;
+          break;
+        case 'undefined':
+          string += copySeparator;
+          break;
+        default:
+          string += this[i] + copySeparator;
+      }
+    }
+
+    const lastSeparator = string.lastIndexOf(copySeparator);
+
+    return string.slice(0, lastSeparator);
   };
 }
 
