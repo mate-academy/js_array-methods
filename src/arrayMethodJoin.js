d9d3c97f2037d5068d3fc47dab9bcbefd79f59e5
Andrew Borys <borysandrey1988@gmail.com>
2023-01-02 16:51:20 +0200

    solution_1

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,29 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let result = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (this[this.length - 1] === undefined) {
+      this[this.length - 1] = '';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      if (separator !== undefined) {
+        result += this[i] + `${separator}`;
+      } else {
+        result += this[i] + ',';
+      }
+    }
+
+    return result + this[this.length - 1];
   };
 }
 
