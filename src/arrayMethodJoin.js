c6f12cc55f4b17cc65b2aed0d89744a0f19ac13d
viktor.ionov <viktorionovcvlrs@gmail.com>
2023-01-02 12:08:03 +0100

    Implement method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,27 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let joinedString = '';
+    let thisSeparator = separator;
+
+    if (separator === undefined) {
+      thisSeparator = ',';
+    }
+
+    for (let i = 0; i <= this.length - 1; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        if (i !== this.length - 1) {
+          joinedString += thisSeparator;
+        }
+      } else if (i === this.length - 1) {
+        joinedString += this[i];
+      } else {
+        joinedString += this[i];
+        joinedString += thisSeparator;
+      }
+    }
+
+    return joinedString;
   };
 }
 
