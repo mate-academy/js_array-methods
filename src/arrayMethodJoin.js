080cd72ea7d2d65e48124790ae6639ce6539fa47
Tretiak Serhii <tretyak8936@gmail.com>
2023-01-02 21:37:45 +0200

    solved task

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinString = '';
+    let changeSeparator = separator;
+
+    if (changeSeparator === null) {
+      changeSeparator = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        if (i === this.length - 1) {
+          joinString += '';
+        } else {
+          joinString += '' + changeSeparator;
+        }
+      } else if (i === this.length - 1) {
+        joinString += this[i];
+      } else {
+        joinString += this[i] + changeSeparator;
+      }
+    }
+
+    return joinString;
   };
 }
 
