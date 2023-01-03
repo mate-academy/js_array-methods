e34b70488066364a9ab12cf70aa7368c15901b1c
Bohdan Kutasevich <obvinmusic@gmail.com>
2023-01-03 13:16:21 +0200

    implemented join method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    const separatorToString = String(separator);
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        result += separatorToString;
+      } else {
+        result += this[i] + separatorToString;
+      }
+    }
+
+    if (this[this.length - 1]) {
+      result += this[this.length - 1];
+    }
+
+    return result;
   };
 }
 
