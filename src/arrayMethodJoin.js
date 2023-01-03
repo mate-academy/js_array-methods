f823cf376025822054fbc28d7e28ac2abf3257d7
Oleksandr Prokopenkop <alex.prokop2206@gmail.com>
2023-01-03 14:38:29 +0200

    comleted

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,41 @@
 'use strict';
 
-/**
+/*
  * Implement method join
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+
+    let newStr = `${this[0]}`;
+
+    if (this[0] === undefined || this[0] === null) {
+      newStr = '';
+    }
+
+    let separatorDefault = separator;
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (separatorDefault === null) {
+      separatorDefault = 'null';
+    }
+
+    if (separatorDefault === undefined) {
+      separatorDefault = ',';
+    }
+
+    for (let i = 1; this.length > i; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        newStr += separatorDefault;
+      } else {
+        newStr += separatorDefault + this[i];
+      }
+    }
+
+    return newStr;
   };
 }
 
