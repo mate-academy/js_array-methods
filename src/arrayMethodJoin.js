054458226805520cf6645efe946a81ebcfe050ca
nkondd <nkond222@gmail.com>
2023-01-03 19:34:42 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    let modifiedSeparator = separator;
+
+    if (separator === null) {
+      modifiedSeparator = 'null';
+    }
+
+    const unsupportedTypes = [undefined, null];
+
+    for (let i = 0; i < this.length; i++) {
+      const currentItem = unsupportedTypes.includes(this[i]) ? '' : this[i];
+
+      if (i === this.length - 1) {
+        result += currentItem;
+        continue;
+      }
+
+      result += currentItem + modifiedSeparator;
+    }
+
+    return result;
   };
 }
 
