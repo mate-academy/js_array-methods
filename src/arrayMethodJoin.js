7111594ef2d5893d05f41468a95b0c6ad2ab6ef4
Oleksandr Prokopenkop <alex.prokop2206@gmail.com>
2023-01-03 19:21:00 +0200

    add default + upgrade process for add separator

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,34 +4,17 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let newStr = '';
 
-    let newStr = `${this[0]}`;
+    for (let i = 0; this.length > i; i++) {
+      if (!(this[i] === undefined) && !(this[i] === null)) {
+        newStr += this[i];
+      }
 
-    if (this[0] === undefined || this[0] === null) {
-      newStr = '';
-    }
-
-    let separatorDefault = separator;
-
-    if (this.length === 0) {
-      return '';
-    }
-
-    if (separatorDefault === null) {
-      separatorDefault = 'null';
-    }
-
-    if (separatorDefault === undefined) {
-      separatorDefault = ',';
-    }
-
-    for (let i = 1; this.length > i; i++) {
-      if (this[i] === undefined || this[i] === null) {
-        newStr += separatorDefault;
-      } else {
-        newStr += separatorDefault + this[i];
+      if (i !== this.length - 1) {
+        newStr += separator;
       }
     }
 
