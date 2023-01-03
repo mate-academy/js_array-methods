e0fd10cb23a709709a211f6db88199c874584663
Margo <margo@Margos-MBP.home>
2023-01-03 14:03:49 +0000

    Issues were fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,19 +4,16 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    const mySeparator = separator === undefined ? ',' : separator;
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === '' || this[i] === undefined) {
-        result += '';
-      } else {
+      if (this[i] !== null && this[i] !== undefined) {
         result += this[i];
       }
 
       if (i !== this.length - 1) {
-        result += mySeparator;
+        result += separator;
       }
     }
 
