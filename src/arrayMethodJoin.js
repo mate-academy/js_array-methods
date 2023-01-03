a85c70e98a862b518dd5a8895289da283dddbe16
Oleksandr Burovetskyi <oleksandr.burovetskyi.knm.2020@lpnu.ua>
2023-01-03 15:07:59 +0200

    Added solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let str = '';
+    let arrayString = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== undefined && this[i] !== null) {
-        str += this[i];
+        arrayString += this[i];
       }
 
       if (i < this.length - 1) {
-        str += separator;
+        arrayString += separator;
       }
     }
 
-    return str;
+    return arrayString;
   };
 }
 
