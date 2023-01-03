79a60f9be332b420fb1c70bac2b52f85e71f364a
Andrey Berezyanskiy <andrey.berezyanskiy@gmail.com>
2023-01-03 16:33:40 +0200

    was added dafault parametr for separator and changed if condition for null and undefined

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,25 +4,18 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let joinString = '';
-    let stringSeparator = '';
-
-    separator === undefined
-      ? stringSeparator = ','
-      : stringSeparator = '' + separator;
+    const stringSeparator = separator;
 
     for (let i = 0; i < this.length; i++) {
-      let element = this[i];
 
-      if (element === null || element === undefined) {
-        element = '';
+      if (this[i] !== null && this[i] !== undefined) {
+        joinString += this[i];
       }
 
       if (i !== this.length - 1) {
-        joinString += element + stringSeparator;
-      } else {
-        joinString += element;
+        joinString += stringSeparator;
       }
     }
 
