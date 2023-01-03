dbce9153f13e71de952f834e40fbeb7eb60a6904
Oleksandr Burovetskyi <oleksandr.burovetskyi.knm.2020@lpnu.ua>
2023-01-03 16:08:49 +0200

    Fixed variable name

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let arrayString = '';
+    let resultString = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== undefined && this[i] !== null) {
-        arrayString += this[i];
+        resultString += this[i];
       }
 
       if (i < this.length - 1) {
-        arrayString += separator;
+        resultString += separator;
       }
     }
 
-    return arrayString;
+    return resultString;
   };
 }
 
