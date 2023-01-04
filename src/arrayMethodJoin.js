ff442ad178e3c5de19bead3fd733d74808a66b68
Bohdan Kutasevich <obvinmusic@gmail.com>
2023-01-04 13:31:51 +0200

    rewrite code from opposite

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,14 +6,15 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    const separatorToString = String(separator);
 
     for (let i = 0; i < this.length - 1; i++) {
-      if (this[i] === undefined || this[i] === null) {
-        result += separatorToString;
-      } else {
-        result += this[i] + separatorToString;
+      if (this[i] !== undefined && this[i] !== null) {
+        result += this[i];
       }
+
+      if (i !== this.length - 1) {
+        result += separator;
+     }
     }
 
     if (this[this.length - 1]) {
