501659af010053f8debc5b38e3bf441038ebf31d
Samir Mamedov <samirmamedoviphone@gmail.com>
2023-01-05 14:08:10 +0200

    MethodJoin was made

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,37 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (separator === null) {
+        if (i === this.length - 1) {
+          result += this[i];
+          break;
+        }
+
+        result += this[i] + 'null';
+        continue;
+      }
+
+      if (i !== this.length - 1) {
+        if (this[i] === null
+          || this[i] === undefined) {
+          result += separator;
+          continue;
+        }
+
+        result += this[i] + separator;
+      } else if (i === this.length - 1
+        && this[i] === undefined) {
+        break;
+      } else {
+        result += this[i];
+      }
+    }
+
+    return result;
   };
 }
 
