9faa41c96d7a90b38fc7d20eafcc2bc4fe511940
YouRa Allakhverdov <allakhverdovyuriy@gmail.com>
2023-01-06 11:18:26 +0200

    Finish task

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,43 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let newString = '';
+    const newArray = [];
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (this.length === 1) {
+      return '' + this[0];
+    }
+
+    for (const item of this) {
+      if (separator === undefined) {
+        newArray.push(item, ',');
+        continue;
+      }
+
+      if (item === undefined || item === null) {
+        newArray.push('', ',');
+      } else {
+        newArray.push(item, separator);
+      }
+    }
+
+    if (separator === '') {
+      for (const item of newArray) {
+        newString += item;
+      }
+
+      return newString;
+    }
+
+    for (let i = 0; i < newArray.length - 1; i++) {
+      newString += newArray[i];
+    }
+
+    return newString;
   };
 }
 
