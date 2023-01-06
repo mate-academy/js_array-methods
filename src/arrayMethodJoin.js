642ade7a945f8bbfd1dc2cf1f1d1c13e71771acb
Nazar Medushevskyi <nazar378378379379@gmail.com>
2023-01-06 19:20:03 +0200

    develop

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,21 @@
 /**
  * Implement method join
  */
-function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+ function applyCustomJoin() {
+  [].__proto__.join2 = function(separator = ',') {
+    let resultString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        resultString += this[i];
+      }
+
+      if (i < this.length - 1) {
+        resultString += separator;
+      }
+    }
+
+    return resultString;
   };
 }
 
