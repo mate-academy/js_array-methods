c26da5b37fdf4b34622c09a5e5bca3e7506bf319
Samir Mamedov <samirmamedoviphone@gmail.com>
2023-01-07 14:30:36 +0200

    Changes were made

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,29 +8,12 @@ function applyCustomJoin() {
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (separator === null) {
-        if (i === this.length - 1) {
-          result += this[i];
-          break;
-        }
-
-        result += this[i] + 'null';
-        continue;
+      if (this[i] !== undefined && this[i] !== null) {
+        result += this[i];
       }
 
       if (i !== this.length - 1) {
-        if (this[i] === null
-          || this[i] === undefined) {
-          result += separator;
-          continue;
-        }
-
-        result += this[i] + separator;
-      } else if (i === this.length - 1
-        && this[i] === undefined) {
-        break;
-      } else {
-        result += this[i];
+        result += separator;
       }
     }
 
