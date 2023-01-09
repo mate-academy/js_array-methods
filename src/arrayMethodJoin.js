25b29bf4818d7abc4644ca7cff0ce01dc0ee71cd
Kseniya Gonchar <34249412+KseniyaGonchar@users.noreply.github.com>
2023-01-09 21:02:37 +0200

    Update arrayMethodJoin.js

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,16 +5,16 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let joinedString = '';
+    let joinedStr = '';
 
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
-        joinedString += this[i];
+        joinedStr += this[i];
       }
 
          if (i < (this.length - 1)) {
-          joinedString += separator;
+          joinedStr += separator;
         }
     }
 
