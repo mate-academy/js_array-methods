f73767a57d7723ff542093313398f5607ace612b
Nick <tortickk8475@gmail.com>
2023-01-10 14:52:53 +0200

    add solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,19 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const str = this.toString();
+    let res = '';
+
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === ',') {
+        res += str[i].replace(str[i], separator);
+      } else {
+        res += str[i];
+      }
+    }
+
+    return res;
   };
 }
 
