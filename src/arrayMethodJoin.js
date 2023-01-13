ddd927693ed3e723293e3e93f82ecab306576297
Ch0p3r84 <marin.iliev@yahoo.com>
2023-01-13 19:50:33 +0100

    solution revision

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,30 +7,14 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
 
-    if (this.length === 0) {
-      return result;
-    }
-
-    for (let i = 0; i < this.length - 1; i++) {
-      if (this.length === 1) {
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
         result += this[i];
-
-        return result;
       }
 
-      if (this[i] === null || this[i] === undefined) {
-        result += '';
-        result += separator;
-      } else {
-        result += this[i];
+      if (i < this.length - 1) {
         result += separator;
       }
-    }
-
-    if (this[this.length - 1] === undefined) {
-      result += '';
-    } else {
-      result += this[this.length - 1];
     }
 
     return result;
