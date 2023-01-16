3b14549fd66d1b5ab6dde9ad20d2735f0d141300
andbilous <andbilous@gmail.com>
2023-01-16 19:39:19 +0200

    refactor

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,20 @@
 'use strict';
 
-/**
- * Implement method join
- */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function (separator = ',') {
+    let finalString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        finalString += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        finalString += separator;
+      }
+    }
+
+    return finalString;
   };
 }
 
