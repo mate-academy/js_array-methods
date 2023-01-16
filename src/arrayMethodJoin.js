24f419aed01fcf57f33e95b74409dda222d45401
Edgar Kocuba <edgarkocuba@gmail.com>
2023-01-16 21:13:48 +0200

    arr join solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let sourceString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      let string = this[i];
+
+      if (string === undefined || string === null) {
+        string = '';
+      }
+
+      if (i === (this.length - 1)) {
+        sourceString += `${string}`;
+      } else {
+        sourceString += `${string}${separator}`;
+      }
+    }
+
+    return sourceString;
   };
 }
 
