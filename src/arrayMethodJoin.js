ce55d32befc7053654c7d02862512674acebdf85
Vlad <bojko.vladislav16@gmail.com>
2023-01-17 18:45:58 +0200

    Final solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,14 +6,8 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     const copy = [...this];
-
     let res = '';
-
     let sep = separator + '';
-
-    if (separator === undefined) {
-      sep = ',';
-    }
 
     if (typeof separator === 'number') {
       return;
