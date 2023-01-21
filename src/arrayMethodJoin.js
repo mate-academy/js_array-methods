64f20e50f289a853102ab1e05df88e47bfe899c4
Dmitriy Rybak <ryba@MacBook-Air-Dmitriy.local>
2023-01-20 18:39:18 -0600

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,33 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    let sign = separator;
+    const length = this.length;
+
+    if (separator === null) {
+      sign = 'null';
+    }
+
+    for (let item of this) {
+      if (item === undefined && this.indexOf(item) === length - 1) {
+        return result;
+      }
+
+      if (item === null || item === undefined) {
+        // eslint-disable-next-line no-new-wrappers
+        item = new String();
+      }
+
+      if (this.indexOf(item) < length - 1) {
+        result += item + sign;
+      } else {
+        result += item;
+      }
+    }
+
+    return result;
   };
 }
 
