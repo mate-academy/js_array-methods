3dc184f0f0c44cfc18b4db2806b02f008e9c926a
Peti51 <galpeter31@gmail.com>
2023-01-26 19:25:18 +0100

    commit message

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,33 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let joined = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this.length === 1) {
+        joined += this[i];
+        break;
+      }
+
+      if (isNaN(separator)) {
+        joined += this[i] + ',';
+      }
+
+      if (this[i] === undefined && i + 1 === this.length) {
+        break;
+      } else if (i + 1 === this.length) {
+        joined += this[i];
+        break;
+      }
+
+      if (this[i] === null || this[i] === undefined) {
+        joined += '' + separator;
+      } else if (this[i] !== null || this[i] !== undefined) {
+        joined += this[i] + separator;
+      }
+    }
+
+    return joined;
   };
 }
 
