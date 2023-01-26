0c0a4337c955051879972286257e48ad3a6cf482
Yevhenii Hura <yevheniihura@gmail.com>
2023-01-26 17:47:14 +0200

    function was created

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    if (this.length === 0) {
+      return result;
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (typeof this[i] === 'undefined' || this[i] === null) {
+        this[i] = '';
+      }
+      result += this[i];
+      result += separator;
+    }
+
+    if (typeof this[this.length - 1] === 'undefined'
+    || this[this.length - 1] === null) {
+      this[this.length - 1] = '';
+    }
+
+    result += this[this.length - 1];
+
+    return result;
   };
 }
 
