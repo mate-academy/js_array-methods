29c3314a2520221caa83b68596b219117f1cf109
Peti51 <galpeter31@gmail.com>
2023-01-27 10:05:36 +0100

    commit message

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,28 +6,19 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     let joined = '';
+    const coma = ',';
 
     for (let i = 0; i < this.length; i++) {
-      if (this.length === 1) {
+      if (this[i] !== null && this[i] !== undefined) {
         joined += this[i];
-        break;
       }
 
-      if (isNaN(separator)) {
-        joined += this[i] + ',';
+      if (i !== this.length - 1 && separator !== undefined) {
+        joined += separator;
       }
 
-      if (this[i] === undefined && i + 1 === this.length) {
-        break;
-      } else if (i + 1 === this.length) {
-        joined += this[i];
-        break;
-      }
-
-      if (this[i] === null || this[i] === undefined) {
-        joined += '' + separator;
-      } else if (this[i] !== null || this[i] !== undefined) {
-        joined += this[i] + separator;
+      if (i !== this.length - 1 && separator === undefined) {
+        joined += coma;
       }
     }
 
@@ -36,3 +27,4 @@ function applyCustomJoin() {
 }
 
 module.exports = applyCustomJoin;
+// git push origin develop --no-verify
