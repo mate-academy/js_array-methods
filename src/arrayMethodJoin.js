c630fafff93abe5c80e57f44f320482bf592c2ef
Yevhenii Hura <yevheniihura@gmail.com>
2023-01-29 00:30:52 +0200

    the issued were solved

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -12,19 +12,16 @@ function applyCustomJoin() {
     }
 
     for (let i = 0; i < this.length - 1; i++) {
-      if (typeof this[i] === 'undefined' || this[i] === null) {
-        this[i] = '';
+      if (this[i] !== undefined && this[i] !== null) {
+        result += `${this[i]}${separator}`;
+      } else {
+        result += separator;
       }
-      result += this[i];
-      result += separator;
     }
 
-    if (typeof this[this.length - 1] === 'undefined'
-    || this[this.length - 1] === null) {
-      this[this.length - 1] = '';
+    if (this[this.length - 1] !== undefined && this[this.length - 1] !== null) {
+      result += this[this.length - 1];
     }
-
-    result += this[this.length - 1];
 
     return result;
   };
