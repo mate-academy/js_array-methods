e73b16b0b0ad8d3c68fc8e05eee5b7c3fc511de2
Nataliia Batiukova <batiukova.nataliia@gmail.com>
2023-01-28 09:59:26 +0000

    the task was solved

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,25 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let sep = String(separator);
+
+    if (separator === undefined) {
+      sep = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        this[i] = '';
+      }
+
+      if (i === this.length - 1) {
+        sep = '';
+      }
+      result += `${this[i] + sep}`;
+    }
+
+    return result;
   };
 }
 
