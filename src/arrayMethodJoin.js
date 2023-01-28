19ce677247bd426d3ada36bed2f953ff5c8be3a8
Roman Hrytsiuk <vasylguk7@gmail.com>
2023-01-28 14:34:41 +0200

    joinMethod

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,27 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        str += separator;
+        continue;
+      }
+      str += this[i];
+      str += separator;
+    }
+
+    if (this[this.length - 1] !== undefined) {
+      str += this[this.length - 1];
+    }
+
+    return str;
   };
 }
 
