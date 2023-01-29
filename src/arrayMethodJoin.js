69063c8cbec728ccf5d184d75872d4ef1b8f79a9
Vitalii Lola <lola1v41@gmail.com>
2023-01-29 20:23:08 +0200

    Sol1

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,33 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
+  //   let arr = '';
+  //   let separatorN = separator;
+
+  //   for (let i = 0; i < this.length; i++) {
+  //     if (i === this.length - 1) {
+  //       separatorN = '';
+  //     }
+
+  //     if (this[i] !== undefined && this[i] !== null) {
+  //       arr += this[i];
+  //     };
+
+  //     arr += separatorN;
+  //   };
+
+  //   return arr;
+  // };
     let arr = '';
-    let separatorN = separator;
 
     for (let i = 0; i < this.length; i++) {
-      if (i === this.length - 1) {
-        separatorN = '';
-      }
-
       if (this[i] !== undefined && this[i] !== null) {
         arr += this[i];
       };
 
-      arr += separatorN;
+      if (i !== this.length - 1) {
+        arr += separator;
+      };
     };
 
     return arr;
