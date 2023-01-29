6eae2f66cee09f5575dbf246900c2c5e4cf91d8d
Vitalii Lola <lola1v41@gmail.com>
2023-01-29 20:09:21 +0200

    develop

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let arr = '';
+    let separatorN = separator;
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1) {
+        separatorN = '';
+      }
+
+      if (this[i] !== undefined && this[i] !== null) {
+        arr += this[i];
+      };
+
+      arr += separatorN;
+    };
+
+    return arr;
   };
-}
+};
 
 module.exports = applyCustomJoin;
