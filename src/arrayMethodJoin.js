12fa321f627135d1709c7bd6aaa082fb3d86e280
Nazarii Piatnochka <nazar.piatnochka@gmail.com>
2023-01-30 15:19:24 +0200

    The tests were completed successfully

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    if (this.length === 0) {
+      return result;
+    }
+
+    function notNullOrUndefined(element) {
+      return element !== null && element !== undefined;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (notNullOrUndefined(this[i])) {
+        result += this[i];
+      }
+
+      if (i < this.length - 1) {
+        result += separator;
+      }
+    }
+
+    return result;
   };
 }
 
