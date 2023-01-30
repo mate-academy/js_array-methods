20ddd8713a7228f9464237d1042a48012dfbcf63
Peti51 <galpeter31@gmail.com>
2023-01-30 11:18:01 +0100

    commit message

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,23 +4,18 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let joined = '';
-    const coma = ',';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
         joined += this[i];
       }
 
-      if (i !== this.length - 1 && separator !== undefined) {
+      if (i !== this.length - 1) {
         joined += separator;
       }
-
-      if (i !== this.length - 1 && separator === undefined) {
-        joined += coma;
-      }
-    }
+    };
 
     return joined;
   };
