549aee54e3011ee2523dab709a5fd596ab8893bf
Oksana Tytanych <oksanatytanych@gmail.com>
2023-02-01 17:19:17 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,44 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let likeJoin = '';
+    let sep = separator;
+
+    if (sep === undefined) {
+      sep = ',';
+    }
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      switch (this[i]) {
+        case null:
+          likeJoin += '';
+          break;
+        case undefined:
+          likeJoin += '';
+          break;
+        case false:
+          likeJoin += 'false';
+          break;
+        case NaN:
+          likeJoin += 'NaN';
+          break;
+        default :
+          likeJoin += this[i];
+      }
+      likeJoin += sep;
+    }
+
+    if (this[this.length - 1] === undefined) {
+      likeJoin += '';
+    } else {
+      likeJoin += this[this.length - 1];
+    }
+
+    return likeJoin;
   };
 }
 
