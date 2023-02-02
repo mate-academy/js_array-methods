b61f9dc27050ba2bf65961e1a3dddfd61809c16e
Oksana Tytanych <oksanatytanych@gmail.com>
2023-02-02 13:31:57 +0200

    Code was improved

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,41 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let likeJoin = '';
-    let sep = separator;
-
-    if (sep === undefined) {
-      sep = ',';
-    }
+    const sep = separator;
 
     if (this.length === 0) {
       return '';
     }
 
     for (let i = 0; i < this.length - 1; i++) {
-      switch (this[i]) {
-        case null:
-          likeJoin += '';
-          break;
-        case undefined:
-          likeJoin += '';
-          break;
-        case false:
-          likeJoin += 'false';
-          break;
-        case NaN:
-          likeJoin += 'NaN';
-          break;
-        default :
-          likeJoin += this[i];
+      if (this[i] !== null && this[i] !== undefined) {
+        likeJoin += this[i];
       }
       likeJoin += sep;
     }
 
-    if (this[this.length - 1] === undefined) {
-      likeJoin += '';
-    } else {
+    if (this[this.length - 1]) {
       likeJoin += this[this.length - 1];
     }
 
