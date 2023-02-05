f707ddf72dcf3ce92ad8dc9a63aaacb313367de0
Roma Tsvindin <tsvindin1995@gmail.com>
2023-02-05 20:10:18 +0200

    Method Join solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,37 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let separatorString;
+    let finalString = '';
+
+    switch (separator) {
+      case null: separatorString = 'null';
+        break;
+      case undefined: separatorString = 'undefined';
+        break;
+      case '': separatorString = '';
+        break;
+      default: separatorString = separator;
+    }
+
+    for (const element of this) {
+      switch (element) {
+        case undefined: finalString += '';
+          break;
+        case null: finalString += '';
+          break;
+        case NaN: finalString += 'NaN';
+          break;
+        default: finalString += element;
+      }
+
+      if (this.indexOf(element) !== this.length - 1) {
+        finalString += separatorString;
+      }
+    };
+
+    return finalString;
   };
 }
-
 module.exports = applyCustomJoin;
