3a918b38e0ea13e16425c665f0ae26ac702644ad
anton-chornobai <antonchornobajj@gmail.com>
2023-02-07 06:42:21 -0800

    task was solved

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,21 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    if (this[this.length - 1] === undefined) {
+      this[this.length - 1] = '';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+      result += this[i] + `${separator}`;
+    }
+
+    return result + this[this.length - 1];
   };
 }
 
