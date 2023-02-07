0bcc23d830442265202ef9e7b38309c920e020c7
Serhii Melnychenko <8melya8@gmail.com>
2023-02-07 20:45:31 +0200

    Foxed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,23 +4,16 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    let separ = separator;
+  [].__proto__.join2 = function(separator = ',') {
     let str = '';
 
-    if (separ === undefined) {
-      separ = ',';
-    }
-
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        this[i] = '';
+      if (this[i] !== null && this[i] !== undefined) {
+        str += this[i];
       }
 
-      if (i === this.length - 1) {
-        str += this[i];
-      } else {
-        str += `${this[i]}${separ}`;
+      if (i < this.length - 1) {
+        str += separator;
       }
     }
 
