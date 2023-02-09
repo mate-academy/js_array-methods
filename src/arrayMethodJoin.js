856c3ad14c3f2a0b6cc754b6aac22da0fea5e0ea
Roma Tsvindin <tsvindin1995@gmail.com>
2023-02-09 11:37:57 +0200

    Method Join solution ver.1.0.1

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,25 +8,13 @@ function applyCustomJoin() {
     let separatorString;
     let finalString = '';
 
-    switch (separator) {
-      case null: separatorString = 'null';
-        break;
-      case undefined: separatorString = 'undefined';
-        break;
-      case '': separatorString = '';
-        break;
-      default: separatorString = separator;
+    if (separator !== ('null' || 'undefined')) {
+      separatorString = separator;
     }
 
     for (const element of this) {
-      switch (element) {
-        case undefined: finalString += '';
-          break;
-        case null: finalString += '';
-          break;
-        case NaN: finalString += 'NaN';
-          break;
-        default: finalString += element;
+      if ((element !== null) && (element !== undefined)) {
+        finalString += element;
       }
 
       if (this.indexOf(element) !== this.length - 1) {
