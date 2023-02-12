03aeeb7f177b5ca3c9cd448bc1623bed459d47e4
Vadim Drobyazko <volandemortuwka@gmail.com>
2023-02-12 15:49:33 +0200

    join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let stroke = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        stroke += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        stroke += separator;
+      }
+    }
+
+    return stroke;
   };
 }
 
