a176faffbb6d2f71e02bb05e359561b79b0f7290
Kateryna Babina <katerynababina01@gmail.com>
2023-02-12 21:46:12 +0200

    join solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,34 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let strJoin = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    let fixedSeparator = separator;
+
+    if (separator === null) {
+      fixedSeparator = 'null';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      let element = this[i];
+
+      if (element === undefined || element === null) {
+        element = '';
+      }
+
+      strJoin += element + fixedSeparator;
+    }
+
+    if (this[this.length - 1]) {
+      strJoin += this[this.length - 1];
+    }
+
+    return strJoin;
   };
 }
 
