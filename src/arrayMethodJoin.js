aa0d615e544c3a71a9b3bb9fdbf1727ec0844cf7
Sergey Mikhalskiy <msa1991work@gmail.com>
2023-02-12 13:30:58 +0200

    fixes

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,6 +8,10 @@ function applyCustomJoin() {
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
+      // eslint won't let me do it
+      // this[i] = this[i] ?? '';
+      // result += this[i];
+
       if (this[i] !== null && this[i] !== undefined) {
         result += this[i];
       }
