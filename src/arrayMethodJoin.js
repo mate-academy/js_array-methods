913960d25ff5e74aacfa271ac38930aeefd13903
Serhii Zelinskyi <zelinskyi.serh@gmail.com>
2023-02-13 18:56:11 +0200

    solution Method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,30 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let between = separator;
+
+    if (separator === undefined) {
+      between = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if ((this[i] === null || this[i] === undefined) & i + 1 !== this.length) {
+        result += `${between}`;
+        continue;
+      }
+
+      if (i + 1 !== this.length) {
+        result += `${this[i]}${between}`;
+        continue;
+      }
+
+      if (this[this.length - 1] !== undefined) {
+        result += this[i];
+      }
+    }
+
+    return result;
   };
 }
 
