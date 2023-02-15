cadfe2a55d7b047a3c8d89aadc87c0e6da2e396c
danulyk05 <ivan.danulyk2016@gmail.com>
2023-02-15 11:51:13 +0200

    solved

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,44 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+    let separators = separator;
+
+    if (this.length === 0) {
+      return '';
+    };
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        this[i] = '';
+      };
+    };
+
+    if (separators === null) {
+      separators = 'null';
+    }
+
+    str = this[0] + '';
+
+    if (separators) {
+      for (let i = 1; i < this.length; i++) {
+        if (this[i] || isNaN(this[i]) || this[i] === 0) {
+          str += separators + this[i];
+        } else {
+          str += separators;
+        };
+      }
+    } else {
+      for (let i = 1; i < this.length; i++) {
+        if (this[i] || isNaN(this[i]) || this[1] === 0) {
+          str += this[i];
+        }
+      };
+    }
+
+    return str;
   };
-}
+};
 
 module.exports = applyCustomJoin;
