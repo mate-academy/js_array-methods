779cf1fdae4024a6514917187ba7538073f582dc
Tolik Petras <toliketras4@gmail.com>
2023-02-16 18:39:58 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,63 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    if (separator === undefined) {
+      let result = '';
+
+      for (let j = 0; j < this.length; j++) {
+        result += this[j];
+
+        if (j !== this.length - 1) {
+          result += ',';
+        }
+      }
+
+      return result;
+    } else if (separator === null) {
+      let result = '';
+
+      for (let i = 0; i < this.length; i++) {
+        result += this[i];
+
+        if (i !== this.length - 1) {
+          result += 'null';
+        }
+      }
+
+      return result;
+    } else if (typeof separator === 'object') {
+      let result = '';
+
+      for (let i = 0; i < this.length; i++) {
+        result += this[i];
+
+        if (i !== this.length - 1) {
+          result += separator;
+        }
+      }
+
+      return result;
+    } else {
+      switch (typeof separator) {
+        case Number:
+          return this;
+
+        case 'string':
+          let resultString = '';
+
+          for (let j = 0; j < this.length; j++) {
+            if (this[j] !== undefined && this[j] !== null) {
+              resultString += this[j];
+            }
+
+            if (j !== this.length - 1) {
+              resultString += separator;
+            }
+          }
+
+          return resultString;
+      }
+    }
   };
 }
 
