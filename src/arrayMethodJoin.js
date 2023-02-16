8c61b0cbed0a31ce4507d9cf0d3d569798cdcad8
Nazar Kharkevych <nkharkevych@gmail.com>
2023-02-16 19:41:16 +0200

    Removed redundant 'if' statement

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,10 +7,6 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
 
-    if (this.length === 0) {
-      return '';
-    }
-
     for (let i = 0; i < this.length; i++) {
       let element = this[i];
 
