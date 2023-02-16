2d9521ce21b3b5c922c6754988790bb555593d1c
Bozhena Bodnar <bozhena2705bodnar@gmail.com>
2023-02-16 18:18:23 +0100

    create join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+    const len = this.length;
+
+    if (len === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < len; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        str += this[i];
+      }
+
+      if (i !== len - 1) {
+        str += separator;
+      }
+    }
+
+    return str;
   };
 }
 
