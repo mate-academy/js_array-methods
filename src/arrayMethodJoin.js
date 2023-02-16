269555a945a7e88d3c8f69e99e35128d514a372c
andrej <andersontwentytwo@gmail.com>
2023-02-16 19:54:43 +0200

    join method recreated

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedArr = '';
+    let pseudoSeparator = separator;
+
+    if (separator === null) {
+      pseudoSeparator = 'null';
+    }
+
+    for (const n of this) {
+      let item = n + '';
+
+      if (typeof n === 'undefined' || n === null) {
+        item = '';
+      }
+
+      if (this.lastIndexOf(n) === this.length - 1) {
+        joinedArr += item;
+        break;
+      }
+      joinedArr += item + pseudoSeparator;
+    }
+
+    return joinedArr;
   };
 }
 
