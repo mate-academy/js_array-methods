d24b3bf9cd1238a0263ca9efda564342d95a2ff8
Rufat Mammadov <rufat.mammadov.m@gmail.com>
2023-02-16 23:41:52 +0200

    join method first try

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,41 @@
 'use strict';
 
+const nullString = 'null';
+const emptyString = '';
 /**
  * Implement method join
  */
+
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+
+    if (this.length === 0) {
+      return emptyString;
+    } else if (this.length === 1) {
+      return this[0].toString();
+    }
+
+    let character = separator;
+
+    if (character === null) {
+      character = nullString;
+    }
+
+    let result = this[0];
+
+    if (result === null) {
+      result = emptyString;
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = emptyString;
+      }
+      result += character + this[i];
+    }
+
+    return result;
   };
 }
 
