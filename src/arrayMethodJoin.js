ca319573d5d9b54706c891bb315ed152be9163eb
Tolik Petras <toliketras4@gmail.com>
2023-02-17 14:30:05 +0200

    fixed solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,38 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    switch (typeof separator) {
-      case Number:
-        return this;
+    let resultString = '';
 
-      case 'string':
-        let resultString = '';
+    for (let j = 0; j < this.length; j++) {
+      if (this[j] !== undefined && this[j] !== null) {
+        resultString += this[j];
+      }
 
-        for (let j = 0; j < this.length; j++) {
-          if (this[j] !== undefined && this[j] !== null) {
-            resultString += this[j];
-          }
-
-          if (j !== this.length - 1) {
-            resultString += separator;
-          }
-        }
-
-        return resultString;
-
-      case 'object':
-        let result = '';
-
-        for (let i = 0; i < this.length; i++) {
-          result += this[i];
-
-          if (i !== this.length - 1) {
-            result += separator;
-          }
-        }
-
-        return result;
+      if (j !== this.length - 1) {
+        resultString += separator;
+      }
     }
+
+    return resultString;
   };
 }
 
