06702d63006e40affc0f2553c84778817bc32e56
vadosik15 <vadim.bratus@gmail.com>
2023-02-17 12:32:12 +0200

    created program of join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let newstr = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (i !== 0 && i < this.length) {
+        newstr += separator;
+      }
+
+      if (this[i] !== undefined && this[i] !== null) {
+        newstr += this[i];
+      }
+    }
+
+    return newstr;
   };
 }
 
