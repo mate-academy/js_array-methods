459e652a973fe06233c925fa5cb32f0fe162ce1d
vadym4che <vadym4che@gmail.com>
2023-02-17 20:12:24 +0200

    removed unnessecary peaces of code in method implementation (with kindly help of teammates)

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,26 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    if (this.length === 0) {
-      return '';
-    }
-
-    let joined = '';
+    let joinedString = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (i === this.length - 1) {
-        joined += this[i] || '';
-        break;
+      if (this[i] !== undefined && this[i] !== null) {
+        joinedString += this[i];
       }
 
-      if (this[i] === undefined || this[i] === null) {
-        joined += '' + separator;
-        continue;
+      if (i !== this.length - 1) {
+        joinedString += separator;
       }
-      joined += this[i] + separator;
     }
 
-    return joined;
+    return joinedString;
   };
 }
 
