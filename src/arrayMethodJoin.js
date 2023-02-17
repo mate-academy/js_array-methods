cfc34ef7a15abef165e7a4f5fba79df250cc4f11
Bogdan <bodia-00wot@ukr.net>
2023-02-17 19:15:18 +0200

    removed separator check for null value,removed else

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,21 +3,14 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let res = '';
-    let newSep = separator;
-
-    if (separator === null) {
-      newSep = 'null';
-    }
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === undefined || this[i] === null) {
-        this[i] = '';
+      if (this[i] !== undefined && this[i] !== null) {
+        res += this[i];
       }
 
       if (i !== this.length - 1) {
-        res += this[i] + newSep;
-      } else {
-        res += this[i];
+        res += separator;
       }
     }
 
