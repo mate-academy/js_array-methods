315ecea2b8b1cb0f9d84a75291ad01bdddc405d3
Yaroslav <yaroslav.dzyadiv@gmail.com>
2023-02-17 19:41:39 +0200

    Task done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,28 @@
 /**
  * Implement method join
  */
-function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+function applyCustomJoin(input) {
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    if (this.length === 0) {
+      return result;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        this[i] = '';
+      }
+
+      if (i < this.length - 1) {
+        result += this[i];
+        result += separator;
+      } else {
+        result += this[i];
+      }
+    }
+
+    return result;
   };
 }
 
