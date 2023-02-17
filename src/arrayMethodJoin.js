15082c6db81ee827d0afe077677c7a3861a61117
gretoq <dovhun3d@gmail.com>
2023-02-17 12:29:11 +0200

    Created a "join" method for creating a string from an array

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedToString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      let itemOfThis = this[i];
+
+      if (itemOfThis === null || itemOfThis === undefined) {
+        itemOfThis = '';
+      }
+
+      joinedToString += itemOfThis;
+
+      if (i !== this.length - 1) {
+        joinedToString += separator;
+      }
+    }
+
+    return joinedToString;
   };
 }
 
