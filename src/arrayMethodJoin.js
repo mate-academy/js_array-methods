31937e2881924567e32b4514fa66955679c24640
Bogdan <bodia-00wot@ukr.net>
2023-02-17 18:55:05 +0200

    sol

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,27 @@
 'use strict';
 
-/**
- * Implement method join
- */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let res = '';
+    let newSep = separator;
+
+    if (separator === null) {
+      newSep = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        this[i] = '';
+      }
+
+      if (i !== this.length - 1) {
+        res += this[i] + newSep;
+      } else {
+        res += this[i];
+      }
+    }
+
+    return res;
   };
 }
 
