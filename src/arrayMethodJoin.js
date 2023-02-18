88bfab9215ca7cfe1b2ac8bd89ab604e1586fc6c
siddhadatri <volvik24@gmail.com>
2023-02-18 21:12:36 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,32 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let newSeparator = separator;
+
+    if (newSeparator === undefined) {
+      newSeparator = ',';
+    }
+
+    if (newSeparator === null) {
+      newSeparator = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      let current = this[i];
+
+      if (current === null || current === undefined) {
+        current = '';
+      }
+
+      if (i !== this.length - 1) {
+        result += current + newSeparator;
+      } else {
+        result += current;
+      }
+    }
+
+    return result;
   };
 }
 
