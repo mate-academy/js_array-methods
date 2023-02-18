97a4ed4da68dbcdd0790d65de73e50b45ead9e2e
Yevhenii Leichenko <eugeniy0536@gmail.com>
2023-02-18 20:41:15 +0200

    Method Join - Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let answerStrig = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        answerStrig += this[i];
+      } else {
+        answerStrig += '';
+      }
+
+      if (i < this.length - 1) {
+        answerStrig += separator;
+      }
+    }
+
+    return answerStrig;
   };
 }
 
