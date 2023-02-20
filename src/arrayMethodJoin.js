873b1cd796079ef0cb74798ca4651dd323800bf7
Kateryna Babina <katerynababina01@gmail.com>
2023-02-20 17:42:32 +0200

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,27 +5,21 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let strJoin = '';
+    let joined = '';
 
-    if (this.length === 0) {
-      return '';
-    }
-
-    for (let i = 0; i < this.length - 1; i++) {
+    for (let i = 0; i < this.length; i++) {
       const element = this[i];
 
       if (element !== undefined && element !== null) {
-        strJoin += element;
+        joined += element;
       }
 
-      strJoin += separator;
+      if (i !== this.length - 1) {
+        joined += separator;
+      }
     }
 
-    if (this[this.length - 1]) {
-      strJoin += this[this.length - 1];
-    }
-
-    return strJoin;
+    return joined;
   };
 }
 
