2eec3fc560bf5994bd9aadbee05b2a055d096dc1
Maksym Huriev <makspsih.73@gmail.com>
2023-02-20 14:41:43 +0200

    array-method-join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (i > 0) {
+        result += separator;
+      }
+
+      if (this[i] !== undefined && this[i] !== null) {
+        result += this[i];
+      }
+    }
+
+    return result;
   };
 }
 
