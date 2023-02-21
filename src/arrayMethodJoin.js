5ee3d049392586a7e4c0ea717b77351433a0a052
Yuliia Vereshko <pyliatko@gmail.com>
2023-02-21 17:06:57 +0100

    fixed mistakes

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let createString = '';
+    let joinedString = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== undefined && this[i] !== null) {
-        createString += this[i];
+        joinedString += this[i];
       }
 
-      if (i !== this.length - 1) {
-        createString += separator;
+      if (i < this.length - 1) {
+        joinedString += separator;
       }
     }
 
-    return createString;
+    return joinedString;
   };
 }
 
