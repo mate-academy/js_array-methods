964884a0a360c7c37b190d8594d1d65cbff35d83
Yaroslav Dudar <dudar.komod@gmail.com>
2023-02-22 21:17:17 +0200

    Code was simplifyed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,44 +5,20 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let string = '';
     let result = '';
 
-    switch (separator) {
-      case '':
-        for (let i = 0; i < this.length; i++) {
-          string += this[i];
-        }
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        result += this[i];
+      }
 
-        return string;
-
-      case null:
-        for (let i = 0; i < this.length; i++) {
-          string += this[i] + 'null';
-        }
-
-        for (let i = 0; i < string.length - 4; i++) {
-          result += string[i];
-        }
-
-        return result;
-
-      default:
-        for (let i = 0; i < this.length; i++) {
-          if (this[i] === undefined || this[i] === null) {
-            string += '' + separator;
-            continue;
-          }
-          string += this[i] + separator;
-        }
-
-        for (let i = 0; i < string.length - separator.toString().length; i++) {
-          result += string[i];
-        }
-
-        return result;
+      if (this[i] !== this[this.length - 1]) {
+        result += separator;
+      }
     }
+
+    return result;
   };
-}
+};
 
 module.exports = applyCustomJoin;
