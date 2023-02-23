7379a1e5062bc0b80f364f68a2153c879e120b4c
siddhadatri <volvik24@gmail.com>
2023-02-23 22:15:48 +0200

    fixed code

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,29 +4,17 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let result = '';
     let newSeparator = separator;
 
-    if (newSeparator === undefined) {
-      newSeparator = ',';
-    }
-
-    if (newSeparator === null) {
-      newSeparator = 'null';
-    }
-
     for (let i = 0; i < this.length; i++) {
-      let current = this[i];
-
-      if (current === null || current === undefined) {
-        current = '';
+      if (this[i] !== undefined && this[i] !== null) {
+        result += this[i];
       }
 
       if (i !== this.length - 1) {
-        result += current + newSeparator;
-      } else {
-        result += current;
+        result += newSeparator;
       }
     }
 
