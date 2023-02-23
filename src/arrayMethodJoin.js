09335baf21d8657e2987d018c86fb78a041ceb07
OlegRak86 <rakolegit@gmail.com>
2023-02-23 15:27:49 +0200

    method-join-fix

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -16,24 +16,31 @@ function applyCustomJoin() {
         if (this[i] === undefined || this[i] === null) {
           this[i] = '';
         }
-        str += `${this[i] + ''}${separator}`;
+        str += `${this[i]}${separator}`;
       }
     }
 
     if (separator === '') {
       for (let i = 0; i < this.length - 1; i++) {
+        if (this[i] === undefined || this[i] === null) {
+          this[i] = '';
+        }
         str += `${this[i]}`;
       }
     }
 
     if (separator === undefined) {
       for (let i = 0; i < this.length - 1; i++) {
+        if (this[i] === undefined || this[i] === null) {
+          this[i] = '';
+        }
         str += `${this[i]},`;
       }
     }
 
-    this[this.length - 1] === undefined ? str = str + ''
-      : str = str + this[this.length - 1]; ;
+    if (this[this.length - 1]) {
+      str = str + this[this.length - 1];
+    }
 
     return str;
   };
