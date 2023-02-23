e5eebe0cf9b3ce353f07b94d172c8ad1c51d1740
Pavlo Kostyshyn <kostishinpavlo24@gmail.com>
2023-02-23 11:38:47 +0200

    .join() was done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,26 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    const arr = this;
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] !== undefined && arr[i] !== null) {
+        result += arr[i];
+      } else {
+        result += '';
+      }
+
+      if (i !== arr.length - 1) {
+        if (separator === undefined) {
+          result += ',';
+        } else {
+          result += separator;
+        }
+      }
+    }
+
+    return result;
   };
 }
 
