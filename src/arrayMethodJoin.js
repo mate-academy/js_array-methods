d7b31b21eb8b48437e0e489028f86604be89a8e2
OlegRak86 <rakolegit@gmail.com>
2023-02-25 18:22:08 +0200

    method-join-apparently-finish

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,36 +4,17 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let str = '';
 
-    if (this.length === 0) {
-      return '';
-    }
-
-    for (let i = 0; i < this.length - 1; i++) {
-      if (separator) {
-        if (this[i] === undefined || this[i] === null) {
-          this[i] = '';
-        }
-        str += `${this[i]}${separator}`;
-      }
-
-      if (separator === null) {
-        str += `${this[i] + 'null'}`;
-      }
-
-      if (separator === '') {
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
         str += `${this[i]}`;
       }
 
-      if (separator === undefined) {
-        str += `${this[i]},`;
+      if (i !== this.length - 1) {
+        str += `${separator}`;
       }
-    }
-
-    if (this[this.length - 1]) {
-      str = str + this[this.length - 1];
     }
 
     return str;
