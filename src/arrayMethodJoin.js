ab43323185acc74e0728aa6f37a6acaaaeaf119a
Pavlo Petrashevskyi <dragonthunderaddsdp@gmail.com>
2023-03-01 23:09:58 +0200

    join() method was created

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,36 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let string = '';
+
+    for (let i = 0; i < this.length; i++) {
+      switch (this[i]) {
+        case this[this.length - 1]:
+          switch (this[this.length - 1]) {
+            case null:
+            case undefined:
+              string += '';
+              break;
+
+            default:
+              string += this[this.length - 1];
+              break;
+          };
+          break;
+
+        case null:
+        case undefined:
+          string += '' + `${separator}`;
+          break;
+
+        default:
+          string += this[i] + `${separator}`;
+          break;
+      }
+    }
+
+    return string;
   };
 }
 
