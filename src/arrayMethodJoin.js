6ca353de29e7149aecdd8710cc5fac53d1d1ac52
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-03-02 15:01:04 +0100

    complete task

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,32 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (const [idx, el] of this.entries()) {
+      if (idx === this.length - 1 && el !== undefined) {
+        result += `${el}`;
+      }
+
+      if (idx === this.length - 1 && el === undefined) {
+        return result;
+      }
+
+      if (el === undefined || el === null) {
+        result += `${separator}`;
+        continue;
+      }
+
+      if (idx < this.length - 1) {
+        result += `${el}${separator}`;
+        continue;
+      }
+    }
+
+    return result;
+
+    // sorry, I dont find a better solution
   };
 }
 
