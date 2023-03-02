e2679d1d486ec2602b1cfd7bd587b75d15c0863f
Vladyslav Kulybaba <106146104+VladGraund@users.noreply.github.com>
2023-03-02 19:51:37 +0100

    fix task

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,28 +8,16 @@ function applyCustomJoin() {
     let result = '';
 
     for (const [idx, el] of this.entries()) {
-      if (idx === this.length - 1 && el !== undefined) {
-        result += `${el}`;
-      }
-
-      if (idx === this.length - 1 && el === undefined) {
-        return result;
-      }
-
-      if (el === undefined || el === null) {
-        result += `${separator}`;
-        continue;
+      if (el !== null && el !== undefined) {
+        result += el;
       }
 
       if (idx < this.length - 1) {
-        result += `${el}${separator}`;
-        continue;
+        result += separator;
       }
     }
 
     return result;
-
-    // sorry, I dont find a better solution
   };
 }
 
