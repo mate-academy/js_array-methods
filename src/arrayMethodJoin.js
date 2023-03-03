8989ea2e2972b9aa707c0547a6e9224aec12baab
Yurii Hlushenko <yauglushenko@gmail.com>
2023-03-03 19:36:54 +0200

    code optimized

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -9,14 +9,12 @@ function applyCustomJoin() {
     const len = this.length;
 
     for (let i = 0; i < len; i++) {
-      if ((this[i] === null || this[i] === undefined) && (i !== len - 1)) {
+      if (this[i] !== null && this[i] !== undefined) {
+        result += this[i];
+      }
+
+      if (i < len - 1) {
         result += separator;
-      } else if (this[i] === null || this[i] === undefined) {
-        result += '';
-      } else if (i === len - 1) {
-        result += `${this[i]}`;
-      } else {
-        result += `${this[i]}${separator}`;
       }
     }
 
