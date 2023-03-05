ef6ef22c3a393a5547253a448ac7e65e7fa76df6
Adrian Yevchuk <adrianyevchuk@MacBook-Air-Adrian.local>
2023-03-05 23:40:41 +0200

    task complete

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,24 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let sep = separator;
+
+    if (sep === undefined) {
+      sep = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        result += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        result += sep;
+      }
+    }
+
+    return result;
   };
 }
 
