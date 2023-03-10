019a6fa04b7937425a8abb879279cccae0718041
Daryna Hnidash <melissa998877@gmail.com>
2023-03-10 15:38:29 +0200

    changed loop

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -13,13 +13,21 @@ function applyCustomJoin() {
 
     let str = '';
 
-    for (const elem of this) {
-      if (elem === null || elem === undefined) {
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
         str += separ;
       } else {
-        str += elem + separ;
+        str += this[i] + separ;
       }
     }
+
+    // for (const elem of this) {
+    // if (elem === null || elem === undefined) {
+    //   str += separ;
+    // } else {
+    //   str += elem + separ;
+    // }
+    // }
 
     const result = str.slice(0, str.lastIndexOf(separ));
 
