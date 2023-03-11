2eb694ff8d052e74dffc30106e421c43528633c1
Leonid <leonidbondarchuk6@gmail.com>
2023-03-11 15:38:32 +0200

    rename variable

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let newArray = '';
+    let result = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
-        newArray += this[i];
+        result += this[i];
       }
 
       if (i < this.length - 1) {
-        newArray += separator;
+        result += separator;
       }
     }
 
-    return newArray;
+    return result;
   };
 }
 
