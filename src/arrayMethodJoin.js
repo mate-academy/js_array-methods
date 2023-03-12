87f9db6222d3d8fbf009caed142736c78e2da8d6
Vladyslav Fedenko <vadondrummer@gmail.com>
2023-03-12 21:19:08 +0200

    FedenkoArrayMethod

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,32 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    const newArr = [];
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        newArr[i] = '';
+      } else {
+        newArr[i] = this[i];
+      }
+    }
+
+    let result = `${newArr[0]}`;
+    let newSep = separator;
+
+    if (separator === undefined) {
+      newSep = ',';
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      result += `${newSep}`+ `${newArr[i]}`;
+    }
+
+    if (this.length === 0) {
+      result = '';
+    }
+
+    return result;
   };
 }
 
