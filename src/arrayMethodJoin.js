2ae60a121442aa4209b6d1b0e72507dafecb4b7a
Alla Gutsul <allagutsul1990@gmail.com>
2023-03-14 15:27:43 +0200

    fix method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,21 +5,15 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    const joinSeparator = separator;
-
-    const arr = [...this];
-
     let res = '';
 
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === null || arr[i] === undefined) {
-        arr[i] = '';
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        res += this[i];
       }
 
-      if (i === arr.length - 1) {
-        res += arr[i];
-      } else {
-        res += arr[i] + '' + joinSeparator;
+      if (i !== this.length - 1) {
+        res += separator;
       }
     }
 
