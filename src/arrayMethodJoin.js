d9e6d23064d3b40da59d9d5c9c5c67dc95aebbb7
Volodymyr Zadorozhnyi <Torsida@gmail.com>
2023-03-14 10:09:31 +0200

    first version

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,29 @@
 /**
  * Implement method join
  */
+
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let value = separator;
+
+    if (value === undefined) {
+      value = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] == null) {
+        result += '';
+      } else {
+        result += this[i];
+      }
+
+      if (i < this.length - 1) {
+        result += value;
+      }
+    }
+
+    return result;
   };
 }
 
