3cf48a25f089a5374044c6a6635018542b65abf3
Alla Gutsul <allagutsul1990@gmail.com>
2023-03-14 11:23:24 +0200

    create method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,27 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const joinSeparator = separator;
+
+    const arr = [...this];
+
+    let res = '';
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === null || arr[i] === undefined) {
+        arr[i] = '';
+      }
+
+      if (i === arr.length - 1) {
+        res += arr[i];
+      } else {
+        res += arr[i] + '' + joinSeparator;
+      }
+    }
+
+    return res;
   };
-}
+};
 
 module.exports = applyCustomJoin;
