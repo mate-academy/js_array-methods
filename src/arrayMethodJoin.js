dac32415fb4683c41693c85db809694dcdd0eeff
Serhii Stepura <stepurasergiy@gmail.com>
2023-03-15 12:21:22 +0200

    fixed according to recommendation

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,20 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let result = '';
+    let joinedString = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
-        result += this[i];
+        joinedString += this[i];
       }
 
-      if (i === this.length - 1) {
-        break;
+      if (i !== this.length - 1) {
+        joinedString += separator;
       }
-      result += separator;
     }
 
-    return result;
+    return joinedString;
   };
 }
 
