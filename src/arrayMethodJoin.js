842d2c7d8ef654890d6adedff9f0a536db6384e5
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2023-03-16 23:04:42 +0200

    join method ready for use

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,31 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    if (this.length === 0) {
+      return '';
+    }
+
+    const sourceAarrayCopy = [...this];
+
+    for (let i = 0; i < sourceAarrayCopy.length; i++) {
+      if (sourceAarrayCopy[i] === null || sourceAarrayCopy[i] === undefined) {
+        sourceAarrayCopy[i] = '';
+      }
+    }
+
+    let divider = separator;
+
+    if (divider === undefined) {
+      divider = ',';
+    }
+
+    let string = '' + sourceAarrayCopy[0];
+
+    for (let i = 1; i < sourceAarrayCopy.length; i++) {
+      string += `${divider}${sourceAarrayCopy[i]}`;
+    }
+
+    return string;
   };
 }
 
