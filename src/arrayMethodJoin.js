55f7154e5f4f73d9124d1baecfa2daa26159d8eb
Alex Rog <sasharg303@gmail.com>
2023-03-17 18:37:45 +0100

    Finished

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedStr = '';
+
+    for (let i = 0; i < this.length; i++) {
+      // for last symbol without separator
+      if (i === (this.length - 1) && (this[i] === null || this[i] === undefined)) { // eslint-disable-line
+        joinedStr += ``;
+        break;
+      } else if (i === (this.length - 1)) {
+        joinedStr += `${this[i]}`;
+        break;
+      }
+
+      if (this[i] === null || this[i] === undefined) {
+        joinedStr += '' + `${separator}`;
+        continue;
+      }
+
+      joinedStr += `${this[i]}` + `${separator}`;
+    }
+
+    return joinedStr;
   };
 }
 
