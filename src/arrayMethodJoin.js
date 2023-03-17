65290df87b58a6e465206fd91d230c9e776be820
Hamuud <artemlask2014@gmail.com>
2023-03-17 16:22:48 +0200

    Create custom Join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,26 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    // Who invented this task.. i want to cry
+    const copyThis = [...this];
+    let str = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (copyThis[i] === null || copyThis[i] === undefined) {
+        copyThis[i] = '';
+      }
+
+      if (i === this.length - 1) {
+        str += copyThis[i];
+        continue;
+      }
+
+      str += copyThis[i];
+      str += separator;
+    }
+
+    return str;
   };
 }
 
