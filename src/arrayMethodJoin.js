e6c781b12b36751cbca5010699988ecfde051224
Pavlo Reutskiy <pavel.reutskiy@gmail.com>
2023-03-17 14:49:17 +0200

    improved my code

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,33 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     if (this.length === 0) {
       return '';
     }
 
-    const sourceAarrayCopy = [...this];
+    let string = '';
 
-    for (let i = 0; i < sourceAarrayCopy.length; i++) {
-      if (sourceAarrayCopy[i] === null || sourceAarrayCopy[i] === undefined) {
-        sourceAarrayCopy[i] = '';
+    for (let i = 0; i < this.length; i++) {
+      (this[i] === null || this[i] === undefined)
+        ? string += ''
+        : string += `${this[i]}`;
+
+      if (i !== this.length - 1) {
+        string += `${separator}`;
       }
-    }
-
-    let divider = separator;
-
-    if (divider === undefined) {
-      divider = ',';
-    }
-
-    let string = '' + sourceAarrayCopy[0];
-
-    for (let i = 1; i < sourceAarrayCopy.length; i++) {
-      string += `${divider}${sourceAarrayCopy[i]}`;
     }
 
     return string;
   };
-}
+};
 
 module.exports = applyCustomJoin;
