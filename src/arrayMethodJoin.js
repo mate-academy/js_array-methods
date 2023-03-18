b90817ab0b142157a8ccac20eef644d85d71313f
Vyacheslav Stolyarchuk <haunt.beta@gmail.com>
2023-03-18 23:22:08 +0200

    join solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,24 @@
 /**
  * Implement method join
  */
+
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let outString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        outString += '';
+      } else {
+        outString += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        outString += separator;
+      }
+    }
+
+    return outString;
   };
 }
 
