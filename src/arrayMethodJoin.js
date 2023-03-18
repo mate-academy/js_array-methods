646d5185b00bd0be3143dc79dfad88dfc538f52a
Mark Boiko <boikomark555@gmail.com>
2023-03-18 15:22:59 +0200

    create function CustomJoin

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        joinedString += '';
+      } else {
+        joinedString += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        joinedString += String(separator);
+      }
+    }
+
+    return joinedString;
   };
 }
+
+const source = [0, 1, 2, 3];
+
+applyCustomJoin();
+
+source.join2(null);
 
 module.exports = applyCustomJoin;
