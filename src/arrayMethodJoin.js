51bb4f4aa92767971c91a69ae24fef509c5b9051
domkab <dominykas.bobinas@gmail.com>
2023-03-18 22:18:12 +0200

    initial

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      result += this[i].toString();
+
+      if (i < this.length - 1) {
+        result += separator;
+      }
+    }
+
+    return result;
   };
 }
+applyCustomJoin();
 
-module.exports = applyCustomJoin;
+const myArray = [1, 2, 3];
+
+myArray.join2('-');
+
+// module.exports = applyCustomJoin;
