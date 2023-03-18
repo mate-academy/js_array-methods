7469acd2cd73dcfa59713c11c0da9f63d6ce7685
victor buldenko <gookidoo@gmail.com>
2023-03-18 19:33:23 +0300

    done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,3 +1,5 @@
+/* eslint-disable use-isnan */
+/* eslint-disable no-console */
 'use strict';
 
 /**
@@ -5,7 +7,30 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    const sep = String(separator);
+
+    for (let i = 0; i < this.length; i++) {
+      switch (true) {
+        case (i + 1) !== this.length && this[i] === undefined:
+        case (i + 1) !== this.length && this[i] === null:
+        case (i + 1) !== this.length && this[i] === NaN:
+          result += ',';
+          break;
+        case this[i] === undefined:
+          break;
+        case (i + 1) !== this.length && separator === undefined:
+          result += String(this[i]) + ',';
+          break;
+        case (i + 1) !== this.length:
+          result += String(this[i]) + sep;
+          break;
+        default:
+          result += String(this[i]);
+      }
+    }
+
+    return result;
   };
 }
 
