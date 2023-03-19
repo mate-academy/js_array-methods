ee52747a385ece4306acd57ce005b3de763b393b
Dima <dmitriy.movchan.02@gmail.com>
2023-03-19 13:06:27 +0200

    arreyMethodJoin

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+
+    for (const one of this) {
+      if (one === this[this.length - 1]) {
+        if (one === undefined) {
+          continue;
+        }
+
+        str += one;
+        continue;
+      } else {
+        str += one !== undefined && one !== null ? one : '';
+        str += separator;
+      }
+    }
+
+    return str;
   };
 }
 
