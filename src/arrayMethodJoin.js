be10ec6159f1c4089a180a85592a003af7670c33
Kyryl <kika_domination@yahoo.com>
2023-03-19 23:48:24 +0200

    remove not needed condition

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,18 +7,16 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let stringFromArray = '';
 
-    if (this.length === 0) {
-      return '';
-    }
+    if (this.length !== 0) {
+      for (let i = 0; i < this.length; i++) {
+        const stringItem = this[i];
 
-    for (let i = 0; i < this.length; i++) {
-      const stringItem = this[i];
+        stringFromArray += stringItem === undefined || stringItem === null
+          ? '' : stringItem;
 
-      stringFromArray += stringItem === undefined || stringItem === null
-        ? '' : stringItem;
-
-      if (i !== this.length - 1) {
-        stringFromArray += separator;
+        if (i !== this.length - 1) {
+          stringFromArray += separator;
+        }
       }
     }
 
