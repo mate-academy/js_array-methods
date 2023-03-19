f81c35930cefd6fd5b8f040d1a552507c9f6cc25
Aleksandr <alexnovaklawyer@gmail.com>
2023-03-19 23:35:48 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,33 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let copyOfSeparator = separator;
+    let joinedStr = '';
+
+    if (copyOfSeparator === undefined) {
+      copyOfSeparator = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1
+          && (this[i] === null || this[i] === undefined)) {
+        joinedStr += '';
+        continue;
+      }
+
+      if (i === this.length - 1) {
+        joinedStr += this[i].toString();
+        continue;
+      }
+
+      if (this[i] === null || this[i] === undefined) {
+        joinedStr += '' + copyOfSeparator;
+      } else {
+        joinedStr += this[i].toString() + copyOfSeparator;
+      }
+    }
+
+    return joinedStr;
   };
 }
 
