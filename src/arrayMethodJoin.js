17c4d7665a7da2e252f4a6eecb00e01c348882c0
Victor Buldenko <gookidoo@gmail.com>
2023-03-19 16:55:13 +0200

    to if statement

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -9,18 +9,14 @@ function applyCustomJoin() {
     const sep = String(separator);
 
     for (let i = 0; i < this.length; i++) {
-      switch (true) {
-        case i === (this.length - 1) && !this[i]:
-          return result;
-        case this[i] === null:
-        case this[i] === undefined:
-          result += sep;
-          break;
-        case (i + 1) !== this.length:
-          result += this[i] + sep;
-          break;
-        default:
-          result += this[i];
+      if (i === (this.length - 1) && !this[i]) {
+        return result;
+      } else if (this[i] === null || this[i] === undefined) {
+        result += sep;
+      } else if ((i + 1) !== this.length) {
+        result += this[i] + sep;
+      } else {
+        result += this[i];
       }
     }
 
