83e078adb27397272007773ee3acf226dcb2e1cc
Kyryl <kika_domination@yahoo.com>
2023-03-19 15:56:59 +0200

    add solution for custom join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,27 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let stringFromArray = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      const stringItem = this[i];
+
+      // debugger;
+
+      stringFromArray += stringItem === undefined || stringItem === null
+        ? '' : stringItem;
+
+      if (i !== this.length - 1) {
+        stringFromArray += String(separator);
+      }
+    }
+
+    return stringFromArray;
   };
 }
 
