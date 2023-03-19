e004996e529aab10dc55abd52fd78f9ba1f4d2c6
VladyslavaT <v.titarenko@student.csn.khai.edu>
2023-03-19 21:05:36 +0200

    join solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let resultStr = '';
+
+    if (this.length !== 0) {
+      for (let i = 0; i < this.length; i++) {
+        resultStr += this[i] === undefined || this[i] === null ? '' : this[i];
+
+        if (i !== this.length - 1) {
+          resultStr += separator;
+        }
+      }
+    }
+
+    return resultStr;
   };
 }
 
