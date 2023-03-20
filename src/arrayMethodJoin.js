bcc4021ecf4e7737a18c140475b9ca712aa24088
Victor Buldenko <gookidoo@gmail.com>
2023-03-20 13:21:17 +0200

    fed up, code stolen

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,17 +6,14 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    const sep = String(separator);
 
     for (let i = 0; i < this.length; i++) {
-      if (i === (this.length - 1) && !this[i]) {
-        return result;
-      } else if (this[i] === null || this[i] === undefined) {
-        result += sep;
-      } else if ((i + 1) !== this.length) {
-        result += this[i] + sep;
-      } else {
+      if (this[i] !== null && this[i] !== undefined) {
         result += this[i];
+      }
+
+      if (i !== (this.length - 1)) {
+        result += separator;
       }
     }
 
