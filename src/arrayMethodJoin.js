0aeb0b516f16f3cfa8d467b6fbe05e923aa0b250
kukla1989 <kukla.1989@ukr.net>
2023-03-20 09:11:22 +0200

    solve task with func function applyCustomJoin

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,27 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let res = '';
+
+    for (let el of this) {
+      if (el === null || el === undefined) {
+        el = '';
+      }
+
+      res += el + '' + separator;
+    }
+
+    if (separator === '') {
+      return res;
+    }
+
+    if (separator === null) {
+      return res.slice(0, -4);
+    }
+
+    // slice(0, -15) need if separator is {}
+    return res.slice(0, -separator.length) || res.slice(0, -15);
   };
 }
 
