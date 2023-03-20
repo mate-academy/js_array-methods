adfae5c711a866dd42e107d5a00f6e187e0eba61
Oleksandr Fenchuk <ofenchuk@proton.me>
2023-03-20 19:07:38 +0200

    alternative way to make join array function

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,30 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let result = '';
+    let separ = separator;
+
+    if (separator === null) {
+      separ = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      let wordToAdd = this[i];
+
+      if (this[i] === null || this[i] === undefined) {
+        wordToAdd = '';
+      }
+
+      if (i + 1 === this.length) {
+        return (result += wordToAdd);
+      }
+
+      result += wordToAdd + separ;
+    }
+
+    return result;
   };
 }
 
