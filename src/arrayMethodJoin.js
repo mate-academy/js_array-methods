691e08c79385e8b544bfa6ad86de1cf10a4a56e5
Aleksandr <alexnovaklawyer@gmail.com>
2023-03-20 19:19:59 +0200

    one if removed, variable added, else removed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,22 +8,21 @@ function applyCustomJoin() {
     let joinedStr = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (i === this.length - 1
-          && (this[i] === null || this[i] === undefined)) {
-        joinedStr += '';
-        continue;
-      }
+      const checker = this[i]
+        ? this[i].toString()
+        : '';
 
       if (i === this.length - 1) {
-        joinedStr += this[i].toString();
+        joinedStr += checker;
         continue;
       }
 
       if (this[i] === null || this[i] === undefined) {
-        joinedStr += '' + separator;
-      } else {
-        joinedStr += this[i].toString() + separator;
+        joinedStr += checker + separator;
+        continue;
       }
+
+      joinedStr += this[i].toString() + separator;
     }
 
     return joinedStr;
