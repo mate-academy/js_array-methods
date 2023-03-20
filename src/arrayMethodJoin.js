557318636d121ed62160b655498ba623def8bb49
kukla1989 <kukla.1989@ukr.net>
2023-03-20 10:55:35 +0200

    fully rewrite applyCustomJoin() because inspiration during code review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,24 +7,16 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let res = '';
 
-    for (let el of this) {
-      if (el === null || el === undefined) {
-        el = '';
+    for (const el of this) {
+      res += el === null || el === undefined
+        ? '' : el;
+
+      if (el !== this[this.length - 1]) {
+        res += separator;
       }
-
-      res += el + '' + separator;
     }
 
-    if (separator === '') {
-      return res;
-    }
-
-    if (separator === null) {
-      return res.slice(0, -4);
-    }
-
-    // slice(0, -15) need if separator is {}
-    return res.slice(0, -separator.length) || res.slice(0, -15);
+    return res;
   };
 }
 
