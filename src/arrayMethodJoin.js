542c784ba240eaaedc1338bfe59b303ad78cab62
DanielSorokowski <sorokowskidaniel0@gmail.com>
2023-03-20 13:42:28 +0100

    no string methods

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,17 +8,15 @@ function applyCustomJoin() {
     let concatening = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
+      if (this.length - i === 1 && this[i] === undefined) {
+        concatening += ``;
+      } else if ((this[i] === null || this[i] === undefined)) {
         concatening += `${separator}`;
       } else if (this.length - 1 === i) {
         concatening += `${this[i]}`;
       } else {
         concatening += `${this[i]}${separator}`;
       }
-    }
-
-    if (this[this.length - 1] === undefined) {
-      concatening = concatening.substr(0, concatening.length - 1);
     }
 
     return concatening;
