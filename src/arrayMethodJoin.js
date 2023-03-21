ebb9c56bae479a668038e369d329611a8ab542ef
Yurii Komanitskiy <komanitskiy200320@gmail.com>
2023-03-21 12:56:11 +0200

    loin method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (const element of this) {
+      if (element === null || element === undefined) {
+        result += '';
+      } else {
+        result += element;
+      }
+
+      if (element !== this[this.length - 1]) {
+        result += separator;
+      }
+    }
+
+    return result;
   };
 }
 
