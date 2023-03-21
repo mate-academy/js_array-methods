1704b8d8eec52f6df7a38f7fedec6cc13a790b13
Vasyl Zhyliakov <snak871@gmail.com>
2023-03-21 13:06:08 +0200

    push method function was created v 1.0

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,26 +7,13 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let string = '';
 
-    if (separator === null) {
-      // eslint-disable-next-line no-param-reassign
-      separator = 'null';
-    }
-
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        if (i < this.length - 1) {
-          string += separator;
-        }
-
-        continue;
+      if (this[i] !== null && this[i] !== undefined) {
+        string += this[i];
       }
 
-      if (i < this.length - 1) {
-        string += this[i] + separator;
-      }
-
-      if (i === this.length - 1) {
-        string += this[i].toString();
+      if (i !== this.length - 1) {
+        string += separator;
       }
     }
 
