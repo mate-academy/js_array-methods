16b4d2c600927568c8bc903ad8f7a38c9a2dc2ab
BondarenkoDmitriy <bondarenko.dima.kiev@gmail.com>
2023-03-21 12:43:42 +0200

    remove var separator and fixed nesting

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,21 +6,16 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let joinedStr = '';
-    const tempSeparator = separator;
 
     for (let i = 0; i < this.length; i++) {
       const element = this[i];
 
-      if (element === undefined || element === null) {
-        joinedStr += '';
-      } else if (element === 'null') {
-        joinedStr += 'null';
-      } else {
+      if (element !== undefined && element !== null) {
         joinedStr += element;
       }
 
       if (i < this.length - 1) {
-        joinedStr += tempSeparator;
+        joinedStr += separator;
       }
     }
 
