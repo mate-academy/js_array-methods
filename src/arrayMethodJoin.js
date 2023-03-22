6f69903b6a2bda1f2d714aec45f69c884cf81657
Artem Kurinniy <artem.kyrinniy@gmail.com>
2023-03-22 21:57:58 +0200

    made join methos

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     // write code here
+    let str = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const item = this[i];
+
+      if (typeof item === 'string'
+        || typeof item === 'number'
+        || typeof item === 'boolean') {
+        str += '' + separator + item;
+      } else {
+        str += '' + separator;
+      }
+    }
+
+    if (separator !== '') {
+      const sep = `${separator}`;
+
+      str = str.substring(sep.length);
+    }
+
+    return str;
   };
 }
 
