ddcd7658c05d363bb1452e6f5b079af1211ae2d4
Natali <swetswet1234@gmail.com>
2023-03-23 21:57:37 +0200

    develop

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,32 +5,23 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let newProto = '';
-    let separatorNew = separator;
+    let joinedString = '';
 
-    if (this.length === 0) {
-      return newProto;
-    }
-
-    if (separatorNew === null) {
-      separatorNew = 'null';
-    }
-
-    for (let i = 0; i <= this.length - 1; i++) {
+    for (let i = 0; i < this.length; i++) {
       let item = this[i];
 
       if ((item === null) || (item === undefined)) {
         item = '';
       }
 
-      newProto += item;
+      joinedString += item;
 
-      if (i !== this.length - 1) {
-        newProto += separatorNew;
+      if (i < this.length - 1) {
+        joinedString += separator;
       }
     }
 
-    return newProto;
+    return joinedString;
   };
 }
 
