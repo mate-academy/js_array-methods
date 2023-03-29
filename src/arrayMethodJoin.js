b4a2ae12d286983a6c8329b63ff805097e51f644
Mikołaj <ostermi@wp.pl>
2023-03-29 15:42:10 +0100

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,25 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let finalString = '';
+    const finalSeparator = separator === undefined ? ',' : '' + separator;
+    let currentElement;
+
+    for (let i = 0; i < this.length; i++) {
+      currentElement = this[i];
+
+      if (currentElement === null || currentElement === undefined) {
+        currentElement = '';
+      }
+
+      if (i === this.length - 1) {
+        finalString += currentElement;
+      } else {
+        finalString += currentElement + finalSeparator;
+      }
+    }
+
+    return finalString;
   };
 }
 
