ea7347a20b4eb17e0a9debb083c7462c265ea523
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-03-30 20:22:13 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,19 +8,16 @@ function applyCustomJoin() {
     let joiner = '';
 
     for (let i = 0; i < this.length; i++) {
-      if ((this[i] === undefined) || (this[i] === null)) {
-        if (this[i] === this[this.length - 1]) {
-          break;
-        }
-        joiner += separator;
-        continue;
-      }
-
-      if (this[i] === this[this.length - 1]) {
-        joiner += this[i];
+      if ((this[this.length - 1] === undefined) && (i === this.length - 1)) {
         break;
+      } else if ((this[i] === undefined) || (this[i] === null)) {
+        joiner += `${separator}`;
+        continue;
+      } else if (this[i] === this[this.length - 1]) {
+        joiner += `${this[i]}`;
+      } else {
+        joiner += `${this[i]}${separator}`;
       }
-      joiner += this[i] + separator;
     }
 
     return joiner;
