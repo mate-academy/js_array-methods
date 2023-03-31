2b068c490a0da0f3b62ba199d33ec2ed408068ee
Daniel <henio09@wp.pl>
2023-03-31 16:37:00 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,20 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    const sep = separator !== undefined ? separator : ',';
+    let resoult = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (i > 0) {
+        resoult += sep;
+      }
+
+      const element = this[i] === null || this[i] === undefined ? '' : this[i];
+
+      resoult += element;
+    }
+
+    return resoult;
   };
 }
 
