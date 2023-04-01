c96a2085e1699d37ca0e57bf892bceae1bd48d5c
Mateusz Michalowski <mateusz.michalowski000@gmail.com>
2023-04-01 18:42:12 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,24 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let joinedStr = '';
+    const arrLength = this.length;
+
+    for (let i = 0; i < arrLength; i++) {
+      const type = typeof (this[i]);
+
+      if (type === 'number' || type === 'string' || type === 'boolean') {
+        joinedStr += this[i];
+      }
+
+      if (typeof (separator) !== 'undefined' && i < arrLength - 1) {
+        joinedStr += separator;
+      } else if (i < arrLength - 1) {
+        joinedStr += ',';
+      }
+    }
+
+    return joinedStr;
   };
 }
 
