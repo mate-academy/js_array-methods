4f58f588d8b4cdaf6bd59dcde1e5a7c9df988725
Paweł Smoliński <ski1993@o2.pl>
2023-04-02 14:25:15 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,31 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    if (this.length === 0) {
+      return '';
+    }
+
+    let separateSign = separator;
+
+    if (separateSign === undefined) {
+      separateSign = ',';
+    } else if (separateSign === null) {
+      separateSign = 'null';
+    }
+
+    let returnedValue = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        returnedValue += (i === this.length - 1) ? '' : separateSign;
+      } else if (i === this.length - 1) {
+        returnedValue += this[i];
+      } else {
+        returnedValue += this[i] + separateSign;
+      }
+    }
+
+    return returnedValue;
   };
 }
 
