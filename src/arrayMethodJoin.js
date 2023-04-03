facb660ca8504b59e7ae390dc22050b963e5cefc
Katarzyna Bogiel <katarzyna.bogiel@poczta.onet.pl>
2023-04-03 12:57:32 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,32 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedArray = '';
+
+    if (this[0] !== null) {
+      joinedArray += this[0];
+    }
+
+    let tool = separator;
+
+    if (tool === null) {
+      tool = 'null';
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        joinedArray += tool + this[i];
+      } else {
+        joinedArray += tool + '';
+      }
+    }
+
+    if (this.length === 0) {
+      joinedArray = '';
+    }
+
+    return joinedArray;
   };
 }
 
