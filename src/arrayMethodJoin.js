707970ae54b7680a479b34848eca6854e8726451
kq333 <afugowski@yandex.com>
2023-04-03 23:20:29 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,33 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    if (this.length === 0) {
+      return '';
+    }
+
+    let separateSign = separator;
+
+    if (separateSign === undefined) {
+      separateSign = ',';
+    } else if (separateSign === null) {
+      separateSign = 'null';
+    } else if (typeof separateSign === 'object') {
+      separateSign = '[object Object]';
+    }
+
+    let returnedValue = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        returnedValue += (i === this.length - 1) ? '' : separateSign;
+      } else if (i === this.length - 1) {
+        returnedValue += this[i];
+      } else {
+        returnedValue += this[i] + separateSign;
+      }
+    }
+
+    return returnedValue;
   };
 }
 
