633e26e7aee193d89f5c8c551bb86d11f51f9beb
Atlas Werecik <mw.greybeard72@gmail.com>
2023-04-03 21:34:39 +0200

    task solution approach 03/04/2023 21:34

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,29 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+
+    let result = '';
+    let comma = separator;
+
+    if (comma === undefined) {
+      comma = ',';
+    } else if (comma === null) {
+      comma = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        result += '';
+      } else {
+        result += this[i];
+      }
+
+      if (i < this.length - 1) {
+        result += comma;
+      }
+    }
+
+    return result;
   };
 }
 
