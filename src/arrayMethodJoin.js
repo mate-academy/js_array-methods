24cd53d0aa244940a91794eb6abbe0a1b983ecde
Max Hyrych <gggirich@gmail.com>
2023-04-04 23:59:22 +0300

    The implementation of the join method is complete

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,27 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let delimiter = separator;
+    const copyArr = [...this];
+
+    if (delimiter === 0 || delimiter === undefined) {
+      delimiter = ',';
+    }
+
+    for (let i = 0; i < copyArr.length; i++) {
+      if (copyArr[i] === null || copyArr[i] === undefined) {
+        copyArr[i] = '';
+      }
+
+      result += copyArr[i];
+
+      if (i < copyArr.length - 1) {
+        result += delimiter;
+      }
+    }
+
+    return result;
   };
 }
 
