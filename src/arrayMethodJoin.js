1f8ce1bc504857890058906f1964a12afec0a41a
Nazar Yants <nazyants@gmail.com>
2023-04-06 14:45:09 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1 && (this[i] == null || this[i] === undefined)) {
+        str += '';
+      } else if (i === this.length - 1) {
+        str += this[i];
+      } else if (this[i] == null || this[i] === undefined) {
+        str += separator;
+      } else {
+        str = str + this[i] + separator;
+      }
+    }
+
+    return str;
   };
 }
 
