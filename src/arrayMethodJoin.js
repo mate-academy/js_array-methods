cbce1f650ab244dfc5d8b80382cf738d5c72ac23
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-04-06 08:13:26 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,22 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let joiner = '';
+    let stringOutput = '';
 
     for (let i = 0; i < this.length; i++) {
-      if ((this[this.length - 1] === undefined) && (i === this.length - 1)) {
-        break;
-      } else if ((this[i] === undefined) || (this[i] === null)) {
-        joiner += `${separator}`;
-        continue;
-      } else if (this[i] === this[this.length - 1]) {
-        joiner += `${this[i]}`;
-      } else {
-        joiner += `${this[i]}${separator}`;
+      if ((this[i] !== undefined && this[i] !== null)) {
+        stringOutput += `${this[i]}`;
+      }
+
+      if (i !== this.length - 1) {
+        stringOutput += `${separator}`;
       }
     }
 
-    return joiner;
+    return stringOutput;
   };
 }
 
