0f3d893d9f21dc4843acb3f6cefed00320ba7f79
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-04-06 14:20:18 +0200

    sol

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let stringOutput = '';
+    let joinedString = '';
 
     for (let i = 0; i < this.length; i++) {
       if ((this[i] !== undefined && this[i] !== null)) {
-        stringOutput += `${this[i]}`;
+        joinedString += this[i];
       }
 
       if (i !== this.length - 1) {
-        stringOutput += `${separator}`;
+        joinedString += separator;
       }
     }
 
-    return stringOutput;
+    return joinedString;
   };
 }
 
