b1d54bf5c24705b0dfe9969bbc9a9b5ea4c101ec
Barbara Jeziorek <barbarajeziorek@gmail.com>
2023-04-07 16:03:40 +0200

    clean code

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,37 +4,40 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function (separator = ',') {
+  [].__proto__.join2 = function(separator = ',') {
     let joinedString = '';
 
     if (this.length === 0) {
       return '';
     }
 
-    if (this.length === 0) {
-      return [];
-    }
-
     if (this.length === 1) {
       return '' + this[0];
     }
 
-    if (!this[1] && this[0]) {
+    if (this[1] === undefined) {
       return this;
     }
 
     for (let i = 0; i < this.length - 1; i++) {
-      joinedString += ''
-        + ((this[i] === undefined || this[i] === null)
-          ? ''
-          : this[i])
-        + '' + ((separator === null ? 'null' : separator) || '');
+      if (this[i] === undefined || this[i] === null) {
+        joinedString += '';
+      } else {
+        joinedString += this[i];
+      }
+
+      if (separator === null) {
+        joinedString += 'null';
+      }
+
+      if (separator !== null) {
+        joinedString += separator;
+      }
     }
 
-    joinedString += ''
-      + ((this[this.length - 1] === undefined || this[this.length - 1] === null)
-        ? ''
-        : this[this.length - 1]);
+    if (this[this.length - 1]) {
+      joinedString += this[this.length - 1];
+    }
 
     return joinedString;
   };
