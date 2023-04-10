d4177903f1a451f531866c72c54a404f6c2ee048
DefaultGitUser <usingforgit@gmail.com>
2023-04-10 10:57:44 +0300

    Sol

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,21 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let val of this) {
+      if (val === null || val === undefined) {
+        val = '';
+      }
+      result += `${val}${separator}`;
+    }
+
+    if (separator === '') {
+      return result;
+    }
+
+    return result.slice(0, result.lastIndexOf(separator));
   };
 }
 
