baf00c4e46a49b44307a08d0b8469588942e4c2e
Ivan Orinchak <124816409+IvanOrinchak@users.noreply.github.com>
2023-04-10 16:02:53 +0300

    array-method-join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let join = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        join += this[i];
+
+        join += i !== this.length - 1
+          && this.length - 1 !== undefined
+          ? separator : '';
+      } else if (i !== this.length - 1 && this.length - 1 !== undefined) {
+        join += separator;
+      }
+    }
+
+    return join;
   };
 }
 
