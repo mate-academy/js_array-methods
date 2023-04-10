aaa05656d57d98bc6844a28f76ff6e4bd59ee874
Jedrzej Kielpinski <jedrzej.kielpinski@gmail.com>
2023-04-10 23:30:10 +0200

    fix

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,21 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let result = '';
+    let joinedText = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === undefined || this[i] === null) {
-        if (i !== this.length - 1) {
-          result += String(separator);
-        }
-      } else if (i !== this.length - 1) {
-        result += this[i] + String(separator);
-      } else {
-        result += this[i];
+      if (this[i] !== undefined && this[i] !== null) {
+        joinedText += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        joinedText += separator;
       }
     }
 
-    return result;
+    return joinedText;
   };
 }
 
