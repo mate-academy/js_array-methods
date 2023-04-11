3f65bc1c9a9093cfdb7770678738b0b9f158642c
mariuszzawierucha <mariusz.zawierucha@gmail.com>
2023-04-11 02:29:50 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,28 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let resultString = '';
+    let tempSeparator = separator;
+
+    if (tempSeparator === undefined) {
+      tempSeparator = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      let currentItem = this[i];
+
+      if (this[i] === undefined || this[i] === null) {
+        currentItem = '';
+      }
+      resultString += currentItem;
+
+      // on last element don't add separator
+      if (i !== this.length - 1) {
+        resultString += tempSeparator;
+      };
+    }
+
+    return resultString;
   };
 }
 
