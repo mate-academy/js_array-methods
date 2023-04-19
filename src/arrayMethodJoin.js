6990c711f31306b74b5c8343b9d4d1b2ab8c6e13
Artem Pinchuk <aviktorovich193@gmail.com>
2023-04-19 16:04:57 +0300

    fixes after code review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,41 +4,18 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    const arrCopy = [...this];
+  [].__proto__.join2 = function(separator = ',') {
     let string = '';
 
-    if (arrCopy.length === 0) {
-      return string;
-    }
-
-    for (let i = 0; i < arrCopy.length; i++) {
-      if (arrCopy[i] === null || arrCopy[i] === undefined) {
-        arrCopy[i] = '';
-      }
-    }
-
-    if (separator || separator === null) {
-      for (let i = 0; i < arrCopy.length - 1; i++) {
-        string = string + arrCopy[i] + separator;
-      }
-      string = string + arrCopy[arrCopy.length - 1];
-
-      return string;
-    }
-
-    if (separator === '' || separator === null) {
-      for (let i = 0; i < arrCopy.length; i++) {
-        string = string + arrCopy[i];
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] != null) {
+        string += this[i];
       }
 
-      return string;
+      if (i < this.length - 1) {
+        string += separator;
+      }
     }
-
-    for (let i = 0; i < arrCopy.length - 1; i++) {
-      string = string + arrCopy[i] + ',';
-    }
-    string = string + arrCopy[arrCopy.length - 1];
 
     return string;
   };
