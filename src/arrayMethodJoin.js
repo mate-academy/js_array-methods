b9d2e9c76a07c413b0e432bd25d116faa077b1b8
Roman <rdyakovich@gmail.com>
2023-04-19 11:28:43 +0300

    join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,25 @@
 'use strict';
 
-/**
- * Implement method join
- */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        result += '';
+      } else {
+        result += this[i];
+      }
+
+      if (i < this.length - 1) {
+        result += separator === undefined ? ','
+          : separator === null ? 'null'
+            : typeof separator === 'number' && isNaN(separator) ? ','
+              : separator;
+      }
+    }
+
+    return result;
   };
 }
 
