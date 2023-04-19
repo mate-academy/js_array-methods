fdcb7d77525a3faa79eff0766b68c87c70bf998d
Denis Sizemov <96919055+Denis-Sizemov@users.noreply.github.com>
2023-04-20 00:26:00 +0300

    solution updated

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,10 +8,8 @@ function applyCustomJoin() {
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        result += '';
-      } else {
-        result += String(this[i]);
+      if (this[i] !== null && this[i] !== undefined) {
+        result += this[i];
       }
 
       if (i < this.length - 1) {
