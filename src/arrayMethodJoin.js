5a47187e0844f472c4483a6e78a50cc56f17a62d
Filippov Serhii <kg2301@gmail.com>
2023-04-22 21:17:43 +0300

    fix 2

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,23 +6,19 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     // write code here
-    const arr = [...this];
     let str = '';
 
-    for (let i = 0; i < arr.length; i++) {
-      switch (arr[i]) {
-        case undefined:
-          arr[i] = '';
-          break;
-        case null:
-          arr[i] = '';
-          break;
+    for (let i = 0; i < this.length; i++) {
+      let tmp = this[i];
+
+      if (tmp === undefined || tmp === null) {
+        tmp = '';
       }
 
-      if (i === 0) {
-        str = str + arr[i];
-      } else {
-        str = str + separator + arr[i];
+      str = str + tmp;
+
+      if (i !== this.length - 1) {
+        str = str + separator;
       }
     }
 
