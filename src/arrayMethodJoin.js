6d68073db7a3c8bd18e14f1f44918d897cf00d26
Liliia Danylenko <liliya.danylenko96@gmail.com>
2023-04-23 12:21:37 +0300

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,25 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let a = '';
+    let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (separator === null && i !== this.length - 1) {
-        a += this[i] + 'null';
-      } else if (this[i] === null) {
-        a += separator;
-      } else if (this[i] === undefined && i !== this.length - 1) {
-        a += separator;
-      } else if (this[i] === undefined && i === this.length - 1) {
-        continue;
-      } else if (i === this.length - 1) {
-        a += this[i];
-      } else {
-        a += this[i] + separator;
+      if (this[i] !== null && this[i] !== undefined) {
+        result += this[i];
+      }
+
+      if (i < this.length - 1) {
+        result += separator;
       }
     }
 
-    return a;
+    return result;
   };
 }
 
