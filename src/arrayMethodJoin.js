64fdd6c61b89274c65e474b4711649d6d8015c67
Volodymyr Konoplianyi <volodymyr.konoplianyi@gmail.com>
2023-04-24 23:22:41 +0300

    Reduced the code

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,22 +4,15 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    let sep = separator;
+  [].__proto__.join2 = function(separator = ',') {
     let str = '';
-
-    if (separator === undefined) {
-      sep = ',';
-    }
 
     for (let i = 0; i < this.length; i++) {
       if (i !== 0) {
-        str += sep;
+        str += separator;
       }
 
-      if (this[i] === null || this[i] === undefined) {
-        str += '';
-      } else {
+      if (this[i] !== null && this[i] !== undefined) {
         str += this[i];
       }
     }
