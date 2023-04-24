dedddbe718c14ed61fc006b438ac45a2b4ddd923
Alla Nebozhenko <111634874+AllaNebozhenko@users.noreply.github.com>
2023-04-24 12:58:46 +0200

    Join solution 2

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,11 +5,10 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    // write code here
     let resultString = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] != null) {
+      if (this[i] !== undefined && this[i] !== null) {
         resultString += this[i];
       }
 
