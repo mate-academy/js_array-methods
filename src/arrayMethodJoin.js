b9827e5a113783bedd679c9902ea8d9f2a6b9a01
Volodymyr Konoplianyi <volodymyr.konoplianyi@gmail.com>
2023-04-24 23:13:25 +0300

    Array join solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,26 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let sep = separator;
+    let str = '';
+
+    if (separator === undefined) {
+      sep = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i !== 0) {
+        str += sep;
+      }
+
+      if (this[i] === null || this[i] === undefined) {
+        str += '';
+      } else {
+        str += this[i];
+      }
+    }
+
+    return str;
   };
 }
 
