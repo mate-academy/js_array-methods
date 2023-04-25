bfa1bcb53560787e4503e58975fa6d4ca6df1ee1
Ivan Kalyenichenko <ikalenicenko205@gmail.com>
2023-04-25 12:33:55 +0300

    Solution of array method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    if (this[0] !== null && this[0] !== undefined && this[0] !== '') {
+      result = `${this[0]}`;
+    }
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 1; i <= this.length - 1; i++) {
+      result += separator;
+
+      if (this[i] === null || this[i] === undefined || this[i] === '') {
+        continue;
+      } else {
+        result += this[i];
+      }
+    }
+
+    return result;
   };
 }
 
