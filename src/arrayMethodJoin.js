2ab4a9672c30f5e919adcc15b5faf87c1f41ebbd
Ivan Kalyenichenko <ikalenicenko205@gmail.com>
2023-04-25 13:11:02 +0300

    Rework mistakes! Delete unnecessary thinks, and remake loop.

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,21 +7,13 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
 
-    if (this[0] !== null && this[0] !== undefined && this[0] !== '') {
-      result = `${this[0]}`;
-    }
-
-    if (this.length === 0) {
-      return '';
-    }
-
-    for (let i = 1; i <= this.length - 1; i++) {
-      result += separator;
-
-      if (this[i] === null || this[i] === undefined || this[i] === '') {
-        continue;
-      } else {
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
         result += this[i];
+      }
+
+      if (i < this.length - 1) {
+        result += separator;
       }
     }
 
