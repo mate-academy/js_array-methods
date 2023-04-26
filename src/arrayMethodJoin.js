20d80ee28a594faacdb25cf6846920634f73a574
Rostysalav Kuts <2002qazedc@gmail.com>
2023-04-26 12:49:52 +0300

    Join via for of

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    let count = 0;
+
+    for (const mass of this) {
+      if (mass !== undefined && mass !== null) {
+        result += mass;
+      }
+
+      if (count !== this.length - 1) {
+        result += separator;
+      }
+
+      count++;
+    }
+
+    return result;
   };
 }
 
