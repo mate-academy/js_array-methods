d495d541a88380018c67d7f165bd4cb6c56fc02e
Evhen Ivchenko <ivchenkoevhen@gmail.com>
2023-04-30 21:09:12 +0300

    finished cersion

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let string = '';
+    let lastIndex = this.length - 1;
+    let separatorChecked = (this.length <= 1) ? '' : separator;
+
+    for (let i = 0; i < this.length; i++) {
+      let value = this[i];
+
+      if (i === lastIndex) {
+        separatorChecked = '';
+      }
+
+      if (value === null || value === undefined) {
+        value = '';
+      }
+      string = string + value + separatorChecked;
+    }
+
+    return string;
   };
 }
 
