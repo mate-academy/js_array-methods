f38058a1498a47d2765ef91d98cdc7c3c5ff089d
Roman Didusenko <albertushoffman@proton.me>
2023-05-01 13:15:14 +0300

    removed redundant code

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,24 +7,17 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     let joined = '';
 
-    for (let i = 0; i <= this.length - 1; i++) {
-      if (i === this.length - 1 && this[i] === undefined) {
-        break;
+    const lastIndex = this.length - 1;
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        joined += this[i];
       }
 
-      if (i === this.length - 1) {
-        joined += this[i];
-        break;
-      } else if (separator === null) {
-        joined += this[i] + 'null';
-      } else if (separator === undefined) {
-        joined += this[i] + ',';
-      } else if (this[i] === null || this[i] === undefined) {
-        joined += ',';
-      } else {
-        joined += this[i] + separator;
+      if (i !== lastIndex) {
+        joined += separator;
       }
-    };
+    }
 
     return joined;
   };
