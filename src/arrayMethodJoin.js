3060241786aa5a334eda2def9d76e7e60692d675
Dmytro Vovk <bellboy.dv@gmail.com>
2023-05-03 12:39:06 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,24 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let result = '';
+    let separatorCopy = separator;
+
+    if (separatorCopy === undefined) {
+      separatorCopy = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i > 0) {
+        result += separatorCopy;
+      }
+
+      if (this[i] !== null && this[i] !== undefined) {
+        result += this[i];
+      }
+    }
+
+    return result;
   };
 }
 
