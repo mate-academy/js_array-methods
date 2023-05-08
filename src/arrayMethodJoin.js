f2322ac059c78e18a0045b6902c1c635a906b25a
Vladyslav Orlenko <vlad.pos1bl@gmail.com>
2023-05-08 19:48:50 +0300

    delete extra separ and make code more readable

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,27 +6,20 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    let separ = 'null';
-
-    if (separator !== null) {
-      separ = separator.toString();
-    }
 
     if (this.length === 0) {
       return result;
     }
 
     for (let i = 0; i < this.length; i++) {
-      let value = this[i];
+      const value = this[i];
 
-      if (value === undefined || value === null) {
-        value = '';
+      if (value !== undefined && value !== null) {
+        result += value;
       }
 
-      result += value;
-
       if (i !== this.length - 1) {
-        result += separ;
+        result += separator;
       }
     }
 
