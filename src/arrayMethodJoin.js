6ab0283f62ee1daeb60684e691b140d57c2b6cc9
pasternakv19@gmail.com <pasternakv19@gmail.com>
2023-05-08 14:59:23 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,7 +6,6 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let str = '';
-    const chengeddArr = [];
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
@@ -15,16 +14,6 @@ function applyCustomJoin() {
 
       if (i !== this.length - 1) {
         str += separator;
-      }
-    }
-
-    for (const item of this) {
-      if (separator === null) {
-        chengeddArr.push(item);
-        chengeddArr.push('null');
-      } else {
-        chengeddArr.push(item);
-        chengeddArr.push(separator);
       }
     }
 
