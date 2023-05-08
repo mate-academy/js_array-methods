7b5fb9bf4e6e8c8b26c6315d2860f3518b9acaf5
Vladyslav Orlenko <vlad.pos1bl@gmail.com>
2023-05-08 15:07:03 +0300

    crate join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,33 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    let separ = 'null';
+
+    if (separator !== null) {
+      separ = separator.toString();
+    }
+
+    if (this.length === 0) {
+      return result;
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      let value = this[i];
+
+      if (value === undefined || value === null) {
+        value = '';
+      }
+
+      result += value;
+
+      if (i !== this.length - 1) {
+        result += separ;
+      }
+    }
+
+    return result;
   };
 }
 
