75259f3a48137a2ba347a9731cc3b897a902b9b5
Diana Tuz <dianatuz12@gmail.com>
2023-05-09 07:51:49 +0300

    join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,14 @@
 'use strict';
 
-/**
- * Implement method join
- */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (const element of this) {
+      result += element + separator;
+    }
+
+    return result.slice(0, -separator.length); ;
   };
 }
 
