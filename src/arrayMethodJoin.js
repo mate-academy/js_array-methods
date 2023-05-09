1f05ea910d327208e3ada5951c0d6de67be5730b
Wowa Snuber <snuber.w@gmail.com>
2023-05-09 23:44:09 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,43 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    if (this.length === 0) {
+      return '';
+    }
+
+    let resultString = '';
+    let newSeparator = separator;
+
+    if (this[0] === null || this[0] === undefined) {
+      resultString = '';
+    } else {
+      resultString = this[0].toString();
+    }
+
+    switch (newSeparator) {
+      case (undefined):
+        newSeparator = ',';
+        break;
+
+      case (null):
+        newSeparator = 'null';
+        break;
+
+      default:
+        newSeparator = separator.toString();
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      let item = this[i];
+
+      if (item === null || item === undefined) {
+        item = '';
+      }
+
+      resultString += newSeparator + item.toString();
+    }
+
+    return resultString;
   };
 }
 
