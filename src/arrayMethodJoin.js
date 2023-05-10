3b0a6545c64d3730d8c7fe9758ab4525e629861c
Stepan Haida <stepanhaida@gmail.com>
2023-05-10 13:56:01 +0300

    add solution but some tests are not passed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let newStr = '';
+
+    for (let indexOfItem = 0; indexOfItem < this.length; indexOfItem++) {
+      if (indexOfItem === this.length - 1) {
+        newStr += this[indexOfItem];
+      } else {
+        newStr += this[indexOfItem] + separator;
+      }
+    }
+
+    return newStr;
   };
 }
+
+// applyCustomJoin();
+
+// ['test', 'test'].join2('--');
 
 module.exports = applyCustomJoin;
