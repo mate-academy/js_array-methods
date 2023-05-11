6013055977987346cfdc949f9f86a53848aa8784
Serj984 <serj90klishin@gmail.com>
2023-05-11 18:27:13 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,29 @@
 /**
  * Implement method join
  */
+
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        result += '';
+      } else if (typeof this[i] === 'string'
+      && this[i] === 'null') {
+        result += 'null';
+      } else if (isNaN(this[i])) {
+        result += 'NaN';
+      } else {
+        result += this[i];
+      }
+
+      if (i < this.length - 1) {
+        result += separator;
+      }
+    }
+
+    return result;
   };
 }
 
