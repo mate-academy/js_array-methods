15bab45493c4dca727e8b542c9f21df90236d7de
Oleksii Lysenko <nano77@ukr.net>
2023-05-11 10:55:32 +0300

    add functionality to applyCustomJoin

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,26 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let string = '';
+
+    for (let i = 0; i < this.length; i++) {
+      let element;
+
+      if (typeof this[i] === 'undefined' || this[i] === null) {
+        element = '';
+      } else {
+        element = this[i];
+      }
+
+      string += element;
+
+      if (i !== this.length - 1) {
+        string += separator;
+      }
+    }
+
+    return string;
   };
 }
 
