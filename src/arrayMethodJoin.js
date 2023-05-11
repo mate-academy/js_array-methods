d1500915620f3b910631da39763d05d0e71f0ebd
Artem <artem.ivashchenko.exe@gmail.com>
2023-05-11 12:51:34 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      let copyElement = this[i];
+
+      if (this[i] == null) {
+        copyElement = '';
+      }
+
+      result += copyElement;
+
+      if (i !== this.length - 1) {
+        result += separator;
+      }
+    }
+
+    return result;
   };
 }
 
