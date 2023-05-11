1699884e64381183fe4cbc50fe12eb490ca50b77
Oleksandr Kosohor <kosogor2007@gmail.com>
2023-05-11 13:26:39 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,29 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let joinResult = '';
+    let newSeparator = separator;
+
+    if (newSeparator === undefined) {
+      newSeparator = ',';
+    }
+
+    for (const word of this) {
+      switch (word) {
+        case undefined:
+        case null:
+          break;
+
+        default:
+          joinResult += word;
+      }
+
+      if (word !== this[this.length - 1]) {
+        joinResult += newSeparator;
+      }
+    }
+
+    return joinResult;
   };
 }
 
