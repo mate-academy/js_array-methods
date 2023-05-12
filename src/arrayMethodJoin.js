8920415d48aa56f4d4350b65921436114e141c64
Maksym Bobryk <booobryyyk@gmail.com>
2023-05-12 19:23:48 +0300

    add arrayMethodJoin task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,36 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(initialSeparator) {
+    let joinedArrString = '';
+    let separator = initialSeparator;
+    const newArr = [...this];
+
+    if (separator === undefined) {
+      separator = ',';
+    }
+
+    if (separator === null) {
+      separator = 'null';
+    }
+
+    for (let i = 0; i < newArr.length; i++) {
+      if (newArr[i] === null) {
+        newArr[i] = '';
+      }
+
+      if (newArr[i] === undefined) {
+        newArr[i] = '';
+      }
+
+      joinedArrString += newArr[i];
+
+      if (i < newArr.length - 1) {
+        joinedArrString += separator;
+      }
+    }
+
+    return joinedArrString;
   };
 }
 
