7ad3337fe4a7cfc94fba5eb95f74850e62098a9d
Serj984 <serj90klishin@gmail.com>
2023-05-12 18:56:03 +0300

    fixed solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -9,14 +9,7 @@ function applyCustomJoin() {
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        result += '';
-      } else if (typeof this[i] === 'string'
-      && this[i] === 'null') {
-        result += 'null';
-      } else if (isNaN(this[i])) {
-        result += 'NaN';
-      } else {
+      if (this[i] !== null && this[i] !== undefined) {
         result += this[i];
       }
 
