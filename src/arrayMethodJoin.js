d290c5f5652480833dd68005a4c8670904ead613
Vladyslav Lapshyn <lapshin.vlad96@gmail.com>
2023-05-13 13:45:49 +0300

    made task array method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let returnedString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        returnedString += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        returnedString += separator;
+      }
+    }
+
+    return returnedString;
   };
 }
 
