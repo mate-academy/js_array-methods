343609be4d512388f56fc232746e2518b9a23a36
Dima-Petrov <danielgromph@gmail.com>
2023-05-14 18:27:44 +0300

    fixed after code review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,38 +6,21 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     const length = this.length;
-    let firstElement = String(this[0]);
-    let separatorValue = separator;
+    const separatorValue = separator;
+    let result = '';
 
-    if (length === 0) {
-      return '';
-    }
-
-    if (firstElement === 'undefined' || firstElement === 'null') {
-      firstElement = '';
-    }
-
-    if (length === 1) {
-      return firstElement;
-    }
-
-    if (separatorValue === undefined) {
-      separatorValue = ',';
-    }
-
-    if (separatorValue === null) {
-      separatorValue = 'null';
-    }
-
-    let result = firstElement;
-
-    for (let i = 1; i < length; i++) {
+    for (let i = 0; i < length; i++) {
       let element = this[i];
 
       if (element === undefined || element === null) {
         element = '';
       }
-      result += separatorValue + element;
+
+      if (i === length - 1) {
+        result += element;
+        break;
+      }
+      result = result + element + separatorValue;
     }
 
     return result;
