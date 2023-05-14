51d14bb62c0a436cecfa82b7c04dc90a48a55f7a
Kiril Shorin <kirillshorin8838@gmail.com>
2023-05-14 15:51:34 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,23 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    const copyArray = [...this];
+
+    for (let i = 0; i < copyArray.length; i++) {
+      if (copyArray[i] === null || copyArray[i] === undefined) {
+        copyArray[i] = '';
+      }
+
+      if (i === copyArray.length - 1) {
+        result = result + copyArray[i];
+      } else {
+        result = result + copyArray[i] + separator;
+      }
+    }
+
+    return result;
   };
 }
 
