9f5fd5413dad0ccb03c0891abd21dd597086bbee
Maksym Bobryk <booobryyyk@gmail.com>
2023-05-15 12:34:52 +0300

    fixed separator

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,8 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(initialSeparator = ',') {
+  [].__proto__.join2 = function(separator = ',') {
     let joinedArrString = '';
-    const separator = initialSeparator;
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
