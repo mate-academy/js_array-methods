f96677a0964003ef1a9c7e88f594f8f2c5d4d830
Vo7kov <drainys1234@gmail.com>
2023-05-15 22:05:03 +0300

    fix

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,21 +6,14 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    let divider = separator;
-
-    if (divider === undefined) {
-      divider = ',';
-    } else {
-      divider = String(separator);
-    }
 
     for (let i = 0; i < this.length; i++) {
       if (i > 0) {
-        result += divider;
+        result += separator;
       }
 
       if (this[i] !== null && this[i] !== undefined) {
-        result += String(this[i]);
+        result += this[i];
       }
     }
 
