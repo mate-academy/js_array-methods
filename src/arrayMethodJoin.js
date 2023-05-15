80a0e4bf244962ec8300b08e2985ddccd1c2bf13
Yehor Myronov <2726048@gmail.com>
2023-05-15 18:43:25 +0300

    simplification of the code

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,12 +8,12 @@ function applyCustomJoin() {
     let output = '';
 
     for (let i = 0; i < this.length; i++) {
-      output += this[i] === null || this[i] === undefined
-        ? ''
-        : this[i];
+      if (this[i] !== null & this[i] !== undefined) {
+        output += this[i];
+      }
 
       if (i !== this.length - 1) {
-        output += `${separator}`;
+        output += separator;
       }
     }
 
