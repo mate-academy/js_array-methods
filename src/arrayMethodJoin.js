cca5edb7f73ca069aa8ac7775484e742f1835147
Evhen Holub <golub0208@gmail.com>
2023-05-21 10:29:38 +0300

    joinIsDone

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const arr = [...this];
+
+    let str = '';
+
+    if (arr.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === null || arr[i] === undefined) {
+        arr[i] = '';
+      }
+
+      str += arr[i];
+
+      if (i < arr.length - 1) {
+        str += separator;
+      }
+    }
+
+    return str;
   };
 }
 
