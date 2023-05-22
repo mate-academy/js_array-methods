6aa3b4c04f1ce38666b342fb49fba059e865e62d
Evhen Holub <golub0208@gmail.com>
2023-05-22 09:56:33 +0300

    codeWasFixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,22 +5,14 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    const arr = [...this];
-
     let str = '';
 
-    if (arr.length === 0) {
-      return '';
-    }
-
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === null || arr[i] === undefined) {
-        arr[i] = '';
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        str += this[i];
       }
 
-      str += arr[i];
-
-      if (i < arr.length - 1) {
+      if (i < this.length - 1) {
         str += separator;
       }
     }
