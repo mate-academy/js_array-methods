961c51daf6aab973c9640239f4e2ae7f7a8c96d8
Liza Strykharchuk <ol220681vol@gmail.com>
2023-05-22 16:09:33 +0300

    clear

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,22 +4,16 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    let newLine = separator;
+  [].__proto__.join2 = function(separator = ',') {
+    const newLine = separator;
     let newPhrase = '';
-
-    if (newLine === undefined) {
-      newLine = ',';
-    }
 
     for (let i = 0; i < this.length; i++) {
       if (i > 0) {
         newPhrase += newLine;
       }
 
-      if (this[i] === null || this[i] === undefined) {
-        newPhrase += '';
-      } else {
+      if (this[i] !== null && this[i] !== undefined) {
         newPhrase += this[i];
       }
     }
