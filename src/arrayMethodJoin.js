f52148da75466c41cb0cbc40427ca607534813ee
Volodymyr Shostak <shostak_1@proton.me>
2023-05-23 18:32:58 +0300

    add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,9 +3,28 @@
 /**
  * Implement method join
  */
+
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let str = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this.length - 1 === i) {
+        if (this[i] !== undefined) {
+          str += this[i];
+        }
+      } else if (this[i] === null || this[i] === undefined) {
+        str += separator;
+      } else if (separator === null) {
+        str += this[i] + 'null';
+      } else if (separator === undefined) {
+        str += this[i] + ',';
+      } else {
+        str += this[i] + separator;
+      }
+    }
+
+    return str;
   };
 }
 
