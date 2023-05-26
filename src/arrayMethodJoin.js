38a93b8769cf2ad3ffff0acc5d79d57b1e5623e4
Amadeusz Lisiecki <amadeusz.lisiecki.1993@gmail.com>
2023-05-26 08:36:33 +0200

    Add join2

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,26 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    if (separator === undefined) {
+      return this.join2(',');
+    }
+
+    let joinedString = '';
+    const arrayLength = this.length;
+
+    for (let e = 0; e < arrayLength; e++) {
+      const arrayElement = this[e];
+
+      if (arrayElement !== null && arrayElement !== undefined) {
+        joinedString += arrayElement;
+      }
+
+      if (e !== arrayLength - 1) {
+        joinedString += separator;
+      }
+    }
+
+    return joinedString;
   };
 }
 
