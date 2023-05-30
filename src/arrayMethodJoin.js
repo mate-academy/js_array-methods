65db2e7ace50a448b475c1c6d25d52e54b449910
Iryna Polchaninova <ira.polchanin@gmail.com>
2023-05-30 13:27:31 +0300

    Solution changed condition

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -12,7 +12,7 @@ function applyCustomJoin() {
         result += this[i];
       }
 
-      while (i < (this.length - 1)) {
+      if (i < (this.length - 1)) {
         result += separator;
       }
     }
