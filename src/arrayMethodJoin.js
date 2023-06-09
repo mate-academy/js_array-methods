7b6471d9e76f9864671b9c26d8937919a352f4f5
MaxSchmide <kuznetsov.max.v@gmail.com>
2023-06-09 13:32:11 +0300

    add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,27 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = this[0] === null ? '' : `${this[0]}`;
+
+    const separate = separator === null ? 'null' : separator;
+
+    if (!this.length) {
+      return '';
+    } else if (this.length === 1) {
+      return result;
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      let element = this[i];
+
+      if (this[i] === null || this[i] === undefined) {
+        element = '';
+      }
+      result += separate + element;
+    }
+
+    return result;
   };
 }
 
