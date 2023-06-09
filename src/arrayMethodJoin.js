118b9477aa52581dedba5fbb42a48305c3b5303a
Yuliia-Hrinchenko1 <yulya9801@gmail.com>
2023-06-09 20:14:07 +0300

    Join was created

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,32 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let separatorMeaning;
+
+    if (separator === undefined) {
+      separatorMeaning = ',';
+    } else if (separator === null) {
+      separatorMeaning = 'null';
+    } else if (typeof separator === 'object') {
+      separatorMeaning = separator.toString();
+    } else {
+      separatorMeaning = String(separator);
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        result += '';
+      } else {
+        result += String(this[i]);
+      }
+
+      if (i !== this.length - 1) {
+        result += separatorMeaning;
+      }
+    }
+
+    return result;
   };
 }
 
