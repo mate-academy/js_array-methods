8386be5386a98c1c93d9bdeebda93e5896279c5f
Dima Astapenko <bonum_taurus@ukr.net>
2023-06-09 13:09:57 +0300

    task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,40 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let string = '';
+    let delimiter = separator;
+    const exceptions = [null, undefined];
+
+    if (delimiter === undefined) {
+      delimiter = ',';
+    }
+
+    if (delimiter === null) {
+      delimiter = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === this[this.length - 1] && this[i] === undefined) {
+        return string;
+      }
+
+      if (exceptions.includes(this[i])) {
+        string += delimiter;
+        continue;
+      }
+
+      if (this[i] === this[this.length - 1]) {
+        string += this[i];
+
+        return string;
+      }
+
+      const elem = this[i] + delimiter;
+
+      string += elem;
+    }
+
+    return string;
   };
 }
 
