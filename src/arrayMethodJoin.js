87e967b9afe072bc2446d622e125f453af62e841
Ruslan Melnyk <ruslan_melnyk@ukr.net>
2023-06-09 19:21:20 +0300

    Solution MR

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const arr = [...this];
+    let str = arr[0] === null ? '' : arr[0];
+
+    if (!arr.length) {
+      return '';
+    }
+
+    if (arr.length === 1) {
+      return `${str}`;
+    }
+
+    for (let index = 1; index < arr.length; index++) {
+      if (arr[index] === undefined) {
+        str += `${separator}`;
+      } else if (arr[index] === 0 || isNaN(arr[index])) {
+        str += `${separator}${arr[index]}`;
+      } else {
+        str += `${separator}${arr[index] || ''}`;
+      }
+    }
+
+    return str;
   };
 }
 
