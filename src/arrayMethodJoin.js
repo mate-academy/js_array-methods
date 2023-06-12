397931d122419204eabd52f6010e868258b93ff6
Iryna <irinabolbat98@gmail.com>
2023-06-12 15:50:36 +0300

    add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,30 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length === 1) {
+      return this.toString();
+    }
+
+    let sep = separator;
+
+    if (sep === null) {
+      sep = 'null';
+    }
+
+    let str = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const sepCurrent = i === this.length - 1 ? '' : sep;
+
+      if (this[i] === null || this[i] === undefined) {
+        str += '' + sepCurrent;
+      } else {
+        str += this[i] + sepCurrent;
+      }
+    }
+
+    return str;
   };
 }
 
