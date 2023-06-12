8fd16a1682454f05d7940c7c226bc6f20cf7e2d3
MaxSchmide <kuznetsov.max.v@gmail.com>
2023-06-12 12:04:34 +0300

    fixes after code review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,16 +7,14 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let resultString = '';
 
-    const separate = `${separator}`;
-
     for (let i = 0; i < this.length; i++) {
       const element = this[i] === null || this[i] === undefined
         ? ''
         : this[i];
 
-      i === this.length - 1
-        ? resultString += element
-        : resultString += element + separate;
+      resultString += i === this.length - 1
+        ? `${element}`
+        : `${element}${separator}`;
     }
 
     return resultString;
