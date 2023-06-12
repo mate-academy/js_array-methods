beb1528413d2fc7f73a669280c97d7a4a791fe01
GGsmou <svyatoslav.golovin.2005@gmail.com>
2023-06-12 17:48:54 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,18 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let returnString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      returnString += this[i] === undefined || this[i] === null ? '' : this[i];
+
+      if (i !== this.length - 1) {
+        returnString += separator;
+      }
+    }
+
+    return returnString;
   };
 }
 
