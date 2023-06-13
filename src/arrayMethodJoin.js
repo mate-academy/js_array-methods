899118e0f0ef1a4e905371557ac4579d3d5b5f66
Daniel Linker <danlinker17@gmail.com>
2023-06-13 21:25:50 +0200

    if sts

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,9 +8,7 @@ function applyCustomJoin() {
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        result += '';
-      } else {
+      if (this[i] !== null && this[i] !== undefined) {
         result += this[i];
       }
 
