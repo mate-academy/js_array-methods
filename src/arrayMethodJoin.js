a6c76622b46175d5ec63924d60ab8bf3c22b3f51
artkas03 <zhonayer2017@gmail.com>
2023-06-13 06:46:28 +0300

    finished the task

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,35 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let resultString = '';
+    let skipSeparator = true;
+
+    for (const item of this) {
+      let newPart = '';
+
+      if (skipSeparator) {
+        newPart += nextElement(item);
+        skipSeparator = false;
+      } else {
+        newPart += separator + nextElement(item);
+      }
+
+      resultString += newPart;
+    }
+
+    return resultString;
   };
+}
+
+function nextElement(item) {
+  let resultItem = item;
+
+  if ([null, undefined].includes(resultItem)) {
+    resultItem = '';
+  }
+
+  return resultItem + '';
 }
 
 module.exports = applyCustomJoin;
