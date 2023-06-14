3b8de37babe2f42d3953c1dc3948caadf98f8ec6
Vlad Makohon <98763083+vladmakohon@users.noreply.github.com>
2023-06-14 20:50:16 +0300

    Add suggested changes

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,29 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let resultStr = '';
-    let normalizedSeparator;
 
-    if (separator === undefined) {
-      normalizedSeparator = ',';
-    } else {
-      normalizedSeparator = String(separator);
-    }
+    for (let i = 0; i < this.length; i++) {
+      let value = this[i];
 
-    for (let value of this) {
       if (value === null || value === undefined) {
         value = '';
       }
 
-      resultStr += String(value) + normalizedSeparator;
+      resultStr += value;
+
+      if (i !== this.length - 1) {
+        resultStr += separator;
+      }
     }
 
-    if (normalizedSeparator === '') {
-      return resultStr;
-    }
-
-    return resultStr.slice(0, -normalizedSeparator.length);
+    return resultStr;
   };
 }
 
