555c3ee9ac02bfe9ef227e6a153b57d524b7dddd
Yevhen <ko4etov.evgeniy@gmail.com>
2023-06-15 00:54:24 +0300

    create join metod

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,29 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let strFromArr = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const lastCheckSeparator = this[this.length - 1] === undefined
+      || this[this.length - 1] === null;
+
+      if (this[i] === null || this[i] === undefined) {
+        if (lastCheckSeparator) {
+          continue;
+        }
+        strFromArr += separator;
+        continue;
+      }
+
+      if (i === this.length - 1) {
+        strFromArr += `${this[i]}`;
+        continue;
+      }
+      strFromArr += `${this[i]}${separator}`;
+    }
+
+    return strFromArr;
   };
 }
 
