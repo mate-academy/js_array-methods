e0cb22744699274572c990d0adafcf5b4634a855
Ihor Oksentiuk <winbka.lem@gmail.com>
2023-06-14 19:02:05 +0300

    Finished task

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const newArray = [];
+    let str = '';
+    let divider = separator;
+
+    if (separator === null) {
+      divider = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        newArray[i] = '';
+      } else {
+        newArray[i] = this[i];
+      }
+      str += newArray[i] + (i !== this.length - 1 ? divider : '');
+    }
+
+    return str;
   };
 }
 
