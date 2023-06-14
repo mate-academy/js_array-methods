16afe9c36803be7f2951f0443a559df9cf7f987e
Vlad Makohon <98763083+vladmakohon@users.noreply.github.com>
2023-06-14 17:18:00 +0300

    Add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,28 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let resultStr = '';
+    let normalizedSeparator;
+
+    if (separator === undefined) {
+      normalizedSeparator = ',';
+    } else {
+      normalizedSeparator = String(separator);
+    }
+
+    for (let value of this) {
+      if (value === null || value === undefined) {
+        value = '';
+      }
+
+      resultStr += String(value) + normalizedSeparator;
+    }
+
+    if (normalizedSeparator === '') {
+      return resultStr;
+    }
+
+    return resultStr.slice(0, -normalizedSeparator.length);
   };
 }
 
