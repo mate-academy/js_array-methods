09be226d46eb06808adb8620bd1269f292acdb93
AnastasiiaKhemych <anastasiia.khemych@gmail.com>
2023-06-14 14:47:46 +0300

    add task method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      let item = this[i];
+
+      if (item === null || item === undefined) {
+        item = '';
+      }
+
+      if (i === 0) {
+        result += `${item}`;
+      } else {
+        result += `${separator}${item}`;
+      }
+    }
+
+    return result;
   };
 }
 
