dabbd25a02a368ccc4a08ca97741d6d74e9680c3
Anton Baranskyi <antonbaranskij12@gmail.com>
2023-06-14 19:34:35 +0300

    Task done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,23 @@
-'use strict';
+/*eslint-disable */
+"use strict";
 
 /**
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function (separator = ",") {
+    let separatedArr = "";
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        separatedArr += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        separatedArr += separator;
+      }
+    }
+    return separatedArr;
   };
 }
 
