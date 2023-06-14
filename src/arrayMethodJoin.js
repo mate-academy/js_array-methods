040c3bbe3c649aca5839933c525f085670d632a3
Ihor Oksentiuk <winbka.lem@gmail.com>
2023-06-14 19:13:28 +0300

    Fixed task, removed copy of array, removed optional if statements

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,21 +5,10 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    const newArray = [];
     let str = '';
-    let divider = separator;
-
-    if (separator === null) {
-      divider = 'null';
-    }
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        newArray[i] = '';
-      } else {
-        newArray[i] = this[i];
-      }
-      str += newArray[i] + (i !== this.length - 1 ? divider : '');
+      str += String(this[i]) + (i !== this.length - 1 ? separator : '');
     }
 
     return str;
