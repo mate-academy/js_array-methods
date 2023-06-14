6821262863084930f3092604e592eda356659a32
outofs <rtranscendental@gmail.com>
2023-06-14 14:58:23 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,30 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let correctSeparator = separator;
+
+    if (separator === null) {
+      correctSeparator = 'null';
+    }
+
+    let newString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      let element = this[i];
+
+      if (element === undefined || element === null) {
+        element = '';
+      }
+
+      if (i === this.length - 1) {
+        newString += element;
+      } else {
+        newString += element + correctSeparator;
+      }
+    }
+
+    return newString;
   };
 }
 
