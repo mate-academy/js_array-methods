8b839d387f1820cf75d8614e9580046a210ef206
Ivan Bilotserkivskiy <ivanbilotserkivskiy56@gmail.com>
2023-06-14 14:00:37 +0300

    arrayMethodJoin solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,26 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let stringFromArray = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1 && typeof this[i] !== 'undefined') {
+        stringFromArray += `${this[i]}`;
+
+        return stringFromArray;
+      } else if (i === this.length - 1) {
+        return stringFromArray;
+      }
+
+      if (typeof this[i] !== 'undefined' && typeof this[i] !== 'object') {
+        stringFromArray += `${this[i]}${separator}`;
+        continue;
+      }
+      stringFromArray += `${separator}`;
+    }
+
+    return stringFromArray;
   };
 }
 
