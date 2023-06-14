712474387af25c90e11ccef5566a20386f2fadb9
Nataliia Hrushanyk <nataligrushanyk@gmail.com>
2023-06-14 13:47:24 +0300

    Method Join solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,20 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const indexOfLastElem = this.length - 1;
+    let joinResult = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const element = this[i];
+      const lastElement = (i === indexOfLastElem);
+      const invalidEl = (element === null || element === undefined);
+
+      joinResult += invalidEl ? '' : element;
+      joinResult += lastElement ? '' : separator;
+    }
+
+    return joinResult;
   };
 }
 
