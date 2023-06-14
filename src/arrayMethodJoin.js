6899fe5658ba88ffe78ea6370af4336e493f845e
Artem Andrushchenko <artem.abdrushenko213654@gmail.com>
2023-06-14 15:42:26 +0300

    add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,25 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let finalSeparator = separator;
+
+    if (finalSeparator === undefined) {
+      finalSeparator = ',';
+    }
+
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (i !== 0) {
+        result += finalSeparator;
+      }
+
+      if (this[i] !== null && this[i] !== undefined) {
+        result += this[i];
+      }
+    }
+
+    return result;
   };
 }
 
