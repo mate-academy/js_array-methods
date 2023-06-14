1c639cca326afbc1d472e6b9500abaf2a4845e6a
Nataliia Hrushanyk <nataligrushanyk@gmail.com>
2023-06-14 20:07:16 +0300

    Changed names

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    const indexOfLastElem = this.length - 1;
-    let joinResult = '';
+    const lastIndex = this.length - 1;
+    let joinedResult = '';
 
     for (let i = 0; i < this.length; i++) {
       const element = this[i];
-      const lastElement = (i === indexOfLastElem);
-      const invalidEl = (element === null || element === undefined);
+      const isLastIndex = (i === lastIndex);
+      const isInvalidElement = (element === null || element === undefined);
 
-      joinResult += invalidEl ? '' : element;
-      joinResult += lastElement ? '' : separator;
+      joinedResult += isInvalidElement ? '' : element;
+      joinedResult += isLastIndex ? '' : separator;
     }
 
-    return joinResult;
+    return joinedResult;
   };
 }
 
