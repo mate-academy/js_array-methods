8b577100f57ed5fdfff60861a93f87b7cd8eee24
outofs <rtranscendental@gmail.com>
2023-06-15 15:01:10 +0300

    simplifying solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,30 +5,21 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let correctSeparator = separator;
-
-    if (separator === null) {
-      correctSeparator = 'null';
-    }
-
     let newString = '';
 
     for (let i = 0; i < this.length; i++) {
-      let element = this[i];
+      const element = this[i];
 
-      if (element === undefined || element === null) {
-        element = '';
+      if (i !== 0) {
+        newString += separator;
       }
 
-      if (i === this.length - 1) {
+      if (element !== null && element !== undefined) {
         newString += element;
-      } else {
-        newString += element + correctSeparator;
       }
     }
 
     return newString;
   };
 }
-
 module.exports = applyCustomJoin;
