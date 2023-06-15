a2cb01af74e9162fb3beedda0e4465a4d24de85f
Serhii <gashysh@protonmail.com>
2023-06-15 23:47:59 +0300

    made code more readable

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,15 +7,14 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
 
-    if (this.length < 1) {
-      return '';
-    }
-
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === undefined || this[i] === null) {
-        i === 0 ? result += '' : result += `${separator}`;
-      } else {
-        i === 0 ? result += `${this[i]}` : result += `${separator}${this[i]}`;
+      if (this[i] !== undefined && this[i] !== null) {
+        result += `${this[i]}`;
+      }
+
+      if (i !== this.length - 1) {
+        result += `${separator}`;
+        continue;
       }
     }
 
