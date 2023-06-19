a2973e55a22189017d599fecea2db9d4a8336c74
Diana Pinchuk <diana9360@gmail.com>
2023-06-19 12:19:37 +0300

    fixed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,30 +3,20 @@
 'use strict';
 
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    let addition = separator;
-
-    if (arguments.length === 0 || separator === undefined) {
-      addition = ',';
-    }
-
-    if (separator === null) {
-      addition = 'null';
-    }
-
-    let str = '';
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedString = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
-        str += this[i];
+        joinedString += this[i];
       }
 
       if (i !== this.length - 1) {
-        str += addition;
+        joinedString += separator;
       }
     }
 
-    return str;
+    return joinedString;
   };
 }
 
