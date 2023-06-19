99f458b8df9affd2685f539f7e931e91de779db2
zipic <zpc23041988@gmail.com>
2023-06-19 12:11:10 +0300

    join task

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,27 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let separatorElement = separator;
+
+    if (separatorElement === undefined) {
+      separatorElement = ',';
+    }
+
+    const copyArray = [...this];
+    let result = '';
+
+    for (let i = 0; i < copyArray.length; i++) {
+      if (copyArray[i] === undefined || copyArray[i] === null) {
+        copyArray[i] = '';
+      }
+      result += copyArray[i];
+
+      if (i !== copyArray.length - 1) {
+        result += separatorElement;
+      }
+    }
+
+    return result;
   };
 }
 
