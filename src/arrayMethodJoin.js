f4b81ea4f6aada0cc3fbf548790bb7764f8c5b69
unknown <fanton81@gmail.com>
2023-06-20 17:12:56 +0300

    mySolution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,27 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    let separatorSign = separator;
+
+    if (separator === undefined) {
+      separatorSign = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      let item = this[i];
+
+      if (item === undefined || item === null) {
+        item = '';
+      }
+      result += item;
+
+      if (i !== this.length - 1) {
+        result += separatorSign;
+      }
+    }
+
+    return result;
   };
 }
 
