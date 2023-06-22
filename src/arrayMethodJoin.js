a2cd7f20ca2e56796a51f8459d8d06275201f501
Lukasz Warda <lukaszwarda1989@gmail.com>
2023-06-22 12:55:20 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,33 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = String(this[0]);
+    let separatorVariable = separator;
+
+    if (this[0] === undefined || this[0] === null) {
+      result = '';
+    }
+
+    if (this.length === 0) {
+      result = '';
+
+      return result;
+    }
+
+    if (separator === undefined) {
+      separatorVariable = ',';
+    }
+
+    const thisCopy = [...this];
+
+    for (let i = 1; i < this.length; i++) {
+      thisCopy[this.indexOf(undefined, i)] = '';
+      thisCopy[this.indexOf(null, i)] = '';
+
+      result += String(separatorVariable) + (thisCopy[i]);
+    }
+
+    return result;
   };
 }
 
