ccdb82e079ff5041197e1b132f382c905de6c3c5
Anton <tony.tarovyk@gmail.com>
2023-06-27 10:52:14 +0300

    add solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,27 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+    const length = this.length;
+    const lastElementIndex = length - 1;
+
+    for (let i = 0; i < length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        result += '';
+      } else {
+        result += this[i];
+      }
+
+      if (i !== lastElementIndex) {
+        if (separator === undefined) {
+          result += ',';
+        } else {
+          result += separator;
+        }
+      }
+    }
+
+    return result;
   };
 }
 
