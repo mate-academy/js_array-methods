89fe53c86be8f736d7f2cf30819af6303a6463c5
Sergey Arnautov <sirius9312@ukr.net>
2023-06-28 13:16:27 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,81 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  let result = '';
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
 
-  [].__proto__.join2 = function(separator) {
-    if (this.length === 0) {
-      return '';
-    } else if (this.length === 1) {
-      return this[0].toString();
-    }
-
-    switch (String(separator)) {
-      case '':
-        result = '';
-
-        for (const ch of this) {
-          result += (ch + separator);
-        } break;
-
-      case '-':
-        result = '';
-
-        for (let i = 0; i < this.length; i++) {
+    if (separator === null) {
+      for (let i = 0; i < this.length; i++) {
+        result += (i !== this.length - 1)
+          ? this[i] + String(separator) : this[i];
+      }
+    } else {
+      for (let i = 0; i < this.length; i++) {
+        if (this[i] !== null && this[i] !== undefined) {
           result += (i !== this.length - 1)
             ? this[i] + separator : this[i];
-        } break;
-
-      case ' ':
-        result = '';
-
-        for (let i = 0; i < this.length; i++) {
+        } else {
           result += (i !== this.length - 1)
-            ? this[i] + separator : this[i];
-        } break;
-
-      case String(null):
-        result = '';
-
-        for (let i = 0; i < this.length; i++) {
-          result += (i !== this.length - 1)
-            ? this[i] + String(separator) : this[i];
-        } break;
-
-      case String(undefined):
-        result = '';
-
-        for (let i = 0; i < this.length; i++) {
-          result += (i !== this.length - 1)
-            ? this[i] + ',' : this[i];
-        } break;
-
-      case '[object Object]':
-        result = '';
-
-        for (let i = 0; i < this.length; i++) {
-          result += (i !== this.length - 1)
-            ? this[i] + separator : this[i];
-        } break;
-
-      case ',':
-        result = '';
-
-        for (let i = 0; i < this.length; i++) {
-          if (this[i] !== null && this[i] !== undefined) {
-            result += (i !== this.length - 1)
-              ? this[i] + separator : this[i];
-          } else {
-            result += (i !== this.length - 1) ? '' + separator : '';
-          }
+            ? '' + separator : '';
         }
-        break;
+      }
     }
 
     return result;
   };
-
-  // arr.join2('-');
 };
 
 module.exports = applyCustomJoin;
