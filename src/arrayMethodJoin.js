7c6554f9d1fbb54ec00fa41bb511901d21c07b69
Faridun Mainaev <farid.mainaiev@gmail.com>
2023-06-29 19:29:59 +0300

    first commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,19 +4,15 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    const finalSeparator = separator !== undefined ? separator : ',';
-
+  [].__proto__.join2 = function(separator = ',') {
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
       if (i > 0) {
-        result += finalSeparator;
+        result += separator;
       }
 
-      if (this[i] === null || this[i] === undefined) {
-        result += '';
-      } else {
+      if (this[i] !== null && this[i] !== undefined) {
         result += this[i];
       }
     }
