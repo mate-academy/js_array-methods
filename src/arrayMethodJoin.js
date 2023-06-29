b248a7a8979d521d9a387508e3cf4afb23a778e7
Vitalii Hovenko <vet.hov.main@gmail.com>
2023-06-29 13:53:25 +0300

    add join method solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let separatedString = '';
+    const lastIndex = this.length - 1;
+
+    for (let i = 0; i < this.length; i++) {
+      let temp = this[i];
+
+      if (this[i] === null || this[i] === undefined) {
+        temp = '';
+      }
+
+      if (i !== lastIndex) {
+        separatedString += temp + String(separator);
+      } else {
+        separatedString += temp;
+      }
+    }
+
+    return separatedString;
   };
 }
 
