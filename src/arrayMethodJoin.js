4c17a33182251330564af6465ff5a2eb306a9e9a
George Savostikov <georgesavostikov@gmail.com>
2023-07-01 17:02:40 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,33 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let complete = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        complete += '';
+
+        if (i < this.length - 1) {
+          if (separator === undefined) {
+            complete += ',';
+          } else {
+            complete += separator;
+          }
+        }
+      } else {
+        complete += this[i];
+
+        if (i < this.length - 1) {
+          if (separator === undefined) {
+            complete += ',';
+          } else {
+            complete += separator;
+          }
+        }
+      }
+    }
+
+    return complete;
   };
 }
 
