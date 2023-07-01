12252d1e826470d69362d19c50a5e2c4edd81adc
NataliMax <maximovs@1876gmail.com>
2023-07-01 12:13:33 +0200

    ssolution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,46 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+
+    switch (this[0]) {
+      case null:
+        break;
+
+      case undefined:
+        return result;
+
+      default:
+        result += this[0];
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      switch (separator) {
+        case '':
+          result += this[i];
+          break;
+
+        case null:
+          result += 'null';
+          result += this[i];
+          break;
+
+        case (undefined):
+          result += ',';
+          result += this[i];
+          break;
+
+        default:
+          if (this[i] === null || this[i] === undefined) {
+            result += separator;
+            break;
+          }
+          result += separator;
+          result += this[i];
+      }
+    }
+
+    return result;
   };
 }
 
