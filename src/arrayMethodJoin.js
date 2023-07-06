1c474987cc3ec34b284113920d4ab25e6c62ddcd
Orchibald <orestkozak17@gmail.com>
2023-07-06 14:49:10 +0200

    method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,24 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    if (this.length === 0) {
+      return '';
+    }
+
+    const sep = separator === undefined ? ',' : String(separator);
+
+    let result = this[0] === null ? '' : String(this[0]);
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] === undefined
+        || this[i] === null) {
+        result += sep;
+      } else {
+        result += sep + String(this[i]);
+      }
+    }
+
+    return result;
   };
 }
 
