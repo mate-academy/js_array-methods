c90ac0898dd173e704494c843c23684d1132113e
Dmytro Chadov <Dmytrochd@gmail.com>
2023-07-12 19:06:16 +0300

    add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedLine = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const element = this[i];
+
+      if (i > 0) {
+        joinedLine += separator;
+      }
+
+      if (element !== null && element !== undefined) {
+        joinedLine += String(element);
+      }
+    }
+
+    return joinedLine;
   };
 }
 
