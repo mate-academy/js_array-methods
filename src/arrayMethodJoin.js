509dbd48c7987c90f77eaa40df0ccb99954b07b7
Serhii Dranuta <sirojka8@gmail.com>
2023-07-12 21:21:58 +0300

    Fix unnecessary separator checks

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,28 +7,13 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let resultString = '';
 
-    if (this.length === 0) {
-      return resultString;
-    }
-
-    if (this.length === 1) {
-      resultString += this[0];
-
-      return resultString;
-    }
-
     for (let i = 0; i < this.length; i++) {
-      if (separator === '') {
+      if (this[i] !== null && this[i] !== undefined) {
         resultString += this[i];
-      } else {
-        let currentElement = this[i];
+      }
 
-        if (currentElement === null || currentElement === undefined) {
-          currentElement = '';
-        }
-
-        resultString += currentElement
-        + `${this[i] !== this[this.length - 1] ? separator : ''}`;
+      if (i < this.length - 1) {
+        resultString += separator;
       }
     }
 
