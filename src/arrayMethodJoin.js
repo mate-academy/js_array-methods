7a1a20614ab5b7bfd3852cf865e7da2a0ebb4831
andrew gidrovich <mr.androvich55555@gmail.com>
2023-07-16 17:14:09 +0300

    add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -3,10 +3,21 @@
 /**
  * Implement method join
  */
-function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
-  };
-}
+function applyCustomJoin(arr, separator = ',') {
+  let result = '';
 
+  for (let i = 0; i < arr.length; i++) {
+    const currentElement = arr[i];
+
+    if (i !== 0) {
+      result += separator;
+    }
+
+    if (currentElement != null) {
+      result += currentElement;
+    }
+  }
+
+  return result;
+}
 module.exports = applyCustomJoin;
