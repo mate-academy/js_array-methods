e01028a6411a4475073d2f85817c3f58e3407afa
Roman Bilichenko <romabilichenko1@gmail.com>
2023-07-17 16:24:51 +0300

    added a check for empty strings

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,7 +8,7 @@ function applyCustomJoin() {
     let res = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] !== undefined && this[i] !== null) {
+      if (this[i] !== undefined && this[i] !== null && this[i] !== '') {
         res += this[i];
       }
 
