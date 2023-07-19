754c886012bf2b5c5def7d2513eb60d915767d8c
Oleksandr Shtonda <oleksandrshtonda@gmail.com>
2023-07-19 08:47:27 +0200

    implement Array.prototype.join method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,29 @@
 'use strict';
 
-/**
- * Implement method join
- */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length === 0) {
+      return '';
+    }
+
+    let resultString = '';
+    let firstSymbol = true;
+
+    for (let charachter of this) {
+      if (charachter === null || charachter === undefined) {
+        charachter = '';
+      }
+
+      if (firstSymbol) {
+        resultString += charachter;
+        firstSymbol = false;
+        continue;
+      }
+
+      resultString += `${separator}` + charachter;
+    }
+
+    return resultString;
   };
 }
 
