362db24055d4b6b669dcc4370318332df59810b9
Ivan Baranovskyi <baranovskyiivan@gmail.com>
2023-07-20 18:14:43 +0200

    made method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,30 @@
+/* eslint-disable no-param-reassign */
 'use strict';
 
 /**
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const result = this.slice();
+
+    let joinedString = '';
+
+    for (let i = 0; i < result.length; i++) {
+      const value = result[i];
+
+      if (value === null || value === undefined) {
+        joinedString += '';
+      } else {
+        joinedString += value;
+      }
+
+      if (i < result.length - 1) {
+        joinedString += separator;
+      }
+    }
+
+    return joinedString;
   };
 }
 
