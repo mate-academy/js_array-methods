e76149722cc8d9d93430263c0e0b3a40be6651db
Olena Shapkun <shapkunkh@gmail.com>
2023-07-20 16:23:21 +0300

    code fixing

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,24 +4,18 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    const actualSeparator = separator === undefined ? ',' : separator;
-
+  [].__proto__.join2 = function(separator = ',') {
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      const element = this[i];
-
-      if (i > 0) {
-        result += actualSeparator;
+      if (this[i] !== undefined && this[i] !== null) {
+        result += this[i];
       }
 
-      if (element === null || element === undefined) {
-        result += '';
-      } else {
-        result += element.toString();
+      if (i !== this.length - 1) {
+        result += separator;
       }
-    }
+    };
 
     return result;
   };
