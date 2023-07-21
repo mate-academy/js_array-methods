2c7aeabfc8a2fa0f28cdeeed2b3b51e255191f49
Albert Kifiuk <albertkifiuk@gmail.com>
2023-07-21 15:26:02 +0300

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,20 +5,16 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let count = 0;
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] !== null
-        && this[i] !== undefined) {
+      if (this[i] !== null && this[i] !== undefined) {
         result += this[i];
       }
 
-      if (count !== this.length - 1) {
+      if (i !== this.length - 1) {
         result += separator;
       }
-
-      count++;
     }
 
     return result;
