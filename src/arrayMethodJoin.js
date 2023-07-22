b67c868aa77c2eaa8090d00409763e61a4477df4
4xmplme <andreykav2002@gmail.com>
2023-07-22 17:52:40 +0300

    add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    const lastIndex = this.length - 1;
+
+    for (let i = 0; i < lastIndex; i++) {
+      if (isValid(this[i])) {
+        result += this[i];
+      }
+
+      result += separator;
+    }
+
+    if (isValid(this[lastIndex])) {
+      result += this[lastIndex];
+    }
+
+    return result;
   };
+}
+
+function isValid(value) {
+  return value !== null && value !== undefined;
 }
 
 module.exports = applyCustomJoin;
