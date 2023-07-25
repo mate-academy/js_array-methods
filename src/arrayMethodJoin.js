2fecfb0811eab1ecaa3e854443baccf58710f0c7
Anna Shevtsova <aniashevtsova@gmail.com>
2023-07-25 12:08:31 +0200

    comments adressed

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,22 +6,15 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    const validElementsList = [];
 
     for (let i = 0; i < this.length; i++) {
       const element = this[i];
 
       if (element !== null && element !== undefined) {
-        validElementsList.push(element);
-      } else {
-        validElementsList.push('');
+        result += element;
       }
-    }
 
-    for (let i = 0; i < validElementsList.length; i++) {
-      result += validElementsList[i];
-
-      if (i < validElementsList.length - 1) {
+      if (i < this.length - 1) {
         result += separator;
       }
     }
