7ff2d584ae4f7d1b51ca943db536d1c507693fbc
Anna Shevtsova <aniashevtsova@gmail.com>
2023-07-25 11:16:10 +0200

    all tests pass

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,9 +4,30 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    const validElementsList = [];
+
+    for (let i = 0; i < this.length; i++) {
+      const element = this[i];
+
+      if (element !== null && element !== undefined) {
+        validElementsList.push(element);
+      } else {
+        validElementsList.push('');
+      }
+    }
+
+    for (let i = 0; i < validElementsList.length; i++) {
+      result += validElementsList[i];
+
+      if (i < validElementsList.length - 1) {
+        result += separator;
+      }
+    }
+
+    return result;
   };
-}
+};
 
 module.exports = applyCustomJoin;
