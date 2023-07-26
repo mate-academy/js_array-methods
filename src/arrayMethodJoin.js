f6b601874dba9055cb5866a2941786f24f4caace
Olena Matsyshyna <helenmarth@Olenas-MacBook-Air.local>
2023-07-26 12:12:56 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,23 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this.length === 1) {
+        return '1';
+      }
+
+      if (this[i] !== undefined && this[i] !== null) {
+        result += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        result += separator === undefined ? ',' : separator;
+      }
+    }
+
+    return result;
   };
 }
 
