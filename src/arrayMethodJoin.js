cdeb65cb382e3518a7ff096e7197a0d304d0b5e7
kabaktech <kabacznikmobile@gmail.com>
2023-07-26 16:36:45 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,11 +1,24 @@
 'use strict';
 
-/**
- * Implement method join
- */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length === 0) {
+      return '';
+    }
+
+    let joinedString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        joinedString += String(this[i]);
+      }
+
+      if (i < this.length - 1) {
+        joinedString += separator;
+      }
+    }
+
+    return joinedString;
   };
 }
 
