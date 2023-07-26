72182ff0518ae5a3c5468c4639158eba5c8d0af9
Marzena Sówka <marzena.sowka.pl@gmail.com>
2023-07-26 20:21:28 +0200

    add custom join method

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,21 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    const sep = typeof separator !== 'string' ? `${separator}` : separator;
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        result += '';
+      } else {
+        result += typeof this[i] !== 'string' ? `${this[i]}` : this[i];
+      }
+
+      result += i !== (this.length - 1) ? sep : '';
+    }
+
+    return result;
   };
 }
 
