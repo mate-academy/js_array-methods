53b424592b1a4dfdd619b4c6c4895ffcaa539e63
Marzena Sówka <marzena.sowka.pl@gmail.com>
2023-07-26 20:37:19 +0200

    code refactoring

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,13 +6,11 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    const sep = typeof separator !== 'string' ? `${separator}` : separator;
+    const sep = String(separator);
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        result += '';
-      } else {
-        result += typeof this[i] !== 'string' ? `${this[i]}` : this[i];
+      if (this[i] !== null && this[i] !== undefined) {
+        result += String(this[i]);
       }
 
       result += i !== (this.length - 1) ? sep : '';
