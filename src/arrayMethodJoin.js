e1febb3370b7947d4a3f9fadda6fd2a78fea6793
Matylda Lewandowska <matylda.lewandowska111@gmail.com>
2023-07-27 15:18:28 +0200

    Task Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === 0 && (this[i] === null || this[i] === undefined)) {
+        continue;
+      } else if (this[i] === null || this[i] === undefined) {
+        joinedString += `${separator}`;
+      } else if (i === 0) {
+        joinedString = `${this[0]}`;
+      } else {
+        joinedString += `${separator}` + `${this[i]}`;
+      }
+    }
+
+    return joinedString;
   };
 }
 
