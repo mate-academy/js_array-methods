248c184dcfa0044c2d81d26e493fc61533627495
Hania Nassalska <hanna.nassalska@gmail.com>
2023-07-27 14:51:20 +0200

    changes after review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,19 +4,16 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let result = '';
-    const resultSeparator = separator !== undefined ? `${separator}` : ',';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] === null || this[i] === undefined) {
-        (i === this.length - 1)
-          ? result += ''
-          : result += '' + resultSeparator;
-      } else {
-        (i === this.length - 1)
-          ? result += this[i]
-          : result += this[i] + resultSeparator;
+      if (this[i] !== null && this[i] !== undefined) {
+        result += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        result += separator;
       }
     };
 
