dd944f7d5b31c9e4142f57fd742cc671f591e79d
Andrzej Pietkiewicz <pietan@me.com>
2023-07-28 15:47:20 +0200

    fixed to comments

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,36 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
+  [].__proto__.join2 = function(separator = ',') {
     let result = '';
 
     if (this.length === 0) {
-      return '';
+      return result;
     }
 
-    if (this.length === 0) {
-      return '';
-    }
-
-    if (separator === undefined && this.length >= 2) {
-      for (let i = 1; i < this.length; i++) {
-        result += `,${this[i]}`;
-      }
-    } else if (this.length >= 2) {
-      for (let i = 1; i < this.length; i++) {
-        if (this[i] === null || this[i] === undefined) {
-          result += ',';
-        } else {
-          result += `${separator}${this[i]}`;
-        }
-      }
-    }
-
-    if (this[0] === null) {
-      return `${result}`;
+    if (this[0] !== undefined && this[0] !== null) {
+      result += `${this[0]}`;
     } else {
-      return `${this[0]}${result}`;
-    };
+      result += '';
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        result += `${separator}${this[i]}`;
+      } else {
+        result += ',';
+      }
+    }
+
+    return result;
   };
 };
 
