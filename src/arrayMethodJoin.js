21ad31a31b919a873cfd25d2eab02be9a586b92c
Kamil <majcherczak.kamil@protonmail.com>
2023-07-28 10:14:15 +0200

    improved solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,20 +8,13 @@ function applyCustomJoin() {
     let newString = '';
 
     for (let i = 0; i < this.length; i++) {
-      if ((this[i] === null || this[i] === undefined) && i === 0) {
-        continue;
-      }
-
-      if (this[i] === null || this[i] === undefined) {
-        newString += separator;
-        continue;
-      }
-
-      if (i === 0) {
+      if (this[i] !== null && this[i] !== undefined) {
         newString += this[i];
-        continue;
       }
-      newString += `${separator}${this[i]}`;
+
+      if (i < this.length - 1) {
+        newString += separator;
+      }
     }
 
     return newString;
