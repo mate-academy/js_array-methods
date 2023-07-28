27e41bcc03e087d168b50954b861871dd1a73d9a
Max Oleksyuk <maksoleksyuk@gmail.com>
2023-07-28 15:26:14 +0300

    We can join without join()

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,31 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const currentLength = this.length;
+
+    if(currentLength === 0) {
+
+      return '';
+    }
+
+    let str = '';
+
+    for (let i = 0; i < currentLength; i++) {
+
+      if(this[i] === null || this[i] === undefined || this[i] === '') {
+        if (i < this.length - 1) {
+          str += separator
+        }
+        continue;
+      } else {
+        str += this[i];
+        str += i < this.length -1 ? separator : ''
+      }
+
+    }
+
+    return str
   };
 }
 
