9af66a9b633d1da34622823d14d81fdeb8d03c31
Krzysztof Janiewicz <spojrzenie@gmail.com>
2023-07-29 15:42:50 +0200

    Add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    const finalSeparator = separator === undefined ? ',' : separator;
+
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      result += this[i] === undefined || this[i] === null ? '' : this[i];
+
+      if (i < this.length - 1) {
+        result += finalSeparator;
+      }
+    }
+
+    return result;
   };
 }
 
