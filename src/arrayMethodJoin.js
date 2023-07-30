0f8b73e987d1cd0bb01b81152e962240b2535810
Piotr Dzialo <piotr.sluzbowy@wp.pl>
2023-07-31 01:27:43 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let secondJoin = '';
+
+    const separatorNew = separator;
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        secondJoin += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        secondJoin += separatorNew;
+      }
+    }
+
+    return secondJoin;
   };
 }
 
