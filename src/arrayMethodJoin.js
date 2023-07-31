2eb25cf47d831eaa2b26976e9763b529f43558ce
Kacper <leman_kacper@o2.pl>
2023-07-31 13:13:18 +0200

    Soultion for tests

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,34 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let separatorValue = separator;
+
+    if (separatorValue === undefined) {
+      separatorValue = ',';
+    }
+
+    const arr = this;
+    let result = '';
+
+    if (arr.length === 0) {
+      return '';
+    }
+
+    for (const element of arr) {
+      const str = element === null
+      || element === undefined ? '' : element.toString();
+
+      result += str;
+
+      if (arr.indexOf(element) < arr.length - 1) {
+        if (separatorValue === null) {
+          separatorValue = 'null';
+        }
+        result += separatorValue.toString();
+      }
+    }
+
+    return result;
   };
 }
 
