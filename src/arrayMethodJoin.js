fd7e098443716c54fe8361bf8e2a98108dbd4270
Oleh Hurmanchuk <gyrmanchyk230506@gmail.com>
2023-08-01 19:19:53 +0300

    add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,24 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    if (this.length === 0) {
+      return '';
+    }
+
+    const STRING_SEPARATOR = String(separator);
+    let finalString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === undefined || this[i] === null) {
+        finalString += STRING_SEPARATOR;
+        continue;
+      }
+
+      finalString += this[i] + STRING_SEPARATOR;
+    }
+
+    return finalString.slice(0, finalString.length - STRING_SEPARATOR.length);
   };
 }
 
