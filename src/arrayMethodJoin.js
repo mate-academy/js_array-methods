6cead26e7489e979eb505517345360701d607c48
Andrii Kuz <andrykuz4556@gmail.com>
2023-08-01 17:48:12 +0300

    solved task

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,25 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let stringSeparator = separator;
+    let result = '';
+
+    if (stringSeparator === null || typeof stringSeparator === 'object') {
+      stringSeparator = String(stringSeparator);
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (i > 0) {
+        result += stringSeparator;
+      }
+
+      const element = this[i] === null || this[i] === undefined ? '' : this[i];
+
+      result += element;
+    }
+
+    return result;
   };
 }
 
