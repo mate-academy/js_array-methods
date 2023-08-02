8b7f947b35e4c5e9034734326c304e2ace40ad22
Anton Tsurkan <wesses1999@gmail.com>
2023-08-02 21:25:34 +0300

    add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,32 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let string = '';
+    let newSeparator = separator;
+    let lastElement = this[this.length - 1];
+
+    if (this.length === 0) {
+      return string;
+    }
+
+    if (separator === null) {
+      newSeparator = 'null';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        string += '' + newSeparator;
+        continue;
+      }
+      string += String(this[i]) + newSeparator;
+    }
+
+    if (lastElement === undefined || lastElement === null) {
+      lastElement = '';
+    }
+
+    return string + lastElement;
   };
 }
 
