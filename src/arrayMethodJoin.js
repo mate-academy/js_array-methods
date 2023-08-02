1bc1080c76e52efc258b826c02de5da8b058a679
Lina Do <dykaya0916@gmail.com>
2023-08-03 01:23:58 +0300

    task was done

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,19 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+    const separatorString = `${separator}`;
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        result += this[i] + separatorString;
+      } else {
+        result += separatorString;
+      }
+    }
+
+    return result.slice(0, result.length - separatorString.length);
   };
 }
 
