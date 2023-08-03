b8f7d191670eb5b8e898dbda300886672c86dd8b
Vitalii Serhiichuk <vitalya.sergiychuk19999@gmail.com>
2023-08-03 14:44:28 +0300

    JS array method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let str = '';
+    let valueSeparatore = separator;
+
+    if (separator === null) {
+      valueSeparatore = 'null';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      const a = this[i] === undefined
+        || this[i] === null
+        ? ''
+        : this[i];
+
+      if (i < this.length - 1) {
+        str += a + valueSeparatore;
+      } else {
+        str += a;
+      }
+    }
+
+    return str;
   };
 }
 
