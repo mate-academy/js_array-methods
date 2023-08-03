a24fcc735ff31cccb3ea730fd82e1ba1a503d849
maksym-mishchanchuk <mishchanchuk.maksym@gmail.com>
2023-08-03 23:07:29 +0300

    initial commit

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,32 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let stringAfterJoin = '';
+    const copyArray = this.slice();
+    let finallySeparator = separator;
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    if (finallySeparator === null) {
+      finallySeparator = 'null';
+    }
+
+    for (let i = 0; i < copyArray.length; i++) {
+      if (copyArray[i] === undefined || copyArray[i] === null) {
+        copyArray[i] = '';
+      }
+
+      if (i === copyArray.length - 1) {
+        stringAfterJoin += copyArray[i];
+      } else {
+        stringAfterJoin += copyArray[i] + finallySeparator;
+      }
+    }
+
+    return stringAfterJoin;
   };
 }
 
