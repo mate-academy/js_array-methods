4d6d3d70fbca7e4f1d7c54337da8c813bcf6ce52
KlymSudakov <sudakov.klym@gmail.com>
2023-08-03 17:13:24 +0300

    add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joined = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (i > 0) {
+        joined += separator;
+      }
+
+      if (this[i] === null || this[i] === undefined) {
+        continue;
+      }
+
+      joined += this[i];
+    }
+
+    return joined;
   };
 }
 
