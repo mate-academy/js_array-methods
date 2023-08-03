e254d452c14de02b9f48c920d5f9104c6a833a2a
Ivanna Romanovych <ivankaromanovych12@gmail.com>
2023-08-03 14:47:01 +0300

    add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,22 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joined = '';
+
+    if (this[0] !== undefined && this[0] !== null) {
+      joined += this[0];
+    }
+
+    for (let i = 1; i < this.length; i++) {
+      joined += separator;
+
+      if (this[i] !== undefined && this[i] !== null) {
+        joined += this[i];
+      }
+    }
+
+    return joined;
   };
 }
 
