9788cb38603e49674623b1ed19e6a175792eb774
AndrMar1939 <advo737@gmail.com>
2023-08-04 16:17:05 +0300

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const currentArrayLength = this.length;
+    const joinSeparator = separator + '';
+    let resultingString = '';
+
+    if (!currentArrayLength) {
+      return resultingString;
+    }
+
+    this.forEach((item, index) => {
+      /* const stringCharacter = item ?? '' is the best choice,
+      but I couldn`t disable eslint for the nullish operator */
+      const stringCharacter = item === null || item === undefined
+        ? ''
+        : item;
+
+      currentArrayLength - 1 === index
+        ? resultingString += stringCharacter
+        : resultingString += stringCharacter + joinSeparator;
+    });
+
+    return resultingString;
   };
 }
 
