8f805386c16ed5366d13f87747fc1e57d6cc18f8
Ivanna Romanovych <ivankaromanovych12@gmail.com>
2023-08-04 11:46:00 +0300

    without if

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -7,16 +7,16 @@ function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let joined = '';
 
-    if (this[0] !== undefined && this[0] !== null) {
-      joined += this[0];
-    }
+    joined += this[0] !== undefined && this[0] !== null
+      ? this[0]
+      : '';
 
     for (let i = 1; i < this.length; i++) {
       joined += separator;
 
-      if (this[i] !== undefined && this[i] !== null) {
-        joined += this[i];
-      }
+      joined += this[i] !== undefined && this[i] !== null
+        ? this[i]
+        : '';
     }
 
     return joined;
