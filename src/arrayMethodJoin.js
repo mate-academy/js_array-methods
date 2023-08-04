8eaf98bca57536f8662d67b5e8912ece6474591d
KatOlista^^! <olya.katyushina@gmail.com>
2023-08-04 23:53:46 +0200

    create solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,8 +5,41 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let separ = separator;
+
+    if (separ === undefined) {
+      separ = ',';
+    }
+
+    if (separ === null) {
+      separ = 'null';
+    }
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    let joinedString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (i === this.length - 1) {
+        separ = '';
+      }
+
+      joinedString += this[i] === null || this[i] === undefined
+        ? '' + separ
+        : this[i] + separ;
+    }
+
+    // joinedString += this[this.length - 1] === null
+    // || this[this.length - 1] === undefined
+    //   ? ''
+    //   : this[this.length - 1];
+
+    return joinedString;
   };
 }
+
+applyCustomJoin([0, 1, 2, 3]);
 
 module.exports = applyCustomJoin;
