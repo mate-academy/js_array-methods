0487bd0306665789cd3372c6baac64906dcbd4f0
Olha Momot <helgammt@gmail.com>
2023-08-04 22:32:13 +0300

    add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,26 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let resultString = '';
+
+    if (this.length === 0) {
+      return '';
+    }
+
+    for (let i = 0; i < this.length - 1; i++) {
+      if (this[i] !== undefined && this[i] !== null) {
+        resultString += this[i];
+      }
+
+      resultString += separator;
+    }
+
+    if (!this[this.length - 1]) {
+      return resultString;
+    }
+
+    return resultString + this[this.length - 1];
   };
 }
 
