cfaef6072d644b9a69c213e7bf67bcc516856568
Tamara Chernetska <cty2802@gmail.com>
2023-08-06 12:26:10 +0300

    add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,20 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let joined = '';
+    const formattedSeparator = separator === undefined ? ',' : '' + separator;
+
+    for (let i = 0; i < this.length; i++) {
+      const formatted = this[i] == null ? '' : this[i];
+
+      if (i !== this.length - 1) {
+        joined += formatted + formattedSeparator;
+      } else {
+        joined += formatted;
+      }
+    }
+
+    return joined;
   };
 }
 
