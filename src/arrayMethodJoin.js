0b758b790fda08015587b161a54c9c6664c06600
Oleksandr Shevchenko <aleksandrtarasovi44@gmail.com>
2023-08-06 18:02:59 +0300

    add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,17 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let result = '';
+
+    this.forEach((item, index) => {
+      const isLastItem = index < this.length - 1;
+      const value = item !== undefined && item !== null ? item : '';
+
+      result += isLastItem ? `${value}${separator}` : `${value}`;
+    });
+
+    return result;
   };
 }
 
