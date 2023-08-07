c3fe049230f725efdf1600f088bf8b4e1674e0ed
Denys14Samoilenko <d.samoilenko92@gmail.com>
2023-08-07 12:31:07 +0300

    custom join solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -1,12 +1,26 @@
+/* eslint-disable space-before-function-paren */
 'use strict';
 
 /**
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function (separator = ',') {
+    let arrayToString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const arrayItem = this[i];
+
+      if (arrayItem !== null && arrayItem !== undefined) {
+        arrayToString += arrayItem;
+      }
+
+      if (i !== this.length - 1) {
+        arrayToString += separator;
+      }
+    };
+
+    return arrayToString;
   };
 }
-
 module.exports = applyCustomJoin;
