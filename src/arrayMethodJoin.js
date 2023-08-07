a3687127490d2b294250d2055949f71917cb08f1
vanom <envinenvin@gmail.com>
2023-08-07 20:35:05 +0300

    add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,27 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let joinString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] === null || this[i] === undefined) {
+        joinString += '';
+      } else {
+        joinString += this[i];
+      }
+
+      if (i === this.length - 1) {
+        break;
+      }
+
+      if (separator === undefined) {
+        joinString += ',';
+      } else {
+        joinString += separator;
+      }
+    }
+
+    return joinString;
   };
 }
 
