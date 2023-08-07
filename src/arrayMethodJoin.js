9a96ee6afa9b7134ba5c39527bc73cd0aeeadb6d
Andrii Kuz <andrykuz4556@gmail.com>
2023-08-07 23:00:41 +0300

    Changes after review

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,16 +5,11 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let stringSeparator = separator;
     let result = '';
-
-    if (stringSeparator === null || typeof stringSeparator === 'object') {
-      stringSeparator = String(stringSeparator);
-    }
 
     for (let i = 0; i < this.length; i++) {
       if (i > 0) {
-        result += stringSeparator;
+        result += separator;
       }
 
       const element = this[i] === null || this[i] === undefined ? '' : this[i];
