9c8301e1cab2cc34cd3576905ed207ada734911f
Taras Voievoda <voevoda.taras@gmail.com>
2023-08-07 21:13:59 +0300

    create variable

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,21 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let str = '';
+    const STRING_SEPARATOR = String(separator);
+    let finalString = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (this[i] !== null && this[i] !== undefined) {
-        str += this[i];
+      const arrayElement = this[i];
+
+      if (arrayElement === undefined || arrayElement === null) {
+        finalString += STRING_SEPARATOR;
+        continue;
       }
 
-      if (i !== this.length - 1) {
-        str += separator;
-      }
+      finalString += arrayElement + STRING_SEPARATOR;
     }
 
-    return str;
+    return finalString.slice(0, finalString.length - STRING_SEPARATOR.length);
   };
 }
 
