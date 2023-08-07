6e39a4151fd08486e2d910ccfe303580b08fc9c2
Andrii Korobkin <flouzeek@gmail.com>
2023-08-07 21:03:42 +0300

    add task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,22 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    let string = '';
+    const array = [...this];
+
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] === null || array[i] === undefined) {
+        array[i] = '';
+      }
+
+      string += array[i];
+
+      if (i < array.length - 1) {
+        string += separator !== undefined ? separator : ',';
+      };
+    }
+
+    return string;
   };
 }
 
