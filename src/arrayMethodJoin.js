1681683cce8c9c0360628ee7a9dd40ea3963b118
myName <vlad.svorin2008@gmail.com>
2023-08-08 10:34:47 +0200

    solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,31 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    const arrCopy = [ ...this ];
+    let ConcatenatedStr = '';
+
+    for (let i = 0; i < arrCopy.length; i++) {
+      if (arrCopy[i] === undefined || arrCopy[i] === null) {
+        arrCopy[i] = '';
+      }
+    }
+
+    if (arrCopy.length > 0) {
+      for (let i = 0; i < arrCopy.length - 1; i++) {
+        arrCopy[i] += separator;
+
+        if (separator === null) {
+          arrCopy[i] += 'null';
+        }
+
+        ConcatenatedStr += arrCopy[i];
+      }
+
+      ConcatenatedStr += arrCopy[arrCopy.length - 1];
+    }
+
+    return ConcatenatedStr;
   };
 }
 
