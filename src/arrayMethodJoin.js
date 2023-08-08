ad217467885ba7d0e0ae29f13512577060cdd9c7
myName <vlad.svorin2008@gmail.com>
2023-08-08 10:37:43 +0200

    task solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -8,25 +8,27 @@ function applyCustomJoin() {
     const arrCopy = [ ...this ];
     let ConcatenatedStr = '';
 
+    if (arrCopy.length === 0) {
+      return '';
+    }
+
     for (let i = 0; i < arrCopy.length; i++) {
       if (arrCopy[i] === undefined || arrCopy[i] === null) {
         arrCopy[i] = '';
       }
     }
 
-    if (arrCopy.length > 0) {
-      for (let i = 0; i < arrCopy.length - 1; i++) {
-        arrCopy[i] += separator;
+    for (let i = 0; i < arrCopy.length - 1; i++) {
+      arrCopy[i] += separator;
 
-        if (separator === null) {
-          arrCopy[i] += 'null';
-        }
-
-        ConcatenatedStr += arrCopy[i];
+      if (separator === null) {
+        arrCopy[i] += 'null';
       }
 
-      ConcatenatedStr += arrCopy[arrCopy.length - 1];
+      ConcatenatedStr += arrCopy[i];
     }
+
+    ConcatenatedStr += arrCopy[arrCopy.length - 1];
 
     return ConcatenatedStr;
   };
