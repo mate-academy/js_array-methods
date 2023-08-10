940f49ebe5e4bd9aca57a33032c01f322a5ece3e
Sergey <serg9205@gmail.com>
2023-08-10 15:21:37 +0300

    rebuild solution with using tips

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,23 +6,19 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let resultString = '';
-    let mainSeparatorClone = separator;
-
-    if (separator === null) {
-      mainSeparatorClone = 'null';
-    }
+    let mainSeparatorClone = String(separator);
 
     for (let i = 0; i < this.length; i++) {
-      if ((this[i] === null || this[i] === undefined)
-      && i !== this.length - 1) {
-        resultString += '' + mainSeparatorClone;
-      } else if (this[i] === null || this[i] === undefined) {
-        resultString += '';
-      } else if (i === this.length - 1) {
-        resultString += this[i];
-      } else {
-        resultString += this[i] + mainSeparatorClone;
+      let currentElem = this[i];
+
+      if (currentElem === null || currentElem === undefined) {
+        currentElem = '';
       }
+
+      if (i === this.length - 1) {
+        mainSeparatorClone = '';
+      }
+      resultString += currentElem + mainSeparatorClone;
     }
 
     return resultString;
