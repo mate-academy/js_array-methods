486d34fda746256912be86a57018293d368d67e5
Ihor Dovbenko <dovbenkoin@gmail.com>
2023-08-10 09:13:11 +0300

    create variables

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,13 +6,16 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
     let joinedString = '';
+    const currentLength = this.length;
 
-    for (let i = 0; i < this.length; i++) {
-      if (this[i] !== undefined && this[i] !== null) {
-        joinedString += `${this[i]}`;
+    for (let i = 0; i < currentLength; i++) {
+      const currentValue = this[i];
+
+      if (currentValue !== undefined && currentValue !== null) {
+        joinedString += `${currentValue}`;
       }
 
-      if (i < this.length - 1) {
+      if (i < currentLength - 1) {
         joinedString += `${separator}`;
       }
     }
