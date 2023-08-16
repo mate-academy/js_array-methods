909d09116e6d95131fc0bd69659cdf1c3c441d42
Kriss Kozak <misskrissretro@gmail.com>
2023-08-16 11:08:17 +0300

    method join()

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,17 +5,30 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
+    let separator1 = separator;
+
+    if (separator1 === undefined) {
+      separator1 = ',';
+    }
+
     let result = '';
 
     for (let i = 0; i < this.length; i++) {
-      if (i !== 0 && separator !== undefined) {
-        result += separator;
+      if (this[i] === null || this[i] === undefined) {
+        if (i !== this.length - 1) {
+          result += separator1;
+        }
+      } else {
+        result += this[i];
+
+        if (i !== this.length - 1) {
+          result += separator1;
+        }
       }
-      result += this[i];
     }
 
     return result;
   };
-};
+}
 
 module.exports = applyCustomJoin;
