64274d9c8735200344ef63f7b6e27e3a6d306ec1
Vasyl Kurtiak <pocophonef1start@gmail.com>
2023-08-21 00:57:29 +0300

    metod join3

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,19 +5,19 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator = ',') {
-    let result = '';
+    let res = '';
 
     for (let i = 0; i < this.length; i++) {
       if (this[i] !== null && this[i] !== undefined) {
-        result += this[i];
+        res += this[i];
       }
 
       if (i < this.length - 1) {
-        result += separator;
+        res += separator;
       }
     }
 
-    return result;
+    return res;
   };
 }
 
