70403b992b528df3c8c7caa4b8cbeb29a4851bac
Annbpiu <Annbpiu@gmail.com>
2023-08-20 18:34:19 +0300

    complete the tusk

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -5,7 +5,29 @@
  */
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
-    // write code here
+    if (this.length === 0) {
+      return '';
+    }
+
+    let finalSeparator = separator;
+
+    if (separator === undefined) {
+      finalSeparator = ',';
+    }
+
+    let string = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        string += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        string += finalSeparator;
+      }
+    }
+
+    return string;
   };
 }
 
