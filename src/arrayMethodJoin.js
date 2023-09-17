815a5b568bc926dc51cc142d7dcc536409581a26
Anna Kovnatska <anna0725k@gmail.com>
2023-09-17 18:31:57 -0500

    array method join

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,19 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let result = '';
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] !== null && this[i] !== undefined) {
+        result += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        result += separator;
+      }
+    };
+
+    return result;
   };
 }
 
