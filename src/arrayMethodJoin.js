db30ca44a150ff00c1d1c091628e6792a9093739
Barbara Krysiak <b.krysiakk@gmail.com>
2023-10-05 10:38:43 +0200

    Solution

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -6,6 +6,26 @@
 function applyCustomJoin() {
   [].__proto__.join2 = function(separator) {
     // write code here
+    let newArr = '';
+    let normalizedSeparator = separator;
+
+    if (normalizedSeparator === undefined) {
+      normalizedSeparator = ',';
+    }
+
+    for (let i = 0; i < this.length; i++) {
+      if (this[i] == null) {
+        newArr += '';
+      } else {
+        newArr += this[i];
+      }
+
+      if (i !== this.length - 1) {
+        newArr += normalizedSeparator;
+      }
+    }
+
+    return newArr;
   };
 }
 
