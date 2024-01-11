62e4302d5ac771f6a3a7053fd3c783d76fe8659d
kkrasnoshchok <krasnoshchok4work@gmail.com>
2024-01-11 14:11:15 +0100

    Solution:)

--- src/arrayMethodJoin.js
+++ src/arrayMethodJoin.js
@@ -4,8 +4,28 @@
  * Implement method join
  */
 function applyCustomJoin() {
-  [].__proto__.join2 = function(separator) {
-    // write code here
+  [].__proto__.join2 = function(separator = ',') {
+    let joinedString = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const element = this[i];
+      const lastElement = i === this.length - 1;
+
+      /**
+       * wanted to use `joinedString += element ?? ''`,
+       * meaning without any `if()` construction,
+       * but mate's eslint unables me to do it
+       */
+      if (element !== undefined && element !== null) {
+        joinedString += element;
+      }
+
+      if (!lastElement) {
+        joinedString += separator;
+      }
+    }
+
+    return joinedString;
   };
 }
 
